01214b0673882065088f2558c17bc04f99d53b87
GregHib <gregoryhib@gmail.com>
2020-04-09 11:25:52 +0100

    PlayerFactoryTest.kt fix

--- engine/src/test/kotlin/org/redrune/engine/entity/factory/PlayerFactoryTest.kt
+++ engine/src/test/kotlin/org/redrune/engine/entity/factory/PlayerFactoryTest.kt
@@ -1,15 +1,16 @@
 package org.redrune.engine.entity.factory
 
-import io.mockk.Runs
-import io.mockk.every
-import io.mockk.just
-import io.mockk.verify
-import org.junit.Assert.assertEquals
+import io.mockk.*
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.koin.dsl.module
 import org.koin.test.mock.declareMock
+import org.redrune.engine.client.IndexAllocator
+import org.redrune.engine.data.PlayerLoader
 import org.redrune.engine.entity.event.Registered
+import org.redrune.engine.entity.model.Player
 import org.redrune.engine.event.EventBus
 import org.redrune.engine.event.eventBusModule
 import org.redrune.engine.script.KoinMock
@@ -23,21 +24,35 @@ internal class PlayerFactoryTest : KoinMock() {
 
     @BeforeEach
     fun setup() {
-        loadModules(module { single { PlayerFactory() } }, eventBusModule)
+        loadModules(module {
+            single { PlayerFactory() }
+        }, eventBusModule)
     }
 
     @Test
-    fun `Spawn registers`() {
+    fun `Spawn registers`() = runBlocking {
         // Given
         val factory: PlayerFactory = get()
+        val player: Player = mockk(relaxed = true)
+        val loader: PlayerLoader = declareMock {
+            every { load("Test") } returns player
+        }
         val bus: EventBus = declareMock {
             every { emit(any<Registered>()) } just Runs
         }
+        val indexer: IndexAllocator = declareMock {
+            every { obtain() } returns 4
+        }
         // When
-        val player = factory.spawn(4)
+        val result = factory.spawn("Test").await()
         // Then
-        assertEquals(4, player.id)
-        verify { bus.emit<Registered>(any()) }
+        assertNotNull(result)
+        verifyOrder {
+            loader.load("Test")
+            indexer.obtain()
+            player.index = 4
+            bus.emit<Registered>(any())
+        }
     }
 
 }
