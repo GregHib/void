1bceaef2ca7db22ee681f7aa66f468af8acf20d8
GregHib <gregoryhib@gmail.com>
2020-05-05 20:09:00 +0100

    Limit logins per tick

--- engine/src/main/kotlin/rs/dusk/engine/client/PlayerLoginQueue.kt
+++ engine/src/main/kotlin/rs/dusk/engine/client/PlayerLoginQueue.kt
@@ -8,7 +8,7 @@ import rs.dusk.core.network.model.session.Session
 import rs.dusk.engine.EngineTasks
 import rs.dusk.engine.entity.factory.PlayerFactory
 import rs.dusk.utility.inject
-import java.util.*
+import java.util.concurrent.ConcurrentLinkedDeque
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -19,14 +19,14 @@ import kotlin.coroutines.suspendCoroutine
  */
 @Suppress("USELESS_CAST")
 val clientLoginQueueModule = module {
-    single { PlayerLoginQueue(get()) as LoginQueue }
+    single { PlayerLoginQueue(get(), getProperty("loginPerTickCap")) as LoginQueue }
 }
 
-class PlayerLoginQueue(tasks: EngineTasks) : LoginQueue(tasks, 10) {
+class PlayerLoginQueue(tasks: EngineTasks, private val loginPerTickCap: Int) : LoginQueue(tasks, 10) {
 
     private val factory: PlayerFactory by inject()
     private val logger = InlineLogger()
-    val queue = LinkedList<Continuation<Unit>>()
+    val queue = ConcurrentLinkedDeque<Continuation<Unit>>()
 
     override fun add(username: String, session: Session?) = GlobalScope.async {
         suspendCoroutine<Unit> {
@@ -48,10 +48,15 @@ class PlayerLoginQueue(tasks: EngineTasks) : LoginQueue(tasks, 10) {
 
 
     override fun run() {
+        var count = 0
         var next = queue.poll()
         while (next != null) {
             next.resume(Unit)
+            if (count++ >= loginPerTickCap) {
+                break
+            }
             next = queue.poll()
         }
     }
+
 }
