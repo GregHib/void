29d88b5485091394ca7e70c9d9ba4e71cd03fa9e
GregHib <greghib@users.noreply.github.com>
2020-05-25 11:58:03 +0100

    Update external dependencies to fix support for jdk 14

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,18 @@ buildscript {
     }
 
     dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.3.71"))
+        classpath(kotlin("gradle-plugin", version = "1.3.72"))
     }
 }
 
 plugins {
-    kotlin("jvm") version "1.3.71"
+    kotlin("jvm") version "1.3.72"
 }
 
 val koinVersion = "2.1.5"
+val junitVersion = "5.6.2"
+val exposedVersion = "0.24.1"
+val jacksonVersion = "2.11.0"
 
 allprojects {
     apply(plugin = "kotlin")
@@ -22,7 +25,7 @@ allprojects {
     group = "rs.dusk"
     version = "0.0.2"
 
-    java.sourceCompatibility = JavaVersion.VERSION_11
+    java.sourceCompatibility = JavaVersion.VERSION_14
 
     repositories {
         mavenCentral()
@@ -35,40 +38,44 @@ allprojects {
 
     dependencies {
         //Main
-	    implementation(group="rs.dusk.core", name="network", version="0.1.1")
-	    implementation(group="rs.dusk.core", name="utility", version="0.1.1")
-	    
+        implementation(group = "rs.dusk.core", name = "network", version = "0.1.1")
+        implementation(group = "rs.dusk.core", name = "utility", version = "0.1.1")
+
         implementation(kotlin("stdlib-jdk8"))
         implementation(kotlin("reflect"))
         implementation("io.netty:netty-all:4.1.44.Final")
-        implementation(group = "com.displee", name = "rs-cache-library", version = "6.3")
-        implementation(group = "org.yaml", name = "snakeyaml", version = "1.8")
-        implementation(group = "io.github.classgraph", name = "classgraph", version = "4.6.3")
-        implementation(group = "com.michael-bull.kotlin-inline-logger", name = "kotlin-inline-logger-jvm", version = "1.0.2")
+        implementation(group = "com.displee", name = "rs-cache-library", version = "6.4")
+        implementation(group = "org.yaml", name = "snakeyaml", version = "1.26")
+        implementation(group = "io.github.classgraph", name = "classgraph", version = "4.8.78")
+        implementation(
+            group = "com.michael-bull.kotlin-inline-logger",
+            name = "kotlin-inline-logger-jvm",
+            version = "1.0.2"
+        )
         implementation(group = "org.koin", name = "koin-core", version = koinVersion)
         implementation(group = "org.koin", name = "koin-logger-slf4j", version = koinVersion)
-        implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.3.5")
+        implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "1.3.7")
 
         //Logging
         implementation("org.slf4j:slf4j-api:1.7.30")
         implementation("ch.qos.logback:logback-classic:1.2.3")
 
         //Utilities
-        implementation("com.google.guava:guava:19.0")
-        implementation("org.apache.commons:commons-lang3:3.0")
-        implementation("org.jetbrains.exposed:exposed-core:0.22.1")
-        implementation("org.jetbrains.exposed:exposed-jdbc:0.22.1")
-        implementation("com.fasterxml.jackson.core:jackson-core:2.10.3")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.3")
-        implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.10.3")
-        implementation("org.postgresql:postgresql:42.2.11")
+        implementation("com.google.guava:guava:29.0-jre")
+        implementation("org.apache.commons:commons-lang3:3.10")
+        implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+        implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+        implementation("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+        implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
+        implementation("org.postgresql:postgresql:42.2.12")
         implementation("it.unimi.dsi:fastutil:8.3.1")
 
         //Testing
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.1")
-        testImplementation("org.junit.jupiter:junit-jupiter-params:5.6.1")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+        testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
         testImplementation(group = "org.koin", name = "koin-test", version = koinVersion)
-        testImplementation(group = "io.mockk", name = "mockk", version = "1.9.3")
+        testImplementation(group = "io.mockk", name = "mockk", version = "1.10.0")
     }
 
     tasks {
