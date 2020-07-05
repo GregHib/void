4a883a50937c440929208007b065ea13acfb0cc8
Greg <gregoryhib@gmail.com>
2020-07-05 11:59:17 +0100

    Floor item re-spawn changes

--- engine/src/main/kotlin/rs/dusk/world/entity/item/ItemSpawns.kts
+++ engine/src/main/kotlin/rs/dusk/world/entity/item/ItemSpawns.kts
@@ -1,11 +1,12 @@
-import kotlinx.coroutines.Job
+import ItemSpawns.ItemSpawn
 import rs.dusk.engine.action.Scheduler
 import rs.dusk.engine.action.delay
 import rs.dusk.engine.data.file.FileLoader
 import rs.dusk.engine.event.EventBus
-import rs.dusk.engine.event.on
 import rs.dusk.engine.event.then
+import rs.dusk.engine.event.where
 import rs.dusk.engine.model.engine.Startup
+import rs.dusk.engine.model.entity.Registered
 import rs.dusk.engine.model.entity.Unregistered
 import rs.dusk.engine.model.entity.item.FloorItem
 import rs.dusk.engine.model.world.Region
@@ -22,7 +23,7 @@ val scheduler: Scheduler by inject()
 data class ItemSpawn(val id: Int, val amount: Int = 1, val delay: Int = 100, val tile: Tile)
 
 val spawns: MutableMap<Region, MutableList<ItemSpawn>> = mutableMapOf()
-val timers = mutableMapOf<ItemSpawn, Job?>()
+val links = mutableMapOf<FloorItem, ItemSpawn>()
 
 fun ItemSpawn.drop() = bus.emit(Drop(id, amount, tile, 0))
 
@@ -34,20 +35,36 @@ Startup then {
     }
 }
 
+// We can do it this way as events and scheduler are guaranteed to be sequential.
+var spawnPoint: ItemSpawn? = null
+
+/**
+ * When a region is loaded spawn [FloorItem]'s and link to their [ItemSpawn]'s
+ */
 MapLoaded then {
     val spawns = spawns[region] ?: return@then
     spawns.forEach { spawn ->
+        spawnPoint = spawn
         spawn.drop()
-        // TODO better way of doing this.
-        on<Unregistered> {
-            where { entity is FloorItem && entity.id == spawn.id && entity.amount == spawn.amount && entity.tile == spawn.tile }
-            then {
-                timers[spawn]?.cancel()
-                timers[spawn] = scheduler.add {
-                    delay(ticks = spawn.delay)
-                    spawn.drop()
-                }
-            }
-        }
+    }
+    spawnPoint = null
+}
+
+Registered where { entity is FloorItem && spawnPoint != null } then {
+    val item = entity as FloorItem
+    links[item] = spawnPoint!!
+}
+
+/**
+ * When a spawn points item is removed, wait for respawn delay before spawning another.
+ */
+Unregistered where { entity is FloorItem && links.containsKey(entity) } then {
+    val item = entity as FloorItem
+    val spawn = links.remove(item)!!
+    scheduler.add {
+        delay(ticks = spawn.delay)
+        spawnPoint = spawn
+        spawn.drop()
+        spawnPoint = null
     }
 }
