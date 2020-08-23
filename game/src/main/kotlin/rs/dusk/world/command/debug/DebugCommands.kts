4bbfe9d290e2bcedb8cb18f3882b18ee5c89270e
Greg <greghib@users.noreply.github.com>
2020-08-23 14:11:27 +0100

    Collecting trade interface data WIP

--- game/src/main/kotlin/rs/dusk/world/command/debug/DebugCommands.kts
+++ game/src/main/kotlin/rs/dusk/world/command/debug/DebugCommands.kts
@@ -1,9 +1,58 @@
-import rs.dusk.engine.entity.character.player.skill.Skill
-import rs.dusk.engine.entity.character.player.skill.exp
+import rs.dusk.engine.client.send
+import rs.dusk.engine.client.variable.setVar
+import rs.dusk.engine.entity.character.player.Player
 import rs.dusk.engine.event.then
 import rs.dusk.engine.event.where
+import rs.dusk.network.rs.codec.game.encode.message.ScriptMessage
 import rs.dusk.world.command.Command
 
 Command where { prefix == "test" } then {
-    player.exp(Skill.valueOf(content.capitalize()), 10050.0)
+
+    //Self
+    player.send(
+        ScriptMessage(
+            150, "", "", "", "Value<col=FF9040>", "Remove-X", "Remove-All", "Remove-10",
+            "Remove-5", "Remove", -1, 0, 7, 4, 90,
+            335 shl 16 or 31
+        )
+    )
+    player.interfaces.sendSetting(335, 31, 0, 27, 1150)
+    // Other
+    player.send(
+        ScriptMessage(
+            695, "", "", "", "", "", "", "", "", "Value<col=FF9040>", -1, 0, 7, 4, 90,
+            335 shl 16 or 34
+        )
+    )
+    player.interfaces.sendSetting(335, 34, 0, 27, 1026)
+    // Side
+    player.send(
+        ScriptMessage(
+            150, "", "", "Lend", "Value<col=FF9040>", "Offer-X", "Offer-All", "Offer-10",
+            "Offer-5", "Offer", -1, 0, 7, 4, 93,
+            336 shl 16
+        )
+    )
+    player.interfaces.sendSetting(336, 0, 0, 27, 1278)
+
+    sendFlash(player, 335, 33, 4, 7, 0)
+    player.interfaces.apply {
+        open("trade_main")
+        open("trade_side")
+        sendText("trade_main", "status", "This is a status update")
+        sendText("trade_main", "title", "Trading with: Someone")
+        sendText("trade_main", "slots", "Many free inventory slots.")
+//        sendText("trade_main", "loanTime", "1 hour")
+//        sendVisibility("trade_main", "loan", true)
+//        sendSetting("trade_main", "loan", 0, 1, 264190)
+    }
+    player.setVar("other_trader_name", "Bob")
+}
+
+fun sendFlash(player: Player, id: Int, component: Int, width: Int, height: Int, slot: Int) {
+    player.send(
+        ScriptMessage(
+            143, slot, height, width, (id shl 16) or component
+        )
+    )
 }
---
+++ game/src/main/kotlin/rs/dusk/world/community/trade/RequestTrade.kts
@@ -0,0 +1,61 @@
+import com.github.michaelbull.logging.InlineLogger
+import rs.dusk.engine.client.variable.BooleanVariable
+import rs.dusk.engine.client.variable.IntVariable
+import rs.dusk.engine.client.variable.Variable
+import rs.dusk.engine.client.variable.setVar
+import rs.dusk.engine.entity.character.contain.container
+import rs.dusk.engine.entity.character.get
+import rs.dusk.engine.entity.character.player.Player
+import rs.dusk.engine.entity.character.player.PlayerOption
+import rs.dusk.engine.entity.character.player.chat.ChatType
+import rs.dusk.engine.entity.character.player.chat.message
+import rs.dusk.engine.entity.character.update.visual.player.name
+import rs.dusk.engine.event.then
+import rs.dusk.engine.event.where
+
+/**
+ * Requesting to trade with another player, accepting the request and setting up the trade
+ */
+
+val logger = InlineLogger()
+
+BooleanVariable(1042, Variable.Type.VARP).register("offer_modified")
+BooleanVariable(1043, Variable.Type.VARP).register("other_offer_modified")
+IntVariable(5026, Variable.Type.VARBIT).register("lend_time")
+IntVariable(5070, Variable.Type.VARBIT).register("other_lend_time")
+
+IntVariable(729, Variable.Type.VARC).register("offer_value")
+IntVariable(697, Variable.Type.VARC).register("other_offer_value")
+
+IntVariable(203, Variable.Type.VARCSTR).register("other_trader_name")
+
+PlayerOption where { option == "Trade with" } then {
+    val filter = target["trade_filter", "on"]
+    if (filter == "off" || (filter == "friends" && !target.hasFriend(player))) {
+        return@then
+    }
+    if (player.requests.has(target, "trade")) {
+        player.message("Sending trade offer...", ChatType.GameTrade)
+    } else {
+        player.message("Sending trade offer...", ChatType.GameTrade)
+        target.message("wishes to trade with you.", ChatType.Trade, name = player.name)
+    }
+    target.requests.add(player, "trade") { requester, acceptor ->
+        println("Trade!")
+    }
+}
+
+fun modified(player: Player) {
+    player.setVar("other_trader_name", "Bob")
+    player.setVar("offer_modified", true)
+    player.setVar("offer_value", 12345)
+    player.container("trade_item_loan").set(0, 11694)
+    player.setVar("lend_time", 0)
+}
+
+fun unlockLend(player: Player) {
+    player.interfaces.sendSettings("trade_main", "loanItem", -1, -1, 9, 0)// 9 - examine, 0 - remove
+    player.interfaces.sendSettings("trade_main", "loanTime", -1, -1, 1, 0)// 1 - 'until logout'/specify 0 - edit
+}
+
+fun Player.hasFriend(other: Player) = true// TODO friends chat
