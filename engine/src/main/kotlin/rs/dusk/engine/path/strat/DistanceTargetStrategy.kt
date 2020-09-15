f2e36c6717037409bd88f87e727172a6cbf2b03d
Greg <greghib@users.noreply.github.com>
2020-09-15 10:55:50 +0100

    Fix strange distance target strategy delegate override behaviour

--- engine/src/main/kotlin/rs/dusk/engine/path/strat/DistanceTargetStrategy.kt
+++ engine/src/main/kotlin/rs/dusk/engine/path/strat/DistanceTargetStrategy.kt
@@ -2,6 +2,7 @@ package rs.dusk.engine.path.strat
 
 import rs.dusk.engine.entity.Size
 import rs.dusk.engine.entity.character.player.Player
+import rs.dusk.engine.map.Tile
 import rs.dusk.engine.path.TargetStrategy
 
 /**
@@ -13,9 +14,15 @@ data class DistanceTargetStrategy(
     private val player: Player,
     private var distance: Int,
     private val delegate: TargetStrategy
-) : TargetStrategy by delegate {
+) : TargetStrategy {
+
+    override val tile: Tile
+        get() = delegate.tile
+
+    override val size: Size
+        get() = delegate.size
 
     override fun reached(currentX: Int, currentY: Int, plane: Int, size: Size): Boolean {
-        return player.tile.within(currentX, currentY, plane, distance) || delegate.reached(currentX, currentY, plane, size)
+        return delegate.tile.within(currentX, currentY, plane, distance) || delegate.reached(currentX, currentY, plane, size)
     }
 }
