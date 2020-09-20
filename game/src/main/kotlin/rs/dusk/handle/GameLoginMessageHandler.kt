3396a61e805079759de66ed7e9f16b1985ac8af0
Greg <greghib@users.noreply.github.com>
2020-09-20 13:56:27 +0100

    Fix login codec mix-up close #221

--- game/src/main/kotlin/rs/dusk/handle/GameLoginMessageHandler.kt
+++ game/src/main/kotlin/rs/dusk/handle/GameLoginMessageHandler.kt
@@ -17,6 +17,8 @@ import rs.dusk.engine.entity.Registered
 import rs.dusk.engine.entity.character.player.PlayerRegistered
 import rs.dusk.engine.event.EventBus
 import rs.dusk.engine.map.region.RegionLogin
+import rs.dusk.engine.task.TaskExecutor
+import rs.dusk.engine.task.sync
 import rs.dusk.network.rs.codec.game.GameCodec
 import rs.dusk.network.rs.codec.login.LoginCodec
 import rs.dusk.network.rs.codec.login.LoginMessageHandler
@@ -37,6 +39,7 @@ class GameLoginMessageHandler : LoginMessageHandler<GameLoginMessage>() {
     val sessions: Sessions by inject()
     val bus: EventBus by inject()
     val repository: CodecRepository by inject()
+    val executor: TaskExecutor by inject()
 
     override fun handle(ctx: ChannelHandlerContext, msg: GameLoginMessage) {
         val channel = ctx.channel()
@@ -60,24 +63,28 @@ class GameLoginMessageHandler : LoginMessageHandler<GameLoginMessage>() {
                     replace("message.encoder", GenericMessageEncoder(PacketBuilder(keyPair.outCipher)))
                 }
 
-                channel.setCodec(repository.get(GameCodec::class))
+                executor.sync {
+                    channel.setCodec(repository.get(GameCodec::class))
 
-                bus.emit(RegionLogin(player))
-                bus.emit(PlayerRegistered(player))
-                player.start()
-                bus.emit(Registered(player))
+                    bus.emit(RegionLogin(player))
+                    bus.emit(PlayerRegistered(player))
+                    player.start()
+                    bus.emit(Registered(player))
+                }
             } else {
                 pipeline.writeAndFlush(GameLoginConnectionResponseMessage(response.code))
             }
         }
 
-        bus.emit(
-            Login(
-                msg.username,
-                session,
-                callback,
-                msg
+        executor.sync {
+            bus.emit(
+                Login(
+                    msg.username,
+                    session,
+                    callback,
+                    msg
+                )
             )
-        )
+        }
     }
 }
