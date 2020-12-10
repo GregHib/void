f41a6e236bbe7552e55af09390ceab6fd7d138bf
Greg <greghib@users.noreply.github.com>
2020-12-10 21:17:28 +0000

    Update to core 0.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,8 +38,8 @@ allprojects {
 
     dependencies {
         //Main
-        implementation(group = "rs.dusk.core", name = "network", version = "0.1.1")
-        implementation(group = "rs.dusk.core", name = "utility", version = "0.1.1")
+        implementation(group = "rs.dusk.core", name = "network", version = "0.1.2")
+        implementation(group = "rs.dusk.core", name = "utility", version = "0.1.2")
 
         implementation(kotlin("stdlib-jdk8"))
         implementation(kotlin("reflect"))
