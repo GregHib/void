05585d3ef6bfe1df0dc1296401f2b979259d102d
GregHib <greghib@users.noreply.github.com>
2020-12-28 21:51:50 +0000

    Move network into engine

--- game/build.gradle.kts
+++ game/build.gradle.kts
@@ -7,7 +7,6 @@ dependencies {
     implementation(project(":engine"))
     implementation(project(":cache"))
     implementation(project(":utility"))
-    implementation(project(":network"))
     implementation("io.github.classgraph:classgraph:4.8.65")
     implementation(kotlin("script-runtime"))
 
--- network/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-dependencies {
-    implementation(project(":core"))
-    implementation(project(":buffer"))
-    implementation(project(":cache"))
-    implementation(project(":utility"))
-}
