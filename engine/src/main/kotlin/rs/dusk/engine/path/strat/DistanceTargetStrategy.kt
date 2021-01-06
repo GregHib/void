b77d6ee4a17839ab7835c37a12e432937d516a33
GregHib <greghib@users.noreply.github.com>
2021-01-06 21:04:20 +0000

    Add banker interact strategy

--- engine/src/main/kotlin/rs/dusk/engine/path/strat/DistanceTargetStrategy.kt
+++ engine/src/main/kotlin/rs/dusk/engine/path/strat/DistanceTargetStrategy.kt
@@ -1,7 +1,6 @@
 package rs.dusk.engine.path.strat
 
 import rs.dusk.engine.entity.Size
-import rs.dusk.engine.entity.character.player.Player
 import rs.dusk.engine.map.Tile
 import rs.dusk.engine.path.TargetStrategy
 
@@ -11,18 +10,12 @@ import rs.dusk.engine.path.TargetStrategy
  * @since September 9, 2020
  */
 data class DistanceTargetStrategy(
-    private val player: Player,
     private var distance: Int,
-    private val delegate: TargetStrategy
+    override val tile: Tile,
+    override val size: Size = Size.TILE
 ) : TargetStrategy {
 
-    override val tile: Tile
-        get() = delegate.tile
-
-    override val size: Size
-        get() = delegate.size
-
     override fun reached(currentX: Int, currentY: Int, plane: Int, size: Size): Boolean {
-        return delegate.tile.within(currentX, currentY, plane, distance) || delegate.reached(currentX, currentY, plane, size)
+        return tile.within(currentX, currentY, plane, distance)
     }
 }
