eda1c029be11f216786cb0974437c067de380a2f
GregHib <greghib@users.noreply.github.com>
2021-01-20 21:17:09 +0000

    Dusk Version 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     group = "rs.dusk"
-    version = "0.0.2"
+    version = "1.0.0"
 
     java.sourceCompatibility = JavaVersion.VERSION_15
 
