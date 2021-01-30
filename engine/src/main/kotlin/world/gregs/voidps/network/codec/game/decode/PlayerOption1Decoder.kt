d533c17e756211d67c6280084cf01ed41b87adee
GregHib <greghib@users.noreply.github.com>
2021-01-30 20:13:43 +0000

    WIP

--- engine/src/main/kotlin/world/gregs/voidps/network/codec/game/decode/PlayerOption1Decoder.kt
+++ engine/src/main/kotlin/world/gregs/voidps/network/codec/game/decode/PlayerOption1Decoder.kt
@@ -4,14 +4,14 @@ import io.netty.channel.ChannelHandlerContext
 import world.gregs.voidps.buffer.read.Reader
 import world.gregs.voidps.network.codec.Decoder
 
-class PlayerOptionDecoder(private val index: Int) : Decoder(3) {
+class PlayerOption1Decoder : Decoder(3) {
 
     override fun decode(context: ChannelHandlerContext, packet: Reader) {
-        packet.readByte()//0
+        packet.readByte()
         handler?.playerOption(
             context,
             index = packet.readShort(),
-            option = index + 1
+            option = 1
         )
     }
 
