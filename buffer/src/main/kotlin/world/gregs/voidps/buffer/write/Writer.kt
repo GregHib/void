9c4615c3de33368f74ddfc6dd7050134fcaff739
GregHib <greghib@users.noreply.github.com>
2021-03-05 19:53:41 +0000

    Replace writer with individual calls

--- buffer/src/main/kotlin/world/gregs/voidps/buffer/write/Writer.kt
+++ buffer/src/main/kotlin/world/gregs/voidps/buffer/write/Writer.kt
@@ -1,9 +1,6 @@
 package world.gregs.voidps.buffer.write
 
 import io.netty.buffer.ByteBuf
-import world.gregs.voidps.buffer.DataType
-import world.gregs.voidps.buffer.Endian
-import world.gregs.voidps.buffer.Modifier
 
 /**
  * All functions relative to writing directly to a packet are done by this class
@@ -15,114 +12,42 @@ interface Writer {
 
     fun setByte(index: Int, value: Int): Writer
 
-    /**
-     * Writes a byte to [buffer].
-     * @param value [Int]
-     */
-    fun writeByte(value: Int): Writer {
-        write(DataType.BYTE, value, Modifier.NONE)
-        return this
-    }
+    fun writeByte(value: Int): Writer
 
-    fun writeByteAdd(value: Int): Writer {
-        write(DataType.BYTE, value, Modifier.ADD)
-        return this
-    }
+    fun writeByteAdd(value: Int): Writer
 
-    fun writeByteInverse(value: Int): Writer {
-        write(DataType.BYTE, value, Modifier.INVERSE)
-        return this
-    }
+    fun writeByteInverse(value: Int): Writer
 
-    fun writeByteSubtract(value: Int): Writer {
-        write(DataType.BYTE, value, Modifier.SUBTRACT)
-        return this
-    }
+    fun writeByteSubtract(value: Int): Writer
 
-    /**
-     * Writes a boolean as a byte to [buffer].
-     * @param value [Boolean]
-     */
     fun writeByte(value: Boolean): Writer {
         writeByte(if (value) 1 else 0)
         return this
     }
 
-    /**
-     * Writes a [Short] to [buffer].
-     * @param value [Int]
-     */
-    fun writeShort(value: Int, type: Modifier = Modifier.NONE, order: Endian = Endian.BIG): Writer {
-        write(DataType.SHORT, value, type, order)
-        return this
-    }
+    fun writeShort(value: Int): Writer
 
-    fun writeShortAdd(value: Int): Writer {
-        write(DataType.SHORT, value, Modifier.ADD, Endian.BIG)
-        return this
-    }
+    fun writeShortAdd(value: Int): Writer
 
-    fun writeShortLittle(value: Int): Writer {
-        write(DataType.SHORT, value, Modifier.NONE, Endian.LITTLE)
-        return this
-    }
+    fun writeShortLittle(value: Int): Writer
 
-    fun writeShortAddLittle(value: Int): Writer {
-        write(DataType.SHORT, value, Modifier.ADD, Endian.LITTLE)
-        return this
-    }
+    fun writeShortAddLittle(value: Int): Writer
 
-    /**
-     * Writes a Medium [Int] to [buffer]
-     * @param value [Int]
-     */
-    fun writeMedium(value: Int): Writer {
-        write(DataType.MEDIUM, value)
-        return this
-    }
+    fun writeMedium(value: Int): Writer
 
-    /**
-     * Writes a [Int] to [buffer].
-     * @param value [Int]
-     */
-    fun writeInt(value: Int): Writer {
-        write(DataType.INT, value, Modifier.NONE, Endian.BIG)
-        return this
-    }
+    fun writeInt(value: Int): Writer
 
-    fun writeIntMiddle(value: Int): Writer {
-        write(DataType.INT, value, Modifier.NONE, Endian.MIDDLE)
-        return this
-    }
+    fun writeIntMiddle(value: Int): Writer
 
-    fun writeIntInverse(value: Int): Writer {
-        write(DataType.INT, value, Modifier.INVERSE, Endian.BIG)
-        return this
-    }
+    fun writeIntInverse(value: Int): Writer
 
-    fun writeIntInverseMiddle(value: Int): Writer {
-        write(DataType.INT, value, Modifier.INVERSE, Endian.MIDDLE)
-        return this
-    }
+    fun writeIntInverseMiddle(value: Int): Writer
 
-    fun writeIntLittle(value: Int): Writer {
-        write(DataType.INT, value, Modifier.NONE, Endian.LITTLE)
-        return this
-    }
+    fun writeIntLittle(value: Int): Writer
 
-    fun writeIntInverseLittle(value: Int): Writer {
-        write(DataType.INT, value, Modifier.INVERSE, Endian.LITTLE)
-        return this
-    }
+    fun writeIntInverseLittle(value: Int): Writer
 
-    /**
-     * Writes a [Long] to [buffer].
-     * @param value [Long]
-     */
-    fun writeLong(value: Long): Writer {
-        write(DataType.LONG, value)
-        return this
-    }
+    fun writeLong(value: Long): Writer
 
     fun writeSmart(value: Int): Writer {
         if (value >= 128) {
@@ -184,8 +109,5 @@ interface Writer {
     fun clear()
 
     fun remaining(): Int
-
-    @Throws(IllegalArgumentException::class)
-    fun write(type: DataType, value: Number, modifier: Modifier = Modifier.NONE, order: Endian = Endian.BIG)
 
 }
