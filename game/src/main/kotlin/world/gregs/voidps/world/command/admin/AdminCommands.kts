09bb0e38dc8b6e385d0fe9691825ce8243e0e69b
GregHib <greghib@users.noreply.github.com>
2021-03-13 16:06:44 +0000

    Spawn npcs by area, clear npc extras except npcs in use, Move npc loading and spawning into engine

--- game/src/main/kotlin/world/gregs/voidps/world/command/admin/AdminCommands.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/command/admin/AdminCommands.kts
@@ -3,6 +3,7 @@ import world.gregs.voidps.engine.client.variable.setVar
 import world.gregs.voidps.engine.data.StorageStrategy
 import world.gregs.voidps.engine.entity.Direction
 import world.gregs.voidps.engine.entity.character.contain.inventory
+import world.gregs.voidps.engine.entity.character.npc.NPCLoader
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.chat.ChatType
 import world.gregs.voidps.engine.entity.character.player.chat.Command
@@ -11,13 +12,13 @@ import world.gregs.voidps.engine.entity.character.player.login.LoginQueue
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.update.visual.player.tele
 import world.gregs.voidps.engine.entity.definition.ItemDefinitions
+import world.gregs.voidps.engine.entity.definition.NPCDefinitions
 import world.gregs.voidps.engine.event.EventBus
 import world.gregs.voidps.engine.event.then
 import world.gregs.voidps.engine.event.where
 import world.gregs.voidps.network.encode.message
 import world.gregs.voidps.utility.get
 import world.gregs.voidps.utility.inject
-import world.gregs.voidps.world.interact.entity.npc.spawn.NPCSpawn
 import java.util.concurrent.atomic.AtomicInteger
 
 val bus: EventBus by inject()
@@ -37,16 +38,22 @@ Command where { prefix == "tele" || prefix == "tp" } then {
 }
 
 Command where { prefix == "npc" } then {
-    val id = content.toIntOrNull() ?: 1
-    println(
-        """
-        - id: $id
-          x: ${player.tile.x}
-          y: ${player.tile.y}
-          plane: ${player.tile.plane}
-    """.trimIndent()
-    )
-    val npc = bus.emit(NPCSpawn(id, player.tile, Direction.NORTH))
+    val id = content.toIntOrNull()
+    val defs: NPCDefinitions = get()
+    val spawns: NPCLoader = get()
+    val npc = if (id != null) {
+        spawns.spawn(id, player.tile, Direction.NORTH)
+    } else {
+        println(
+                """
+            - name: $content
+              x: ${player.tile.x}
+              y: ${player.tile.y}
+              plane: ${player.tile.plane}
+        """.trimIndent()
+        )
+        spawns.spawn(content, player.tile, Direction.NORTH)
+    }
 //    npc?.movement?.frozen = true
 }
 
@@ -65,10 +72,11 @@ Command where { prefix == "item" } then {
     val parts = content.split(" ")
     val id = parts[0].toIntOrNull() ?: definitions.getId(parts[0].toLowerCase())
     var amount = parts.getOrNull(1) ?: "1"
-    if(amount == "max") {
+    if (amount == "max") {
         amount = Int.MAX_VALUE.toString()
     }
     player.inventory.add(id, amount.toInt())
+    println(player.inventory.result)
 }
 
 Command where { prefix == "find" } then {
@@ -100,4 +108,9 @@ Command where { prefix == "master" } then {
 
 Command where { prefix == "hide" } then {
     player.effects.toggle(Hidden)
+}
+
+Command where { prefix == "pos" || prefix == "mypos" } then {
+    player.message(player.tile.toString())
+    println(player.tile)
 }
