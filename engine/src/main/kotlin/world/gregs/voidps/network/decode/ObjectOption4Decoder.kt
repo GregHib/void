69ae0a7fdd15c35390f8a30b9ded5d3e9b33d925
GregHib <greghib@users.noreply.github.com>
2021-03-18 19:09:05 +0000

    Fix object decoders

--- engine/src/main/kotlin/world/gregs/voidps/network/decode/ObjectOption4Decoder.kt
+++ engine/src/main/kotlin/world/gregs/voidps/network/decode/ObjectOption4Decoder.kt
@@ -2,7 +2,10 @@ package world.gregs.voidps.network.decode
 
 import io.ktor.utils.io.core.*
 import world.gregs.voidps.engine.entity.character.player.Player
-import world.gregs.voidps.network.*
+import world.gregs.voidps.network.Decoder
+import world.gregs.voidps.network.Handler
+import world.gregs.voidps.network.readBooleanAdd
+import world.gregs.voidps.network.readUnsignedShortAdd
 
 class ObjectOption4Decoder(handler: Handler? = null) : Decoder(7, handler) {
 
@@ -11,7 +14,7 @@ class ObjectOption4Decoder(handler: Handler? = null) : Decoder(7, handler) {
             player = player,
             run = packet.readBooleanAdd(),
             objectId = packet.readUnsignedShortAdd(),
-            x = packet.readShortAdd(),
+            x = packet.readUnsignedShortAdd(),
             y = packet.readShortLittleEndian().toInt(),
             option = 4
         )
