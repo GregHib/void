3cfea270877e2a9055185ccdd6332c0eaaa18b91
GregHib <greghib@users.noreply.github.com>
2021-04-04 14:36:35 +0100

    Add chunk and region drawing to map viewer

--- tools/src/main/kotlin/world/gregs/voidps/tools/map/view/draw/WorldMap.kt
+++ tools/src/main/kotlin/world/gregs/voidps/tools/map/view/draw/WorldMap.kt
@@ -1,6 +1,7 @@
 package world.gregs.voidps.tools.map.view.draw
 
 import world.gregs.voidps.tools.map.view.RegionLoader
+import java.awt.Color
 import java.awt.Graphics
 import kotlin.math.max
 import kotlin.math.min
@@ -44,10 +45,22 @@ class WorldMap(private val view: MapView) {
     fun draw(g: Graphics) {
         for (regionX in minRegionX..maxRegionX) {
             for (regionY in minRegionY..maxRegionY) {
+                val mapY = view.regionToMapY(regionY) + 1
                 val viewX = view.regionToViewX(regionX)
-                val viewY = view.imageToViewY(view.mapToImageY(view.regionToMapY(regionY) + 1))
+                val viewY = view.imageToViewY(view.mapToImageY(mapY))
                 val region = regions.getRegion(regionX, flipRegionY(regionY), view.plane)
                 g.drawImage(region, viewX, viewY, view.regionToImageX(1), view.regionToImageY(1), null)
+
+                // Chunks
+                g.color = Color.ORANGE
+                for (i in 0..64 step 8) {
+                    g.drawLine(viewX + view.mapToImageX(i), viewY, viewX + view.mapToImageX(i), view.mapToViewY(mapY + 64))
+                    g.drawLine(viewX, viewY + view.mapToImageY(i), viewX + view.mapToImageX(64), viewY + view.mapToImageY(i))
+                }
+                // Regions
+                g.color = Color.MAGENTA
+                g.drawLine(viewX, viewY, viewX, view.mapToViewY(mapY + 64))
+                g.drawLine(viewX, viewY, viewX + view.mapToImageX(64), viewY)
             }
         }
     }
