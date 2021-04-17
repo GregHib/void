8c6d8647750f666e8257ba664a305f4dee76850d
GregHib <greghib@users.noreply.github.com>
2021-04-17 14:27:27 +0100

    Update equipment bonus names

---
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/equip/EquipBonuses.kt
@@ -0,0 +1,22 @@
+package world.gregs.voidps.world.interact.entity.player.equip
+
+import world.gregs.voidps.cache.definition.data.ItemDefinition
+
+object EquipBonuses {
+
+    fun getValue(item: ItemDefinition, key: String): String? {
+        val value = item[key, 0]
+        if (value == 0) {
+            return null
+        }
+        return format(key, value, false)
+    }
+
+    fun format(key: String, value: Int, bonuses: Boolean): String {
+        return when (key) {
+            "magic_damage", "absorb_melee", "absorb_magic", "absorb_range" -> "${if (value >= 0) "+" else "-"}${value}%"
+            "str", "ranged_str" -> "${if (value > 0) "+" else if (value < 0) "-" else ""}${value / 10.0}"
+            else -> if(bonuses) "${if (value >= 0) "+" else "-"}${value}" else value.toString()
+        }
+    }
+}
