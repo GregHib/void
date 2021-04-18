cc9be63513245f79373bac7846fdaa241f3adaa7
GregHib <greghib@users.noreply.github.com>
2021-04-18 16:24:59 +0100

    Add int and hex colour support

---
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/Colour.kt
@@ -0,0 +1,27 @@
+package world.gregs.voidps.engine.client
+
+sealed class Colour(val int: Int) {
+    abstract class Chat(int: Int) : Colour(int) {
+        object ChatBlue : Chat(0x0000ff)
+        object ChatRed : Chat(0x800000)
+        object TradePurple : Chat(0x800080)
+        object DuelBrown : Chat(0x7e3200)
+        object AssistPurple : Chat(0x8824e3)
+        object DropGreen : Chat(0x005100)
+    }
+
+    object Green : Colour(0x00ff00)
+    object Orange : Colour(0xff981f)
+    object Red : Colour(0xff0000)
+    object Yellow : Colour(0xffff00)
+    object Lime : Colour(0x00ff80)
+    object White : Colour(0xffffff)
+
+    val string = Integer.toHexString(int)
+
+    fun wrap(text: String): String {
+        val builder = StringBuilder()
+        builder.append("<col=$string>").append(text).append("</col>")
+        return builder.toString()
+    }
+}
