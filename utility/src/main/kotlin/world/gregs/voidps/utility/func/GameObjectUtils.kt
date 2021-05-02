aecadabd21dcf3dfefdb3fc675b72bea39a60350
GregHib <greghib@users.noreply.github.com>
2021-05-02 18:09:21 +0100

    Add custom object config reloading

--- utility/src/main/kotlin/world/gregs/voidps/utility/func/GameObjectUtils.kt
+++ utility/src/main/kotlin/world/gregs/voidps/utility/func/GameObjectUtils.kt
@@ -3,4 +3,5 @@ package world.gregs.voidps.utility.func
 import world.gregs.voidps.cache.definition.data.ObjectDefinition
 
 fun ObjectDefinition.isDoor() = (name.contains("door", true) && !name.contains("trap", true)) || isGate()
+
 fun ObjectDefinition.isGate() = name.contains("gate", true)
