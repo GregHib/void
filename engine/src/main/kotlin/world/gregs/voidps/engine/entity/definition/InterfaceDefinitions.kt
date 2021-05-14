2faa450570a41c1520e68814c17c1547d1f09803
GregHib <greghib@users.noreply.github.com>
2021-05-14 20:56:06 +0100

    Tidy up

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/definition/InterfaceDefinitions.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/definition/InterfaceDefinitions.kt
@@ -14,6 +14,7 @@ private const val DEFAULT_TYPE = "main_screen"
 private const val DEFAULT_FIXED_PARENT = GAME_FRAME_NAME
 private const val DEFAULT_RESIZE_PARENT = GAME_FRAME_RESIZE_NAME
 
+@Suppress("UNCHECKED_CAST")
 class InterfaceDefinitions(
     override val decoder: InterfaceDecoder
 ) : DefinitionsDecoder<InterfaceDefinition, InterfaceDecoder> {
@@ -51,118 +52,90 @@ class InterfaceDefinitions(
         typePath: String = getProperty("interfaceTypesPath")
     ): InterfaceDefinitions {
         timedLoad("interface") {
-            load(
-                loader.load(path),
-                loader.load<Map<String, Map<String, Any>>>(typePath)
-            )
+            load(loader.load(path), loader.load<Map<String, Map<String, Any>>>(typePath))
         }
         return this
     }
 
     fun load(data: Map<String, Map<String, Any>>, typeData: Map<String, Map<String, Any>>): Int {
-        this.names = data.map { (name, values) -> values.getId() to name }.toMap()
+        this.names = data.map { (name, values) ->
+            val id = values["id"] as? Int
+            checkNotNull(id) { "Missing interface id $id" }
+            id to name
+        }.toMap()
         val types = loadTypes(typeData)
-        extras = loadDetails(data, types, loadComponentNames2(data))
-
-        componentNames = loadComponentNames(data)
-        componentExtras = loadComponentDetails(data)
+        val components = getComponentsMap(data)
+        val idToNames = components.mapValues { it.value.toMap() }
+        componentNames = components.mapValues { entry -> entry.value.associate { it.second to it.first } }
+        extras = loadInterfaceExtras(data, types, idToNames)
+        componentExtras = loadComponentExtras(data)
         return names.size
     }
 
-
-    private fun loadComponentNames(data: Map<String, Map<String, Any>>) = data.mapNotNull { (name, values) ->
-        val components = values["components"] as? Map<*, *> ?: return@mapNotNull null
-        name to components.mapNotNull components@{
-            when (it.value) {
-                is Int -> it.value as Int
-                is Map<*, *> -> (it.value as Map<*, *>)["id"] as Int
-                else -> return@components null
-            } to it.key as String
-        }.toMap()
-    }.toMap()
-
-    private fun loadComponentNames2(data: Map<String, Map<String, Any>>) = data.mapNotNull { (name, values) ->
-        val components = values["components"] as? Map<*, *> ?: return@mapNotNull null
-        name to components.mapNotNull components@{
-            it.key as String to when (it.value) {
-                is Int -> it.value as Int
-                is Map<*, *> -> (it.value as Map<*, *>)["id"] as Int
+    private fun getComponentsMap(data: Map<String, Map<String, Any>>) = data.mapNotNull { (name, values) ->
+        val map = values["components"] as? Map<*, *> ?: return@mapNotNull null
+        name to map.mapNotNull components@{
+            it.key as String to when (val value = it.value) {
+                is Int -> value
+                is Map<*, *> -> value["id"] as Int
                 else -> return@components null
             }
-        }.toMap()
+        }
     }.toMap()
 
-    private fun loadTypes(data: Map<String, Map<String, Any>>) = data.map { (name, values) ->
+    private fun loadTypes(data: Map<String, Map<String, Any>>) = data.mapValues { (_, values) ->
         val index = values["index"] as? Int
-        val fixedIndex = index ?: values["fixedIndex"] as Int
-        val resizeIndex = index ?: values["resizeIndex"] as Int
-
         val parent = values["parent"] as? String
-        val fixedParentName = parent ?: values["fixedParent"] as? String ?: DEFAULT_FIXED_PARENT
-        val resizeParentName = parent ?: values["resizeParent"] as? String ?: DEFAULT_RESIZE_PARENT
-        name to mapOf(
-            "parent_fixed" to fixedParentName,
-            "parent_resize" to resizeParentName,
-            "index_fixed" to fixedIndex,
-            "index_resize" to resizeIndex
+        mapOf(
+            "parent_fixed" to (parent ?: values["fixedParent"] as? String ?: DEFAULT_FIXED_PARENT),
+            "parent_resize" to (parent ?: values["resizeParent"] as? String ?: DEFAULT_RESIZE_PARENT),
+            "index_fixed" to (index ?: values["fixedIndex"] as Int),
+            "index_resize" to (index ?: values["resizeIndex"] as Int)
         )
-    }.toMap()
+    }
 
-    private fun loadDetails(
+    private fun loadInterfaceExtras(
         data: Map<String, Map<String, Any>>,
         types: Map<String, Map<String, Any>>,
         components: Map<String, Map<String, Int>>
-    ) = data.map { (name, values) ->
+    ) = data.mapValues { (name, values) ->
         val typeName = values["type"] as? String ?: DEFAULT_TYPE
         val type = types[typeName]
         checkNotNull(type) { "Missing interface type $typeName" }
-        name to values.toMutableMap().apply {
+        values.toMutableMap().apply {
             putAll(type)
             this["name"] = name
             components[name]?.let {
                 this["componentNames"] = it
             }
         }
-    }.toMap()
+    }
 
-    private fun loadComponentDetails(
+    private fun loadComponentExtras(
         data: Map<String, Map<String, Any>>
     ) = data.mapNotNull { (name, values) ->
-        val parent = values.getId()
-        name to ((values["components"] as? Map<*, *>)?.map {
-            componentExtras(it.key as String, it.value!!, parent)
-        }?.toMap() ?: return@mapNotNull null)
-    }.toMap()
-
-    private fun componentExtras(name: String, value: Any, parent: Int): Pair<Int, Map<String, Any>> {
-        var id = value as? Int
-        val out = mutableMapOf<String, Any>(
-            "name" to name,
-            "parent" to parent,
-        )
-        (value as? Map<*, *>)?.let { extras ->
-            id = extras["id"] as Int
-            (extras["container"] as? String)?.let {
-                out["container"] = it
-            }
-            (extras["primary"] as? Boolean)?.let {
-                out["primary"] = it
-            }
-            (extras["options"] as? Map<*, *>)?.let {
-                val options = Array(it.maxOf { it.value as Int } + 1) { "" }
-                it.forEach { (option, index) ->
-                    options[index as Int] = option as String
+        val parent = values["id"] as Int
+        val map = values["components"] as? Map<String, Any> ?: return@mapNotNull null
+        name to map.map { (name, value) ->
+            var id = value as? Int
+            val out = mutableMapOf<String, Any>(
+                "name" to name,
+                "parent" to parent,
+            )
+            (value as? Map<*, *>)?.let { extras ->
+                id = extras["id"] as Int
+                (extras["container"] as? String)?.let { out["container"] = it }
+                (extras["primary"] as? Boolean)?.let { out["primary"] = it }
+                (extras["options"] as? Map<*, *>)?.let {
+                    val options = Array(it.maxOf { it.value as Int } + 1) { "" }
+                    it.forEach { (option, index) ->
+                        options[index as Int] = option as String
+                    }
+                    out["options"] = options
                 }
-                out["options"] = options
             }
-        }
-        return id!! to out
-    }
-
-    private fun Map<String, Any>.getId(): Int {
-        val id = this["id"] as? Int
-        checkNotNull(id) { "Missing interface id $id" }
-        return id
-    }
+            id!! to out
+        }.toMap()
+    }.toMap()
 
 }
