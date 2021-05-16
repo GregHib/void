de6075f362da1cdfb783f4bf2d5090b4ea1558a6
GregHib <greghib@users.noreply.github.com>
2021-05-16 23:33:15 +0100

    Refactor Variable internals

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/variable/Variables.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/variable/Variables.kt
@@ -8,20 +8,22 @@ import world.gregs.voidps.network.encode.sendVarc
 import world.gregs.voidps.network.encode.sendVarcStr
 import world.gregs.voidps.network.encode.sendVarp
 
+@Suppress("UNCHECKED_CAST")
 class Variables(
     val variables: MutableMap<String, Any> = mutableMapOf()
 ) {
     @JsonIgnore
     val temporaryVariables: MutableMap<String, Any> = mutableMapOf()
+
     @JsonIgnore
     lateinit var player: Player
+
     @JsonIgnore
     lateinit var manager: VariableManager
 
     fun <T : Any> set(key: String, value: T, refresh: Boolean) {
         val variable = manager.get(key) as? Variable<T> ?: return logger.debug { "Cannot find variable for key '$key'" }
-        val store = player.store(variable)
-        store.set(key, variable, value)
+        set(key, variable, value)
         if (refresh) {
             send(key)
         }
@@ -29,31 +31,27 @@ class Variables(
 
     fun send(key: String) {
         val variable = manager.get(key) ?: return logger.debug { "Cannot find variable for key '$key'" }
-        val store = player.store(variable)
-        variable.send(store, key)
+        variable.send(key)
     }
 
     fun <T : Any> get(key: String): T {
-        val variable = manager.get(key) as? Variable<T> ?: throw IllegalArgumentException("Unknown variable '$key'")
-        val store = player.store(variable)
-        return store.get(key, variable)
+        val variable = manager.get(key) as Variable<T>
+        return get(key, variable)
     }
 
     fun <T : Any> get(key: String, default: T): T {
         val variable = manager.get(key) as? Variable<T> ?: return default
-        val store = player.store(variable)
-        return store.get(key, variable)
+        return get(key, variable)
     }
 
     fun <T : Any> add(key: String, id: T, refresh: Boolean) {
         val variable = manager.get(key) as? BitwiseVar<T> ?: return logger.debug { "Cannot find variable for key '$key'" }
-        val store = player.store(variable)
 
         val power = variable.getValue(id) ?: return logger.debug { "Invalid bitwise value '$id'" }
-        val value = store.get(key, variable)
+        val value = get(key, variable)
 
-        if (!value.has(power)) {//If isn't already added
-            store.set(key, variable, value + power)//Add
+        if (!value.has(power)) {// If isn't already added
+            set(key, variable, value + power)// Add
             if (refresh) {
                 send(key)
             }
@@ -62,13 +60,12 @@ class Variables(
 
     fun <T : Any> remove(key: String, id: T, refresh: Boolean) {
         val variable = manager.get(key) as? BitwiseVariable<T> ?: return logger.debug { "Cannot find variable for key '$key'" }
-        val store = player.store(variable)
 
         val power = variable.getValue(id) ?: return logger.debug { "Invalid bitwise value '$id'" }
-        val value = store.get(key, variable)
+        val value = get(key, variable)
 
-        if (value.has(power)) {//If is added
-            store.set(key, variable, value - power)//Remove
+        if (value.has(power)) {// If is added
+            set(key, variable, value - power)// Remove
             if (refresh) {
                 send(key)
             }
@@ -77,16 +74,14 @@ class Variables(
 
     fun <T : Any> has(key: String, id: T): Boolean {
         val variable = manager.get(key) as? BitwiseVariable<T> ?: return false
-        val store = player.store(variable)
-
         val power = variable.getValue(id) ?: return false
-        val value = store.get(key, variable)
+        val value = get(key, variable)
 
         return value.has(power)
     }
 
-    internal fun <T : Any> Variable<T>.send(store: MutableMap<String, Any>, key: String) {
-        val value = store.get(key, this)
+    internal fun <T : Any> Variable<T>.send(key: String) {
+        val value = get(key, this)
         when (type) {
             Variable.Type.VARP -> player.sendVarp(id, toInt(value))
             Variable.Type.VARBIT -> player.sendVarbit(id, toInt(value))
@@ -95,32 +90,33 @@ class Variables(
         }
     }
 
+    private fun store(variable: Variable<*>): MutableMap<String, Any> = if (variable.persistent) variables else temporaryVariables
+
+    /**
+     * Gets Player variables current value or [variable] default
+     */
+    private fun <T : Any> get(key: String, variable: Variable<T>): T {
+        return store(variable)[key] as? T ?: variable.defaultValue
+    }
+
+    /**
+     * Sets Player variables value, removes if [variable] default
+     */
+    private fun <T : Any> set(key: String, variable: Variable<T>, value: T) {
+        if (value == variable.defaultValue) {
+            store(variable).remove(key)
+        } else {
+            store(variable)[key] = value
+        }
+    }
+
     companion object {
         private val logger = InlineLogger()
-
-        private fun Player.store(variable: Variable<*>): MutableMap<String, Any> = if (variable.persistent) variables.variables else variables.temporaryVariables
 
         /**
          * Checks if value [this] contains value [power]
          */
         private fun Int.has(power: Int) = (this and power) != 0
 
-        /**
-         * Gets Player variables current value or [variable] default
-         */
-        private fun <T : Any> MutableMap<String, Any>.get(key: String, variable: Variable<T>): T {
-            return this[key] as? T ?: variable.defaultValue
-        }
-
-        /**
-         * Sets Player variables value, removes if [variable] default
-         */
-        private fun <T : Any> MutableMap<String, Any>.set(key: String, variable: Variable<T>, value: T) {
-            if (value == variable.defaultValue) {
-                remove(key)
-            } else {
-                this[key] = value
-            }
-        }
     }
 }
