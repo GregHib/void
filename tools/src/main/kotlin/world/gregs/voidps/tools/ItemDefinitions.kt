8184a9a169ff8a84aa9b5a267adf78cb02e7bc1f
GregHib <greghib@users.noreply.github.com>
2021-05-27 15:00:19 +0100

    Remove _0 suffix off degradable item definitions

--- tools/src/main/kotlin/world/gregs/voidps/tools/ItemDefinitions.kt
+++ tools/src/main/kotlin/world/gregs/voidps/tools/ItemDefinitions.kt
@@ -4,6 +4,8 @@ import org.koin.core.context.startKoin
 import world.gregs.voidps.cache.definition.decoder.ItemDecoder
 import world.gregs.voidps.engine.client.cacheDefinitionModule
 import world.gregs.voidps.engine.client.cacheModule
+import world.gregs.voidps.engine.data.file.FileLoader
+import world.gregs.voidps.engine.entity.definition.ItemDefinitions
 
 object ItemDefinitions {
     @JvmStatic
@@ -13,12 +15,11 @@ object ItemDefinitions {
             modules(cacheModule, cacheDefinitionModule)
         }.koin
         val decoder = ItemDecoder(koin.get())
-        println(decoder.size)
-        println(decoder.get(1305))
+        val definitions = ItemDefinitions(decoder).load(FileLoader())
         for (i in 0 until decoder.size) {
-            val def = decoder.getOrNull(i) ?: continue
-            if (def.params?.containsKey(687) == true) {
-                println("$i ${def.name} ${def.params?.get(687)}")
+            val def = definitions.getOrNull(i) ?: continue
+            if(!def.has("weight") && !def.noted && !def.lent && def.stackable == 0) {
+                println("${definitions.getName(i)} ${def.extras}")
             }
         }
     }
