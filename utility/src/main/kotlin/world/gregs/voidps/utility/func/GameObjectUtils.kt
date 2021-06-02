c4242cc42cd3f88cd1f40fe5e24560c901dabea3
GregHib <greghib@users.noreply.github.com>
2021-06-02 19:25:43 +0100

    Fix al-kharid toll "gate"

--- utility/src/main/kotlin/world/gregs/voidps/utility/func/GameObjectUtils.kt
+++ utility/src/main/kotlin/world/gregs/voidps/utility/func/GameObjectUtils.kt
@@ -2,6 +2,6 @@ package world.gregs.voidps.utility.func
 
 import world.gregs.voidps.cache.definition.data.ObjectDefinition
 
-fun ObjectDefinition.isDoor() = (name.contains("door", true) && !name.contains("trap", true)) || isGate()
+fun ObjectDefinition.isDoor() = (name.contains("door", true) && !name.contains("trap", true)) || name.contains("gate", true)
 
-fun ObjectDefinition.isGate() = name.contains("gate", true)
+fun ObjectDefinition.isGate() = name.contains("gate", true) && id != 10565 && id != 10566
