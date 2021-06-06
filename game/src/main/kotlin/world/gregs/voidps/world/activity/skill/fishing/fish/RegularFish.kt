68b3c684febafe75e598b122e97b1ca28586b7ef
GregHib <greghib@users.noreply.github.com>
2021-06-06 20:45:01 +0100

    Add lumbridge crayfish spots

--- game/src/main/kotlin/world/gregs/voidps/world/activity/skill/fishing/fish/RegularFish.kt
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/skill/fishing/fish/RegularFish.kt
@@ -9,7 +9,7 @@ enum class RegularFish(
     override val chance: IntRange,
 ) : Catch {
     Shrimps(1, 10.0, 48..256),
-    Crayfish(1, 10.0, 1..1),
+    Crayfish(1, 10.0, 48..256),
     Sardine(5, 20.0, 32..192),
     Karambwanji(5, 5.0, 100..250),
     Herring(10, 30.0, 24..128),
