8a983dd4c96746c05418b4c50101fdb499fd7d16
GregHib <greghib@users.noreply.github.com>
2021-06-12 23:57:06 +0100

    Add priority to Events

--- engine/src/main/kotlin/world/gregs/voidps/engine/event/EventHandlerStore.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/event/EventHandlerStore.kt
@@ -33,27 +33,27 @@ class EventHandlerStore {
         return handlers[entity] ?: emptyMap()
     }
 
-    fun add(entity: KClass<out Entity>, event: KClass<out Event>, condition: Event.(Entity) -> Boolean, block: Event.(Entity) -> Unit) {
-        handlers.getOrPut(entity) { mutableMapOf() }.getOrPut(event) { mutableListOf() }.add(EventHandler(event, condition, block))
+    fun add(entity: KClass<out Entity>, event: KClass<out Event>, condition: Event.(Entity) -> Boolean, priority: Priority, block: Event.(Entity) -> Unit) {
+        handlers.getOrPut(entity) { mutableMapOf() }.getOrPut(event) { mutableListOf() }.add(EventHandler(event, condition, priority, block))
     }
 }
 
 @Suppress("UNCHECKED_CAST")
-inline fun <reified T : Entity, reified E : Event> on(noinline condition: E.(T) -> Boolean = { true }, noinline block: E.(T) -> Unit) {
-    get<EventHandlerStore>().add(T::class, E::class, condition as Event.(Entity) -> Boolean, block as Event.(Entity) -> Unit)
+inline fun <reified T : Entity, reified E : Event> on(noinline condition: E.(T) -> Boolean = { true }, priority: Priority = Priority.MEDIUM, noinline block: E.(T) -> Unit) {
+    get<EventHandlerStore>().add(T::class, E::class, condition as Event.(Entity) -> Boolean, priority, block as Event.(Entity) -> Unit)
 }
 
 @JvmName("onPlayer")
-inline fun <reified E : Event> on(noinline condition: E.(Player) -> Boolean = { true }, noinline block: E.(Player) -> Unit) = on<Player, E>(condition, block)
+inline fun <reified E : Event> on(noinline condition: E.(Player) -> Boolean = { true }, priority: Priority = Priority.MEDIUM, noinline block: E.(Player) -> Unit) = on<Player, E>(condition, priority, block)
 
 @JvmName("onNPC")
-inline fun <reified E : Event> on(noinline condition: E.(NPC) -> Boolean = { true }, noinline block: E.(NPC) -> Unit) = on<NPC, E>(condition, block)
+inline fun <reified E : Event> on(noinline condition: E.(NPC) -> Boolean = { true }, priority: Priority = Priority.MEDIUM, noinline block: E.(NPC) -> Unit) = on<NPC, E>(condition, priority, block)
 
 @JvmName("onItem")
-inline fun <reified E : Event> on(noinline condition: E.(FloorItem) -> Boolean = { true }, noinline block: E.(FloorItem) -> Unit) = on<FloorItem, E>(condition, block)
+inline fun <reified E : Event> on(noinline condition: E.(FloorItem) -> Boolean = { true }, priority: Priority = Priority.MEDIUM, noinline block: E.(FloorItem) -> Unit) = on<FloorItem, E>(condition, priority, block)
 
 @JvmName("onWorld")
-inline fun <reified E : Event> on(noinline condition: E.(World) -> Boolean = { true }, noinline block: E.(World) -> Unit) = on<World, E>(condition, block)
+inline fun <reified E : Event> on(noinline condition: E.(World) -> Boolean = { true }, priority: Priority = Priority.MEDIUM, noinline block: E.(World) -> Unit) = on<World, E>(condition, priority, block)
 
 @JvmName("onBot")
-inline fun <reified E : Event> on(noinline condition: E.(Bot) -> Boolean = { true }, noinline block: E.(Bot) -> Unit) = on<Bot, E>(condition, block)
+inline fun <reified E : Event> on(noinline condition: E.(Bot) -> Boolean = { true }, priority: Priority = Priority.MEDIUM, noinline block: E.(Bot) -> Unit) = on<Bot, E>(condition, priority, block)
---
+++ engine/src/main/kotlin/world/gregs/voidps/engine/event/Priority.kt
@@ -0,0 +1,7 @@
+package world.gregs.voidps.engine.event
+
+enum class Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
