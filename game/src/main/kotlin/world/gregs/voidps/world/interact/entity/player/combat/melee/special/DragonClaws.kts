37afc30a86eca619323539b3d940ef5eab267ca2
GregHib <greghib@users.noreply.github.com>
2021-08-14 16:35:48 +0100

    Add simplified dragon claws special attack

---
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/special/DragonClaws.kts
@@ -0,0 +1,30 @@
+package world.gregs.voidps.world.interact.entity.player.combat.melee.special
+
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.update.visual.setAnimation
+import world.gregs.voidps.engine.entity.character.update.visual.setGraphic
+import world.gregs.voidps.engine.entity.item.Item
+import world.gregs.voidps.engine.event.Priority
+import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.world.interact.entity.combat.CombatSwing
+import world.gregs.voidps.world.interact.entity.combat.hit
+import world.gregs.voidps.world.interact.entity.combat.weapon
+import world.gregs.voidps.world.interact.entity.player.combat.range.special.MAX_SPECIAL_ATTACK
+import world.gregs.voidps.world.interact.entity.player.combat.range.special.drainSpecialEnergy
+import world.gregs.voidps.world.interact.entity.player.combat.range.special.specialAttack
+
+fun isDClaws(weapon: Item?) = weapon != null && weapon.name == "dragon_claws"
+
+on<CombatSwing>({ !swung() && isDClaws(it.weapon) }, Priority.LOW) { player: Player ->
+    if (player.specialAttack && !drainSpecialEnergy(player, MAX_SPECIAL_ATTACK / 2)) {
+        delay = -1
+        return@on
+    }
+    player.setAnimation("slice_and_dice")
+    player.setGraphic("slice_and_dice")
+    player.hit(target)
+    player.hit(target)
+    player.hit(target, delay = 1)
+    player.hit(target, delay = 1)
+    delay = 4
+}
