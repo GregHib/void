e1ebd96436d69bc362f87b80a2452b3848b59983
GregHib <greghib@users.noreply.github.com>
2021-08-14 16:56:33 +0100

    Add armadyl godsword special attack

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/special/ArmadylGodsword.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/special/ArmadylGodsword.kts
@@ -1,12 +1,38 @@
 package world.gregs.voidps.world.interact.entity.player.combat.melee.special
 
 import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.update.visual.setAnimation
+import world.gregs.voidps.engine.entity.character.update.visual.setGraphic
+import world.gregs.voidps.engine.entity.item.Item
 import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.world.interact.entity.combat.CombatSwing
 import world.gregs.voidps.world.interact.entity.combat.HitDamageModifier
+import world.gregs.voidps.world.interact.entity.combat.hit
+import world.gregs.voidps.world.interact.entity.combat.weapon
+import world.gregs.voidps.world.interact.entity.player.combat.melee.specialAccuracyMultiplier
+import world.gregs.voidps.world.interact.entity.player.combat.melee.specialDamageMultiplier
+import world.gregs.voidps.world.interact.entity.player.combat.range.special.MAX_SPECIAL_ATTACK
+import world.gregs.voidps.world.interact.entity.player.combat.range.special.drainSpecialEnergy
 import world.gregs.voidps.world.interact.entity.player.combat.range.special.specialAttack
 import kotlin.math.floor
 
-on<HitDamageModifier>({ player -> type == "melee" && player.specialAttack && weapon?.name == "armadyl_godsword" }, Priority.LOW) { _: Player ->
+fun isArmadylGodsword(weapon: Item?) = weapon != null && weapon.name.startsWith("armadyl_godsword")
+
+on<HitDamageModifier>({ player -> type == "melee" && player.specialAttack && isArmadylGodsword(weapon) }, Priority.LOW) { _: Player ->
     damage = floor(damage * 1.25)
+}
+
+specialDamageMultiplier(1.1, ::isArmadylGodsword)
+specialAccuracyMultiplier(2.0, ::isArmadylGodsword)
+
+on<CombatSwing>({ !swung() && it.specialAttack && isArmadylGodsword(it.weapon) }) { player: Player ->
+    if (!drainSpecialEnergy(player, MAX_SPECIAL_ATTACK / 2)) {
+        delay = -1
+        return@on
+    }
+    player.setAnimation("the_judgement")
+    player.setGraphic("the_judgement")
+    player.hit(target)
+    delay = 6
 }
