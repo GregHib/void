b65384ddf79aaa652d3bb6f02e8dde7096177fb5
GregHib <greghib@users.noreply.github.com>
2021-08-25 19:41:36 +0100

    Add brine sabre special attack

---
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/special/BrineSabre.kts
@@ -0,0 +1,37 @@
+package world.gregs.voidps.world.interact.entity.player.combat.melee.special
+
+import world.gregs.voidps.engine.client.variable.VariableSet
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.update.visual.setAnimation
+import world.gregs.voidps.engine.entity.character.update.visual.setGraphic
+import world.gregs.voidps.engine.entity.item.Item
+import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.network.encode.message
+import world.gregs.voidps.world.interact.entity.combat.CombatSwing
+import world.gregs.voidps.world.interact.entity.combat.hit
+import world.gregs.voidps.world.interact.entity.combat.weapon
+import world.gregs.voidps.world.interact.entity.player.combat.melee.specialAccuracyMultiplier
+import world.gregs.voidps.world.interact.entity.player.combat.range.special.drainSpecialEnergy
+import world.gregs.voidps.world.interact.entity.player.combat.range.special.specialAttack
+
+fun isBrineSabre(item: Item?) = item != null && item.name == "brine_sabre"
+
+specialAccuracyMultiplier(2.0, ::isBrineSabre)
+
+on<VariableSet>({ key == "special_attack" && to == true && isBrineSabre(it.weapon) }) { player: Player ->
+    if (player.tile.region.id != 11924) {
+        player.message("You can only use this special attack under water.")
+        player.specialAttack = false
+    }
+}
+
+on<CombatSwing>({ !swung() && it.specialAttack && isBrineSabre(it.weapon) }) { player: Player ->
+    if (!drainSpecialEnergy(player, 750)) {
+        delay = -1
+        return@on
+    }
+    player.setAnimation("liquify")
+    player.setGraphic("liquify", height = 100)
+    player.hit(target)
+    delay = 4
+}
