7f5021755b37468b3e793857ff2e69ed6af78c1b
GregHib <greghib@users.noreply.github.com>
2021-08-30 18:11:32 +0100

    Add dragon halberd special attack

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/weapon/Halberd.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/weapon/Halberd.kts
@@ -12,32 +12,27 @@ import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.world.interact.entity.combat.*
 
-fun isSpear(item: Item?) = item != null && (item.name.endsWith("spear") || item.name.endsWith("hasta"))
+fun isHalberd(item: Item?) = item != null && item.name.endsWith("halberd")
 
-on<Registered>({ isSpear(it.equipped(EquipSlot.Weapon)) }) { player: Player ->
+on<Registered>({ isHalberd(it.equipped(EquipSlot.Weapon)) }) { player: Player ->
     updateWeapon(player, player.equipped(EquipSlot.Weapon))
 }
 
-on<ItemChanged>({ container == "worn_equipment" && index == EquipSlot.Weapon.index && isSpear(item) }) { player: Player ->
+on<ItemChanged>({ container == "worn_equipment" && index == EquipSlot.Weapon.index && isHalberd(item) }) { player: Player ->
     updateWeapon(player, item)
 }
 
 fun updateWeapon(player: Player, weapon: Item) {
-    player["attack_range"] = 1
+    player["attack_range"] = 2
     player.weapon = weapon
 }
 
-on<CombatSwing>({ !swung() && isSpear(it.weapon) }, Priority.LOWER) { player: Player ->
-    player.setAnimation("spear_${
-        when (player.attackType) {
-            "block" -> "lunge"
-            else -> player.attackType
-        }
-    }")
+on<CombatSwing>({ !swung() && isHalberd(it.weapon) }, Priority.LOWER) { player: Player ->
+    player.setAnimation("halberd_${player.attackType}")
     player.hit(target)
-    delay = 4
+    delay = 7
 }
 
-on<CombatHit>({ isSpear(weapon) }, Priority.LOW) { player: Player ->
+on<CombatHit>({ isHalberd(weapon) }, Priority.LOW) { player: Player ->
     player.setAnimation("spear_block")
 }
