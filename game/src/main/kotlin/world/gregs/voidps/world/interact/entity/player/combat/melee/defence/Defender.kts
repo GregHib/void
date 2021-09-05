95e488e72acb9a718c9c926dffbe59667d8d25eb
GregHib <greghib@users.noreply.github.com>
2021-09-05 23:37:35 +0100

    Fix defence flinch animations

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/defence/Defender.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/defence/Defender.kts
@@ -2,13 +2,15 @@ package world.gregs.voidps.world.interact.entity.player.combat.melee.defence
 
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.update.visual.setAnimation
+import world.gregs.voidps.engine.entity.item.EquipSlot
 import world.gregs.voidps.engine.entity.item.Item
+import world.gregs.voidps.engine.entity.item.equipped
 import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.world.interact.entity.combat.CombatHit
 
 fun isDefender(item: Item?) = item != null && item.name.endsWith("defender")
 
-on<CombatHit>({ isDefender(weapon) }, Priority.LOWER) { player: Player ->
-    player.setAnimation("defender_block")
+on<CombatHit>({ isDefender(it.equipped(EquipSlot.Shield)) }, Priority.LOWER) { player: Player ->
+    player.setAnimation("defender_block", override = true)
 }
