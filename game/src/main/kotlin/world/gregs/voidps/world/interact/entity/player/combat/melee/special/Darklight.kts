55486f459a5ff572708bd14c82adc22dd686d755
GregHib <greghib@users.noreply.github.com>
2021-09-05 16:39:31 +0100

    Add darklight special attack

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/special/Darklight.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/special/Darklight.kts
@@ -3,11 +3,15 @@ package world.gregs.voidps.world.interact.entity.player.combat.melee.special
 import world.gregs.voidps.engine.entity.character.Character
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.engine.entity.character.update.visual.setAnimation
+import world.gregs.voidps.engine.entity.character.update.visual.setGraphic
 import world.gregs.voidps.engine.entity.item.Item
 import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.engine.event.on
-import world.gregs.voidps.world.interact.entity.combat.CombatHit
-import world.gregs.voidps.world.interact.entity.combat.HitDamageModifier
+import world.gregs.voidps.world.interact.entity.combat.*
+import world.gregs.voidps.world.interact.entity.player.combat.MAX_SPECIAL_ATTACK
+import world.gregs.voidps.world.interact.entity.player.combat.drainSpecialEnergy
+import world.gregs.voidps.world.interact.entity.player.combat.specialAttack
 import kotlin.math.floor
 
 fun isDemon(target: Character?): Boolean = target != null
@@ -16,6 +20,17 @@ fun isDarklight(weapon: Item?) = weapon != null && weapon.name == "darklight"
 
 on<HitDamageModifier>({ type == "melee" && isDarklight(weapon) && isDemon(target) }, Priority.LOW) { _: Player ->
     damage = floor(damage * 1.60)
+}
+
+on<CombatSwing>({ !swung() && it.specialAttack && isDarklight(it.weapon) }) { player: Player ->
+    if (!drainSpecialEnergy(player, MAX_SPECIAL_ATTACK / 2)) {
+        delay = -1
+        return@on
+    }
+    player.setAnimation("weaken")
+    player.setGraphic("weaken")
+    player.hit(target)
+    delay = 5
 }
 
 on<CombatHit>({ isDarklight(weapon) && special }) { character: Character ->
