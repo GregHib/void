6a400162d361dcaa900711a31b079a3cfa77a227
GregHib <greghib@users.noreply.github.com>
2021-09-18 20:30:16 +0100

    Fix brutal arrow shooting gfx

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/range/weapon/Bows.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/range/weapon/Bows.kts
@@ -14,7 +14,7 @@ fun isBow(item: Item) = (item.name.endsWith("bow") && !item.name.endsWith("cross
 on<CombatSwing>({ player -> !swung() && isBow(player.weapon) }, Priority.LOW) { player: Player ->
     player.setAnimation("bow_shoot")
     val ammo = player.ammo
-    player.setGraphic("${ammo}_shoot")
+    player.setGraphic("${if (ammo.endsWith("brutal")) "brutal" else ammo}_shoot")
     player.shoot(name = if (ammo.endsWith("brutal")) "brutal_arrow" else ammo, target = target, delay = 40, height = if (ammo.contains("ogre") || ammo.endsWith("brutal")) 40 else 43, endHeight = target.height, curve = 8)
     player.hit(target)
     val speed = player.weapon.def["attack_speed", 4]
