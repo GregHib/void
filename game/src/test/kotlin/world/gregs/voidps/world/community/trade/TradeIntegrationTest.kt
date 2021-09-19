0ba7ceb66086ba11df6c391df33338d45a434b0b
GregHib <greghib@users.noreply.github.com>
2021-09-19 18:18:11 +0100

    Update world content test class

--- game/src/test/kotlin/world/gregs/voidps/world/community/trade/TradeIntegrationTest.kt
+++ game/src/test/kotlin/world/gregs/voidps/world/community/trade/TradeIntegrationTest.kt
@@ -1,16 +1,21 @@
 package world.gregs.voidps.world.community.trade
 
-import io.mockk.spyk
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import world.gregs.voidps.cache.config.data.ContainerDefinition
+import world.gregs.voidps.cache.config.decoder.ContainerDecoder
+import world.gregs.voidps.cache.definition.data.ItemDefinition
+import world.gregs.voidps.cache.definition.decoder.ItemDecoder
 import world.gregs.voidps.engine.client.ui.InterfaceOption
-import world.gregs.voidps.engine.data.PlayerFactory
 import world.gregs.voidps.engine.entity.character.contain.inventory
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.PlayerOption
 import world.gregs.voidps.engine.entity.item.Item
-import world.gregs.voidps.utility.get
 import world.gregs.voidps.world.script.WorldMock
 
 internal class TradeIntegrationTest : WorldMock() {
@@ -18,17 +23,38 @@ internal class TradeIntegrationTest : WorldMock() {
     lateinit var player1: Player
     lateinit var player2: Player
 
+    override fun loadModules(): MutableList<Module> {
+        val modules = super.loadModules()
+        modules.add(module {
+            single(override = true) {
+                mockk<ContainerDecoder> {
+                    every { get(any<Int>()) } answers { ContainerDefinition(id = arg(0)) }
+                    every { get(93) } returns ContainerDefinition(id = 93, length = 28)
+                    every { get(94) } returns ContainerDefinition(id = 94, length = 15)
+                    every { get(90) } returns ContainerDefinition(id = 90, length = 28)
+                }
+            }
+            single(override = true) {
+                mockk<ItemDecoder> {
+                    every { getOrNull(any()) } answers { ItemDefinition(id = arg(0)) }
+                    every { get(any<Int>()) } answers { ItemDefinition(id = arg(0)) }
+                    every { last } returns Short.MAX_VALUE.toInt()
+                    every { get(995) } returns ItemDefinition(
+                        id = 995,
+                        name = "Coins",
+                        stackable = 1
+                    )
+                }
+            }
+        })
+        return modules
+    }
+
     @BeforeEach
     override fun setup() {
         super.setup()
-        val factory: PlayerFactory = get()
-        player1 = spyk(factory.create("1", ""))
-        player1.start()
-        player2 = spyk(factory.create("2", ""))
-        player2.start()
-        setProperty("homeX", 100)
-        setProperty("homeY", 100)
-        setProperty("homePlane", 1)
+        player1 = createPlayer("1")
+        player2 = createPlayer("2")
     }
 
     @Test
@@ -36,7 +62,7 @@ internal class TradeIntegrationTest : WorldMock() {
         player1.inventory.add("coins", 1000)
         player1.events.emit(PlayerOption(player2, "Trade with", 4))
         player2.events.emit(PlayerOption(player1, "Trade with", 4))
-        offerItem("Offer-10", 2, 995, 0)
+        offerItem(player1, "Offer-10", 2, "coins", 0)
         acceptTrade(player1)
         acceptTrade(player2)
         tick()
@@ -49,8 +75,8 @@ internal class TradeIntegrationTest : WorldMock() {
         assertEquals(10, player2.inventory.getAmount(0))
     }
 
-    fun offerItem(option: String, optionId: Int, item: Int, slot: Int) {
-        player1.events.emit(InterfaceOption(336, "trade_side", 0, "offer", optionId, option, Item("item", item), slot))
+    fun offerItem(player: Player, option: String, optionId: Int, item: String, slot: Int) {
+        player.events.emit(InterfaceOption(336, "trade_side", 0, "offer", optionId, option, Item(item), slot))
     }
 
     fun acceptTrade(player: Player) {
--- game/src/test/kotlin/world/gregs/voidps/world/script/WorldMock.kt
+++ game/src/test/kotlin/world/gregs/voidps/world/script/WorldMock.kt
@@ -3,14 +3,24 @@ package world.gregs.voidps.world.script
 import com.github.michaelbull.logging.InlineLogger
 import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.junit.jupiter.api.BeforeEach
+import org.koin.core.module.Module
 import org.koin.dsl.module
 import world.gregs.voidps.cache.Cache
 import world.gregs.voidps.cache.Indices
 import world.gregs.voidps.engine.GameLoop
+import world.gregs.voidps.engine.action.Contexts
 import world.gregs.voidps.engine.client.cacheModule
+import world.gregs.voidps.engine.data.PlayerFactory
 import world.gregs.voidps.engine.entity.World
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.player.login.LoginQueue
 import world.gregs.voidps.engine.event.EventHandlerStore
+import world.gregs.voidps.engine.map.Tile
 import world.gregs.voidps.engine.tick.Startup
 import world.gregs.voidps.getGameModules
 import world.gregs.voidps.getTickStages
@@ -24,17 +34,22 @@ abstract class WorldMock : KoinMock() {
 
     init {
         every { cache.getFile(any(), archive = any(), file = any()) } returns null
+        every { cache.getFile(any(), name = any(), xtea = any()) } returns null
         every { cache.getFile(Indices.ENUMS, archive = any(), file = any()) } returns byteArrayOf(5, 0, 0, 0)
     }
 
-    override val modules = getGameModules().toMutableList().apply {
-        remove(cacheModule)
-        add(module {
-            single(createdAtStart = true) {
-                cache
-            }
-        })
+    open fun loadModules(): MutableList<Module> {
+        return getGameModules().toMutableList().apply {
+            remove(cacheModule)
+            add(module {
+                single(createdAtStart = true) {
+                    cache
+                }
+            })
+        }
     }
+
+    override val modules = loadModules()
 
     override val propertyPaths = listOf("/test.properties")
 
@@ -42,6 +57,27 @@ abstract class WorldMock : KoinMock() {
 
     fun tick() {
         engine.run()
+    }
+
+    fun createPlayer(name: String, tile: Tile = Tile.EMPTY): Player = runBlocking {
+        val loginQueue: LoginQueue = get()
+        val factory: PlayerFactory = get()
+        val index = loginQueue.login(name)!!
+        val player = Player(id = -1, tile = tile, name = name, passwordHash = "")
+        factory.initPlayer(player, index)
+        launch {
+            delay(1)
+            tick()
+            delay(1)
+            tick()
+        }
+        withContext(Contexts.Game) {
+            loginQueue.await()
+            player.login()
+            delay(1)
+            player.viewport.loaded = true
+        }
+        player
     }
 
     @Suppress("UNCHECKED_CAST")
