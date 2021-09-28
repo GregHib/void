adfeb7ffc1cb95a0d7a2b684b75b39fd00428668
GregHib <greghib@users.noreply.github.com>
2021-09-28 20:23:32 +0100

    Add combat bonus experience

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/npc/NPCLevels.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/npc/NPCLevels.kt
@@ -8,6 +8,11 @@ class NPCLevels(private val def: NPCDefinition) : Levels.Level {
     override fun getMaxLevel(skill: Skill): Int {
         return when (skill) {
             Skill.Constitution -> def["hitpoints", 10]
+            Skill.Attack -> def["att", 1]
+            Skill.Strength -> def["str", 1]
+            Skill.Defence -> def["def", 1]
+            Skill.Range -> def["range", 1]
+            Skill.Magic -> def["mage", 1]
             else -> 1
         }
     }
