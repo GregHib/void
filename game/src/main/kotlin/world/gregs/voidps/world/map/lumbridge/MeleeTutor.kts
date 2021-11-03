a4d830943031a1ce705765da0b59a5c8d3fd609d
GregHib <greghib@users.noreply.github.com>
2021-11-03 17:19:20 +0000

    Melee tutor formatting

--- game/src/main/kotlin/world/gregs/voidps/world/map/lumbridge/MeleeTutor.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/map/lumbridge/MeleeTutor.kts
@@ -31,12 +31,12 @@ suspend fun DialogueContext.menu(followUp: String = "") {
         npc("unsure", followUp)
     }
     val choice = choice("""
-            Tell me about melee combat.
-            Tell me about different weapon types I can use.
-            Tell me about skillcapes.
-            I'd like a training sword and shield.
-            Goodbye.
-        """)
+        Tell me about melee combat.
+        Tell me about different weapon types I can use.
+        Tell me about skillcapes.
+        I'd like a training sword and shield.
+        Goodbye.
+    """)
     chosen(choice)
 }
 
@@ -46,135 +46,132 @@ suspend fun DialogueContext.chosen(choice: Int) {
         2 -> weaponTypes()
         3 -> skillcapes()
         4 -> training()
-        5 -> {
-            player("talking", "Goodbye.")
-        }
     }
 }
 
 suspend fun DialogueContext.meleeCombat() {
     player("unsure", "Tell me about melee combat.")// raised eyebrow then quiet
     npc("talking", """
-            Well adventurer, the first thing you will need is a
-            sword and a shield appropriate for your level.
-        """)
+        Well adventurer, the first thing you will need is a
+        sword and a shield appropriate for your level.
+    """)
     // look down talking, look up eyebrow raised then quiet
     player.setVar("tab", Tab.WornEquipment.name)
     npc("talking", """
-            Make sure to equip your sword and shield. Click on
-            them in your inventory, they will disappear from your
-            inventory and move to your worn items. You can see
-            your worn items in the worn items tab here.
-        """)
+        Make sure to equip your sword and shield. Click on
+        them in your inventory, they will disappear from your
+        inventory and move to your worn items. You can see
+        your worn items in the worn items tab here.
+    """)
     player.setVar("tab", Tab.CombatStyles.name)
     npc("talking", """
-            When you are wielding your sword you will then be
-            able to see the correct options in the combat interface.
-        """)
+        When you are wielding your sword you will then be
+        able to see the correct options in the combat interface.
+    """)
     npc("talking", """
-            There are four different melee styles. Accurate,
-            aggressive, defensive and controlled. Not all weapons will
-            have all four styles though.
-        """)
+        There are four different melee styles. Accurate,
+        aggressive, defensive and controlled. Not all weapons will
+        have all four styles though.
+    """)
     player("unsure", "Interesting, what does each style do?")
     npc("talking", """
-            Well I am glad you asked. The accurate style will give
-            you experience points in your Attack skill, you will also
-            find you will deal damage more frequently as a result
-            of being, well, more accurate.
-        """)
+        Well I am glad you asked. The accurate style will give
+        you experience points in your Attack skill, you will also
+        find you will deal damage more frequently as a result
+        of being, well, more accurate.
+    """)
     npc("talking", """
-            Next we have the aggressive style. This style will give
-            you experience points in your Strength skill. When
-            using this style you will notice that your attacks will hit
-            a little harder.
-        """)
+        Next we have the aggressive style. This style will give
+        you experience points in your Strength skill. When
+        using this style you will notice that your attacks will hit
+        a little harder.
+    """)
     npc("talking", """
-            Now for the defensive style, this style will give you
-            experience points in your Defensive skill. When using
-            this style you will notice that you get hit less often.
-        """)
+        Now for the defensive style, this style will give you
+        experience points in your Defensive skill. When using
+        this style you will notice that you get hit less often.
+    """)
     npc("talking", """
-            Finally, we have the controlled style. This style will give
-            you the same amount of experience as the other styles
-            would but shared across all three of the combat skills.
-        """)
+        Finally, we have the controlled style. This style will give
+        you the same amount of experience as the other styles
+        would but shared across all three of the combat skills.
+    """)
     npc("talking", """
-            If you were using the training sword for example, there
-            are four different attack types. Stab, lunge, slash and
-            block.
-        """)
+        If you were using the training sword for example, there
+        are four different attack types. Stab, lunge, slash and
+        block.
+    """)
     npc("talking", """
-            Each type uses one of the attack styles. Stab uses
-            accurate, lunge and slash use aggressive and block uses
-            defensive.
-        """)
+        Each type uses one of the attack styles. Stab uses
+        accurate, lunge and slash use aggressive and block uses
+        defensive.
+    """)
     npc("talking", """
-            To find out which style an attack type uses, hover your
-            mouse cursor over the style button.
-        """)
+        To find out which style an attack type uses, hover your
+        mouse cursor over the style button.
+    """)
     npc("unsure", "Is there anything else you would like to know?")
     val choice = choice("""
-            What if I wanted to fight something a bit more... human.
-            Tell me about different weapon types I can use.
-            Tell me about skillcapes.
-            I'd like a training sword and shield.
-            Goodbye.
-        """)
+        What if I wanted to fight something a bit more... human.
+        Tell me about different weapon types I can use.
+        Tell me about skillcapes.
+        I'd like a training sword and shield.
+        Goodbye.
+    """)
     if (choice == 1) {
         player("talking", """
-                What if I wanted to fight something a bit more...
-                human.
-            """)
+            What if I wanted to fight something a bit more...
+            human.
+        """)
         npc("cheerful", """
-                Well adventurer, there are a few places you might be
-                able to do this.
-            """)
+            Well adventurer, there are a few places you might be
+            able to do this.
+        """)
         npc("talking", """
-                You could try your luck at castle wars. Here, two
-                teams fight each other to defend their respective flags.
-                To win the game you will need to get the other team's
-                flag and return it to your flag stand.
-            """)
+            You could try your luck at castle wars. Here, two
+            teams fight each other to defend their respective flags.
+            To win the game you will need to get the other team's
+            flag and return it to your flag stand.
+        """)
         player("cheerful", "Capture the flag, sounds like a lot of fun.")
         npc("talking", """
-                If you are in a clan, you should gather some clan
-                members and try out clan wars. There you can see
-                which clan is better than the other by fighting each
-                other in an arena.
-            """)
+            If you are in a clan, you should gather some clan
+            members and try out clan wars. There you can see
+            which clan is better than the other by fighting each
+            other in an arena.
+        """)
         npc("talking", """
-                Both activities are safe minigame, which means if you
-                die you will not loose any of your items. You can get
-                to them by using the teleport option in your minigames
-                tab.
-            """)
+            Both activities are safe minigame, which means if you
+            die you will not loose any of your items. You can get
+            to them by using the teleport option in your minigames
+            tab.
+        """)
         npc("suspicious", """
-                There is also the wilderness. The wilderness is north of
-                Varrock and you can fight other players there. But
-                bare in mind if you die to another player in the
-                wilderness you will lose your stuff.
-            """)
+            There is also the wilderness. The wilderness is north of
+            Varrock and you can fight other players there. But
+            bare in mind if you die to another player in the
+            wilderness you will lose your stuff.
+        """)
         npc("suspicious", """
-                But this also means that if you kill another player you
-                will be able to take their stuff too.
-            """)
+            But this also means that if you kill another player you
+            will be able to take their stuff too.
+        """)
         npc("suspicious", """
-                Only go into the wilderness with items you are willing
-                to lose and pay attention to the wilderness level you are
-                in. The higher the level you go, more player will be able
-                to attack you.
-            """)
+            Only go into the wilderness with items you are willing
+            to lose and pay attention to the wilderness level you are
+            in. The higher the level you go, more player will be able
+            to attack you.
+        """)
         npc("suspicious", """
-                You can find which player can attack you by checking
-                your combat level.
-            """)
+            You can find which player can attack you by checking
+            your combat level.
+        """)
         npc("suspicious", """
-                Minus the wilderness level from your combat level to
-                find the lowest level that you can attack, then add the
-                wilderness level to your combat level to find the highest
-                level that you can attack.
-            """)
+            Minus the wilderness level from your combat level to
+            find the lowest level that you can attack, then add the
+            wilderness level to your combat level to find the highest
+            level that you can attack.
+        """)
         menu("Is there anything else you would like to know?")
     } else {
         chosen(choice)
@@ -184,29 +181,29 @@ suspend fun DialogueContext.meleeCombat() {
 suspend fun DialogueContext.weaponTypes() {
     player("talking", "Tell me about different weapon types I can use.")
     npc("cheerful", """
-            Well let me see now...There are stabbing type weapons
-            such as daggers, then you have swords which are
-            slashing, maces that have great crushing abilities, battle
-            axes which are powerful.
-        """)
+        Well let me see now...There are stabbing type weapons
+        such as daggers, then you have swords which are
+        slashing, maces that have great crushing abilities, battle
+        axes which are powerful.
+    """)
     npc("cheerful", """
-            There are also spears. Spears can be good for Defence
-            and many forms of Attack.
-        """)
+        There are also spears. Spears can be good for Defence
+        and many forms of Attack.
+    """)
     npc("talking", """
-            It depends a lot on how you want to fight. Experiment
-            and find out what is best for you. Never be scared to
-            try out a new weapon; you never know, you might like
-            it!
-        """)
+        It depends a lot on how you want to fight. Experiment
+        and find out what is best for you. Never be scared to
+        try out a new weapon; you never know, you might like
+        it!
+    """)
     npc("talking", """
-            While I tried all of them for a while, I settled on this
-            rather good sword.
-        """)
+        While I tried all of them for a while, I settled on this
+        rather good sword.
+    """)
     npc("talking", """
-            You might also find that different weapon types are
-            more accurate against different monsters.
-        """)
+        You might also find that different weapon types are
+        more accurate against different monsters.
+    """)
     menu("Is there anything else you would like to know?")
 }
 
@@ -214,27 +211,27 @@ suspend fun DialogueContext.skillcapes() {
     player("talking", "Tell me about skillcapes.")
     if (player.levels.getMax(Skill.Defence) >= Level.MAX_LEVEL) {
         npc("talking", """
-                Ah, but I can see you're already a master in the fine
-                art of Defence. Perhaps you have come to me to
-                purchase a Skillcape of Defence, and thus join the elite
-                few who have mastered this exacting skill?
-            """)
+            Ah, but I can see you're already a master in the fine
+            art of Defence. Perhaps you have come to me to
+            purchase a Skillcape of Defence, and thus join the elite
+            few who have mastered this exacting skill?
+        """)
         npc("talking", """
-                In recognition of your defensive abilities, when you have
-                it equipped it will act as ring of life, saving you from
-                combat if your hitpoints become low.
-            """)
+            In recognition of your defensive abilities, when you have
+            it equipped it will act as ring of life, saving you from
+            combat if your hitpoints become low.
+        """)
         var choice = choice("""
-                May I buy a Skillcape of Defence, please?
-                Can I ask about something else?
-            """)
+            May I buy a Skillcape of Defence, please?
+            Can I ask about something else?
+        """)
         if (choice == 1) {
             buySkillcape()
         } else {
             choice = choice("""
-                    Skillcape
-                    Hood
-                """)
+                Skillcape
+                Hood
+            """)
             if (choice == 1) {
                 buySkillcape()
             } else {
@@ -246,15 +243,15 @@ suspend fun DialogueContext.skillcapes() {
         }
     } else {
         npc("talking", """
-                Of course. Skillcapes are a symbol of achievement. Only
-                people who have mastered a skill and reached level 99
-                can get their hands on them and gain the benefits they
-                carry.
-            """)
+            Of course. Skillcapes are a symbol of achievement. Only
+            people who have mastered a skill and reached level 99
+            can get their hands on them and gain the benefits they
+            carry.
+        """)
         npc("talking", """
-                The Cape of Defence will act as ring of life, saving you
-                from combat if your hitpoints become low.
-            """)
+            The Cape of Defence will act as ring of life, saving you
+            from combat if your hitpoints become low.
+        """)
         menu("Is there anything else you would like to know?")
     }
 }
@@ -262,31 +259,31 @@ suspend fun DialogueContext.skillcapes() {
 suspend fun DialogueContext.buySkillcape() {
     player("unsure", "May I buy a Skillcape of Defence, please?")
     npc("talking", """
-            You wish to join the elite defenders of this world? I'm
-            afraid such things do not come cheaply - in fact they
-            cost 99000 coins, to be precise!
-        """)
+        You wish to join the elite defenders of this world? I'm
+        afraid such things do not come cheaply - in fact they
+        cost 99000 coins, to be precise!
+    """)
     val choice = choice("""
-            99000 coins? That's much too expensive.
-            I think I have the money right here, actually.
-        """)
+        99000 coins? That's much too expensive.
+        I think I have the money right here, actually.
+    """)
     if (choice == 1) {
         player("unsure", "99000 coins? That's much too expensive.")
         npc("talking", """
-                Not at all; there are many other adventurers who
-                would love the opportunity to purchase such a
-                prestigious item! You can find me here if you change
-                your mind.
-            """)
+            Not at all; there are many other adventurers who
+            would love the opportunity to purchase such a
+            prestigious item! You can find me here if you change
+            your mind.
+        """)
     } else {
         player("cheerful", "I think I have the money right here, actually.")
         if (player.inventory.spaces < 2) {
             npc("upset", """
-                    Unfortunately all Skillcapes are only available with a free
-                    hood, it's part of a skill promotion deal; buy one get one
-                    free, you know. So you'll need to free up some
-                    inventory space before I can sell you one.
-                """)
+                Unfortunately all Skillcapes are only available with a free
+                hood, it's part of a skill promotion deal; buy one get one
+                free, you know. So you'll need to free up some
+                inventory space before I can sell you one.
+            """)
             return
         }
         if (player.inventory.remove("coins", 99000)) {
