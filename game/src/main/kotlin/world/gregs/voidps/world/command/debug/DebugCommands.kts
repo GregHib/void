dc72932adcf269a127af4ba8b22a8400ac444588
GregHib <greghib@users.noreply.github.com>
2021-11-09 21:46:26 +0000

    Dialogue expression test command

--- game/src/main/kotlin/world/gregs/voidps/world/command/debug/DebugCommands.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/command/debug/DebugCommands.kts
@@ -1,10 +1,12 @@
 import world.gregs.voidps.engine.action.action
 import world.gregs.voidps.engine.client.sendContainerItems
+import world.gregs.voidps.engine.client.ui.dialogue.dialogue
 import world.gregs.voidps.engine.client.ui.event.Command
+import world.gregs.voidps.engine.client.ui.open
+import world.gregs.voidps.engine.client.ui.sendAnimation
+import world.gregs.voidps.engine.client.ui.sendText
 import world.gregs.voidps.engine.entity.character.move.walkTo
 import world.gregs.voidps.engine.entity.character.player.Player
-import world.gregs.voidps.engine.entity.character.update.visual.setAnimation
-import world.gregs.voidps.engine.entity.character.update.visual.setGraphic
 import world.gregs.voidps.engine.entity.obj.Objects
 import world.gregs.voidps.engine.entity.obj.spawnObject
 import world.gregs.voidps.engine.event.on
@@ -18,17 +20,40 @@ import world.gregs.voidps.engine.path.strat.NodeTargetStrategy
 import world.gregs.voidps.engine.path.traverse.EdgeTraversal
 import world.gregs.voidps.engine.sync
 import world.gregs.voidps.engine.utility.get
+import world.gregs.voidps.network.encode.npcDialogueHead
+import world.gregs.voidps.network.encode.playerDialogueHead
+import world.gregs.voidps.world.interact.dialogue.sendLines
+import world.gregs.voidps.world.interact.dialogue.type.npc
 import world.gregs.voidps.world.interact.entity.gfx.areaGraphic
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 import kotlin.system.measureNanoTime
 
 on<Command>({ prefix == "test" }) { player: Player ->
-    player.setAnimation("12152")
-    player.setGraphic("2138")
-//    player.setAnimation("12174")
-//    player.setGraphic("2141")
 }
+
+on<Command>({ prefix == "expr" }) { player: Player ->
+    player.dialogue {
+        val id = content.toIntOrNull()
+        if (id != null) {
+            val npc = id < 1000
+            if (player.open("${if (npc) "npc_" else ""}chat1")) {
+                if (npc) {
+                    player.client?.npcDialogueHead(241, 15794178, 2176)
+                } else {
+                    player.client?.playerDialogueHead(64, 4194306)
+                }
+                player.interfaces.sendAnimation("${if (npc) "npc_" else ""}chat1", "head", id)
+                player.interfaces.sendText("${if (npc) "npc_" else ""}chat1", "title", title)
+                player.interfaces.sendLines("${if (npc) "npc_" else ""}chat1", listOf(content))
+                await<Unit>("chat")
+            }
+        } else {
+            npc("1902", "evil_laugh_old", content)
+        }
+    }
+}
+
 
 on<Command>({ prefix == "showcol" }) { player: Player ->
     val area = player.tile.toCuboid(10)
