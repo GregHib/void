f22db17aacc6ec6cba27a3ef1d396eb5e045e45c
GregHib <greghib@users.noreply.github.com>
2021-11-09 14:53:14 +0000

    Add double stairs dialogue

--- game/src/main/kotlin/world/gregs/voidps/world/interact/world/Stairs.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/world/Stairs.kts
@@ -1,22 +1,47 @@
 import world.gregs.voidps.engine.action.ActionType
 import world.gregs.voidps.engine.action.action
+import world.gregs.voidps.engine.client.ui.dialogue.dialogue
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.update.visual.player.move
 import world.gregs.voidps.engine.entity.character.update.visual.setAnimation
+import world.gregs.voidps.engine.entity.obj.GameObject
 import world.gregs.voidps.engine.entity.obj.ObjectClick
 import world.gregs.voidps.engine.entity.obj.ObjectOption
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.engine.utility.inject
+import world.gregs.voidps.world.interact.dialogue.type.choice
 import world.gregs.voidps.world.interact.world.Stairs
 
 val stairs: Stairs by inject()
 
-on<ObjectClick>({ it.action.type == ActionType.Climb }) { player: Player ->
+on<ObjectClick>({ it.action.type == ActionType.Climb }) { _: Player ->
     cancel = true
 }
 
 on<ObjectOption> { player: Player ->
-    val delta = stairs.get(obj.def.id, obj.tile, option ?: return@on) ?: return@on
+    climb(player, obj, option)
+}
+
+on<ObjectOption>({ option == "Climb" }) { player: Player ->
+    player.dialogue {
+        val choice = choice(
+            title = "What would you like to do?",
+            text = """
+                Go up the stairs.
+                Go down the stairs.
+                Never mind.
+            """
+        )
+        when (choice) {
+            1 -> climb(player, obj, "Climb-up")
+            2 -> climb(player, obj, "Climb-down")
+            else -> return@dialogue
+        }
+    }
+}
+
+fun climb(player: Player, obj: GameObject, option: String?) {
+    val delta = stairs.get(obj.def.id, obj.tile, option ?: return) ?: return
     val name = obj.def.name.toLowerCase()
     player.action(ActionType.Climb) {
         if (name.contains("ladder") || name.contains("trapdoor")) {
@@ -24,6 +49,5 @@ on<ObjectOption> { player: Player ->
             delay(2)
         }
         player.move(delta)
-        delay(1)
     }
 }
