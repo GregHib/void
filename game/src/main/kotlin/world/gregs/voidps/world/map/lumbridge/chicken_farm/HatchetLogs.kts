d589ac29fe350fddc2b2d66c01eef53f11d7aed0
GregHib <greghib@users.noreply.github.com>
2021-11-10 22:26:27 +0000

    Add bronze hatchet taking

---
+++ game/src/main/kotlin/world/gregs/voidps/world/map/lumbridge/chicken_farm/HatchetLogs.kts
@@ -0,0 +1,19 @@
+package world.gregs.voidps.world.map.lumbridge.chicken_farm
+
+import world.gregs.voidps.engine.entity.character.contain.inventory
+import world.gregs.voidps.engine.entity.character.contain.inventoryFull
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.obj.ObjectOption
+import world.gregs.voidps.engine.entity.obj.replace
+import world.gregs.voidps.engine.event.Priority
+import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.engine.utility.toTicks
+import java.util.concurrent.TimeUnit
+
+on<ObjectOption>({ obj.id == "hatchet_logs" && option == "Take-hatchet" }, Priority.HIGH) { player: Player ->
+    if (player.inventory.add("bronze_hatchet")) {
+        obj.replace("logs", ticks = TimeUnit.MINUTES.toTicks(5))
+    } else {
+        player.inventoryFull()
+    }
+}
