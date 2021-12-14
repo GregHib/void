8877fc4afe86186fa1626cfd24cdd59ba56edd87
GregHib <greghib@users.noreply.github.com>
2021-12-14 16:36:45 +0000

    Add object examines closes #160

---
+++ network/src/main/kotlin/world/gregs/voidps/network/decode/ObjectExamineDecoder.kt
@@ -0,0 +1,16 @@
+package world.gregs.voidps.network.decode
+
+import io.ktor.utils.io.core.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import world.gregs.voidps.network.Decoder
+import world.gregs.voidps.network.Instruction
+import world.gregs.voidps.network.instruct.ExamineObject
+
+class ObjectExamineDecoder : Decoder(2) {
+
+    override suspend fun decode(instructions: MutableSharedFlow<Instruction>, packet: ByteReadPacket) {
+        val objectId = packet.readUShort().toInt()
+        instructions.emit(ExamineObject(objectId))
+    }
+
+}
