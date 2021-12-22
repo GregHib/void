533562970c7a2e83f0b7283f014e6d8eb9b08745
GregHib <greghib@users.noreply.github.com>
2021-12-22 00:15:33 +0000

    Move firemaking data to config files #211

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/definition/ItemDefinitions.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/definition/ItemDefinitions.kt
@@ -3,6 +3,7 @@ package world.gregs.voidps.engine.entity.definition
 import world.gregs.voidps.cache.definition.data.ItemDefinition
 import world.gregs.voidps.cache.definition.decoder.ItemDecoder
 import world.gregs.voidps.engine.data.file.FileStorage
+import world.gregs.voidps.engine.entity.definition.data.FiremakingFire
 import world.gregs.voidps.engine.entity.definition.data.FishingCatch
 import world.gregs.voidps.engine.entity.item.EquipSlot
 import world.gregs.voidps.engine.entity.item.EquipType
@@ -36,6 +37,7 @@ class ItemDefinitions(
             map["equip"] = equipmentIndices.getOrDefault(map["id"] as Int, -1)
         }
         modifications["fishing"] = { FishingCatch(it as Map<String, Any>) }
+        modifications["firemaking"] = { FiremakingFire(it as Map<String, Any>) }
     }
 
     fun load(storage: FileStorage = get(), path: String = getProperty("itemDefinitionsPath")): ItemDefinitions {
