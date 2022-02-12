7e1a881136d51bb827b3f435881593d705959a79
GregHib <greghib@users.noreply.github.com>
2022-02-12 14:35:51 +0000

    Change parallel processing to use an Executor

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/update/task/ParallelIterator.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/update/task/ParallelIterator.kt
@@ -1,23 +1,33 @@
 package world.gregs.voidps.engine.client.update.task
 
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import world.gregs.voidps.engine.action.Contexts
+import com.github.michaelbull.logging.InlineLogger
+import it.unimi.dsi.fastutil.objects.ObjectArrayFIFOQueue
 import world.gregs.voidps.engine.entity.character.Character
+import java.util.concurrent.Executors
+import java.util.concurrent.Future
 
 class ParallelIterator<C : Character> : TaskIterator<C> {
+    private val queue = ObjectArrayFIFOQueue<Future<*>>()
+    private val logger = InlineLogger()
+
     override fun run(task: CharacterTask<C>) {
-        runBlocking {
-            coroutineScope {
-                task.characters.forEach {
-                    if (task.predicate(it)) {
-                        launch(Contexts.Updating) {
-                            task.run(it)
-                        }
+        for (character in task.characters) {
+            if (task.predicate(character)) {
+                queue.enqueue(executor.submit {
+                    try {
+                        task.run(character)
+                    } catch (t: Throwable) {
+                        logger.warn(t) { "Exception in parallel task." }
                     }
-                }
+                })
             }
         }
+        while (!queue.isEmpty) {
+            queue.dequeue().get()
+        }
+    }
+
+    companion object {
+        private val executor = Executors.newCachedThreadPool()
     }
 }
