c0211fd58249705846c58614b63cb6fe360704fd
GregHib <greghib@users.noreply.github.com>
2022-02-19 17:02:43 +0000

    Use array instead of map in MapDefinition

--- cache/src/main/kotlin/world/gregs/voidps/cache/definition/data/MapDefinition.kt
+++ cache/src/main/kotlin/world/gregs/voidps/cache/definition/data/MapDefinition.kt
@@ -4,19 +4,19 @@ import world.gregs.voidps.cache.Definition
 
 data class MapDefinition(
     override var id: Int = -1,
-    val tiles: MutableMap<Int, MapTile> = mutableMapOf(),
+    val tiles: LongArray = LongArray(64 * 64 * 4),
     val objects: MutableList<MapObject> = mutableListOf()
 ) : Definition {
 
-    fun getTile(localX: Int, localY: Int, plane: Int) = tiles[getHash(localX, localY, plane)] ?: MapTile.EMPTY
+    fun getTile(localX: Int, localY: Int, plane: Int) = MapTile(tiles[getHash(localX, localY, plane)])
 
     fun setTile(localX: Int, localY: Int, plane: Int, tile: MapTile) {
-        tiles[getHash(localX, localY, plane)] = tile
+        tiles[getHash(localX, localY, plane)] = tile.hash
     }
 
     companion object {
         fun getHash(localX: Int, localY: Int, plane: Int): Int {
-            return localY + (localX shl 6) + (plane shl 12)
+            return plane * 64 * 64 + localY * 64 + localX//localY + (localX shl 6) + (plane shl 12)
         }
         fun getLocalX(tile: Int) = tile shr 6 and 0x3f
         fun getLocalY(tile: Int) = tile and 0x3f
