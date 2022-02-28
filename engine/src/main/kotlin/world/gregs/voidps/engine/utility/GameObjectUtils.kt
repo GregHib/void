ce1c48fcf366142ea3463c5c7dc298235bb0cbbf
GregHib <greghib@users.noreply.github.com>
2022-02-28 11:56:14 +0000

    Fix taverley-falador gate

--- engine/src/main/kotlin/world/gregs/voidps/engine/utility/GameObjectUtils.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/utility/GameObjectUtils.kt
@@ -4,4 +4,4 @@ import world.gregs.voidps.cache.definition.data.ObjectDefinition
 
 fun ObjectDefinition.isDoor() = (name.contains("door", true) && !name.contains("trap", true)) || name.contains("gate", true)
 
-fun ObjectDefinition.isGate() = name.contains("gate", true) && id != 10565 && id != 10566
+fun ObjectDefinition.isGate() = name.contains("gate", true) && id != 10565 && id != 10566 && id != 28690 && id != 28691
