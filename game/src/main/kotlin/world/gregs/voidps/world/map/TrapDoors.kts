33d206fdc763bda8e1f8a39b56f7fcf490684745
jarryd229 <15334102+jarryd229@users.noreply.github.com>
2022-03-02 14:43:01 +1100

    Update game/src/main/kotlin/world/gregs/voidps/world/map/TrapDoors.kts
    don't really know how priority works
    
    Co-authored-by: Greg <GregHib@users.noreply.github.com>

--- game/src/main/kotlin/world/gregs/voidps/world/map/TrapDoors.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/map/TrapDoors.kts
@@ -9,7 +9,7 @@ import world.gregs.voidps.engine.entity.obj.replace
 import world.gregs.voidps.engine.utility.toTicks
 import java.util.concurrent.TimeUnit
 
-on<ObjectOption>({ obj.id.startsWith("trapdoor_") && obj.id.endsWith("_closed")}, Priority.HIGH) { player: Player ->
+on<ObjectOption>({ obj.id.startsWith("trapdoor_") && obj.id.endsWith("_closed")}) { player: Player ->
     player.setAnimation("open_chest")
     obj.replace(obj.id.replace("_closed", "_opened"), ticks = TimeUnit.MINUTES.toTicks(3))
 }
