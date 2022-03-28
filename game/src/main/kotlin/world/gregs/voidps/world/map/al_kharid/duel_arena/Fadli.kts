d43e5f56a0853e4e9848a24f21aad049ad27028e
jarryd229 <15334102+jarryd229@users.noreply.github.com>
2022-03-28 19:15:56 +1100

    added more npcs to duel arena
    added more npcs to duel arena

---
+++ game/src/main/kotlin/world/gregs/voidps/world/map/al_kharid/duel_arena/Fadli.kts
@@ -0,0 +1,91 @@
+package world.gregs.voidps.world.map.al_kharid.duel_arena
+
+import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.engine.entity.character.npc.NPCOption
+import world.gregs.voidps.world.interact.dialogue.type.npc
+import world.gregs.voidps.engine.client.ui.dialogue.talkWith
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.client.ui.open
+import world.gregs.voidps.world.interact.dialogue.type.choice
+import world.gregs.voidps.world.interact.dialogue.type.player
+import world.gregs.voidps.world.interact.entity.npc.shop.OpenShop
+import world.gregs.voidps.engine.entity.members
+import world.gregs.voidps.engine.client.message
+import world.gregs.voidps.engine.entity.World
+
+on<NPCOption>({ npc.id == "fadli" && option == "Talk-to" }) { player: Player ->
+    player.talkWith(npc) {
+	    player("cheerful", "Hi.")
+	    npc("roll_eyes", "What?")
+	    val choice = choice("""
+		    What do you do?
+		    What is this place?
+		    I'd like to access my bank, please.
+		    I'd like to collect items.
+		    Do you watch any matches?
+		""")
+        when (choice) {
+			1 -> {
+				player("talking", "What do you do?")
+				npc("roll_eyes", """
+				    You can store your stuff here if you want. You can
+				    dump anything you don't want to carry whilst you're
+			        fighting duels and then pick it up again on the way out.
+				""")
+                npc("roll_eyes", "To be honest I'm wasted here.")
+				npc("angry", """
+				    I should be winning duels in an arena! I'm the best
+				    warrior in Al Kharid!
+				""")
+				player("uncertain", "Easy, tiger!")	
+			}
+			2 -> {
+                player("uncertain", "What is this place?")
+				npc("angry", "Isn't it obvious?")
+                npc("talking", "This is the Duel Arena...duh!")	
+			}
+			3 -> {
+                player("talking", "I'd like to access my bank, please.")
+				npc("roll_eyes", "Sure.")
+				player.open("bank")
+			}
+			4 -> {
+                player("cheerful", "I'd like to collect items.")
+				npc("roll_eyes", "Yeah, okay.")
+				player.open("collection_box")
+			}
+			5 -> {
+                player("talking", "Do you watch any matches?")
+				npc("talking", "When I can.")
+				npc("cheerful", "Most aren't any good so I throw rotten fruit at them!")
+			    player("cheerful", "Heh. Can I buy some?")
+				if (World.members) {
+				    npc("laugh", "Sure.")
+				    player.events.emit(OpenShop("shop_of_distaste"))
+				return@talkWith
+				}
+				npc("roll_eyes", "Nope.")
+				player.message("You need to be on a members world to use this feature.")
+			}
+        }
+    }	
+}		
+
+on<NPCOption>({ npc.id == "fadli" && option == "Bank" }) { player: Player ->
+    player.open("bank")
+}
+
+on<NPCOption>({ npc.id == "fadli" && option == "Collect" }) { player: Player ->
+    player.open("collection_box")
+}
+
+on<NPCOption>({ npc.id == "fadli" && option == "Buy" }) { player: Player ->
+	if (World.members) {
+	    player.events.emit(OpenShop("shop_of_distaste"))
+	return@on	
+	}	
+	player.talkWith(npc) {
+		npc("roll_eyes", "Sorry, I'm not interested.")
+        player.message("You need to be on a members world to use this feature.")
+    }
+}
---
+++ game/src/main/kotlin/world/gregs/voidps/world/map/al_kharid/duel_arena/Lma.kts
@@ -0,0 +1,87 @@
+package world.gregs.voidps.world.map.al_kharid.duel_arena
+
+import world.gregs.voidps.engine.client.ui.dialogue.talkWith
+import world.gregs.voidps.engine.entity.character.npc.NPCOption
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.world.interact.dialogue.type.npc
+import world.gregs.voidps.world.interact.dialogue.type.player
+import kotlin.random.Random
+
+on<NPCOption>({ npc.id == "ima" && option == "Talk-to" }) { player: Player ->
+    player.talkWith(npc) {
+        player("cheerful", "Hi!")
+        when (Random.nextInt(0, 14)) {
+            0 -> {
+                npc("cheerful", "Knock knock!")
+                player("talking", "Who's there?")
+                npc("cheerful", "Boo.")
+                player("uncertain", "Boo who?")
+                npc("laugh", "Don't cry, it's just me!")
+            }
+            1 -> {
+                npc("sad", """
+                    I wouldn't want to be the poor guy that has to clean up
+                    after the duels.
+                """)
+                player("surprised", "Me neither.")
+            }
+            2 -> {
+                npc("sad", "Hmph.")
+            }
+            3 -> {
+                npc("cheerful", "My son just won his first duel!")
+                player("cheerful", "Congratulations!")
+                npc("cheerful", "He ripped his opponent in half!")
+                player("surprised", "That's gotta hurt!")
+                npc("cheerful", "He's only 10 as well!")
+                player("cheerful", "You gotta start 'em young!")
+            }
+            4-> {
+                npc("cheerful", "Hi! I'm here to watch the duels!")
+                player("cheerful", "Me too!")
+            }
+            5-> {
+                npc("cheerful", "Why did the skeleton burp?")
+                player("uncertain", "I don't know?")
+                npc("laugh", "'Cause it didn't have the guts to fart!")
+            }
+            6-> {
+                npc("talking", """
+                Did you know they think this place dates back to the
+                second age?!
+                """)
+                player("talking", "Really?")
+                npc("talking", "Yeah. The guy at the information kiosk was telling me.")
+            }
+            7-> {
+                npc("cheerful", "What did the skeleton say before it ate?")
+                player("uncertain", "I don't know?")
+                npc("laugh", "Bone-appetit.")
+            }
+            8-> {
+                npc("cheerful", "Ooh. This is exciting!")
+                player("cheerful", "Yup!")
+            }
+            9-> {
+                npc("cheerful", "Well. This beats doing the shopping!")
+            }
+            10-> {
+                npc("angry", "Can't you see I'm watching the duels?")
+                player("surprised", "I'm sorry!")
+            }
+            11-> {
+                npc("cheerful", "Hi!")
+            }
+            12-> {
+                npc("cheerful", "My favourite fighter is Mubariz!")
+                player("uncertain", "The guy at the information kiosk?")
+                npc("cheerful", "Yeah! He rocks!")
+                player("roll_eyes", "Takes all sorts, I guess.")
+            }
+            13-> {
+                npc("cheerful", "Waaaaassssssuuuuupp?!")
+            }
+        }
+    }
+}
