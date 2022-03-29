271136abf2f92bb45169ae7b5e21169f8a4fab18
jarryd229 <15334102+jarryd229@users.noreply.github.com>
2022-03-29 16:36:17 +1100

    Make item spawns non-members by default
    Make item spawns non-members by default

--- engine/src/main/kotlin/world/gregs/voidps/engine/map/spawn/ItemSpawn.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/map/spawn/ItemSpawn.kt
@@ -7,7 +7,7 @@ data class ItemSpawn(
     val tile: Tile,
     val amount: Int,
     val delay: Int,
-    val members: Boolean = true
+    val members: Boolean = false
 ) {
 
     companion object {
@@ -16,7 +16,7 @@ data class ItemSpawn(
             tile = Tile.fromMap(it),
             amount = it["amount"] as? Int ?: 1,
             delay = it["delay"] as? Int ?: 60,
-            members = it["members"] as? Boolean ?: true,
+            members = it["members"] as? Boolean ?: false,
         )
     }
 }
