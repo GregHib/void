fe26511b4a0999c62d869551a610d71d81ac9f38
GregHib <greghib@users.noreply.github.com>
2022-06-04 15:17:19 +0100

    Fix draining when skill is boosted

--- engine/src/test/kotlin/world/gregs/voidps/engine/entity/character/player/skill/LevelsTest.kt
+++ engine/src/test/kotlin/world/gregs/voidps/engine/entity/character/player/skill/LevelsTest.kt
@@ -221,6 +221,15 @@ internal class LevelsTest {
     }
 
     @Test
+    fun `Drain constitution by less than boosted amount`() {
+        exp.set(Skill.Constitution, 14000000.0)
+        levels.setOffset(Skill.Constitution, 60)
+        val amount = levels.drain(Skill.Constitution, amount = 40)
+        assertEquals(-40, amount)
+        assertEquals(1010, levels.get(Skill.Constitution))
+    }
+
+    @Test
     fun `Restore by fixed level`() {
         exp.set(Skill.Attack, 1154.0)
         levels.set(Skill.Attack, 1)
