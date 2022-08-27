22a4a24905c67e178faf74399eee1a73e66d0145
GregHib <greghib@users.noreply.github.com>
2022-08-27 14:42:15 +0100

    Upgrade ktor

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -3,8 +3,8 @@ dependencies {
     implementation(project(":cache"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${findProperty("kotlinCoroutinesVersion")}")
 
-    implementation("io.ktor:ktor-server-core:${findProperty("ktorVersion")}")
-    implementation("io.ktor:ktor-network:${findProperty("ktorVersion")}")
+    implementation("io.ktor:ktor-server-core-jvm:${findProperty("ktorVersion")}")
+    implementation("io.ktor:ktor-network-jvm:${findProperty("ktorVersion")}")
 
     implementation("com.michael-bull.kotlin-inline-logger:kotlin-inline-logger-jvm:${findProperty("inlineLoggingVersion")}")
 
