2ac6211dae90b6a670b8f48875b7eada162ee467
GregHib <greghib@users.noreply.github.com>
2022-10-09 16:34:27 +0100

    Fix floor item respawns

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/item/FloorItemRespawn.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/item/FloorItemRespawn.kts
@@ -1,9 +1,7 @@
+import world.gregs.voidps.engine.entity.*
 import world.gregs.voidps.engine.entity.Unregistered
-import world.gregs.voidps.engine.entity.contains
-import world.gregs.voidps.engine.entity.get
 import world.gregs.voidps.engine.entity.item.floor.FloorItem
 import world.gregs.voidps.engine.entity.item.floor.FloorItems
-import world.gregs.voidps.engine.entity.set
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.engine.map.spawn.ItemSpawn
 import world.gregs.voidps.engine.tick.delay
@@ -13,7 +11,7 @@ val items: FloorItems by inject()
 
 on<Unregistered>({ it.contains("respawn") }) { floorItem: FloorItem ->
     val spawn: ItemSpawn = floorItem["respawn"]
-    floorItem.delay(spawn.delay) {
+    World.delay(spawn.delay) {
         val item = items.add(spawn.id, spawn.amount, spawn.tile, revealTicks = 0)
         item["respawn"] = spawn
     }
