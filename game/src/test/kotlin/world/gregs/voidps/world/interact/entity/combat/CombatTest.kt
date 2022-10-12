424d8136a2b8c44f75ddecbdc55575e9e983dac1
GregHib <greghib@users.noreply.github.com>
2022-10-12 16:54:31 +0100

    Add player vs player integration tests

--- game/src/test/kotlin/world/gregs/voidps/world/interact/entity/combat/CombatTest.kt
+++ game/src/test/kotlin/world/gregs/voidps/world/interact/entity/combat/CombatTest.kt
@@ -1,5 +1,6 @@
 package world.gregs.voidps.world.interact.entity.combat
 
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import world.gregs.voidps.engine.entity.character.Levels
@@ -7,11 +8,14 @@ import world.gregs.voidps.engine.entity.character.contain.add
 import world.gregs.voidps.engine.entity.character.contain.equipment
 import world.gregs.voidps.engine.entity.character.contain.inventory
 import world.gregs.voidps.engine.entity.character.npc.NPC
+import world.gregs.voidps.engine.entity.character.player.appearance
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.get
+import world.gregs.voidps.engine.entity.start
 import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.engine.map.Tile
+import world.gregs.voidps.network.instruct.InteractPlayer
 import world.gregs.voidps.network.visual.update.player.EquipSlot
 import world.gregs.voidps.world.script.*
 
@@ -149,6 +153,64 @@ internal class CombatTest : WorldTest() {
         tick(5) // npc death
 
         assertEquals(Tile(3228, 3414), player.tile)
+    }
+
+    @Test
+    fun `Kill player with melee`() {
+        val player = createPlayer("player", emptyTile)
+        val target = createPlayer("target", emptyTile.addY(4))
+
+        player.equipment.set(EquipSlot.Weapon.index, "dragon_longsword")
+        player.experience.set(Skill.Attack, experience)
+        player.experience.set(Skill.Strength, experience)
+        player.experience.set(Skill.Defence, experience)
+        player.levels.boost(Skill.Attack, 25)
+        player.levels.boost(Skill.Strength, 25)
+        player.start("in_wilderness")
+        target.start("in_wilderness")
+        target.appearance.combatLevel = 90
+        target.inventory.add("dragon_longsword", 1)
+        target.inventory.add("magic_shortbow", 1)
+        target.inventory.add("rune_arrow", 10)
+        target.inventory.add("excalibur", 1)
+
+        player.interfaceOption("combat_styles", "style3")
+        player.playerOption(target, "Attack")
+        tickIf { target.levels.get(Skill.Constitution) > 0 }
+        tick(6) // player death
+
+        assertNotEquals(emptyTile, player.tile)
+        assertTrue(player.experience.get(Skill.Attack) > experience)
+        assertTrue(player.experience.get(Skill.Strength) > experience)
+        assertTrue(player.experience.get(Skill.Defence) > experience)
+        val items = floorItems[emptyTile.addY(4).chunk]
+        assertTrue(items.any { it.id == "coins" })
+        assertTrue(items.any { it.id == "rune_arrow" && it.amount > 1 })
+        assertTrue(items.any { it.id == "bones" })
+    }
+
+    @Test
+    fun `Can't attack players`() {
+        val player = createPlayer("player", emptyTile)
+        val target = createPlayer("target", emptyTile.addY(4))
+
+        player.equipment.set(EquipSlot.Weapon.index, "dragon_longsword")
+        player.experience.set(Skill.Attack, experience)
+        player.experience.set(Skill.Strength, experience)
+        player.experience.set(Skill.Defence, experience)
+        player.levels.boost(Skill.Attack, 25)
+        player.levels.boost(Skill.Strength, 25)
+
+        player.interfaceOption("combat_styles", "style3")
+        runTest {
+            player.instructions.emit(InteractPlayer(target.index, 1))
+        }
+        tick(2)
+
+        assertEquals(emptyTile, player.tile)
+        assertEquals(experience, player.experience.get(Skill.Attack))
+        assertEquals(experience, player.experience.get(Skill.Strength))
+        assertEquals(experience, player.experience.get(Skill.Defence))
     }
 
     companion object {
