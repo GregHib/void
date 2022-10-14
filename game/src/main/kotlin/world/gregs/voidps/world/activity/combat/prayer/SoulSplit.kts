2b259653e40b20dada49e2469e717288332cb71c
GregHib <greghib@users.noreply.github.com>
2022-10-14 15:58:40 +0100

    Soul split

--- game/src/main/kotlin/world/gregs/voidps/world/activity/combat/prayer/SoulSplit.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/combat/prayer/SoulSplit.kts
@@ -5,6 +5,7 @@ import world.gregs.voidps.engine.entity.character.setGraphic
 import world.gregs.voidps.engine.entity.hasEffect
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.engine.tick.delay
+import world.gregs.voidps.engine.utility.TICKS
 import world.gregs.voidps.world.activity.skill.summoning.isFamiliar
 import world.gregs.voidps.world.interact.entity.combat.CombatAttack
 import world.gregs.voidps.world.interact.entity.player.combat.magicHitDelay
@@ -15,7 +16,9 @@ fun usingSoulSplit(player: Player) = player.hasEffect("prayer_soul_split") && !p
 on<CombatAttack>({ source -> source is Player && usingSoulSplit(source) && damage >= 5 && type != "deflect" && type != "cannon" && !target.isFamiliar }) { player: Character ->
     val distance = player.tile.distanceTo(target)
     player.shoot("soul_split", target, height = 10, endHeight = 10)
-    target.delay(magicHitDelay(distance)) {
+    val ticks = magicHitDelay(distance)
+    target.setGraphic("soul_split_hit", TICKS.toClientTicks(ticks))
+    target.delay(ticks) {
         var heal = if (target is Player) 0.4 else 0.2
         if (target.hasEffect("dead")) {
             heal += 0.05
@@ -24,7 +27,6 @@ on<CombatAttack>({ source -> source is Player && usingSoulSplit(source) && damag
         if (damage >= 50) {
             target.levels.drain(Skill.Prayer, damage / 50)
         }
-        target.setGraphic("soul_split_hit")
         target.shoot("soul_split", player, height = 10, endHeight = 10)
     }
 }
