1764eed82dae8ca58339d22623e20637ad06fe1d
GregHib <greghib@users.noreply.github.com>
2022-12-20 13:32:57 +0000

    Tidy up ItemStackingRule

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/contain/stack/AlwaysStack.kt
+++
@@ -1,7 +0,0 @@
-package world.gregs.voidps.engine.entity.character.contain.stack
-
-object AlwaysStack : ItemStackingRule {
-    override fun stackable(id: String): Boolean {
-        return true
-    }
-}
--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/contain/stack/NeverStack.kt
+++
@@ -1,7 +0,0 @@
-package world.gregs.voidps.engine.entity.character.contain.stack
-
-object NeverStack : ItemStackingRule {
-    override fun stackable(id: String): Boolean {
-        return false
-    }
-}
