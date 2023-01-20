8de70b8d2ac6473cad0e4e9aed5639b4be45073c
GregHib <greghib@users.noreply.github.com>
2023-01-20 18:25:41 +0000

    Convert hairdresser

--- game/src/main/kotlin/world/gregs/voidps/world/map/falador/Hairdresser.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/map/falador/Hairdresser.kts
@@ -1,15 +1,11 @@
-import kotlinx.coroutines.NonCancellable
-import kotlinx.coroutines.withContext
 import world.gregs.voidps.engine.action.ActionType
-import world.gregs.voidps.engine.action.action
 import world.gregs.voidps.engine.client.ui.*
 import world.gregs.voidps.engine.client.ui.dialogue.dialogue
-import world.gregs.voidps.engine.client.ui.dialogue.talkWith
 import world.gregs.voidps.engine.client.ui.event.InterfaceClosed
 import world.gregs.voidps.engine.client.ui.event.InterfaceOpened
 import world.gregs.voidps.engine.client.variable.getVar
 import world.gregs.voidps.engine.client.variable.setVar
-import world.gregs.voidps.engine.entity.character.npc.NPC
+import world.gregs.voidps.engine.entity.character.mode.interact.interact
 import world.gregs.voidps.engine.entity.character.npc.NPCOption
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.flagAppearance
@@ -18,8 +14,9 @@ import world.gregs.voidps.engine.entity.character.player.sex
 import world.gregs.voidps.engine.entity.character.setGraphic
 import world.gregs.voidps.engine.entity.definition.EnumDefinitions
 import world.gregs.voidps.engine.entity.item.equipped
+import world.gregs.voidps.engine.entity.start
 import world.gregs.voidps.engine.event.on
-import world.gregs.voidps.engine.event.suspend.delayForever
+import world.gregs.voidps.engine.event.suspend.delay
 import world.gregs.voidps.engine.utility.inject
 import world.gregs.voidps.network.visual.update.player.BodyColour
 import world.gregs.voidps.network.visual.update.player.BodyPart
@@ -31,74 +28,61 @@ import world.gregs.voidps.world.interact.dialogue.type.player
 val enums: EnumDefinitions by inject()
 
 on<NPCOption>({ npc.id == "hairdresser" && option == "Talk-to" }) { player: Player ->
-    player.talkWith(npc) {
-        npc("happy", """
-            Good afternoon ${if (player.male) "sir" else "madam"}. In need of a haircut${if (player.male) " or shave" else ""} are
-            we?
-        """)
-        val choice = choice("""
-            Yes, please.
-            No, thank you.
-        """)
-        when (choice) {
-            1 -> {
-                player("talk", "Yes, please.")
-                npc("happy", """
-                    Please select the hairstyle you would like from this
-                    brochure. I'll even throw in a free recolour.
-                """)
-                startHairdressing(player, npc)
-            }
-            2 -> {
-                player("talk", "No, thank you.")
-                npc("talk", "Very well. Come back if you change your mind.")
-            }
+    npc("happy", """
+        Good afternoon ${if (player.male) "sir" else "madam"}. In need of a haircut${if (player.male) " or shave" else ""} are
+        we?
+    """)
+    val choice = choice("""
+        Yes, please.
+        No, thank you.
+    """)
+    when (choice) {
+        1 -> {
+            player("talk", "Yes, please.")
+            npc("happy", """
+                Please select the hairstyle you would like from this
+                brochure. I'll even throw in a free recolour.
+            """)
+            startHairdressing()
+        }
+        2 -> {
+            player("talk", "No, thank you.")
+            npc("talk", "Very well. Come back if you change your mind.")
         }
     }
-    delayForever()
 }
 
 on<NPCOption>({ npc.id == "hairdresser" && option == "Hair-cut" }) { player: Player ->
-    startHairdressing(player, npc)
-    delayForever()
+    startHairdressing()
 }
 
-fun startHairdressing(player: Player, npc: NPC) {
+suspend fun NPCOption.startHairdressing() {
     player.dialogues.clear()
     if (player.equipped(EquipSlot.Weapon).isNotEmpty() || player.equipped(EquipSlot.Shield).isNotEmpty()) {
-        player.talkWith(npc) {
-            npc("afraid", """
-                I don't feel comfortable cutting hair when you are
-                wielding something. Please remove what you are holding
-                first.
-            """)
-        }
+        npc("afraid", """
+            I don't feel comfortable cutting hair when you are
+            wielding something. Please remove what you are holding
+            first.
+        """)
         return
     }
     if (player.equipped(EquipSlot.Hat).isNotEmpty()) {
-        player.talkWith(npc) {
-            npc("upset", "I can't cut your hair with that on your head.")
-        }
+        npc("upset", "I can't cut your hair with that on your head.")
         return
     }
-    player.action(ActionType.Makeover) {
-        try {
-            delay(1)
-            player.setGraphic("dressing_room_start")
-            delay(1)
-            player.open("hairdressers_salon")
-            while (isActive) {
-                player.setGraphic("dressing_room")
-                delay(1)
-            }
-        } finally {
-            player.close("hairdressers_salon")
-            player.setGraphic("dressing_room_finish")
-            player.flagAppearance()
-            withContext(NonCancellable) {
-                delay(1)
-            }
-        }
+    player.interact.onStop = {
+        player.start("delay", 1)
+        player.close("hairdressers_salon")
+        player.setGraphic("dressing_room_finish")
+        player.flagAppearance()
+    }
+    delay(1)
+    player.setGraphic("dressing_room_start")
+    delay(1)
+    player.open("hairdressers_salon")
+    while (suspended) {
+        player.setGraphic("dressing_room")
+        delay(1)
     }
 }
 
