9cc241f6eec8379d4cb488b451abd1ad2909ca00
GregHib <greghib@users.noreply.github.com>
2023-01-31 22:01:42 +0000

    Change tab if collapsed last one

--- game/src/main/kotlin/world/gregs/voidps/world/activity/bank/BankTabCollapse.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/bank/BankTabCollapse.kts
@@ -2,6 +2,7 @@ package world.gregs.voidps.world.activity.bank
 
 import world.gregs.voidps.engine.client.ui.InterfaceOption
 import world.gregs.voidps.engine.client.variable.getVar
+import world.gregs.voidps.engine.client.variable.setVar
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.world.activity.bank.Bank.tabIndex
@@ -18,6 +19,12 @@ on<InterfaceOption>({ id == "bank" && component.startsWith("tab_") && option == 
     if (collapsed) {
         repeat(count) {
             Bank.decreaseTab(player, tab)
+        }
+
+        val current = player.getVar("open_bank_tab", 0)
+        val lastIndex = (Bank.tabs).first { player.getVar("bank_tab_${it}", 0) == 0 }
+        if (current > lastIndex) {
+            player.setVar("open_bank_tab", 1)
         }
     }
 }
