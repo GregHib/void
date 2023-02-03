c90175ccafce8d1d2f43527c89890de03325e3e2
GregHib <greghib@users.noreply.github.com>
2023-02-03 19:13:15 +0000

    Convert display mode to queue

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/ScreenChangeHandler.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/ScreenChangeHandler.kt
@@ -4,15 +4,18 @@ import world.gregs.voidps.engine.client.instruction.InstructionHandler
 import world.gregs.voidps.engine.client.ui.isOpen
 import world.gregs.voidps.engine.client.ui.setDisplayMode
 import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.queue.softQueue
 import world.gregs.voidps.network.instruct.ChangeDisplayMode
 
 class ScreenChangeHandler : InstructionHandler<ChangeDisplayMode>() {
 
     override fun validate(player: Player, instruction: ChangeDisplayMode) {
-        if (player.gameFrame.displayMode == instruction.displayMode || !player.isOpen("graphics_options")) {
-            return
+        player.softQueue {
+            if (player.gameFrame.displayMode == instruction.displayMode || !player.isOpen("graphics_options")) {
+                return@softQueue
+            }
+            player.setDisplayMode(instruction.displayMode)
         }
-        player.setDisplayMode(instruction.displayMode)
     }
 
 }
