783ae5a2404b3a1dfc16f5bc7f8da3859a0a5f6a
GregHib <greghib@users.noreply.github.com>
2023-02-22 00:14:53 +0000

    Fixes

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/PlayerOptionHandler.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/PlayerOptionHandler.kt
@@ -5,10 +5,7 @@ import world.gregs.voidps.engine.client.instruction.InstructionHandler
 import world.gregs.voidps.engine.entity.character.mode.Follow
 import world.gregs.voidps.engine.entity.character.mode.interact.Interact
 import world.gregs.voidps.engine.entity.character.mode.interact.clear
-import world.gregs.voidps.engine.entity.character.player.Player
-import world.gregs.voidps.engine.entity.character.player.PlayerOption
-import world.gregs.voidps.engine.entity.character.player.PlayerOptions
-import world.gregs.voidps.engine.entity.character.player.Players
+import world.gregs.voidps.engine.entity.character.player.*
 import world.gregs.voidps.network.instruct.InteractPlayer
 
 class PlayerOptionHandler(
@@ -23,6 +20,11 @@ class PlayerOptionHandler(
         val option = player.options.get(optionIndex)
         if (option == PlayerOptions.EMPTY_OPTION) {
             logger.info { "Invalid player option $optionIndex ${player.options.get(optionIndex)} for $player on $target" }
+            return
+        }
+        val click = PlayerClick(target, option)
+        player.events.emit(click)
+        if (click.cancelled) {
             return
         }
         player.clear()
