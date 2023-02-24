7d3b511c929ee7cebf168ee7364dc8a87eddac07
GregHib <greghib@users.noreply.github.com>
2023-02-24 21:08:19 +0000

    Delete clock delegates

--- engine/src/main/kotlin/world/gregs/voidps/engine/clock/MapDelegate.kt
+++
@@ -1,7 +0,0 @@
-package world.gregs.voidps.engine.clock
-
-interface MapDelegate {
-    operator fun get(name: String): Int?
-    operator fun set(name: String, value: Int)
-    fun remove(name: String)
-}
