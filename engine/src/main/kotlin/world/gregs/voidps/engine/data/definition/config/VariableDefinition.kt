fa2c28a8e1c1b8dd6a2cd1bcd10a67ee6a10d261
GregHib <greghib@users.noreply.github.com>
2023-02-28 16:42:39 +0000

    Oops remove comment

--- engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/config/VariableDefinition.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/config/VariableDefinition.kt
@@ -27,37 +27,6 @@ data class VariableDefinition(
     fun getValue(key: Any): Int? = format.getValue(this, key)
 
     companion object {
-        /*
-        operator fun invoke(map: Map<String, Any>): VariableDefinition {
-            val transmit = map["transmit"] as? Boolean ?: true
-            if (transmit) {
-                check(map.containsKey("id")) { "Transmitted variables must have an id. $map" }
-                check(map.containsKey("type")) { "Transmitted variables must have a type. $map" }
-                if (!map.containsKey("default") && map["type"] != "varcstr") {
-                    check(map.containsKey("format")) { "Transmitted variables must have a format. $map" }
-                }
-            } else {
-                if (!map.containsKey("default")) {
-                    check(map.containsKey("format")) { "Custom variables must have a format. $map" }
-                }
-                if (map.containsKey("persist")) {
-                    check(map["persist"] as Boolean) { "It's unnecessary to document non-persistent custom variables. $map" }
-                }
-            }
-            val id = map["id"] as? Int ?: -1
-            var default = map["default"]
-            val type = if (transmit) VariableType.byName(map["type"] as String) else VariableType.Custom
-            val format = when {
-                type == VariableType.Varcstr -> VariableFormat.STRING
-                transmit -> VariableFormat.byName(map["format"] as? String ?: default?.javaClass?.kotlin?.simpleName ?: "none")
-                else -> VariableFormat.NONE
-            }
-            val values = map["values"]
-            default = format.default(values)
-            val persist = if (transmit) map["persist"] as? Boolean ?: false else true
-            return VariableDefinition(id, type, format, default, persist, transmit, values)
-        }
-         */
         operator fun invoke(
             map: Map<String, Any>,
             type: VariableType
