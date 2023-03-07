9902406a1760a3c0668c21826423dc0903a45fb8
GregHib <greghib@users.noreply.github.com>
2023-03-07 23:09:27 +0000

    Revert interact change

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/ObjectOptionHandler.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/ObjectOptionHandler.kt
@@ -7,6 +7,7 @@ import world.gregs.voidps.engine.data.definition.DefinitionsDecoder
 import world.gregs.voidps.engine.data.definition.extra.ObjectDefinitions
 import world.gregs.voidps.engine.data.definition.extra.VariableDefinitions
 import world.gregs.voidps.engine.entity.character.mode.interact.Interact
+import world.gregs.voidps.engine.entity.character.mode.interact.clearInteract
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.obj.GameObject
 import world.gregs.voidps.engine.entity.obj.ObjectClick
@@ -62,6 +63,7 @@ class ObjectOptionHandler(
         if (click.cancelled) {
             return
         }
+        player.clearInteract()
         player.mode = Interact(player, target, ObjectOption(player, target, definition, selectedOption), approachRange = target.def["interact_distance", -1])
     }
 
