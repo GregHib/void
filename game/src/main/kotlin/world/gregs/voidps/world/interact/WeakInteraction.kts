06ad9dbe2ce1b0fc0c94ef2785f31f4181f8cdb6
GregHib <greghib@users.noreply.github.com>
2023-03-09 00:33:07 +0000

    Don't close bank when item switching

--- game/src/main/kotlin/world/gregs/voidps/world/interact/WeakInteraction.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/WeakInteraction.kts
@@ -1,14 +1,9 @@
-import world.gregs.voidps.engine.client.ui.InterfaceSwitch
 import world.gregs.voidps.engine.client.ui.closeInterfaces
 import world.gregs.voidps.engine.client.ui.event.CloseInterface
 import world.gregs.voidps.engine.client.ui.interact.InterfaceOnInterface
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.engine.event.on
-
-on<InterfaceSwitch>({ id != "bank" }, Priority.HIGH) { player: Player ->
-    player.closeInterfaces()
-}
 
 on<InterfaceOnInterface>(priority = Priority.HIGH) { player: Player ->
     player.closeInterfaces()
