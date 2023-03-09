74988bb1dd284df5dfd192ce90691536a49ecec5
GregHib <greghib@users.noreply.github.com>
2023-03-09 00:30:27 +0000

    Tweaks

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/WalkHandler.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/WalkHandler.kt
@@ -1,25 +1,22 @@
 package world.gregs.voidps.engine.client.instruction.handle
 
 import world.gregs.voidps.engine.client.instruction.InstructionHandler
-import world.gregs.voidps.engine.client.ui.closeDialogue
-import world.gregs.voidps.engine.client.ui.closeMenu
+import world.gregs.voidps.engine.client.ui.closeInterfaces
 import world.gregs.voidps.engine.entity.character.clearWatch
 import world.gregs.voidps.engine.entity.character.mode.EmptyMode
 import world.gregs.voidps.engine.entity.character.move.walkTo
 import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.suspend.clearSuspension
 import world.gregs.voidps.network.instruct.Walk
 
 class WalkHandler : InstructionHandler<Walk>() {
 
     override fun validate(player: Player, instruction: Walk) {
-        player.closeDialogue()
-        player.closeMenu()
-        player.queue.clearWeak()
+        player.closeInterfaces()
         player.mode = EmptyMode
-        player.suspension?.cancel()
-        player.clearWatch()
-        player.suspension = null
         player.interaction = null
+        player.clearSuspension()
+        player.clearWatch()
         player.walkTo(player.tile.copy(instruction.x, instruction.y))
     }
 
