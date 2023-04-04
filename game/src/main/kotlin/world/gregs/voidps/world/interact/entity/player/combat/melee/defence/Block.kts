62fa01f72b37934e9ee19da817ee2e165e2181ac
GregHib <greghib@users.noreply.github.com>
2023-04-04 22:30:46 +0100

    Fix block animations

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/defence/Block.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/defence/Block.kts
@@ -1,10 +1,20 @@
+import world.gregs.voidps.engine.entity.character.Character
+import world.gregs.voidps.engine.entity.character.npc.NPC
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.setAnimation
 import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.world.interact.entity.combat.CombatAttack
 
-on<CombatAttack>({ !blocked }, Priority.LOWER) { _: Player ->
-    target.setAnimation("block", delay)
+on<CombatAttack>({ !blocked && target is Player }, Priority.LOWER) { _: Character ->
+    target.setAnimation("block")
+    blocked = true
+}
+
+on<CombatAttack>({ !blocked && target is NPC }, Priority.LOWER) { _: Character ->
+    val npc = target as NPC
+    val race: String? = npc.def.getOrNull("race")
+    val animation = if (race != null) "${race}_hit" else npc.def["hit_anim"]
+    target.setAnimation(animation)
     blocked = true
 }
