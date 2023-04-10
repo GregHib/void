6811729536be1b887b1c6e80d5c7266f8b806c76
GregHib <greghib@users.noreply.github.com>
2023-04-10 15:38:30 +0100

    Fix walking not clearing dialogues

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/WalkHandler.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/WalkHandler.kt
@@ -16,6 +16,7 @@ class WalkHandler : InstructionHandler<Walk>() {
         player.mode = EmptyMode
         player.clearSuspension()
         player.clearWatch()
+        player.queue.clearWeak()
         player.walkTo(player.tile.copy(instruction.x, instruction.y))
     }
 
