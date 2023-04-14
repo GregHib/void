b82cbb1d03f3c2d94043d3a90f53d230ac541e25
GregHib <greghib@users.noreply.github.com>
2023-04-14 19:57:31 +0100

    Don't clear suspension on walk to prevent tele queue looping

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/WalkHandler.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/WalkHandler.kt
@@ -3,18 +3,14 @@ package world.gregs.voidps.engine.client.instruction.handle
 import world.gregs.voidps.engine.client.instruction.InstructionHandler
 import world.gregs.voidps.engine.client.ui.closeInterfaces
 import world.gregs.voidps.engine.entity.character.clearWatch
-import world.gregs.voidps.engine.entity.character.mode.EmptyMode
 import world.gregs.voidps.engine.entity.character.move.walkTo
 import world.gregs.voidps.engine.entity.character.player.Player
-import world.gregs.voidps.engine.suspend.clearSuspension
 import world.gregs.voidps.network.instruct.Walk
 
 class WalkHandler : InstructionHandler<Walk>() {
 
     override fun validate(player: Player, instruction: Walk) {
         player.closeInterfaces()
-        player.mode = EmptyMode
-        player.clearSuspension()
         player.clearWatch()
         player.walkTo(player.tile.copy(instruction.x, instruction.y))
     }
