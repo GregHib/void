2805b1e74f4d4eb8002fdb935a45055be8d05ab7
GregHib <greghib@users.noreply.github.com>
2023-04-22 20:01:09 +0100

    Add npc wander radius

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/mode/combat/CombatMovement.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/mode/combat/CombatMovement.kt
@@ -2,31 +2,33 @@ package world.gregs.voidps.engine.entity.character.mode.combat
 
 import world.gregs.voidps.engine.client.ui.dialogue
 import world.gregs.voidps.engine.client.variable.get
+import world.gregs.voidps.engine.client.variable.getOrNull
 import world.gregs.voidps.engine.client.variable.hasClock
 import world.gregs.voidps.engine.entity.character.Character
 import world.gregs.voidps.engine.entity.character.mode.EmptyMode
+import world.gregs.voidps.engine.entity.character.mode.Retreat
 import world.gregs.voidps.engine.entity.character.mode.move.Movement
 import world.gregs.voidps.engine.entity.character.mode.move.target.EntityTargetStrategy
 import world.gregs.voidps.engine.entity.character.mode.move.target.TargetStrategy
+import world.gregs.voidps.engine.entity.character.move.walkTo
 import world.gregs.voidps.engine.entity.character.npc.NPC
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.chat.cantReach
 import world.gregs.voidps.engine.entity.character.watch
+import world.gregs.voidps.engine.map.Tile
 
 /**
  * Keeps [character] within attack range of [target]
  */
 class CombatMovement(
     character: Character,
-    val target: Character,
+    var target: Character,
     strategy: TargetStrategy = EntityTargetStrategy(target),
 ) : Movement(character, strategy) {
 
     override fun start() {
         character.watch(target)
     }
-
-    private var smart = character is Player
 
     override fun tick() {
         if (character is Player && character.dialogue != null) {
@@ -42,12 +44,24 @@ class CombatMovement(
             character.events.emit(CombatReached(target))
             super.tick()
         } else {
-            if (!smart) {
-                character.steps.clearDestination()
-            }
+            character.steps.clearDestination()
             recalculate()
             super.tick()
-            if (character.hasClock("movement_delay") || character.visuals.moved || getTarget() != null) {
+            if (character is NPC) {
+                val wanderRadius = character.def["wander_radius", 8]
+                val spawn: Tile = character["respawn_tile"]
+                if (!character.tile.within(spawn, wanderRadius)) {
+                    character.walkTo(spawn)
+                    return
+                }
+                val attackRadius = character.def["attack_radius", 8]
+                val target = character.getOrNull<Character>("target")
+                if (target != null && !character.tile.within(target.tile, attackRadius)) {
+                    character.mode = Retreat(character, target)
+                    return
+                }
+            }
+            if (character.hasClock("movement_delay") || character.visuals.moved || getTarget() != null || character is NPC) {
                 return
             }
             character.cantReach()
@@ -55,13 +69,6 @@ class CombatMovement(
                 character.mode = EmptyMode
             }
         }
-    }
-
-    override fun recalculate(): Boolean {
-        if (character.steps.isEmpty()) {
-            smart = false
-        }
-        return super.recalculate()
     }
 
     private fun attackRange(): Int = character["attack_range", if (character is NPC) character.def["attack_range", 1] else 1]
