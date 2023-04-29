f9f7a1c4ccb623a8d07f2ba2937576b062a5571f
GregHib <greghib@users.noreply.github.com>
2023-04-29 15:59:25 +0100

    Add cutscene

--- game/src/main/kotlin/world/gregs/voidps/world/map/varrock/Aris.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/map/varrock/Aris.kts
@@ -1,21 +1,42 @@
 package world.gregs.voidps.world.map.varrock
 
+import world.gregs.voidps.engine.client.*
+import world.gregs.voidps.engine.client.ui.close
+import world.gregs.voidps.engine.client.ui.open
 import world.gregs.voidps.engine.client.variable.get
+import world.gregs.voidps.engine.client.variable.set
+import world.gregs.voidps.engine.client.variable.start
+import world.gregs.voidps.engine.client.variable.stop
 import world.gregs.voidps.engine.contain.inventory
 import world.gregs.voidps.engine.contain.remove
-import world.gregs.voidps.engine.entity.character.clearAnimation
-import world.gregs.voidps.engine.entity.character.mode.interact.Interaction
+import world.gregs.voidps.engine.entity.Direction
+import world.gregs.voidps.engine.entity.character.face
+import world.gregs.voidps.engine.entity.character.mode.interact.Interact
+import world.gregs.voidps.engine.entity.character.move.running
+import world.gregs.voidps.engine.entity.character.move.tele
+import world.gregs.voidps.engine.entity.character.move.walkTo
+import world.gregs.voidps.engine.entity.character.npc.NPC
 import world.gregs.voidps.engine.entity.character.npc.NPCOption
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.setAnimation
 import world.gregs.voidps.engine.entity.character.setGraphic
 import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.engine.inject
+import world.gregs.voidps.engine.map.chunk.DynamicChunks
+import world.gregs.voidps.engine.map.instance.InstancePool
+import world.gregs.voidps.engine.map.region.Region
+import world.gregs.voidps.engine.queue.strongQueue
 import world.gregs.voidps.world.interact.dialogue.*
 import world.gregs.voidps.world.interact.dialogue.type.choice
 import world.gregs.voidps.world.interact.dialogue.type.npc
 import world.gregs.voidps.world.interact.dialogue.type.player
+import world.gregs.voidps.world.interact.dialogue.type.statement
+import world.gregs.voidps.world.interact.entity.effect.clearTransform
+import world.gregs.voidps.world.interact.entity.effect.transform
 import world.gregs.voidps.world.interact.entity.player.music.playTrack
 import world.gregs.voidps.world.interact.entity.sound.areaSound
+import world.gregs.voidps.world.interact.entity.sound.playJingle
+import world.gregs.voidps.world.interact.entity.sound.playSound
 
 on<NPCOption>({ npc.id == "gypsy_aris" && option == "Talk-to" }) { player: Player ->
     when (player["demon_slayer", "unstarted"]) {
@@ -29,7 +50,7 @@ on<NPCOption>({ npc.id == "gypsy_aris" && option == "Talk-to" }) { player: Playe
                 player<Upset>("Oh dear. I don't have any money.")
                 return@on
             }
-            val choice = choice("""
+            var choice = choice("""
                 Okay, here you go.
                 Who are you called 'young one'?
                 No, I don't believe in that stuff.
@@ -47,12 +68,12 @@ on<NPCOption>({ npc.id == "gypsy_aris" && option == "Talk-to" }) { player: Playe
                         used silver coins since before you were born! So, do
                         you want your fortune told?
                     """)
-                    val choice = choice("""
+                    choice = choice("""
                         Ok, here you go.
                         No, I don't believe in that stuff.
                     """)
                     when (choice) {
-                        1 -> {}
+                        1 -> hereYouGo()
                         2 -> notBeliever()
                     }
                 }
@@ -62,15 +83,45 @@ on<NPCOption>({ npc.id == "gypsy_aris" && option == "Talk-to" }) { player: Playe
             // https://youtu.be/pN4EMsNEtPs?t=64
             npc<Talking>("Greetings. How goes thy quest?")
         }
+        "stage2" -> {
+            npc<Upset>("Delrith will come forth from the stone circle again.")
+            npc<Upset>("""
+                I would imagine an evil sorcerer is already beginning
+                the rituals to summon Delrith as we speak.
+            """)
+            val choice = choice("""
+                How am I meant to fight a demon who can destroy cities?
+                Okay, where is he? I'll kill him for you.
+                What is the magical incantation?
+                Where can I find Silverlight?
+            """)
+            when (choice) {
+                1 -> cityDestroyer()
+                2 -> whereIsHe()
+                3 -> incantation()
+                4 -> {
+                    player<Angry>("Where can I find Silverlight?")
+                    npc<Talk>("""
+                        Silverlight has been passed down by Wally's
+                        descendants. I believe it is currently in the care of one
+                        of the king's knights called Sir Prysin.
+                    """)
+                    npc<Happy>("""
+                        He shouldn't be too hard to find. He lives in the royal
+                        palace in this city. Tell him Gypsy Aris sent you.
+                    """)
+                    lastQuestions()
+                }
+            }
+        }
         "complete" -> {
-
             npc<Talking>("Greetings young one.")
             npc<Talking>("You're a hero now. That was a good bit of demon-slaying.")
         }
     }
 }
 
-suspend fun Interaction.whatToDo() {
+suspend fun NPCOption.whatToDo() {
     val choice = choice("""
         How am I meant to fight a demon who can destroy cities?
         Okay, where is he? I'll kill him for you.
@@ -83,7 +134,7 @@ suspend fun Interaction.whatToDo() {
     }
 }
 
-suspend fun Interaction.howToDo() {
+suspend fun NPCOption.howToDo() {
     val choice = choice("""
         How am I meant to fight a demon who can destroy cities?
         Okay, where is he? I'll kill him for you.
@@ -96,64 +147,14 @@ suspend fun Interaction.howToDo() {
     }
 }
 
-suspend fun Interaction.howWallyWon() {
+suspend fun NPCOption.howWallyWon() {
     player<Unsure>("So, how did Wally kill Delrith?")
     player.playTrack("wally_the_hero") // TODO
     player.setAnimation("open_map")
-    // TODO camera 6592, 6336, -679..-696, 128, 1192..1371, 512
-    // Cutscene
-    npc<Talk>("""
-        Wally managed to arrive at the stone circle just as
-        Delrith was summoned by a cult of chaos druids.
-    """)
-    // TODO camera
-    npc<Angry>("wally", "Die, foul demon!")
-    player.setAnimation("wally_demon_slay")
-    npc<Unsure>("wally", "Now, what was that incantation again?")
-    player.clearAnimation()
-    npc<Angry>("wally", "Gabindo... Carlem... Camerinthum... Aber... Purchai!") // TODO Random order
-    npc<Happy>("wally", "I am the greatest demon slayer EVER!")
-    player.setAnimation("wally_demon_win") // TODO
-    player.setGraphic("wally_sword_glint")
-    npc<Talk>("""
-        By reciting the correct magical incantation, and
-        thrusting Silverlight into Delrith while he was newly
-        summoned, Wally was able to imprison Delrith in the
-        stone table at the centre of the circle.
-    """)
-    // End cutscene
-    npc<Upset>("Delrith will come forth from the stone circle again.")
-    npc<Upset>("""
-        I would imagine an evil sorcerer is already beginning
-        the rituals to summon Delrith as we speak.
-    """)
-    val choice = choice("""
-        How am I meant to fight a demon who can destroy cities?
-        Okay, where is he? I'll kill him for you.
-        What is the magical incantation?
-        Where can I find Silverlight?
-    """)
-    when (choice) {
-        1 -> cityDestroyer()
-        2 -> whereIsHe()
-        3 -> incantation()
-        4 -> {
-            player<Angry>("Where can I find Silverlight?")
-            npc<Talk>("""
-                Silverlight has been passed down by Wally's
-                descendants. I believe it is currently in the care of one
-                of the king's knights called Sir Prysin.
-            """)
-            npc<Happy>("""
-                He shouldn't be too hard to find. He lives in the royal
-                palace in this city. Tell him Gypsy Aris sent you.
-            """)
-            lastQuestions()
-        }
-    }
+    cutscene(player, npc)
 }
 
-suspend fun Interaction.lastQuestions() {
+suspend fun NPCOption.lastQuestions() {
     val choice = choice("""
         How am I meant to fight a demon who can destroy cities?
         Okay, where is he? I'll kill him for you.
@@ -173,7 +174,7 @@ suspend fun Interaction.lastQuestions() {
     }
 }
 
-suspend fun Interaction.cityDestroyer() {
+suspend fun NPCOption.cityDestroyer() {
     player<Afraid>("""
         How am I meant to fight a demon who can destroy
         cities?
@@ -199,7 +200,7 @@ suspend fun Interaction.cityDestroyer() {
     }
 }
 
-suspend fun Interaction.whereIsHe() {
+suspend fun NPCOption.whereIsHe() {
     player<Cheerful>("Okay, where is he? I'll kill him for you.")
     npc<Chuckle>("Ah, the overconfidence of the young!")
     npc<Talk>("""
@@ -209,7 +210,7 @@ suspend fun Interaction.whereIsHe() {
     howToDo()
 }
 
-suspend fun Interaction.notVeryHeroicName() {
+suspend fun NPCOption.notVeryHeroicName() {
     player<Cheerful>("Wally doesn't sound like a very heroic name.")
     npc<Talk>("""
        Yes, I know. Maybe that is why history doesn't
@@ -223,7 +224,7 @@ suspend fun Interaction.notVeryHeroicName() {
     howToDo()
 }
 
-suspend fun Interaction.incantation() {
+suspend fun NPCOption.incantation() {
     player<Talk>("What is the magical incantation?")
     npc<Talk>("Oh yes, let me think a second.")
     npc<Talking>("""
@@ -285,24 +286,140 @@ suspend fun NPCOption.hereYouGo() {
 
 suspend fun NPCOption.whoYouCallingYoung() {
     player<Angry>("Who are you calling 'young one'?")
-
-    // Unknown
-    //
-
+    npc<Talk>("""
+        You have been on this world a relatively short time. At
+        least compared to me.
+    """)
+    npc<Talk>("So, do you want your fortune told or not?")
     val choice = choice("""
-                        Okay, here you go.
-                        No, I don't believe in that stuff.
-                        Ouh, how old are you then?
-                    """)
+        Okay, here you go.
+        No, I don't believe in that stuff.
+        Ooh, how old are you then?
+    """)
     when (choice) {
-        1 -> {}
+        1 -> hereYouGo()
         2 -> notBeliever()
         3 -> {
             npc<Talking>("""
-                                Count the number of legs on the stools in the Blue
-                                Moon inn, and multiply that number by seven.
-                            """)
+                Count the number of legs on the stools in the Blue
+                Moon inn, and multiply that number by seven.
+            """)
             player<Unsure>("Er, yeah, whatever.")
         }
+    }
+}
+
+
+val instances: InstancePool by inject()
+val chunks: DynamicChunks by inject()
+
+val tabs = listOf(
+    "combat_styles",
+    "task_system",
+    "stats",
+    "quest_journals",
+    "inventory",
+    "worn_equipment",
+    "prayer_list",
+    "modern_spellbook",
+    "emotes",
+    "notes"
+)
+
+fun cutscene(player: Player, npc: NPC) {
+    player.strongQueue("demon_slayer_wally_cutscene") {
+        player.start("movement_delay", -1)
+        player.open("fade_out")
+        player.minimap(Minimap.HideMap)
+        statement("", clickToContinue = false)
+        tabs.forEach {
+            player.close(it)
+        }
+        // TODO blank tab
+        pause(2)
+        val region = Region(12852)
+        val instance = instances.obtain()
+        val offset = instance.tile.minus(region.tile)
+        chunks.copy(region, instance)
+        pause(1)
+        player.tele(offset.add(3225, 3371), clearInterfaces = false)
+        pause(2)
+        player.transform("wally")
+        player.clearCamera()
+        player.moveCamera(offset.add(3227, 3369), 300)
+        player.turnCamera(offset.add(3229, 3367), 250)
+        player.shakeCamera(type = 1, intensity = 0, movement = 10, speed = 10, cycle = 0)
+        player.shakeCamera(type = 3, intensity = 0, movement = 90, speed = 1, cycle = 0)
+        player.playSound("rumbling") // TODO
+        pause(1)
+        player.close("fade_out")
+        npc<Talk>("gypsy_aris", """
+            Wally managed to arrive at the stone circle just as
+            Delrith was summoned by a cult of chaos druids...
+        """)
+        npc<Furious>("wally", "Die, foul demon!", clickToContinue = false)
+        player.face(Direction.NORTH)
+        player.clearCamera()
+        player.turnCamera(offset.add(3227, 3367), height = 200, constantSpeed = 2, variableSpeed = 10)
+        player.turnCamera(offset.add(3227, 3367), height = 100, constantSpeed = 1, variableSpeed = 10)
+        player.shakeCamera(type = 3, intensity = 0, movement = 0, speed = 0, cycle = 0)
+        player.playSound("rumbling")
+        player.tele(offset.add(3225, 3363), clearInterfaces = false)
+        pause(2)
+        player.start("no_clip", 2)
+        player.running = true
+        player.walkTo(offset.add(3227, 3367))
+        pause(2)
+        player.face(Direction.NORTH)
+        player.setAnimation("wally_demon_slay")
+        player.playSound("ds_wally_sword", delay = 10) // TODO
+        pause(4)
+        npc<Unsure>("wally", "Now, what was that incantation again?")
+
+        player.clearCamera()
+        player.moveCamera(offset.add(3227, 3369), height = 100, constantSpeed = 2, variableSpeed = 10)
+        player.shakeCamera(type = 1, intensity = 0, movement = 10, speed = 5, cycle = 0)
+        player.shakeCamera(type = 3, intensity = 0, movement = 2, speed = 50, cycle = 0)
+        player.playSound("rumbling")
+
+
+        npc<Angry>("wally", "Gabindo... Carlem... Camerinthum... Aber... Purchai!") // TODO Random order
+        player.open("fade_out")
+        pause(4)
+        player.close("fade_out")
+        player.clearCamera()
+        player.shakeCamera(type = 1, intensity = 0, movement = 0, speed = 0, cycle = 0)
+        player.shakeCamera(type = 3, intensity = 0, movement = 0, speed = 0, cycle = 0)
+        player.playSound("rumbling")
+        player.moveCamera(offset.add(3225, 3363), height = 500)
+        player.turnCamera(offset.add(3227, 3367), height = 200)
+        player.playSound("equip_silverlight")
+        player.playJingle("152")
+        player.face(Direction.SOUTH_WEST)
+//        player.setAnimation("wally_demon_win")
+        player.setGraphic("wally_sword_glint")
+        npc<Happy>("wally", "I am the greatest demon slayer EVER!")
+        npc<Talk>("""
+            By reciting the correct magical incantation, and
+            thrusting Silverlight into Delrith while he was newly
+            summoned, Wally was able to imprison Delrith in the
+            stone table at the centre of the circle.
+        """)
+        statement("", clickToContinue = false)
+        player.open("fade_out")
+        pause(2)
+        player.tele(3203, 3424)
+        player.face(Direction.WEST)
+        instances.free(instance)
+        tabs.forEach {
+            player.open(it)
+        }
+        player.close("fade_out")
+        player.stop("movement_delay")
+        player.clearMinimap()
+        player.clearCamera()
+        player.clearTransform()
+        player["demon_slayer"] = "stage2"
+        player.mode = Interact(player, npc, NPCOption(player, npc, npc.def, "Talk-to"))
     }
 }
