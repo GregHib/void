309565239395190c4e2294b64b92ab6b7e8abd40
GregHib <greghib@users.noreply.github.com>
2023-05-06 19:52:03 +0100

    Fix action tests

--- engine/src/test/kotlin/world/gregs/voidps/engine/queue/ActionTest.kt
+++ engine/src/test/kotlin/world/gregs/voidps/engine/queue/ActionTest.kt
@@ -3,22 +3,18 @@ package world.gregs.voidps.engine.queue
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import world.gregs.voidps.engine.entity.character.Character
-import world.gregs.voidps.engine.entity.character.npc.NPC
 
 internal class ActionTest {
 
     @Test
     fun `Process immediately`() {
-        val character = NPC(index = -1)
-        val action = action(character)
+        val action = action()
         assertTrue(action.process())
     }
 
     @Test
     fun `Process returns true after delay hits zero`() {
-        val character = NPC(index = -1)
-        val action = action(character, delay = 4)
+        val action = action(delay = 4)
         assertFalse(action.process())
         assertFalse(action.process())
         assertFalse(action.process())
@@ -29,17 +25,15 @@ internal class ActionTest {
 
     @Test
     fun `Cancel stops processing and marks for removal`() {
-        val character = NPC(index = -1)
-        val action = action(character, delay = 0)
+        val action = action(delay = 0)
         assertTrue(action.process())
         action.cancel()
         assertFalse(action.process())
         assertTrue(action.removed)
     }
 
-    private fun action(character: Character, priority: ActionPriority = ActionPriority.Normal, delay: Int = 0, behaviour: LogoutBehaviour = LogoutBehaviour.Discard, action: suspend Action.() -> Unit = {}): Action {
+    private fun action(priority: ActionPriority = ActionPriority.Normal, delay: Int = 0, behaviour: LogoutBehaviour = LogoutBehaviour.Discard, action: suspend Action.() -> Unit = {}): Action {
         return object : Action("action", priority, delay, behaviour, action) {
-            override val character: Character = character
             override var onCancel: (() -> Unit)? = null
         }
     }
