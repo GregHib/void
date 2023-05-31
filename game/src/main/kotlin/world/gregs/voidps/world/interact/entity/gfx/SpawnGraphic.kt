7c5842d8c991cf1f095f0545436f4fe2352df9a1
GregHib <greghib@users.noreply.github.com>
2023-05-31 20:00:16 +0100

    Remove SpawnGraphic.kt

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/gfx/SpawnGraphic.kt
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/gfx/SpawnGraphic.kt
@@ -1,19 +1,12 @@
 package world.gregs.voidps.world.interact.entity.gfx
 
+import world.gregs.voidps.engine.client.update.batch.ChunkBatchUpdates
+import world.gregs.voidps.engine.data.definition.extra.GraphicDefinitions
 import world.gregs.voidps.engine.entity.Direction
-import world.gregs.voidps.engine.entity.World
-import world.gregs.voidps.engine.event.Event
+import world.gregs.voidps.engine.entity.item.floor.offset
+import world.gregs.voidps.engine.inject
 import world.gregs.voidps.engine.map.Tile
-
-data class SpawnGraphic(
-    val id: String,
-    val tile: Tile,
-    val delay: Int = 0,
-    val height: Int = 0,
-    val rotation: Int = 0,
-    val forceRefresh: Boolean = false,
-    val owner: String? = null
-) : Event
+import world.gregs.voidps.network.encode.chunk.GraphicAddition
 
 fun areaGraphic(
     id: String,
@@ -21,16 +14,9 @@ fun areaGraphic(
     delay: Int = 0,
     height: Int = 0,
     rotation: Direction = Direction.SOUTH,
-    forceRefresh: Boolean = false,
     owner: String? = null
-) = World.events.emit(
-    SpawnGraphic(
-        id = id,
-        tile = tile,
-        delay = delay,
-        height = height,
-        rotation = rotation.ordinal,
-        forceRefresh = forceRefresh,
-        owner = owner
-    )
-)
+) {
+    val batches: ChunkBatchUpdates by inject()
+    val definitions: GraphicDefinitions by inject()
+    batches.add(tile.chunk, GraphicAddition(definitions.get(id).id, tile.offset(), height, delay, rotation.ordinal, owner))
+}
