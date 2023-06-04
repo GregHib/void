570357450789505d7f3416c63ad6baa738031f24
GregHib <greghib@users.noreply.github.com>
2023-06-04 22:38:48 +0100

    Improve performance of GameObjectCollision and CollisionReader

--- engine/src/main/kotlin/world/gregs/voidps/engine/map/collision/CollisionFlags.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/map/collision/CollisionFlags.kt
@@ -35,37 +35,37 @@ enum class CollisionFlags {
     val bit: Int = 1 shl ordinal
 
     companion object {
-        val wallFlags = arrayOf(
-            WALL_NORTH_WEST,
-            WALL_NORTH,
-            WALL_NORTH_EAST,
-            WALL_EAST,
-            WALL_SOUTH_EAST,
-            WALL_SOUTH,
-            WALL_SOUTH_WEST,
-            WALL_WEST
+        val wallFlags = intArrayOf(
+            WALL_NORTH_WEST.bit,
+            WALL_NORTH.bit,
+            WALL_NORTH_EAST.bit,
+            WALL_EAST.bit,
+            WALL_SOUTH_EAST.bit,
+            WALL_SOUTH.bit,
+            WALL_SOUTH_WEST.bit,
+            WALL_WEST.bit
         )
 
-        val projectileFlags = arrayOf(
-            PROJECTILE_NORTH_WEST,
-            PROJECTILE_NORTH,
-            PROJECTILE_NORTH_EAST,
-            PROJECTILE_EAST,
-            PROJECTILE_SOUTH_EAST,
-            PROJECTILE_SOUTH,
-            PROJECTILE_SOUTH_WEST,
-            PROJECTILE_WEST
+        val projectileFlags = intArrayOf(
+            PROJECTILE_NORTH_WEST.bit,
+            PROJECTILE_NORTH.bit,
+            PROJECTILE_NORTH_EAST.bit,
+            PROJECTILE_EAST.bit,
+            PROJECTILE_SOUTH_EAST.bit,
+            PROJECTILE_SOUTH.bit,
+            PROJECTILE_SOUTH_WEST.bit,
+            PROJECTILE_WEST.bit
         )
 
-        val routeFlags = arrayOf(
-            ROUTE_NORTH_WEST,
-            ROUTE_NORTH,
-            ROUTE_NORTH_EAST,
-            ROUTE_EAST,
-            ROUTE_SOUTH_EAST,
-            ROUTE_SOUTH,
-            ROUTE_SOUTH_WEST,
-            ROUTE_WEST
+        val routeFlags = intArrayOf(
+            ROUTE_NORTH_WEST.bit,
+            ROUTE_NORTH.bit,
+            ROUTE_NORTH_EAST.bit,
+            ROUTE_EAST.bit,
+            ROUTE_SOUTH_EAST.bit,
+            ROUTE_SOUTH.bit,
+            ROUTE_SOUTH_WEST.bit,
+            ROUTE_WEST.bit
         )
     }
 }
--- engine/src/main/kotlin/world/gregs/voidps/engine/map/collision/GameObjectCollision.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/map/collision/GameObjectCollision.kt
@@ -1,7 +1,8 @@
 package world.gregs.voidps.engine.map.collision
 
 import world.gregs.voidps.cache.definition.data.ObjectDefinition
-import world.gregs.voidps.engine.entity.Direction
+import world.gregs.voidps.engine.entity.Direction.*
+import world.gregs.voidps.engine.entity.obj.GameMapObject
 import world.gregs.voidps.engine.entity.obj.GameObject
 import world.gregs.voidps.engine.entity.obj.ObjectType
 import world.gregs.voidps.engine.map.Tile
@@ -11,64 +12,64 @@ class GameObjectCollision(
 ) {
 
     fun modifyCollision(gameObject: GameObject, add: Boolean) {
-        modifyCollision(gameObject.def, gameObject.tile, gameObject.type, gameObject.rotation, add)
+        modify(gameObject.def, gameObject.tile.x, gameObject.tile.y, gameObject.tile.plane, gameObject.type, gameObject.rotation, add)
     }
 
-    fun modifyCollision(def: ObjectDefinition, tile: Tile, type: Int, rotation: Int, add: Boolean) {
+    fun modify(tile: Tile, obj: GameMapObject, add: Boolean) {
+        modify(obj.def, tile.x, tile.y, tile.plane, obj.type, obj.rotation, add)
+    }
+
+    fun modify(def: ObjectDefinition, x: Int, y: Int, plane: Int, type: Int, rotation: Int, add: Boolean) {
         if (def.solid == 0) {
             return
         }
         val blockSky = def.blocksSky
         val blockRoute = def.ignoreOnRoute
-
         when (type) {
-            ObjectType.LENGTHWISE_WALL -> modifyWall(tile, blockSky, blockRoute, Direction.cardinal[(rotation + 3) and 0x3], add)
-            ObjectType.TRIANGULAR_CORNER, ObjectType.RECTANGULAR_CORNER -> modifyWall(tile, blockSky, blockRoute, Direction.ordinal[rotation], add)
-            ObjectType.WALL_CORNER -> modifyWallCorner(tile, blockSky, blockRoute, Direction.ordinal[rotation], add)
-            in ObjectType.DIAGONAL_WALL until ObjectType.FLOOR_DECORATION -> modifyObject(def, tile, rotation, blockSky, blockRoute, add)
-            ObjectType.FLOOR_DECORATION -> if (def.interactive == 1 && def.solid == 1) modifyCardinal(tile.x, tile.y, tile.plane, blockSky, blockRoute, add)
+            ObjectType.LENGTHWISE_WALL -> modifyWall(x, y, plane, blockSky, blockRoute, cardinal[(rotation + 3) and 0x3], add)
+            ObjectType.TRIANGULAR_CORNER, ObjectType.RECTANGULAR_CORNER -> modifyWall(x, y, plane, blockSky, blockRoute, ordinal[rotation], add)
+            ObjectType.WALL_CORNER -> modifyWallCorner(x, y, plane, blockSky, blockRoute, ordinal[rotation], add)
+            in ObjectType.DIAGONAL_WALL until ObjectType.FLOOR_DECORATION -> modifyObject(def, x, y, plane, rotation, blockSky, blockRoute, add)
+            ObjectType.FLOOR_DECORATION -> if (def.interactive == 1 && def.solid == 1) modifyCardinal(x, y, plane, blockSky, blockRoute, add)
         }
     }
 
-    private fun modifyWall(tile: Tile, blockSky: Boolean, blockRoute: Boolean, direction: Direction, add: Boolean) {
-        modifyTile(tile.x, tile.y, tile.plane, blockSky, blockRoute, direction, add)
-        modifyTile(tile.x + direction.delta.x, tile.y + direction.delta.y, tile.plane, blockSky, blockRoute, direction.inverse(), add)
+    private fun modifyWall(x: Int, y: Int, plane: Int, blockSky: Boolean, blockRoute: Boolean, direction: Int, add: Boolean) {
+        modifyTile(x, y, plane, blockSky, blockRoute, direction, add)
+        modifyTile(x + deltaX[direction], y + deltaY[direction], plane, blockSky, blockRoute, (direction + 4) and 0x3, add)
     }
 
-    private fun modifyWallCorner(tile: Tile, blockSky: Boolean, blockRoute: Boolean, direction: Direction, add: Boolean) {
-        modifyWall(tile, blockSky, blockRoute, direction.vertical(), add)
-        modifyWall(tile, blockSky, blockRoute, direction.horizontal(), add)
+    private fun modifyWallCorner(x: Int, y: Int, plane: Int, blockSky: Boolean, blockRoute: Boolean, direction: Int, add: Boolean) {
+        modifyWall(x, y, plane, blockSky, blockRoute, vertical[direction], add)
+        modifyWall(x, y, plane, blockSky, blockRoute, horizontal[direction], add)
     }
 
-    private fun modifyObject(def: ObjectDefinition, tile: Tile, rotation: Int, blockSky: Boolean, blockRoute: Boolean, add: Boolean) {
+    private fun modifyObject(def: ObjectDefinition, x: Int, y: Int, plane: Int, rotation: Int, blockSky: Boolean, blockRoute: Boolean, add: Boolean) {
         val width = if (rotation and 0x1 == 1) def.sizeY else def.sizeX
         val height = if (rotation and 0x1 == 1) def.sizeX else def.sizeY
         for (dx in 0 until width) {
             for (dy in 0 until height) {
-                modifyCardinal(tile.x + dx, tile.y + dy, tile.plane, blockSky, blockRoute, add)
+                modifyCardinal(x + dx, y + dy, plane, blockSky, blockRoute, add)
             }
         }
     }
 
     private fun modifyCardinal(x: Int, y: Int, plane: Int, blockSky: Boolean, blockRoute: Boolean, add: Boolean) {
-        modifyTile(x, y, plane, blockSky, blockRoute, Direction.NORTH, add)
-        modifyTile(x, y, plane, blockSky, blockRoute, Direction.EAST, add)
-        modifyTile(x, y, plane, blockSky, blockRoute, Direction.SOUTH, add)
-        modifyTile(x, y, plane, blockSky, blockRoute, Direction.WEST, add)
+        modifyTile(x, y, plane, blockSky, blockRoute, 1, add)
+        modifyTile(x, y, plane, blockSky, blockRoute, 3, add)
+        modifyTile(x, y, plane, blockSky, blockRoute, 5, add)
+        modifyTile(x, y, plane, blockSky, blockRoute, 7, add)
     }
 
-    private fun modifyTile(x: Int, y: Int, plane: Int, blockSky: Boolean, blockRoute: Boolean, direction: Direction, add: Boolean) {
-        if (direction == Direction.NONE) {
-            return
-        }
-        val orientation = direction.ordinal
+    private fun modifyTile(x: Int, y: Int, plane: Int, blockSky: Boolean, blockRoute: Boolean, direction: Int, add: Boolean) {
+        var mask = CollisionFlags.wallFlags[direction]
         if (!blockRoute) {
-            modifyMask(x, y, plane, CollisionFlags.routeFlags[orientation].bit, add)
+            mask = mask or CollisionFlags.routeFlags[direction]
         }
         if (blockSky) {
-            modifyMask(x, y, plane, CollisionFlags.projectileFlags[orientation].bit, add)
+            mask = mask or CollisionFlags.projectileFlags[direction]
         }
-        modifyMask(x, y, plane, CollisionFlags.wallFlags[orientation].bit, add)
+        modifyMask(x, y, plane, mask, add)
     }
 
     private fun modifyMask(x: Int, y: Int, plane: Int, mask: Int, add: Boolean) {
@@ -77,5 +78,17 @@ class GameObjectCollision(
         } else {
             collisions.remove(x, y, plane, mask)
         }
+    }
+
+    companion object {
+        // For performance reasons
+        private val deltaX = intArrayOf(-1, 0, 1, 1, 1, 0, -1, -1)
+        private val deltaY = intArrayOf(1, 1, 1, 0, -1, -1, -1, 0)
+
+        private val cardinal = intArrayOf(NORTH.ordinal, SOUTH.ordinal, EAST.ordinal, WEST.ordinal)
+        private val ordinal = intArrayOf(NORTH_WEST.ordinal, NORTH_EAST.ordinal, SOUTH_EAST.ordinal, SOUTH_WEST.ordinal)
+
+        private val vertical = intArrayOf(NORTH.ordinal, NORTH.ordinal, NORTH.ordinal, -1, SOUTH.ordinal, SOUTH.ordinal, SOUTH.ordinal, -1)
+        private val horizontal = intArrayOf(WEST.ordinal, -1, EAST.ordinal, EAST.ordinal, EAST.ordinal, -1, WEST.ordinal, WEST.ordinal)
     }
 }
