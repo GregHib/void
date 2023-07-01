dfc195a360b0826fd76913cdd35c13749e12550f
GregHib <greghib@users.noreply.github.com>
2023-07-01 16:39:59 +0100

    Logger

--- cache/build.gradle.kts
+++ cache/build.gradle.kts
@@ -5,6 +5,7 @@ dependencies {
     implementation("io.insert-koin:koin-core:${findProperty("koinVersion")}")
 
     implementation("it.unimi.dsi:fastutil:${findProperty("fastUtilVersion")}")
+    implementation("ch.qos.logback:logback-classic:${findProperty("logbackVersion")}")
     implementation("com.michael-bull.kotlin-inline-logger:kotlin-inline-logger-jvm:${findProperty("inlineLoggingVersion")}")
 
     testImplementation("io.mockk:mockk:${findProperty("mockkVersion")}")
