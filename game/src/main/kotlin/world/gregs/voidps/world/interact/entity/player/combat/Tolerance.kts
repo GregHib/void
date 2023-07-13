86305fccdddcf0b8702009df3382de27ebdf3464
GregHib <greghib@users.noreply.github.com>
2023-07-13 01:04:37 +0100

    Persist tolerance between logins

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/Tolerance.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/Tolerance.kts
@@ -5,16 +5,18 @@ import world.gregs.voidps.engine.entity.Registered
 import world.gregs.voidps.engine.entity.character.mode.move.Moved
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.event.on
-import world.gregs.voidps.engine.timer.toTicks
+import world.gregs.voidps.engine.timer.epochSeconds
 import world.gregs.voidps.type.area.Cuboid
 import java.util.concurrent.TimeUnit
 
 on<Registered> { player: Player ->
-    player.start("tolerance", TimeUnit.MINUTES.toTicks(10))
+    if (!player.contains("tolerance")) {
+        player.start("tolerance", TimeUnit.MINUTES.toSeconds(10).toInt(), epochSeconds())
+    }
     player["tolerance_area"] = player.tile.toCuboid(10)
 }
 
 on<Moved>({ to !in it.get<Cuboid>("tolerance_area") }) { player: Player ->
     player["tolerance_area"] = player.tile.toCuboid(10)
-    player.start("tolerance", TimeUnit.MINUTES.toTicks(10))
+    player.start("tolerance", TimeUnit.MINUTES.toSeconds(10).toInt(), epochSeconds())
 }
