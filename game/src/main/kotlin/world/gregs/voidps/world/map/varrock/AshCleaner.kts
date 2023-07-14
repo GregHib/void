eac2c5000083a44de700a50378c1627e75d40d43
GregHib <greghib@users.noreply.github.com>
2023-07-14 20:31:46 +0100

    Add delay to cleaner sweeping

--- game/src/main/kotlin/world/gregs/voidps/world/map/varrock/AshCleaner.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/map/varrock/AshCleaner.kts
@@ -1,11 +1,21 @@
 package world.gregs.voidps.world.map.varrock
 
+import world.gregs.voidps.engine.entity.character.clearAnimation
 import world.gregs.voidps.engine.entity.character.mode.interact.Interact
 import world.gregs.voidps.engine.entity.character.npc.NPC
 import world.gregs.voidps.engine.entity.character.npc.hunt.HuntFloorItem
+import world.gregs.voidps.engine.entity.character.setAnimation
 import world.gregs.voidps.engine.entity.item.floor.FloorItemOption
+import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.engine.suspend.delay
 
 on<HuntFloorItem>({ it.id == "ash_cleaner" && mode == "ash_finder" }) { npc: NPC ->
     npc.mode = Interact(npc, target, FloorItemOption(npc, target, "Take"))
+}
+
+on<FloorItemOption>({ operate && option == "Take" && it.id == "ash_cleaner" }, Priority.LOW) { npc: NPC ->
+    npc.setAnimation("cleaner_sweeping")
+    delay(2)
+    npc.clearAnimation()
 }
