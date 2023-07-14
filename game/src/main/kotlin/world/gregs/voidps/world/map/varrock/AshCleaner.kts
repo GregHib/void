3a23d0fa8abdafaeded9b74a1bb7896022a81b56
GregHib <greghib@users.noreply.github.com>
2023-07-14 19:42:18 +0100

    Ash cleaner use interaction to pickup ashes

--- game/src/main/kotlin/world/gregs/voidps/world/map/varrock/AshCleaner.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/map/varrock/AshCleaner.kts
@@ -1,19 +1,11 @@
 package world.gregs.voidps.world.map.varrock
 
-import world.gregs.voidps.engine.entity.character.mode.move.Moved
-import world.gregs.voidps.engine.entity.character.move.walkTo
+import world.gregs.voidps.engine.entity.character.mode.interact.Interact
 import world.gregs.voidps.engine.entity.character.npc.NPC
 import world.gregs.voidps.engine.entity.character.npc.hunt.HuntFloorItem
-import world.gregs.voidps.engine.entity.item.floor.FloorItems
+import world.gregs.voidps.engine.entity.item.floor.FloorItemOption
 import world.gregs.voidps.engine.event.on
-import world.gregs.voidps.engine.inject
-
-val floorItems: FloorItems by inject()
 
 on<HuntFloorItem>({ it.id == "ash_cleaner" && mode == "ash_finder" }) { npc: NPC ->
-    npc.walkTo(target.tile)
-}
-on<Moved>({ it.id == "ash_cleaner" }) { _: NPC ->
-    val ashes = floorItems[to].firstOrNull { it.id == "ashes" } ?: return@on
-    floorItems.remove(ashes)
+    npc.mode = Interact(npc, target, FloorItemOption(npc, target, "Take"))
 }
