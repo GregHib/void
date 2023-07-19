a93b07fbc16ea73549ae8880783ba2dfd91e6470
GregHib <greghib@users.noreply.github.com>
2023-07-19 17:02:39 +0100

    Rename ItemOnItemEvent so it doesn't conflict

--- game/src/main/kotlin/world/gregs/voidps/world/activity/skill/ItemUsedOnItem.kt
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/skill/ItemUsedOnItem.kt
@@ -3,4 +3,4 @@ package world.gregs.voidps.world.activity.skill
 import world.gregs.voidps.engine.data.config.ItemOnItemDefinition
 import world.gregs.voidps.engine.event.Event
 
-data class ItemOnItem(val def: ItemOnItemDefinition) : Event
+data class ItemUsedOnItem(val def: ItemOnItemDefinition) : Event
--- game/src/main/kotlin/world/gregs/voidps/world/activity/skill/crafting/Thread.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/skill/crafting/Thread.kts
@@ -1,14 +1,14 @@
 package world.gregs.voidps.world.activity.skill.crafting
 
 import world.gregs.voidps.engine.client.message
-import world.gregs.voidps.engine.inv.inventory
-import world.gregs.voidps.engine.inv.remove
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.event.on
-import world.gregs.voidps.world.activity.skill.ItemOnItem
+import world.gregs.voidps.engine.inv.inventory
+import world.gregs.voidps.engine.inv.remove
+import world.gregs.voidps.world.activity.skill.ItemUsedOnItem
 
-on<ItemOnItem>({ def.skill == Skill.Crafting && def.requires.any { it.id == "thread" } }) { player: Player ->
+on<ItemUsedOnItem>({ def.skill == Skill.Crafting && def.requires.any { it.id == "thread" } }) { player: Player ->
     val value = player.inc("thread_used")
     if (value == 5) {
         player.clear("thread_used")
