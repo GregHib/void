4056e7bd8cccd2182fd8c2b39d4904af22f1015c
GregHib <greghib@users.noreply.github.com>
2023-07-20 21:50:27 +0100

    Add sending interface colours based on levels and bars

--- game/src/main/kotlin/world/gregs/voidps/world/activity/skill/smithing/Anvil.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/skill/smithing/Anvil.kts
@@ -1,12 +1,20 @@
 package world.gregs.voidps.world.activity.skill.smithing
 
+import net.pearx.kasechange.toSentenceCase
+import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.client.ui.interact.ItemOnObject
 import world.gregs.voidps.engine.client.ui.open
 import world.gregs.voidps.engine.data.definition.InterfaceDefinitions
 import world.gregs.voidps.engine.data.definition.ItemDefinitions
+import world.gregs.voidps.engine.data.definition.data.Smithing
 import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.engine.entity.character.player.skill.level.Level.has
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.engine.inject
+import world.gregs.voidps.engine.inv.inventory
+import world.gregs.voidps.world.activity.quest.quest
+import world.gregs.voidps.world.interact.dialogue.type.statement
 
 val types = listOf(
     "dagger",
@@ -45,15 +53,35 @@ val types = listOf(
 val itemDefinitions: ItemDefinitions by inject()
 val interfaceDefinitions: InterfaceDefinitions by inject()
 
-on<ItemOnObject>({ operate && target.id.startsWith("anvil") }) { player: Player ->
+on<ItemOnObject>({ operate && target.id.startsWith("anvil") && item.id.endsWith("_bar") }) { player: Player ->
+    if (!player.inventory.contains("hammer")) {
+        statement("You need a hammer to work the metal with.")
+        return@on
+    }
     player.open("smithing")
-    val metal = "steel"
+    val bars = player.inventory.count(item.id)
+    val metal = item.id.removeSuffix("_bar")
+    player.interfaces.sendText("smithing", "title", "${metal.toSentenceCase()} Smithing")
     for (type in types) {
-        val id = itemDefinitions.get("${metal}_$type").id
+        val componentDefinition = interfaceDefinitions.getComponent("smithing", type)
+        val itemDefinition = itemDefinitions.get("${metal}_$type")
+        val id = itemDefinition.id
         if (id != -1) {
-            val definition = interfaceDefinitions.getComponent("smithing", type)
-            val amount = definition?.getOrNull("amount") ?: 1
+            val amount = componentDefinition?.getOrNull("amount") ?: 1
             player.interfaces.sendItem("smithing", type, id, amount)
+            val smithing: Smithing = itemDefinition["smithing"]
+            if (player.has(Skill.Smithing, smithing.level)) {
+                player.interfaces.sendColour("smithing", "${type}_name", 31, 31, 31)
+            } else {
+                player.interfaces.sendColour("smithing", "${type}_name", 0, 0, 0)
+            }
+        }
+
+        val required = componentDefinition?.getOrNull("bars") ?: 1
+        if (bars < required) {
+            player.interfaces.sendColour("smithing", "${type}_bar", 29, 19, 5)
+        } else {
+            player.interfaces.sendColour("smithing", "${type}_bar", 0, 31, 0)
         }
     }
 
@@ -63,14 +91,18 @@ on<ItemOnObject>({ operate && target.id.startsWith("anvil") }) { player: Player 
     player.interfaces.sendVisibility("smithing", "bullseye_lantern", metal == "steel")
     player.interfaces.sendItem("smithing", "lantern", itemDefinitions.get("bullseye_lantern_frame").id, 1)
     player.interfaces.sendVisibility("smithing", "grapple", metal == "mithril")
+    player.interfaces.sendVisibility("smithing", "darts", player.quest("tourist_trap") == "completed")
+    player.interfaces.sendVisibility("smithing", "claw", player.quest("death_plateau") == "completed")
+    player.interfaces.sendVisibility("smithing", "pickaxes", player.quest("perils_of_ice_mountain") == "completed")
 
-    // tourist_trap
-    player.interfaces.sendVisibility("smithing", "darts", true)
+//    statement("You need a Smithing level of 8 to make a Bronze Sq Shield.")
+//    statement("You need a bronze bar to smith equipment on this anvil.")
+//    player.message("You hammer the bronze and make an axe.")
+//    player.message("You hammer the bronze and make a dagger.")
+//    player.message("You hammer the bronze and make a mace.")
+}
 
-    // death_plateau
-    player.interfaces.sendVisibility("smithing", "claw", true)
 
-    // perils_of_ice_mountain
-    player.interfaces.sendVisibility("smithing", "pickaxes", true)
-
+on<ItemOnObject>({ operate && target.id.startsWith("anvil") && item.id == "hammer" }) { player: Player ->
+    player.message("To smith metal equipment, you must use the metal bar on the anvil.")
 }
