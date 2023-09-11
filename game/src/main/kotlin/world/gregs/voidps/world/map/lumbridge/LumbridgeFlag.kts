36272025c3d327f9f6b33696774d40cbcc5b1ec2
jarryd229 <15334102+jarryd229@users.noreply.github.com>
2023-09-11 15:11:45 +1000

    npc spawns
    npc spawns

---
+++ game/src/main/kotlin/world/gregs/voidps/world/map/lumbridge/LumbridgeFlag.kts
@@ -0,0 +1,21 @@
+package world.gregs.voidps.world.map.lumbridge
+
+import world.gregs.voidps.engine.entity.character.forceChat
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.setAnimation
+import world.gregs.voidps.engine.entity.obj.ObjectOption
+import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.engine.queue.softQueue
+
+
+on<ObjectOption>({ operate && target.id == "lumbridge_flag" && option == "Raise" }) { player: Player ->
+    player.setAnimation("lumbridge_flag_raise")
+    target.animate("lumbridge_flag")
+    player.softQueue("lumbridge_flag", 8) {
+        player.setAnimation("lumbridge_flag_stop_raise")
+        player.softQueue("salute", 2) {
+            player.forceChat = "All Hail the Duke!"
+            player.setAnimation("emote_salute")
+        }
+    }
+}
