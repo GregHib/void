fe70c1f857c4a701741ab2c090c6028f62495fcf
GregHib <greghib@users.noreply.github.com>
2023-10-01 16:32:36 +0100

    Fix void set test and add slayer helmet test

--- game/src/main/kotlin/world/gregs/voidps/world/activity/skill/slayer/Slayer.kt
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/skill/slayer/Slayer.kt
@@ -1,13 +1,20 @@
 package world.gregs.voidps.world.activity.skill.slayer
 
 import world.gregs.voidps.engine.entity.character.Character
+import world.gregs.voidps.engine.entity.character.npc.NPC
 import world.gregs.voidps.engine.entity.character.player.Player
 
 val Player.hasSlayerTask: Boolean
-    get() = false
+    get() = this["slayer_task", false]
 
 fun Player.isTask(character: Character?): Boolean {
-    return false
+    val target = character ?: return false
+    if (target !is NPC) {
+        return false
+    }
+    val race = target.def["race", ""]
+    val type = this["slayer_type", "null"]
+    return race == type
 }
 
 fun Player.unlocked(reward: String): Boolean {
--- game/src/test/kotlin/world/gregs/voidps/world/interact/entity/player/combat/MeleeCombatFormulaTest.kt
+++ game/src/test/kotlin/world/gregs/voidps/world/interact/entity/player/combat/MeleeCombatFormulaTest.kt
@@ -174,14 +174,15 @@ internal class MeleeCombatFormulaTest : WorldTest() {
         player.equipment.set(EquipSlot.Hat.index, "void_melee_helm")
         player.equipment.set(EquipSlot.Legs.index, "void_knight_robe")
         player.equipment.set(EquipSlot.Chest.index, "void_knight_top")
+        player.equipment.set(EquipSlot.Hands.index, "void_knight_gloves")
         val npc = createNPC("giant_rat")
 
-        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee", Item("armadyl_godsword"), special = true)
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee")
 
-        assertEquals(39592, offensiveRating)
+        assertEquals(6016, offensiveRating)
         assertEquals(704, defensiveRating)
-        assertEquals(550, maxHit)
-        assertEquals(0.9911, chance, 0.0001)
+        assertEquals(122, maxHit)
+        assertEquals(0.9413, chance, 0.0001)
     }
 
     @Test
@@ -199,7 +200,36 @@ internal class MeleeCombatFormulaTest : WorldTest() {
 
         val (_, _, specMaxHit, _) = calculate(player, npc, "melee", Item("darklight"), special = true)
         assertEquals(133, specMaxHit)
+    }
 
+    @Test
+    fun `Slayer helmet not on task`() {
+        val player = createPlayer(Skill.Attack to 75, Skill.Strength to 99)
+        player.equipment.set(EquipSlot.Hat.index, "slayer_helmet")
+        val npc = createNPC("greater_demon")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee")
+
+        assertEquals(5504, offensiveRating)
+        assertEquals(5760, defensiveRating)
+        assertEquals(112, maxHit)
+        assertEquals(0.4777, chance, 0.0001)
+    }
+
+    @Test
+    fun `Slayer helmet on slayer task`() {
+        val player = createPlayer(Skill.Attack to 75, Skill.Strength to 99)
+        player.equipment.set(EquipSlot.Hat.index, "slayer_helmet")
+        player["slayer_task"] = true
+        player["slayer_type"] = "demon"
+        val npc = createNPC("greater_demon")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee")
+
+        assertEquals(5504, offensiveRating)
+        assertEquals(5760, defensiveRating)
+        assertEquals(128, maxHit)
+        assertEquals(0.4777, chance, 0.0001)
     }
 
 }
