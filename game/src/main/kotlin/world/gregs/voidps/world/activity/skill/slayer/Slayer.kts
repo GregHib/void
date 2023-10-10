11a8876caeebfa28590b99e4d8c1c28088e02dc4
GregHib <greghib@users.noreply.github.com>
2023-10-10 16:32:49 +0100

    Fix salve amulet formula

--- game/src/main/kotlin/world/gregs/voidps/world/activity/skill/slayer/Slayer.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/skill/slayer/Slayer.kts
@@ -1,6 +1,7 @@
 package world.gregs.voidps.world.activity.skill.slayer
 
 import world.gregs.voidps.engine.entity.character.Character
+import world.gregs.voidps.engine.entity.character.npc.NPC
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.equip.equipped
 import world.gregs.voidps.engine.event.Priority
@@ -19,20 +20,19 @@ on<HitDamageModifier>(priority = Priority.HIGHER) { player: Player ->
 }
 
 fun getSlayerMultiplier(player: Player, target: Character?, type: String, damage: Boolean): Double {
+    if (type == "melee" && target is NPC && target.undead) {
+        when (player.equipped(EquipSlot.Amulet).id) {
+            "salve_amulet_e" -> return 1.2
+            "salve_amulet" -> return 1.15
+        }
+    }
     if (!player.hasSlayerTask || !player.isTask(target)) {
         return 1.0
     }
     val helm = player.equipped(EquipSlot.Hat).id
-    if (type == "melee") {
-        val amulet = player.equipped(EquipSlot.Amulet).id
-        if (amulet == "salve_amulet_e") {
-            return 1.2
-        }
-        if (amulet == "salve_amulet" || helm.startsWith("black_mask") || helm.startsWith("slayer_helmet")) {
-            return 1.15
-        }
+    if (type == "melee" && (helm.startsWith("black_mask") || helm.startsWith("slayer_helmet"))) {
+        return 1.15
     }
-
     if (type == "range" && (helm == "focus_sight" || helm.startsWith("full_slayer_helmet"))) {
         return 1.15
     }
