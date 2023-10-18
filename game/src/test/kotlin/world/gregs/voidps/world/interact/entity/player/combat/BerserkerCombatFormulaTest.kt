6435b825faee196c4e5de98081180d76fdfa5b16
GregHib <greghib@users.noreply.github.com>
2023-10-18 17:04:00 +0100

    Add void set and berserker necklace tests

---
+++ game/src/test/kotlin/world/gregs/voidps/world/interact/entity/player/combat/BerserkerCombatFormulaTest.kt
@@ -0,0 +1,86 @@
+package world.gregs.voidps.world.interact.entity.player.combat
+
+import org.junit.jupiter.api.Test
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.engine.entity.item.Item
+import world.gregs.voidps.engine.inv.equipment
+import world.gregs.voidps.network.visual.update.player.EquipSlot
+import kotlin.test.assertEquals
+
+internal class BerserkerCombatFormulaTest : CombatFormulaTest() {
+
+    @Test
+    fun `No berserker effect with non-tzhaar weapon`() {
+        val player = createPlayer(Skill.Attack to 75, Skill.Strength to 99)
+        player.equipment.set(EquipSlot.Amulet.index, "berserker_necklace")
+        player.equipment.set(EquipSlot.Weapon.index, "bronze_sword")
+        val npc = createNPC("giant_rat")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee", Item("bronze_sword"))
+
+        assertEquals(5848, offensiveRating)
+        assertEquals(704, defensiveRating)
+        assertEquals(132, maxHit)
+        assertEquals(0.9396, chance, 0.0001)
+    }
+
+    @Test
+    fun `Toktz-xil-ak berserker weapon effect`() {
+        val player = createPlayer(Skill.Attack to 75, Skill.Strength to 99)
+        player.equipment.set(EquipSlot.Amulet.index, "berserker_necklace")
+        player.equipment.set(EquipSlot.Weapon.index, "toktz_xil_ak")
+        val npc = createNPC("giant_rat")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee", Item("toktz_xil_ak"))
+
+        assertEquals(9546, offensiveRating)
+        assertEquals(704, defensiveRating)
+        assertEquals(246, maxHit)
+        assertEquals(0.963, chance, 0.0001)
+    }
+
+    @Test
+    fun `Tzhaar-ket-om berserker weapon effect`() {
+        val player = createPlayer(Skill.Attack to 75, Skill.Strength to 99)
+        player.equipment.set(EquipSlot.Amulet.index, "berserker_necklace")
+        player.equipment.set(EquipSlot.Weapon.index, "tzhaar_ket_om")
+        val npc = createNPC("giant_rat")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee", Item("tzhaar_ket_om"))
+
+        assertEquals(12384, offensiveRating)
+        assertEquals(704, defensiveRating)
+        assertEquals(318, maxHit)
+        assertEquals(0.9714, chance, 0.0001)
+    }
+
+    @Test
+    fun `Tzhaar-ket-em berserker weapon effect`() {
+        val player = createPlayer(Skill.Attack to 75, Skill.Strength to 99)
+        player.equipment.set(EquipSlot.Amulet.index, "berserker_necklace")
+        player.equipment.set(EquipSlot.Weapon.index, "tzhaar_ket_em")
+        val npc = createNPC("giant_rat")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee", Item("tzhaar_ket_em"))
+
+        assertEquals(10836, offensiveRating)
+        assertEquals(704, defensiveRating)
+        assertEquals(260, maxHit)
+        assertEquals(0.9674, chance, 0.0001)
+    }
+
+    @Test
+    fun `Toktz-xil-ek berserker weapon effect`() {
+        val player = createPlayer(Skill.Attack to 75, Skill.Strength to 99)
+        player.equipment.set(EquipSlot.Amulet.index, "berserker_necklace")
+        player.equipment.set(EquipSlot.Weapon.index, "toktz_xil_ek")
+        val npc = createNPC("giant_rat")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee", Item("toktz_xil_ek"))
+
+        assertEquals(9632, offensiveRating)
+        assertEquals(704, defensiveRating)
+        assertEquals(225, maxHit)
+        assertEquals(0.9633, chance, 0.0001)
+    }
+}
---
+++ game/src/test/kotlin/world/gregs/voidps/world/interact/entity/player/combat/VoidSetCombatFormulaTest.kt
@@ -0,0 +1,87 @@
+package world.gregs.voidps.world.interact.entity.player.combat
+
+import org.junit.jupiter.api.Test
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.engine.entity.item.Item
+import world.gregs.voidps.engine.inv.add
+import world.gregs.voidps.engine.inv.equipment
+import world.gregs.voidps.engine.inv.inventory
+import world.gregs.voidps.network.visual.update.player.EquipSlot
+import world.gregs.voidps.world.activity.combat.prayer.PrayerStart
+import world.gregs.voidps.world.interact.entity.combat.combatStyle
+import world.gregs.voidps.world.interact.entity.player.equip.InventoryOption
+import kotlin.math.absoluteValue
+import kotlin.math.sqrt
+import kotlin.test.assertEquals
+
+internal class VoidSetCombatFormulaTest : CombatFormulaTest() {
+
+    @Test
+    fun `No void set effect with one missing item`() {
+        val player = createPlayer(Skill.Attack to 75, Skill.Strength to 99)
+        player.equipment.set(EquipSlot.Chest.index, "void_knight_top")
+        player.equipment.set(EquipSlot.Legs.index, "void_knight_robe")
+        player.equipment.set(EquipSlot.Hands.index, "void_knight_gloves")
+        val npc = createNPC("giant_rat")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee")
+
+        assertEquals(5504, offensiveRating)
+        assertEquals(704, defensiveRating)
+        assertEquals(112, maxHit)
+        assertEquals(0.9359, chance, 0.0001)
+    }
+
+    @Test
+    fun `Melee void set effect`() {
+        val player = createPlayer(Skill.Attack to 75, Skill.Strength to 99)
+        player.equipment.set(EquipSlot.Chest.index, "void_knight_top")
+        player.equipment.set(EquipSlot.Legs.index, "void_knight_robe")
+        player.equipment.set(EquipSlot.Hands.index, "void_knight_gloves")
+        player.equipment.set(EquipSlot.Hat.index, "void_melee_helm")
+        val npc = createNPC("giant_rat")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "melee")
+
+        assertEquals(6016, offensiveRating)
+        assertEquals(704, defensiveRating)
+        assertEquals(122, maxHit)
+        assertEquals(0.9413, chance, 0.0001)
+    }
+
+    @Test
+    fun `Ranged void set effect`() {
+        val player = createPlayer(Skill.Ranged to 75)
+        player.equipment.set(EquipSlot.Chest.index, "void_knight_top")
+        player.equipment.set(EquipSlot.Legs.index, "void_knight_robe")
+        player.equipment.set(EquipSlot.Hands.index, "void_knight_gloves")
+        player.equipment.set(EquipSlot.Hat.index, "void_ranger_helm")
+        player.equipment.set(EquipSlot.Weapon.index, "shortbow")
+        player.equipment.set(EquipSlot.Ammo.index, "bronze_arrow")
+        val npc = createNPC("giant_rat")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "range", Item("shortbow"))
+
+        assertEquals(6768, offensiveRating)
+        assertEquals(704, defensiveRating)
+        assertEquals(109, maxHit)
+        assertEquals(0.9479, chance, 0.0001)
+    }
+
+    @Test
+    fun `Magic void set effect`() {
+        val player = createPlayer(Skill.Magic to 75)
+        player.equipment.set(EquipSlot.Chest.index, "void_knight_top")
+        player.equipment.set(EquipSlot.Legs.index, "void_knight_robe")
+        player.equipment.set(EquipSlot.Hands.index, "void_knight_gloves")
+        player.equipment.set(EquipSlot.Hat.index, "void_mage_helm")
+        val npc = createNPC("giant_rat")
+
+        val (offensiveRating, defensiveRating, maxHit, chance) = calculate(player, npc, "magic", spell = "wind_strike")
+
+        assertEquals(6848, offensiveRating)
+        assertEquals(64, defensiveRating)
+        assertEquals(20, maxHit)
+        assertEquals(0.9951, chance, 0.0001)
+    }
+}
