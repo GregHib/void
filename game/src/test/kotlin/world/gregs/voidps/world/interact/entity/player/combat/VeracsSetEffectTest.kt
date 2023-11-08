78987bcdf0414ee11d3e4f89c914b71df8656da3
GregHib <greghib@users.noreply.github.com>
2023-11-08 21:16:38 +0000

    Add verac's set effect #379

---
+++ game/src/test/kotlin/world/gregs/voidps/world/interact/entity/player/combat/VeracsSetEffectTest.kt
@@ -0,0 +1,65 @@
+package world.gregs.voidps.world.interact.entity.player.combat
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Test
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.engine.entity.item.Item
+import world.gregs.voidps.engine.inv.Inventory
+import world.gregs.voidps.engine.inv.clear
+import world.gregs.voidps.engine.inv.equipment
+import world.gregs.voidps.network.visual.update.player.EquipSlot
+import world.gregs.voidps.world.interact.entity.combat.hit
+import world.gregs.voidps.world.interact.entity.combat.hitChance
+
+internal class VeracsSetEffectTest : CombatFormulaTest() {
+
+    @Test
+    fun `No veracs set effect with one missing item`() {
+        val player = createPlayer(Skill.Attack to 99, Skill.Strength to 99)
+        player.equipment.apply(veracs())
+        player.equipment.clear(EquipSlot.Weapon.index)
+        val target = createPlayer(Skill.Constitution to 990)
+
+        val chance = hitChance(player, target, "magic", Item("veracs_flail"), false)
+        player.hit(target, Item("veracs_flail"), "melee", damage = 10)
+
+        assertNotEquals(1.0, chance)
+        assertEquals(980, target.levels.get(Skill.Constitution))
+    }
+
+    @Test
+    fun `No magic veracs set effect`() {
+        val player = createPlayer(Skill.Attack to 99, Skill.Strength to 99)
+        player.equipment.apply(veracs())
+        val target = createPlayer(Skill.Constitution to 990)
+
+        val chance = hitChance(player, target, "magic", Item("veracs_flail"), false)
+        player.hit(target, Item("veracs_flail"), "magic", damage = 10)
+        tick(2)
+
+        assertNotEquals(1.0, chance)
+        assertEquals(980, target.levels.get(Skill.Constitution))
+    }
+
+    @Test
+    fun `veracs melee set effect`() {
+        val player = createPlayer(Skill.Attack to 99, Skill.Strength to 99)
+        player.equipment.apply(veracs())
+        val target = createPlayer(Skill.Constitution to 990)
+
+        val chance = hitChance(player, target, "melee", Item("veracs_flail"), false)
+        player.hit(target, Item("veracs_flail"), "melee", damage = 0)
+        tick()
+
+        assertEquals(1.0, chance)
+        assertNotEquals(980, target.levels.get(Skill.Constitution))
+    }
+
+    private fun veracs(): Inventory.() -> Unit = {
+        set(EquipSlot.Chest.index, "veracs_brassard")
+        set(EquipSlot.Legs.index, "veracs_plateskirt")
+        set(EquipSlot.Weapon.index, "veracs_flail")
+        set(EquipSlot.Hat.index, "veracs_helm")
+    }
+}
