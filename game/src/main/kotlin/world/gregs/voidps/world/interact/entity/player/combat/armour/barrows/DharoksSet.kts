1be879b6c10cfb0fe42dcbf8099a6384034af511
GregHib <greghib@users.noreply.github.com>
2023-11-08 21:51:09 +0000

    Add dharok's set effect #379

---
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/armour/barrows/DharoksSet.kts
@@ -0,0 +1,40 @@
+package world.gregs.voidps.world.interact.entity.player.combat.armour.barrows
+
+import world.gregs.voidps.engine.entity.Registered
+import world.gregs.voidps.engine.entity.character.Character
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.engine.entity.item.Item
+import world.gregs.voidps.engine.event.Priority
+import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.engine.inv.ItemChanged
+import world.gregs.voidps.type.random
+import world.gregs.voidps.world.interact.entity.combat.HitChanceModifier
+import world.gregs.voidps.world.interact.entity.combat.HitDamageModifier
+import kotlin.math.floor
+
+on<Registered>({ it.hasFullSet() }) { player: Player ->
+    player["dharoks_set_effect"] = true
+}
+
+on<ItemChanged>({ inventory == "worn_equipment" && BarrowsArmour.isSlot(index) && it.contains("dharoks_set_effect") && !isDharoks(item) }) { player: Player ->
+    player.clear("dharoks_set_effect")
+}
+
+on<ItemChanged>({ inventory == "worn_equipment" && BarrowsArmour.isSlot(index) && !it.contains("dharoks_set_effect") && isDharoks(item) && it.hasFullSet() }) { player: Player ->
+    player["dharoks_set_effect"] = true
+}
+
+fun isDharoks(item: Item) = item.id.startsWith("dharoks_")
+
+fun Player.hasFullSet() = BarrowsArmour.hasSet(this,
+    "dharoks_greataxe",
+    "dharoks_helm",
+    "dharoks_platebody",
+    "dharoks_platelegs")
+
+on<HitDamageModifier>({ type == "melee" && weapon?.id?.startsWith("dharoks_greataxe") == true && it.contains("dharoks_set_effect") }, Priority.LOW) { player: Player ->
+    val lost = player.levels.getMax(Skill.Constitution) - player.levels.get(Skill.Constitution) / 100.0
+    val max = player.levels.getMax(Skill.Constitution) / 100.0
+    damage = floor(damage * (1 + lost * max))
+}
--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/weapon/DharoksGreatAxe.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/weapon/DharoksGreatAxe.kts
@@ -2,13 +2,11 @@ package world.gregs.voidps.world.interact.entity.player.combat.melee.weapon
 
 import world.gregs.voidps.engine.entity.character.Character
 import world.gregs.voidps.engine.entity.character.player.Player
-import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.setAnimation
 import world.gregs.voidps.engine.entity.item.Item
 import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.world.interact.entity.combat.*
-import kotlin.math.floor
 
 fun isGreatAxe(item: Item?) = item != null && (item.id.startsWith("dharoks_greataxe") || item.id == "balmung")
 
@@ -26,10 +24,4 @@ on<CombatSwing>({ !swung() && isGreatAxe(it.weapon) }, Priority.LOW) { player: P
 on<CombatAttack>({ !blocked && target is Player && isGreatAxe(target.weapon) }, Priority.LOW) { _: Character ->
     target.setAnimation("dharoks_greataxe_block", delay)
     blocked = true
-}
-
-on<HitDamageModifier>({ player -> type == "melee" && weapon?.id?.startsWith("dharoks_greataxe") == true && player["dharoks_set", false] }, Priority.LOW) { player: Player ->
-    val lost = player.levels.getMax(Skill.Constitution) - player.levels.get(Skill.Constitution) / 100.0
-    val max = player.levels.getMax(Skill.Constitution) / 100.0
-    damage = floor(damage * (1 + lost * max))
 }
