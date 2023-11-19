96c0fd125760e4df278b03f7cdf8061654c2dcda
GregHib <greghib@users.noreply.github.com>
2023-11-19 00:47:45 +0000

    Add ferocious ring effect test

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/armour/FerociousRing.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/armour/FerociousRing.kts
@@ -1,17 +1,18 @@
 package world.gregs.voidps.world.interact.entity.player.combat.armour
 
-import world.gregs.voidps.engine.data.definition.AreaDefinition
+import world.gregs.voidps.engine.data.definition.AreaDefinitions
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.equip.equipped
 import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.engine.inject
 import world.gregs.voidps.network.visual.update.player.EquipSlot
 import world.gregs.voidps.world.interact.entity.combat.HitDamageModifier
 import kotlin.math.floor
 
-on<HitDamageModifier>(
-    { it.getOrNull<AreaDefinition>("map")?.name == "duradals_dungeon" && it.equipped(EquipSlot.Hands).id.startsWith("ferocious_ring") },
-    priority = Priority.LOWER
-) { _: Player ->
+val areas: AreaDefinitions by inject()
+val area = areas["kuradals_dungeon"]
+
+on<HitDamageModifier>({ it.tile in area && it.equipped(EquipSlot.Ring).id.startsWith("ferocious_ring") }, Priority.LOWER) { _: Player ->
     damage = floor(damage * 1.04)
 }
