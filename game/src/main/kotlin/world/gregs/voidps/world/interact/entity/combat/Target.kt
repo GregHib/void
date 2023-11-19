a8c61a8856a48a825352fbb765bc2a4f63fb7e41
GregHib <greghib@users.noreply.github.com>
2023-11-19 16:30:17 +0000

    Remove Combat class

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/combat/Target.kt
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/combat/Target.kt
@@ -1,5 +1,7 @@
 package world.gregs.voidps.world.interact.entity.combat
 
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap
+import it.unimi.dsi.fastutil.objects.ObjectArrayList
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.client.variable.hasClock
 import world.gregs.voidps.engine.data.definition.SpellDefinitions
@@ -72,3 +74,21 @@ internal var Character.target: Character?
 
 val Character.underAttack: Boolean
     get() = hasClock("under_attack")
+
+var Character.attackers: MutableList<Character>
+    get() = getOrPut("attackers") { ObjectArrayList() }
+    set(value) = set("attackers", value)
+
+var Character.damageDealers: MutableMap<Character, Int>
+    get() = getOrPut("damage_dealers") { Object2IntOpenHashMap() }
+    set(value) = set("damage_dealers", value)
+
+var Character.dead: Boolean
+    get() = get("dead", false)
+    set(value) {
+        if (value) {
+            set("dead", true)
+        } else {
+            clear("dead")
+        }
+    }
