4a5d59d37fef4b884d34d0587b234963eede702b
GregHib <greghib@users.noreply.github.com>
2023-11-21 22:26:54 +0000

    Fix god arrow delays

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/range/ammo/GodArrows.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/range/ammo/GodArrows.kts
@@ -2,6 +2,7 @@ package world.gregs.voidps.world.interact.entity.player.combat.range.ammo
 
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.engine.timer.CLIENT_TICKS
 import world.gregs.voidps.type.random
 import world.gregs.voidps.world.interact.entity.combat.hit.CombatAttack
 import world.gregs.voidps.world.interact.entity.combat.hit.Damage
@@ -13,7 +14,7 @@ on<CombatAttack>({ type == "range" && it.ammo == "saradomin_arrows" }) { source:
     if (random.nextDouble() < chance) {
         // water_strike
         val damage = Damage.roll(source, target, type, weapon)
-        target.hit(source, weapon, "magic", delay, damage = damage)
+        source.hit(target, weapon, "magic", CLIENT_TICKS.toTicks(delay), damage = damage)
     }
 }
 
@@ -22,7 +23,7 @@ on<CombatAttack>({ type == "range" && it.ammo == "guthix_arrows" }) { source: Pl
     if (random.nextDouble() < chance) {
         // earth_strike
         val damage = Damage.roll(source, target, type, weapon)
-        target.hit(source, weapon, "magic", delay, damage = damage)
+        source.hit(target, weapon, "magic", CLIENT_TICKS.toTicks(delay), damage = damage)
     }
 }
 
@@ -31,6 +32,6 @@ on<CombatAttack>({ type == "range" && it.ammo == "zamorak_arrows" }) { source: P
     if (random.nextDouble() < chance) {
         // fire_strike
         val damage = Damage.roll(source, target, type, weapon)
-        target.hit(source, weapon, "magic", delay, damage = damage)
+        source.hit(target, weapon, "magic", CLIENT_TICKS.toTicks(delay), damage = damage)
     }
 }
