88512dd8af520325a2c999d8c56a1f6767e35801
GregHib <greghib@users.noreply.github.com>
2023-11-25 01:29:00 +0000

    Fix parameter converter

--- cache/src/main/kotlin/world/gregs/voidps/cache/DefinitionEncoder.kt
+++ cache/src/main/kotlin/world/gregs/voidps/cache/DefinitionEncoder.kt
@@ -4,6 +4,7 @@ import world.gregs.voidps.buffer.write.Writer
 
 interface DefinitionEncoder<T : Definition> {
     open fun Writer.encode(definition: T, members: T) {
+        encode(definition)
     }
 
     open fun Writer.encode(definition: T) {
