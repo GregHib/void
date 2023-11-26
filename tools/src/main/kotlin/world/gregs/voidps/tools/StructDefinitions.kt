77fdfbb75967abb10810c38e50a319e5345ea04f
GregHib <greghib@users.noreply.github.com>
2023-11-26 18:45:23 +0000

    Add struct parameter constants

---
+++ tools/src/main/kotlin/world/gregs/voidps/tools/StructDefinitions.kt
@@ -0,0 +1,43 @@
+package world.gregs.voidps.tools
+
+import world.gregs.voidps.cache.Cache
+import world.gregs.voidps.cache.CacheDelegate
+import world.gregs.voidps.cache.config.decoder.StructDecoder
+import java.io.File
+
+object StructDefinitions {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val cache: Cache = CacheDelegate(property("cachePath"))
+        val decoder = StructDecoder().loadCache(cache)
+        val set = mutableSetOf<Long>()
+        for (i in decoder.indices) {
+            val def = decoder.getOrNull(i) ?: continue
+            if (def.params != null) {
+                set.addAll(def.params!!.keys)
+                println("$i ${def}")
+            }
+        }
+
+        val folder = File("C:\\Users\\Greg\\Documents\\Void\\decompiled-cs2-667\\")
+        val map = mutableMapOf<Int, MutableSet<String>>()
+        for (file in folder.listFiles()!!) {
+            val pattern = "getAttributeMapValue\\(.*, ([0-9]+)\\)".toRegex().toPattern()
+            val matcher = pattern.matcher(file.readText())
+            while (matcher.find()) {
+                val param = matcher.group(1).toInt()
+                map.getOrPut(param) { mutableSetOf() }.add(file.nameWithoutExtension)
+            }
+        }
+
+        for (param in set.sorted()) {
+            val files = map[param.toInt()]
+            if (files != null) {
+                println("    const val PARAM_${param} = ${param}L // ${files.joinToString(", ") { "${it}.cs2" }}")
+            } else {
+                println("    const val PARAM_${param} = ${param}L")
+            }
+        }
+    }
+}
