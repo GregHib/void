15b5ccaad4461a887cce8a1997234e3d9f43d425
GregHib <greghib@users.noreply.github.com>
2023-11-29 00:11:19 +0000

    Add boosted stat draining and berserker 15% slower #293

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/effect/StatRestoration.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/effect/StatRestoration.kts
@@ -1,5 +1,7 @@
 package world.gregs.voidps.world.interact.entity.player.effect
 
+import world.gregs.voidps.engine.client.variable.hasClock
+import world.gregs.voidps.engine.client.variable.start
 import world.gregs.voidps.engine.entity.Registered
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
@@ -13,32 +15,39 @@ import java.util.concurrent.TimeUnit
 
 val skills = Skill.all.filterNot { it == Skill.Prayer || it == Skill.Summoning || it == Skill.Constitution }
 
-on<Registered>({ player -> skills.any { player.levels.getOffset(it) < 0 } }) { player: Player ->
+on<Registered>({ player -> skills.any { player.levels.getOffset(it) != 0 } }) { player: Player ->
     player.softTimers.start("restore_stats")
 }
 
-on<CurrentLevelChanged>({ skill != Skill.Prayer && skill != Skill.Summoning && skill != Skill.Constitution && to < it.levels.getMax(skill) && !it.softTimers.contains("restore_stats") }) { player: Player ->
+on<CurrentLevelChanged>({ skill != Skill.Prayer && skill != Skill.Summoning && skill != Skill.Constitution && to != it.levels.getMax(skill) && !it.softTimers.contains("restore_stats") }) { player: Player ->
     player.softTimers.start("restore_stats")
 }
 
 on<TimerStart>({ timer == "restore_stats" }) { _: Player ->
-    interval = TimeUnit.MINUTES.toTicks(1)
+    interval = TimeUnit.SECONDS.toTicks(60)
 }
 
 on<TimerTick>({ timer == "restore_stats" }) { player: Player ->
-    var total = 0
-    for (skill in skills) {
-        total += player.levels.restore(skill, restoreAmount(player))
+    val berserker = player.praying("berserker") && player.hasClock("berserker_cooldown")
+    val skip = player.praying("berserker") && !player.hasClock("berserker_cooldown")
+    if (skip) {
+        nextInterval = TimeUnit.SECONDS.toTicks(9)
+        player.start("berserker_cooldown", nextInterval)
     }
-    if (total == 0) {
+    var fullyRestored = true
+    for (skill in skills) {
+        val offset = player.levels.getOffset(skill)
+        if (offset != 0) {
+            fullyRestored = false
+        }
+        if (offset > 0 && !skip) {
+            player.levels.drain(skill, 1)
+        } else if (offset < 0 && !berserker) {
+            val restore = if (player.praying("rapid_restore")) 2 else 1
+            player.levels.restore(skill, restore)
+        }
+    }
+    if (fullyRestored) {
         cancel()
     }
-}
-
-fun restoreAmount(player: Player): Int {
-    var restore = 1
-    if (player.praying("rapid_restore")) {
-        restore *= 2
-    }
-    return restore
 }
