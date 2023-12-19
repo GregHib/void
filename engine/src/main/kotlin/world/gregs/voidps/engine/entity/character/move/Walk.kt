b5256012bade9280cb152c0bc0aa05b170657650
GregHib <greghib@users.noreply.github.com>
2023-12-19 16:51:54 +0000

    Comments

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/move/Walk.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/move/Walk.kt
@@ -5,6 +5,11 @@ import world.gregs.voidps.engine.entity.character.mode.move.Movement
 import world.gregs.voidps.engine.entity.character.mode.move.target.TileTargetStrategy
 import world.gregs.voidps.type.Tile
 
+/**
+ * Walks player to [target]
+ * Specify [noCollision] to walk through [GameObject]s and
+ * [slowRun] to force walking even if the player has running active
+ */
 fun Character.walkTo(target: Tile, noCollision: Boolean = false, slowRun: Boolean = false) {
     mode = Movement(this, TileTargetStrategy(target, noCollision, slowRun))
 }
