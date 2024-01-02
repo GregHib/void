7ea7b739e2f2f95e712de878549ee9c9f8b32e52
GregHib <greghib@users.noreply.github.com>
2024-01-02 22:37:29 +0000

    Save openrs2 keys as json files

--- tools/src/main/kotlin/world/gregs/voidps/tools/map/MissingMapFinder.kt
+++ tools/src/main/kotlin/world/gregs/voidps/tools/map/MissingMapFinder.kt
@@ -25,7 +25,7 @@ private data class Cache(
  */
 object MissingMapFinder {
 
-    private val temp = File("./temp/")
+    private val temp = File("./temp/xteas/")
     private val json = jacksonObjectMapper().apply {
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     }
