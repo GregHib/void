8692f81138d3038edb454241f725f18ea3eb0664
GregHib <greghib@users.noreply.github.com>
2024-01-08 16:25:22 +0000

    Update keywords and new matcher approach

--- tools/src/main/kotlin/world/gregs/voidps/tools/cache/HashCodeMatcher.kt
+++ tools/src/main/kotlin/world/gregs/voidps/tools/cache/HashCodeMatcher.kt
@@ -1,186 +1,214 @@
 package world.gregs.voidps.tools.cache
 
 import com.displee.cache.CacheLibrary
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.supervisorScope
 import world.gregs.voidps.cache.Index
 import world.gregs.voidps.tools.property
 import java.io.File
-import kotlin.math.pow
 
 object HashCodeMatcher {
 
 
+    private var counter = 0
+    private val matches = mutableMapOf<Int, MutableSet<String>>()
+    private lateinit var known: Map<Int, String?>
+    private val output = File("./temp/hashes/found-scripts.tsv")
+
     @JvmStatic
     fun main(args: Array<String>) {
-        dumpHashes()
-        val keywords = File("./tools/src/main/resources/keywords.txt").readLines().toMutableList()
-        val separators = separators()
-        val affixes = affixes()
-        val hashes = File("./temp/hashes/hashes.txt").readLines().associate {
-            val parts = it.split(", ")
-            parts.first().toInt() to Triple(parts[1].toInt(), parts[2].toInt(), parts[3].toInt())
+//        dumpHashes()
+        output.delete()
+        val keywords = Keywords("./tools/src/main/resources/keywords.txt")
+        val index = Index.SPRITES
+        known = File("./temp/hashes/hashes-modified.tsv").readLines()
+            .filter {
+                val parts = it.split("\t")
+                parts[0].toInt() == index
+            }
+            .associate {
+                val parts = it.split("\t")
+                parts[3].toInt() to parts.getOrNull(4)
+            }
+
+        for ((key, value) in known) {
+            if (!value.isNullOrBlank() && value.hashCode() != key) {
+                println("Invalid $key $value - ${value.hashCode()}")
+            }
         }
-        val knownHashes = File("./temp/names.tsv").readLines().associate {
-            val parts = it.split("\t")
-            parts[3].toInt() to parts[4]
-        }.toMutableMap()
-        println("Maximum permutations: ${(1..5).sumOf { affixes[it].size * separators[it].size.toLong() * (it.toDouble().pow(keywords.size).toLong()) }}")
-        val debug = File("./temp/hashes/debug.tsv")
-        val found = File("./temp/hashes/found.tsv")
-        found.delete()
-        debug.delete()
-        runBlocking {
-            supervisorScope {
-                for (i in 1..5) {
-                    launch(Dispatchers.IO) {
-                        val findings = mutableListOf<String>()
-                        Permutations().print(keywords, i, separators[i - 1], affixes[i - 1]) { hash, array, length -> // comma
-                            val data = hashes[hash] ?: return@print
-                            if (data.first != Index.CLIENT_SCRIPTS && array[0] == 91) { // [
-                                return@print
-                            }
-                            val string = array.take(length).map { it.toChar() }.toCharArray().concatToString()
-                            if (!knownHashes.containsKey(hash)) {
-                                knownHashes[hash] = string
-                                println("Found $i $hash $string")
-                                findings.add("${data.first}\t${data.second}\t${data.third}\t${hash}\t${string}\n")
-                                if (findings.size > 10_000) {
-                                    for (find in findings) {
-                                        found.appendText(find)
-                                    }
-                                    findings.clear()
-                                }
-                            } else {
-                                val known = knownHashes[hash]
-                                if (known != string) {
-                                    debug.appendText("$i\t$hash\t$string\t$known\n")
-                                    println("Duplicate $i $hash $string ${knownHashes[hash]}")
-                                }
-                            }
+
+        findInterfaces(keywords, depth = -1)
+        findSprites(keywords, depth = 3)
+        findScripts(keywords, depth = -1)
+    }
+
+    fun add(string: String): Boolean {
+        val hash = string.hashCode()
+        if (!known.containsKey(hash)) {
+            return false
+        }
+        if (known[hash] == null && matches.getOrPut(hash) { mutableSetOf() }.add(string)) {
+            println("Found $hash -\t$string")
+            counter++
+            if (counter.rem(10_000) == 0) {
+                writeAll()
+            }
+        }
+        return true
+    }
+
+    private fun numbers(string: String, separator: Char? = null, prefix: String? = null, suffix: String? = null, maxSequence: Int = 10) {
+        for (i in 0..500) {
+            val added = add("${prefix ?: ""}$string${separator ?: ""}${i}${suffix ?: ""}")
+            if (i < maxSequence) {
+                add("${prefix ?: ""}$string${separator ?: ""}${i}${suffix ?: ""}")
+            } else if (!added) {
+                add("${prefix ?: ""}${string}${separator ?: ""}0${i}${suffix ?: ""}")
+                break
+            }
+        }
+    }
+
+    private fun check(string: String, separators: List<Char> = emptyList(), prefix: String? = null, suffix: String? = null, maxSequence: Int = 1) {
+        add("${prefix ?: ""}${string}${suffix ?: ""}")
+        numbers(string, prefix = prefix, suffix = suffix, maxSequence = maxSequence)
+        for (separator in separators) {
+            numbers(string, separator, prefix, suffix, maxSequence)
+        }
+    }
+
+    private fun findInterfaces(keywords: Set<String>, depth: Int) {
+        val underscore = listOf('_')
+        val space = listOf(' ')
+        if (depth == 1) {
+            println("Searching individual keywords")
+            for (a in keywords) {
+                check(a)
+            }
+        }
+        if (depth == 2) {
+            println("Searching double keywords")
+            val underscored = keywords.filter { !it.contains(" ") }
+            val spaced = keywords.filter { !it.contains("_") }
+            for (a in underscored) {
+                for (b in underscored) {
+                    check("${a}_${b}", underscore)
+                    check("${a}${b}")
+                }
+            }
+            for (a in spaced) {
+                for (b in spaced) {
+                    check("${a} ${b}", underscore)
+                    check("${a}${b}")
+                }
+            }
+        }
+
+        if (depth == 3) {
+            println("Searching triple keywords")
+            for (a in keywords) {
+                for (b in keywords) {
+                    for (c in keywords) {
+                        if (!a.contains(" ") && !b.contains(" ") && !c.contains(" ")) {
+                            check("${a}_${b}_${c}", underscore)
+                            check("${a}${b}_${c}", underscore)
+                            check("${a}_${b}${c}", underscore)
                         }
-                        for (find in findings) {
-                            found.appendText(find)
+                        check("${a}${b}${c}", underscore)
+                        check("${a}${b}${c}", space)
+                        if (!a.contains("_") && !b.contains("_") && !c.contains("_")) {
+                            check("$a $b $c", space)
+                            check("$a$b $c", space)
+                            check("$a $b$c", space)
                         }
                     }
                 }
             }
         }
-        File("./temp/hashes/all.csv").writeText(knownHashes.toList().joinToString("\n") { "${it.first}, ${it.second}" })
+        writeAll()
     }
 
-    private fun affixes(): Array<List<Pair<IntArray, IntArray>>> {
-        val process = "[proc,".toCharArray().map { it.code }.toIntArray() to intArrayOf(']'.code)
-        val clientscript = "[clientscript,".toCharArray().map { it.code }.toIntArray() to intArrayOf(']'.code)
-        val empty = intArrayOf() to intArrayOf()
-        val all = listOf(process, clientscript, empty)
-        return arrayOf(all, all, all, all, all, all)
+    private fun findScripts(keywords: Set<String>, depth: Int) {
+        val keywords = keywords.filter { !it.contains(" ") }
+        val underscore = listOf('_')
+        if (depth == 1) {
+            println("Searching individual keywords")
+            for (a in keywords) {
+                check(a, prefix = "[proc,", suffix = "]", maxSequence = 1)
+                check(a, prefix = "[clientscript,", suffix = "]", maxSequence = 1)
+            }
+        }
+        if (depth == 2) {
+            println("Searching double keywords")
+            for (a in keywords) {
+                for (b in keywords) {
+                    if (!a.contains(" ") && !b.contains(" ")) {
+                        check("${a}_${b}", underscore, prefix = "[proc,", suffix = "]", maxSequence = 1)
+                        check("${a}_${b}", underscore, prefix = "[clientscript,", suffix = "]", maxSequence = 1)
+                    }
+                    check("${a}${b}", prefix = "[proc,", suffix = "]", maxSequence = 1)
+                    check("${a}${b}", prefix = "[clientscript,", suffix = "]", maxSequence = 1)
+                }
+            }
+        }
+
+        if (depth == 3) {
+            println("Searching triple keywords")
+            for (a in keywords) {
+                for (b in keywords) {
+                    for (c in keywords) {
+                        if (!a.contains(" ") && !b.contains(" ") && !c.contains(" ")) {
+                            check("${a}_${b}_${c}", underscore, prefix = "[proc,", suffix = "]", maxSequence = 1)
+                            check("${a}${b}_${c}", underscore, prefix = "[proc,", suffix = "]", maxSequence = 1)
+                            check("${a}_${b}${c}", underscore, prefix = "[proc,", suffix = "]", maxSequence = 1)
+                            check("${a}_${b}_${c}", underscore, prefix = "[clientscript,", suffix = "]", maxSequence = 1)
+                            check("${a}${b}_${c}", underscore, prefix = "[clientscript,", suffix = "]", maxSequence = 1)
+                            check("${a}_${b}${c}", underscore, prefix = "[clientscript,", suffix = "]", maxSequence = 1)
+                        }
+                        check("${a}${b}${c}", underscore, prefix = "[proc,", suffix = "]", maxSequence = 1)
+                        check("${a}${b}${c}", underscore, prefix = "[clientscript,", suffix = "]", maxSequence = 1)
+                    }
+                }
+            }
+        }
+        writeAll()
     }
 
-    private fun separators(): Array<List<IntArray>> {
-        val nothing = -1
-        val underscore = '_'.code
-        val dot = '.'.code
-        val comma = ','.code
-        val space = ' '.code
-        return arrayOf(
-            listOf(
-                intArrayOf(nothing)
-            ),
-            listOf(
-                // 2
-                intArrayOf(nothing),
-                intArrayOf(underscore),
-                intArrayOf(dot),
-                intArrayOf(comma),
-                intArrayOf(space),
-            ),
-            listOf(
-                // 3
-                intArrayOf(nothing, nothing),
-                intArrayOf(nothing, underscore),
-                intArrayOf(underscore, nothing),
-                intArrayOf(underscore, underscore),
-                intArrayOf(nothing, dot),
-                intArrayOf(nothing, comma),
-                intArrayOf(underscore, comma),
-                intArrayOf(space, space),
-                intArrayOf(nothing, space),
-                intArrayOf(space, nothing),
-            ),
-            listOf(
-                // 4
-                intArrayOf(nothing, nothing, nothing),
-                intArrayOf(nothing, underscore, underscore),
-                intArrayOf(underscore, nothing, underscore),
-                intArrayOf(underscore, underscore, nothing),
-                intArrayOf(nothing, nothing, underscore),
-                intArrayOf(nothing, underscore, nothing),
-                intArrayOf(underscore, nothing, nothing),
-                intArrayOf(underscore, underscore, underscore),
-                intArrayOf(nothing, nothing, dot),
-                intArrayOf(nothing, nothing, comma),
-                intArrayOf(underscore, underscore, comma),
-                intArrayOf(underscore, nothing, comma),
-                intArrayOf(space, space, space),
-                intArrayOf(nothing, space, space),
-                intArrayOf(space, nothing, space),
-                intArrayOf(space, space, nothing),
-                intArrayOf(space, nothing, nothing),
-                intArrayOf(nothing, space, nothing),
-                intArrayOf(nothing, nothing, space),
-            ),
-            listOf(
-                // 5
-                intArrayOf(nothing, nothing, nothing, nothing),
-                intArrayOf(nothing, underscore, underscore, underscore),
-                intArrayOf(underscore, nothing, underscore, underscore),
-                intArrayOf(underscore, underscore, nothing, underscore),
-                intArrayOf(underscore, underscore, underscore, nothing),
-                intArrayOf(nothing, nothing, underscore, underscore),
-                intArrayOf(nothing, underscore, nothing, underscore),
-                intArrayOf(nothing, underscore, underscore, nothing),
-                intArrayOf(underscore, nothing, underscore, nothing),
-                intArrayOf(underscore, underscore, nothing, nothing),
-                intArrayOf(underscore, nothing, nothing, nothing),
-                intArrayOf(nothing, underscore, nothing, nothing),
-                intArrayOf(nothing, nothing, underscore, nothing),
-                intArrayOf(nothing, nothing, nothing, underscore),
-                intArrayOf(underscore, underscore, underscore, underscore),
-                intArrayOf(nothing, nothing, nothing, dot),
-                intArrayOf(nothing, nothing, nothing, comma),
-                intArrayOf(underscore, nothing, nothing, comma),
-                intArrayOf(nothing, underscore, nothing, comma),
-                intArrayOf(nothing, nothing, underscore, comma),
-                intArrayOf(underscore, underscore, nothing, comma),
-                intArrayOf(underscore, nothing, underscore, comma),
-                intArrayOf(nothing, underscore, underscore, comma),
-                intArrayOf(underscore, underscore, underscore, comma),
-                intArrayOf(space, space, space, space),
-                intArrayOf(nothing, space, space, space),
-                intArrayOf(space, nothing, space, space),
-                intArrayOf(space, space, nothing, space),
-                intArrayOf(space, space, space, nothing),
-                intArrayOf(nothing, nothing, space, space),
-                intArrayOf(nothing, space, nothing, space),
-                intArrayOf(nothing, space, space, nothing),
-                intArrayOf(space, nothing, space, nothing),
-                intArrayOf(space, space, nothing, nothing),
-                intArrayOf(space, nothing, nothing, nothing),
-                intArrayOf(nothing, space, nothing, nothing),
-                intArrayOf(nothing, nothing, space, nothing),
-                intArrayOf(nothing, nothing, nothing, space),
-                intArrayOf(nothing, nothing, nothing, nothing),
-            ),
-            listOf(
-                // 6
-                intArrayOf(nothing, nothing, nothing, nothing, nothing),
-                intArrayOf(underscore, underscore, underscore, underscore, underscore),
-                intArrayOf(space, space, space, space, space),
-            )
-        )
+    private fun findSprites(keywords: Set<String>, depth: Int) {
+        val keywords = keywords.filter { !it.contains(" ") }
+        val comma = listOf(',')
+        if (depth == 1) {
+            println("Searching individual keywords")
+            for (a in keywords) {
+                check(a, comma, maxSequence = 1)
+            }
+        }
+        if (depth == 2) {
+            println("Searching double keywords")
+            for (a in keywords) {
+                for (b in keywords) {
+                    check("${a}_${b}", comma, maxSequence = 1)
+                    check("${a}${b}", comma, maxSequence = 1)
+                }
+            }
+        }
+
+        if (depth == 3) {
+            println("Searching triple keywords")
+            for (a in keywords) {
+                for (b in keywords) {
+                    for (c in keywords) {
+                        check("${a}_${b}_${c}", comma, maxSequence = 1)
+                        check("${a}${b}_${c}", comma, maxSequence = 1)
+                        check("${a}_${b}${c}", comma, maxSequence = 1)
+                        check("${a}${b}${c}", comma, maxSequence = 1)
+                    }
+                }
+            }
+        }
+        writeAll()
+    }
+
+    private fun writeAll() {
+        output.writeText(matches.toList().joinToString("\n") { "${it.first} -\t${it.second.joinToString("\t")}" })
     }
 
     private fun dumpHashes() {
