3336f5d6d6cea73a38ee7c472e317e8a7909d17c
GregHib <greghib@users.noreply.github.com>
2024-01-11 22:41:48 +0000

    Fix equipment stats attack rate and examines closes #154

--- game/src/main/kotlin/world/gregs/voidps/world/map/Examines.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/map/Examines.kts
@@ -1,12 +1,17 @@
 package world.gregs.voidps.world.map
 
 import world.gregs.voidps.engine.client.message
+import world.gregs.voidps.engine.client.ui.InterfaceOption
 import world.gregs.voidps.engine.entity.character.npc.NPCOption
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.chat.ChatType
 import world.gregs.voidps.engine.entity.obj.ObjectOption
 import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.world.interact.entity.player.equip.InventoryOption
+
+on<InterfaceOption>({ id == "equipment_bonuses" && option == "Examine" }) { player: Player ->
+    player.message(item.def.getOrNull("examine") ?: return@on, ChatType.ItemExamine)
+}
 
 on<InventoryOption>({ option == "Examine" }) { player: Player ->
     player.message(item.def.getOrNull("examine") ?: return@on, ChatType.ItemExamine)
