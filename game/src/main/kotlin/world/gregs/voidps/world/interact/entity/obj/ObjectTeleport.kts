7288fc00b42f403b7dbf9830fdc609064e4dd04f
GregHib <greghib@users.noreply.github.com>
2024-02-08 22:15:14 +0000

    Fix

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/obj/ObjectTeleport.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/obj/ObjectTeleport.kts
@@ -2,18 +2,17 @@ package world.gregs.voidps.world.interact.entity.obj
 
 import world.gregs.voidps.engine.client.variable.start
 import world.gregs.voidps.engine.entity.character.player.Player
-import world.gregs.voidps.engine.entity.obj.objectOperate
+import world.gregs.voidps.engine.entity.obj.ObjectOption
 import world.gregs.voidps.engine.event.Priority
+import world.gregs.voidps.engine.event.on
 import world.gregs.voidps.engine.inject
 import world.gregs.voidps.engine.suspend.arriveDelay
 
 val teleports: Teleports by inject()
 
-objectOperate("*") {
-    if (teleports.contains(def.stringId.ifEmpty { def.id.toString() }, target.tile, option)) {
-        arriveDelay()
-        teleports.teleport(this)
-    }
+on<ObjectOption>({ operate && teleports.contains(def.stringId.ifEmpty { def.id.toString() }, target.tile, option) }) { _: Player ->
+    arriveDelay()
+    teleports.teleport(this)
 }
 
 teleport({ takeoff && delay != null }, Priority.LOWEST) { player: Player ->
