e37b3edb97f76156c5b1303dc3ca43fc58207d7b
GregHib <greghib@users.noreply.github.com>
2024-02-10 14:35:40 +0000

    Version 1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     group = "world.gregs.void"
-    version = "1.1.0"
+    version = "1.1.1"
 
     java.sourceCompatibility = JavaVersion.VERSION_19
     java.targetCompatibility = java.sourceCompatibility
