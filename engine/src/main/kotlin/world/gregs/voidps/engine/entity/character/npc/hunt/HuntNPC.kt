1ec7e9f3ec455e62c6430fbfed9947cb0eda1f0f
GregHib <greghib@users.noreply.github.com>
2024-02-21 17:28:42 +0000

    Add hunt mode helper functions closes #452

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/npc/hunt/HuntNPC.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/npc/hunt/HuntNPC.kt
@@ -2,8 +2,20 @@ package world.gregs.voidps.engine.entity.character.npc.hunt
 
 import world.gregs.voidps.engine.entity.character.npc.NPC
 import world.gregs.voidps.engine.event.Event
+import world.gregs.voidps.engine.event.on
+import world.gregs.voidps.engine.event.wildcardEquals
 
 data class HuntNPC(
     val mode: String,
     val target: NPC
 ) : Event
+
+fun huntNPC(npc: String = "*", targetNpc: String = "*", mode: String = "*", block: suspend HuntNPC.(npc: NPC) -> Unit) {
+    on<HuntNPC>({ wildcardEquals(npc, it.id) && wildcardEquals(targetNpc, target.id) && wildcardEquals(mode, this.mode) }, block = block)
+}
+
+fun huntNPC(vararg modes: String, block: suspend HuntNPC.(npc: NPC) -> Unit) {
+    for(mode in modes) {
+        on<HuntNPC>({ wildcardEquals(mode, this.mode) }, block = block)
+    }
+}
