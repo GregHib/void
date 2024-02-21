3f01f1125005b517fe36bd5c2adaa266e5a1078f
GregHib <greghib@users.noreply.github.com>
2024-02-21 18:48:57 +0000

    Add trapped npc re-spawning closes #73

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/mode/Wander.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/mode/Wander.kt
@@ -1,16 +1,31 @@
 package world.gregs.voidps.engine.entity.character.mode
 
 import world.gregs.voidps.engine.entity.character.mode.move.Movement
+import world.gregs.voidps.engine.entity.character.move.previousTile
+import world.gregs.voidps.engine.entity.character.move.tele
 import world.gregs.voidps.engine.entity.character.npc.NPC
 import world.gregs.voidps.type.Tile
 import world.gregs.voidps.type.random
 
+/**
+ * Random walking
+ * NPCs that don't move for more than [stuckLimit] ticks are teleported back to [spawn]
+ */
 class Wander(
     private val npc: NPC,
-    private val spawn: Tile = npc["spawn_tile"]!!
+    private val spawn: Tile = npc["spawn_tile"]!!,
+    private val stuckLimit: Int = npc.def["stuck_limit", 500]
 ) : Movement(npc) {
 
+    private var stuckCounter = 0
+
     override fun tick() {
+        if (npc.tile != npc.previousTile) {
+            stuckCounter = 0
+        } else if (stuckCounter++ >= stuckLimit) {
+            npc.tele(spawn)
+            stuckCounter = 0
+        }
         if (random.nextInt(8) != 0) {
             super.tick()
             return
