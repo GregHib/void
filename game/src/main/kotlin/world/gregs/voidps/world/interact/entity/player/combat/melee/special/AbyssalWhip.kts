05c89f08ea2a1fdc2d65f6255f97852eca8b442a
GregHib <greghib@users.noreply.github.com>
2024-02-24 21:42:13 +0000

    Fix abyssal whip special attack

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/special/AbyssalWhip.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/melee/special/AbyssalWhip.kts
@@ -8,19 +8,20 @@ import world.gregs.voidps.engine.event.Priority
 import world.gregs.voidps.world.interact.entity.combat.attackType
 import world.gregs.voidps.world.interact.entity.combat.hit.block
 import world.gregs.voidps.world.interact.entity.combat.hit.hit
+import world.gregs.voidps.world.interact.entity.combat.specialAttackSwing
 import world.gregs.voidps.world.interact.entity.combat.weaponSwing
 import world.gregs.voidps.world.interact.entity.player.combat.special.MAX_SPECIAL_ATTACK
 import world.gregs.voidps.world.interact.entity.player.combat.special.drainSpecialEnergy
 import world.gregs.voidps.world.interact.entity.player.combat.special.specialAttack
 import world.gregs.voidps.world.interact.entity.player.energy.runEnergy
 
-weaponSwing("abyssal_whip*", Priority.LOW) { player ->
-    if (player.specialAttack && !drainSpecialEnergy(player, MAX_SPECIAL_ATTACK / 2)) {
+specialAttackSwing("abyssal_whip*", priority = Priority.LOW) { player ->
+    if (!drainSpecialEnergy(player, MAX_SPECIAL_ATTACK / 2)) {
         delay = -1
-        return@weaponSwing
+        return@specialAttackSwing
     }
-    player.setAnimation("whip_${if (player.specialAttack) "special" else player.attackType}")
-    if (player.hit(target) != -1 && player.specialAttack) {
+    player.setAnimation("whip_special")
+    if (player.hit(target) != -1) {
         if (target is Player) {
             val tenPercent = (target.runEnergy / 100) * 10
             if (tenPercent > 0) {
