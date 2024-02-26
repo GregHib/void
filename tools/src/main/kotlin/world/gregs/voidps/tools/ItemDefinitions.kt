1ca58b2e9c234e7982403b19876a5a6b24ee2d17
GregHib <greghib@users.noreply.github.com>
2024-02-26 17:43:32 +0000

    Experimental Trie lookup

--- tools/src/main/kotlin/world/gregs/voidps/tools/ItemDefinitions.kt
+++ tools/src/main/kotlin/world/gregs/voidps/tools/ItemDefinitions.kt
@@ -7,7 +7,10 @@ import world.gregs.voidps.engine.data.definition.AmmoDefinitions
 import world.gregs.voidps.engine.data.definition.CategoryDefinitions
 import world.gregs.voidps.engine.data.definition.ItemDefinitions
 import world.gregs.voidps.engine.data.definition.ParameterDefinitions
+import world.gregs.voidps.engine.event.CharTrieWildcardSearch
+import world.gregs.voidps.engine.event.wildcardEquals
 import world.gregs.yaml.Yaml
+import kotlin.system.measureTimeMillis
 
 object ItemDefinitions {
 
@@ -19,11 +22,39 @@ object ItemDefinitions {
         val ammo = AmmoDefinitions().load(yaml, property("ammoDefinitionsPath"))
         val parameters = ParameterDefinitions(categories, ammo).load(yaml, property("parameterDefinitionsPath"))
         val decoder = ItemDefinitions(ItemDecoder(parameters).load(cache)).load(yaml, property("itemDefinitionsPath"))
-        for (i in decoder.definitions.indices) {
-            val def = decoder.getOrNull(i) ?: continue
-            if (def.contains("extra_equipment_option")) {
-                println(def)
-            }
+
+        val root = CharTrieWildcardSearch()
+        for (key in decoder.ids.keys) {
+//            if (!key.endsWith("noted")) {
+                root.insert(key)
+//            }
         }
+        println(decoder.ids.size)
+        val wildcards = listOf(
+            "*_tiara", "castle_wars_brace*", "void_*", "elite_void_*", "ahrims_*", "dharoks_*", "guthans_*",
+            "karils_*",
+            "torags_*",
+            "veracs_*",
+            "avas_*")
+        println("Trie: ${
+            measureTimeMillis {
+                for (wildcard in wildcards) {
+                    println(root.search(wildcard))
+                }
+            }
+        }")
+        println("List: ${
+            measureTimeMillis {
+                for (wildcard in wildcards) {
+                    println(decoder.ids.keys.filter { wildcardEquals(wildcard, it) })
+                }
+            }
+        }")
+//        for (i in decoder.definitions.indices) {
+//            val def = decoder.getOrNull(i) ?: continue
+//            if (def.contains("extra_equipment_option")) {
+//                println(def)
+//            }
+//        }
     }
 }
