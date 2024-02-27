345da1e45373ae595d8a034feb368cdf2860e409
GregHib <greghib@users.noreply.github.com>
2024-02-27 19:15:01 +0000

    Fix combat bots

--- game/src/main/kotlin/world/gregs/voidps/bot/skill/combat/CombatGear.kt
+++ game/src/main/kotlin/world/gregs/voidps/bot/skill/combat/CombatGear.kt
@@ -14,6 +14,7 @@ import world.gregs.voidps.engine.entity.character.player.skill.level.Level.hasRe
 import world.gregs.voidps.engine.entity.character.player.skill.level.Level.hasRequirementsToUse
 import world.gregs.voidps.engine.entity.item.Item
 import world.gregs.voidps.engine.get
+import world.gregs.voidps.engine.inv.clear
 import world.gregs.voidps.engine.inv.inventory
 import world.gregs.voidps.world.activity.bank.bank
 import world.gregs.voidps.world.activity.bank.ownsItem
@@ -28,7 +29,10 @@ suspend fun Bot.setupGear(gear: GearDefinition, buy: Boolean = true) {
 }
 
 suspend fun Bot.setupGear(skill: Skill, buy: Boolean = true) {
-    val gear = getGear(skill) ?: return
+    val gear = getGear(skill) ?: run {
+        player.inventory.clear()
+        throw CancellationException("No gear setup found $skill.")
+    }
     setupGear(gear, buy)
 }
 
