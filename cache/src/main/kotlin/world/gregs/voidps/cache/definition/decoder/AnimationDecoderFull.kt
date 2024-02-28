6ce40f837752ee70fca3ba886be044551337247c
GregHib <greghib@users.noreply.github.com>
2024-02-28 16:19:44 +0000

    Fix animation size

--- cache/src/main/kotlin/world/gregs/voidps/cache/definition/decoder/AnimationDecoderFull.kt
+++ cache/src/main/kotlin/world/gregs/voidps/cache/definition/decoder/AnimationDecoderFull.kt
@@ -1,6 +1,7 @@
 package world.gregs.voidps.cache.definition.decoder
 
 import world.gregs.voidps.buffer.read.Reader
+import world.gregs.voidps.cache.Cache
 import world.gregs.voidps.cache.DefinitionDecoder
 import world.gregs.voidps.cache.Index.ANIMATIONS
 import world.gregs.voidps.cache.definition.data.AnimationDefinitionFull
@@ -12,6 +13,10 @@ class AnimationDecoderFull : DefinitionDecoder<AnimationDefinitionFull>(ANIMATIO
     override fun getFile(id: Int) = id and 0x7f
 
     override fun getArchive(id: Int) = id ushr 7
+
+    override fun size(cache: Cache): Int {
+        return cache.lastArchiveId(index) * 128 + (cache.fileCount(index, cache.lastArchiveId(index)))
+    }
 
     override fun AnimationDefinitionFull.read(opcode: Int, buffer: Reader) {
         when (opcode) {
