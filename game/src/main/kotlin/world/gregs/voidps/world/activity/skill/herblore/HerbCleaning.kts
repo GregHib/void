8066824b7ef5b9996e457ce8e7723a25c1bf8a6d
Syntax <n195674fghj@gmail.com>
2024-03-15 15:20:19 +0000

    * Remove unused imports.

--- game/src/main/kotlin/world/gregs/voidps/world/activity/skill/herblore/HerbCleaning.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/skill/herblore/HerbCleaning.kts
@@ -1,17 +1,10 @@
 package world.gregs.voidps.world.activity.skill.herblore
 
-import net.pearx.kasechange.toSentenceCase
-import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.data.definition.data.Cleaning
-import world.gregs.voidps.engine.entity.character.player.Player
-import world.gregs.voidps.engine.entity.character.player.chat.ChatType
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.player.skill.level.Level.has
-import world.gregs.voidps.engine.entity.item.Item
 import world.gregs.voidps.engine.inv.inventory
 import world.gregs.voidps.engine.inv.replace
-import world.gregs.voidps.engine.queue.weakQueue
-import world.gregs.voidps.world.interact.dialogue.type.makeAmount
 import world.gregs.voidps.world.interact.entity.player.equip.inventoryOption
 
 inventoryOption("Clean", "inventory") {
