9800432d911e90b7a1f05b164a503b732db5d79e
GregHib <greghib@users.noreply.github.com>
2024-03-17 17:04:04 +0000

    Disable game module tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ allprojects {
         }
     }
 
-    if (name != "tools") {
+    if (name != "tools" && name != "game") {
         tasks.test {
             maxHeapSize = "4096m"
             useJUnitPlatform()
@@ -86,7 +86,7 @@ reporting {
 }
 
 dependencies {
-    allprojects.filter { it.name != "tools" }.forEach {
+    allprojects.filter { it.name != "tools" && it.name != "ame" }.forEach {
         jacocoAggregation(it)
     }
 }
