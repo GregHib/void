1c86123edfab146680b681bbe748f69032262e64
GregHib <greghib@users.noreply.github.com>
2024-03-18 11:39:04 +0000

    Set correct flinching retaliation delay #490

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/combat/Retaliation.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/combat/Retaliation.kts
@@ -1,5 +1,7 @@
 package world.gregs.voidps.world.interact.entity.combat
 
+import world.gregs.voidps.engine.GameLoop
+import world.gregs.voidps.engine.client.variable.hasClock
 import world.gregs.voidps.engine.client.variable.start
 import world.gregs.voidps.engine.entity.character.Character
 import world.gregs.voidps.engine.entity.character.mode.combat.CombatMovement
@@ -18,13 +20,19 @@ characterCombatHit { character ->
     if (source == character || !character.retaliates) {
         return@characterCombatHit
     }
+    if (type == "poison" || type == "disease" || type == "healed") {
+        return@characterCombatHit
+    }
     if (character.levels.get(Skill.Constitution) <= 0 || character.inCombat && character.target == source) {
         return@characterCombatHit
     }
-    if (character.mode !is CombatMovement) {
-        val target = source
-        character.mode = CombatMovement(character, target)
-        character.target = target
-        character.start("hit_delay", 1)
+    if (character.mode is CombatMovement) {
+        return@characterCombatHit
+    }
+    val target = source
+    character.mode = CombatMovement(character, target)
+    character.target = target
+    if (character.hasClock("hit_delay", if (character is NPC) GameLoop.tick - 8 else GameLoop.tick)) {
+        character.start("hit_delay", character.attackSpeed / 2)
     }
 }
