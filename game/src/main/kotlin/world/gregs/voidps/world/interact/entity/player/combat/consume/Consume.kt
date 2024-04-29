0f910e298e8fbde7448bb775ffe5dfe5360477da
GregHib <greghib@users.noreply.github.com>
2024-04-29 21:51:58 +0100

    Remove unused Consume parameter

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/consume/Consume.kt
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/consume/Consume.kt
@@ -7,18 +7,17 @@ import world.gregs.voidps.engine.event.EventDispatcher
 import world.gregs.voidps.engine.event.Events
 
 data class Consume(val item: Item, val slot: Int) : CancellableEvent() {
-    override val size = 3
+    override val size = 2
 
     override fun parameter(dispatcher: EventDispatcher, index: Int) = when (index) {
         0 -> "consume"
         1 -> item.id
-        2 -> slot
         else -> null
     }
 }
 
-fun consume(vararg items: String = arrayOf("*"), slot: Int? = null, handler: Consume.(Player) -> Unit) {
+fun consume(vararg items: String = arrayOf("*"), handler: Consume.(Player) -> Unit) {
     for (item in items) {
-        Events.handle("consume", item, slot ?: "*", handler = handler)
+        Events.handle("consume", item, handler = handler)
     }
 }
