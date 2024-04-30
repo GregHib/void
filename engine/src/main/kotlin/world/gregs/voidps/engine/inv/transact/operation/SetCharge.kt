3640880ef93b674f57522b19f46d1c3c321cde96
GregHib <greghib@users.noreply.github.com>
2024-04-30 10:45:56 +0100

    Add transaction set charge operator

---
+++ engine/src/main/kotlin/world/gregs/voidps/engine/inv/transact/operation/SetCharge.kt
@@ -0,0 +1,40 @@
+package world.gregs.voidps.engine.inv.transact.operation
+
+import world.gregs.voidps.engine.inv.transact.TransactionError
+
+/**
+ * Transaction operation for setting charges of an item in an inventory.
+ * Chargeable items charges are set.
+ * Non-chargeable items are ignored.
+ */
+object SetCharge {
+
+    /**
+     * Set the charges of the item at the specified index by the given amount.
+     * @param index the index of the item charge to be set.
+     * @param amount the number of charges to be added.
+     */
+    fun TransactionOperation.setCharge(index: Int, amount: Int) {
+        if (failed) {
+            return
+        }
+        val item = inventory[index]
+        if (item.isEmpty() || amount <= 0 || inventory.stackable(item.id)) {
+            error = TransactionError.Invalid
+            return
+        }
+        val maximum: Int? = item.def.getOrNull("charges_max") ?: item.def.getOrNull("charges")
+        if (maximum == null) {
+            error = TransactionError.Invalid
+            return
+        }
+        // Check if amount exceeds the maximum value
+        if (amount.toLong() > maximum) {
+            error = TransactionError.Full(amount - (amount - maximum))
+            return
+        }
+        // Update the item in the inventory
+        set(index, item.copy(amount = amount))
+    }
+
+}
--- engine/src/test/kotlin/world/gregs/voidps/engine/inv/transact/operation/SetChargeTest.kt
+++ engine/src/test/kotlin/world/gregs/voidps/engine/inv/transact/operation/SetChargeTest.kt
@@ -8,110 +8,93 @@ import world.gregs.voidps.engine.entity.item.Item
 import world.gregs.voidps.engine.inv.stack.AlwaysStack
 import world.gregs.voidps.engine.inv.stack.NeverStack
 import world.gregs.voidps.engine.inv.transact.TransactionError
-import world.gregs.voidps.engine.inv.transact.operation.AddCharge.charge
+import world.gregs.voidps.engine.inv.transact.operation.SetCharge.setCharge
 
-internal class AddChargeTest : TransactionOperationTest() {
+internal class SetChargeTest : TransactionOperationTest() {
 
     @Test
-    fun `Add charge after the transaction has failed`() {
+    fun `Set charge after the transaction has failed`() {
         transaction(stackRule = NeverStack)
         every { itemDefinitions.getOrNull("item") } returns ItemDefinition(extras = mapOf("charges" to 10))
         // Set the transaction to failed
         transaction.set(0, Item("item", 0))
         transaction.error = TransactionError.Invalid
-        transaction.charge(0, 1)
-        // Assert that the charge was not removed from the inventory
+        transaction.setCharge(0, 10)
+        // Assert that the charge was not set
         assertEquals(0, inventory[0].value)
     }
 
     @Test
-    fun `Add invalid index to inventory`() {
+    fun `Set invalid index`() {
         transaction(stackRule = NeverStack)
-        transaction.charge(-1, 0)
+        transaction.setCharge(-1, 0)
         assertFalse(transaction.commit())
         assertEquals(TransactionError.Invalid, transaction.error)
         assertTrue(inventory.isEmpty())
     }
 
     @Test
-    fun `Add charge to empty slot`() {
+    fun `Set charge for empty slot`() {
         transaction(stackRule = NeverStack)
-        transaction.charge(0, 1)
+        transaction.setCharge(0, 1)
         assertFalse(transaction.commit())
         assertEquals(TransactionError.Invalid, transaction.error)
         assertTrue(inventory.isEmpty())
     }
 
     @Test
-    fun `Add invalid charge to item`() {
+    fun `Set invalid charge to item`() {
         transaction(stackRule = NeverStack)
         transaction.set(0, Item("item", 1))
-        transaction.charge(0, -1)
+        transaction.setCharge(0, -1)
         assertFalse(transaction.commit())
         assertEquals(TransactionError.Invalid, transaction.error)
         assertTrue(inventory.isEmpty())
     }
 
     @Test
-    fun `Can't charge stackable items`() {
+    fun `Can't set charge of stackable items`() {
         transaction(stackRule = AlwaysStack)
         every { itemDefinitions.getOrNull("item") } returns ItemDefinition(extras = mapOf("charges" to 50))
         transaction.set(0, Item("item", 10))
-        transaction.charge(0, 1)
+        transaction.setCharge(0, 1)
         assertFalse(transaction.commit())
         assertEquals(TransactionError.Invalid, transaction.error)
         assertTrue(inventory.isEmpty())
     }
 
     @Test
-    fun `Add multiple charges to existing charge`() {
+    fun `Set charges overrides existing charge`() {
         transaction(stackRule = NeverStack)
         every { itemDefinitions.getOrNull("item") } returns ItemDefinition(extras = mapOf("charges" to 10))
         val id = "item"
         val initialAmount = 5
-        val amountToAdd = 3
+        val amountToSet = 3
         transaction.set(0, Item(id, initialAmount))
-        transaction.charge(0, amountToAdd)
+        transaction.setCharge(0, amountToSet)
 
         assertTrue(transaction.commit())
-        assertEquals(initialAmount + amountToAdd, inventory[0].value)
+        assertEquals(amountToSet, inventory[0].value)
     }
 
     @Test
     fun `Can't charges over charge limit`() {
         transaction(stackRule = NeverStack)
-        val chargeMax = 10
-        every { itemDefinitions.getOrNull("item") } returns ItemDefinition(extras = mapOf("charges" to chargeMax))
-        val id = "item"
-        val initialAmount = 5
-        val amountToAdd = 6
-        transaction.set(0, Item(id, initialAmount))
-        transaction.charge(0, amountToAdd)
-
-        assertFalse(transaction.commit())
-        assertEquals(TransactionError.Full(5), transaction.error)
-    }
-
-    @Test
-    fun `Can't charges over integer limit`() {
-        transaction(stackRule = NeverStack)
-        every { itemDefinitions.getOrNull("item") } returns ItemDefinition(extras = mapOf("charges" to Int.MAX_VALUE))
-        val id = "item"
-        val amountToAdd = 15
-        transaction.set(0, Item(id, Int.MAX_VALUE - 10))
-        transaction.charge(0, amountToAdd)
-
+        every { itemDefinitions.getOrNull("item") } returns ItemDefinition(extras = mapOf("charges" to 10))
+        transaction.set(0, Item("item", 1))
+        transaction.setCharge(0, 11)
         assertFalse(transaction.commit())
         assertEquals(TransactionError.Full(10), transaction.error)
+        assertTrue(inventory.isEmpty())
     }
 
     @Test
-    fun `Add charges to not chargeable item`() {
+    fun `Can't set charges for non-chargeable item`() {
         transaction(stackRule = NeverStack)
         val id = "not_chargeable"
         val charge = 5
         transaction.set(0, Item(id, 0))
-        transaction.charge(0, charge)
+        transaction.setCharge(0, charge)
 
         assertFalse(transaction.commit())
         assertEquals(0, inventory[0].value)
