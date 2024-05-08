bf77b4ee68d9f9552f745645e5cb689be4166004
Syntax <79747812+Zag-bop@users.noreply.github.com>
2024-05-08 15:11:15 +0100

    * Change name of an area from `main_wilderness_multi_area` to `wilderness_main_multi_area` to keep the same format as other areas like it. * Add `enterArea` to `Overload.kts` to handle any scenarios where a player would enter the wilderness with any overload effect active * Add check to stop players from using an overload potion while they're in the wilderness * Remove wilderness check to apply a reduced buff while drinking an overload

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/consume/drink/Overload.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/combat/consume/drink/Overload.kts
@@ -1,7 +1,9 @@
 package world.gregs.voidps.world.interact.entity.player.combat.consume.drink
 
 import world.gregs.voidps.engine.client.message
+import world.gregs.voidps.engine.entity.character.mode.move.enterArea
 import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.player.chat.ChatType
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.setAnimation
 import world.gregs.voidps.engine.entity.character.setGraphic
@@ -15,12 +17,28 @@ import world.gregs.voidps.world.interact.entity.combat.inWilderness
 import world.gregs.voidps.world.interact.entity.player.combat.consume.canConsume
 
 canConsume("overload*") { player ->
+    if (player.inWilderness) {
+        player.message("You cannot drink an overload potion while you're in the wilderness.", ChatType.Game)
+        cancel()
+        return@canConsume
+    }
+
     if (player.timers.contains("overload")) {
         player.message("You may only use this potion every five minutes.")
         cancel()
+        return@canConsume
     } else if (player.levels.get(Skill.Constitution) < 500) {
         player.message("You need more than 500 life points to survive the power of overload.")
         cancel()
+        return@canConsume
+    }
+}
+
+enterArea("wilderness*") {
+    if (player.timers.contains("overload")) {
+        player.timers.stop("overload")
+        player["overload_refreshes_remaining"] = 0
+        removeBoost(player)
     }
 }
 
@@ -59,19 +77,11 @@ timerStop("overload") { player ->
 }
 
 fun applyBoost(player: Player) {
-    if (player.inWilderness) {
-        player.levels.boost(Skill.Attack, 5, 0.15)
-        player.levels.boost(Skill.Strength, 5, 0.15)
-        player.levels.boost(Skill.Defence, 5, 0.15)
-        player.levels.boost(Skill.Magic, 5, 0.15)
-        player.levels.boost(Skill.Ranged, 5, 0.15)
-    } else {
-        player.levels.boost(Skill.Attack, 5, 0.22)
-        player.levels.boost(Skill.Strength, 5, 0.22)
-        player.levels.boost(Skill.Defence, 5, 0.22)
-        player.levels.boost(Skill.Magic, 7)
-        player.levels.boost(Skill.Ranged, 4, 0.1923)
-    }
+    player.levels.boost(Skill.Attack, 5, 0.22)
+    player.levels.boost(Skill.Strength, 5, 0.22)
+    player.levels.boost(Skill.Defence, 5, 0.22)
+    player.levels.boost(Skill.Magic, 7)
+    player.levels.boost(Skill.Ranged, 4, 0.1923)
 }
 
 fun removeBoost(player: Player) {
