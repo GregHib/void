7f1ba60e04818b72b0983d4fb2d77da682d5a99f
GregHib <greghib@users.noreply.github.com>
2024-05-22 19:53:16 +0100

    Add manual server side fix for #540

--- cache/src/main/kotlin/world/gregs/voidps/cache/config/decoder/StructDecoder.kt
+++ cache/src/main/kotlin/world/gregs/voidps/cache/config/decoder/StructDecoder.kt
@@ -1,6 +1,7 @@
 package world.gregs.voidps.cache.config.decoder
 
 import world.gregs.voidps.buffer.read.Reader
+import world.gregs.voidps.cache.Cache
 import world.gregs.voidps.cache.Config.STRUCTS
 import world.gregs.voidps.cache.config.ConfigDecoder
 import world.gregs.voidps.cache.config.data.StructDefinition
@@ -11,6 +12,18 @@ class StructDecoder(
 ) : ConfigDecoder<StructDefinition>(STRUCTS) {
 
     override fun create(size: Int) = Array(size) { StructDefinition(it) }
+
+
+    override fun load(cache: Cache): Array<StructDefinition> {
+        val definitions = super.load(cache)
+        // Manually fix values see https://github.com/GregHib/void/issues/540
+        val extras = definitions[1330].extras as MutableMap<String, Any>
+        extras["1296"] = 3
+        extras["1297"] = 19
+        extras["1298"] = 2
+        extras["1299"] = 37
+        return definitions
+    }
 
     override fun StructDefinition.read(opcode: Int, buffer: Reader) {
         if (opcode == 249) {
