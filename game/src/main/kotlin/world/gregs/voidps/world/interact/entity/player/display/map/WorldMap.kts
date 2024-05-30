00aee9ffd5ee4ce49ddf6b1619a5826b52251151
GregHib <greghib@users.noreply.github.com>
2024-05-30 19:06:50 +0100

    Add world map functionality

--- game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/display/map/WorldMap.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/interact/entity/player/display/map/WorldMap.kts
@@ -2,6 +2,7 @@ package world.gregs.voidps.world.interact.entity.player.display.map
 
 import world.gregs.voidps.engine.client.ui.event.interfaceOpen
 import world.gregs.voidps.engine.client.ui.interfaceOption
+import world.gregs.voidps.engine.client.ui.interfaceSlot
 import world.gregs.voidps.engine.client.ui.open
 import world.gregs.voidps.engine.data.definition.InterfaceDefinitions
 import world.gregs.voidps.engine.entity.character.mode.move.move
@@ -13,6 +14,45 @@ val definitions: InterfaceDefinitions by inject()
 
 interfaceOpen("world_map") { player ->
     updateMap(player)
+    player.sendVariable("world_map_hide_player_location")
+    player.sendVariable("world_map_hide_links")
+    player.sendVariable("world_map_hide_labels")
+    player.sendVariable("world_map_hide_tooltips")
+    player.sendVariable("world_map_marker_custom")
+    player.interfaceOptions.unlockAll("world_map", "key_list", 0..182)
+}
+
+interfaceOption("Re-sort key", "order", "world_map") {
+    player["world_map_list_order"] = when (player["world_map_list_order", "categorised"]) {
+        "categorised" -> "traditional"
+        "traditional" -> "alphabetical"
+        "alphabetical" -> "categorised"
+        else -> "categorised"
+    }
+}
+
+interfaceSlot("key_list", "world_map", 1) {
+    player.toggle("world_map_hide_player_location")
+}
+
+interfaceSlot("key_list", "world_map", 4) {
+    player.toggle("world_map_hide_links")
+}
+
+interfaceSlot("key_list", "world_map", 12) {
+    player.toggle("world_map_hide_labels")
+}
+
+interfaceSlot("key_list", "world_map", 16) {
+    player.toggle("world_map_hide_tooltips")
+}
+
+interfaceSlot("key_list", "world_map", 19) {
+    player["world_map_marker_custom"] = 0
+}
+
+interfaceOption("Clear marker", "marker", "world_map") {
+    player["world_map_marker_custom"] = 0
 }
 
 interfaceOption(component = "world_map", id = "toplevel*") {
@@ -32,5 +72,5 @@ move({ it.interfaces.contains("world_map") }) { player ->
 fun updateMap(player: Player) {
     val tile = player.tile.id
     player["world_map_centre"] = tile
-    player["world_map_player"] = tile
+    player["world_map_marker_player"] = tile
 }
---
+++ network/src/main/kotlin/world/gregs/voidps/network/client/instruction/WorldMapClick.kt
@@ -0,0 +1,5 @@
+package world.gregs.voidps.network.client.instruction
+
+import world.gregs.voidps.network.client.Instruction
+
+data class WorldMapClick(val tile: Int) : Instruction
