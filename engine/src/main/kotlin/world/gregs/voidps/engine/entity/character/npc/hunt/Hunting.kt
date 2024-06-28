5f7a2a0b4189d922c71816870c0ac9494c7f6b1b
jarryd229 <15334102+jarryd229@users.noreply.github.com>
2024-06-29 00:21:24 +1000

    Added wolves (#545)
    * added wolves
    
    * Update data/definitions/npcs.yml
    
    * Update data/spawns/npc-spawns.yml
    
    * Update data/spawns/npc-spawns.yml
    
    ---------
    
    Co-authored-by: Greg <GregHib@users.noreply.github.com>

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/npc/hunt/Hunting.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/npc/hunt/Hunting.kt
@@ -49,7 +49,7 @@ class Hunting(
     override fun run() {
         for (npc in npcs) {
             val mode: String = npc["hunt_mode"] ?: npc.def.getOrNull("hunt_mode") ?: continue
-            if (npc.hasClock("delay") || npc.dec("hunt_count_down") >= 0) {
+            if (mode == "null" || npc.hasClock("delay") || npc.dec("hunt_count_down") >= 0) {
                 continue
             }
             val range = npc.def["hunt_range", 5]
