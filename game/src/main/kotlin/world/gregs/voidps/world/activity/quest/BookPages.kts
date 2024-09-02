113ac47c5bdda8c5c0cd427ce85ab79b50eef096
GregHib <greghib@users.noreply.github.com>
2024-09-02 19:24:22 +0100

    Add support for long page books with `long: true`

--- game/src/main/kotlin/world/gregs/voidps/world/activity/quest/BookPages.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/quest/BookPages.kts
@@ -14,8 +14,8 @@ inventoryOption("Read") {
     player.openBook(item.def.getOrNull("book") ?: return@inventoryOption)
 }
 
-interfaceRefresh("book") { player ->
-    refreshBook(player)
+interfaceRefresh("book", "book_long") { player ->
+    refreshBook(player, id)
 }
 
 continueDialogue("book", "turn_page_right") { player ->
@@ -30,19 +30,31 @@ continueDialogue("book", "turn_page_left") { player ->
     player.open("book")
 }
 
-fun refreshBook(player: Player) {
+continueDialogue("book_long", "turn_page_right") { player ->
+    player.inc("book_page")
+    player.close("book_long")
+    player.open("book_long")
+}
+
+continueDialogue("book_long", "turn_page_left") { player ->
+    player.dec("book_page")
+    player.close("book_long")
+    player.open("book_long")
+}
+
+fun refreshBook(player: Player, book: String) {
     val name: String = player["book"] ?: return
     val pageNumber: Int = player["book_page"] ?: return
     player.interfaces.apply {
-        sendText("book", "title", books.title(name))
-        sendText("book", "page_number_left", (pageNumber + 1).toString())
-        sendText("book", "page_number_right", (pageNumber + 2).toString())
+        sendText(book, "title", books.title(name))
+        sendText(book, "page_number_left", (pageNumber + 1).toString())
+        sendText(book, "page_number_right", (pageNumber + 2).toString())
         val pages = books.get(name)
-        sendVisibility("book", "turn_page_left", pageNumber > 0)
-        sendVisibility("book", "turn_page_right", pageNumber < pages.lastIndex)
+        sendVisibility(book, "turn_page_left", pageNumber > 0)
+        sendVisibility(book, "turn_page_right", pageNumber < pages.lastIndex)
         val page = pages.getOrNull(pageNumber)?.lines()
-        for (i in 0 until 30) {
-            sendText("book", "line${i + 1}", page?.getOrNull(i) ?: "")
+        for (i in 0 until if (book == "book_long") 30 else 21) {
+            sendText(book, "line${i + 1}", page?.getOrNull(i) ?: "")
         }
     }
 }
