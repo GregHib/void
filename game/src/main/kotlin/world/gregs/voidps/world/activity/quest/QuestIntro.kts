2d7498af51457dbf1505d25d7ce6b1a3593c9342
GregHib <greghib@users.noreply.github.com>
2024-11-21 21:13:46 +0000

    Tweak quest intro

--- game/src/main/kotlin/world/gregs/voidps/world/activity/quest/QuestIntro.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/quest/QuestIntro.kts
@@ -5,10 +5,11 @@ import world.gregs.voidps.engine.client.ui.interfaceOption
 interfaceOption("Show required items", "items_hidden_button_txt", "quest_intro") {
     player.interfaces.sendVisibility(id, "items_hide_show_layer", false)
     player.interfaces.sendVisibility(id, "items_text_details_layer", true)
+    player.interfaces.sendVisibility(id, "scroll_layer_item", true)
 }
-
 
 interfaceOption("Show rewards", "hidden_button_txt", "quest_intro") {
     player.interfaces.sendVisibility(id, "hide_show_layer", false)
     player.interfaces.sendVisibility(id, "text_details_layer", true)
+    player.interfaces.sendVisibility(id, "scroll_layer_rewards", true)
 }
