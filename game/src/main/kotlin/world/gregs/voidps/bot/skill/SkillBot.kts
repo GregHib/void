6629f5aa24ce5aecc27dfb4325e391d0b12c205c
GregHib <greghib@users.noreply.github.com>
2024-12-03 17:36:52 +0000

    Fix level up dialogues closing automatically for non-bot players

--- game/src/main/kotlin/world/gregs/voidps/bot/skill/SkillBot.kts
+++ game/src/main/kotlin/world/gregs/voidps/bot/skill/SkillBot.kts
@@ -1,8 +1,11 @@
 package world.gregs.voidps.bot.skill
 
+import world.gregs.voidps.bot.isBot
 import world.gregs.voidps.engine.client.ui.event.interfaceOpen
 import world.gregs.voidps.network.client.instruction.InteractDialogue
 
 interfaceOpen("dialogue_level_up") { bot ->
-    bot.instructions.trySend(InteractDialogue(interfaceId = 740, componentId = 3, option = -1))
+    if (bot.isBot) {
+        bot.instructions.trySend(InteractDialogue(interfaceId = 740, componentId = 3, option = -1))
+    }
 }
