0d92d847448fc01c159106714ef018d0284b9d7d
GregHib <greghib@users.noreply.github.com>
2024-12-29 19:10:43 +0000

    Add advanced gnome course

---
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/skill/agility/course/GnomeAdvanced.kts
@@ -0,0 +1,100 @@
+package world.gregs.voidps.world.activity.skill.agility.course
+
+import world.gregs.voidps.engine.client.message
+import world.gregs.voidps.engine.client.variable.start
+import world.gregs.voidps.engine.entity.character.clearAnimation
+import world.gregs.voidps.engine.entity.character.exactMove
+import world.gregs.voidps.engine.entity.character.face
+import world.gregs.voidps.engine.entity.character.move.tele
+import world.gregs.voidps.engine.entity.character.npc.NPCs
+import world.gregs.voidps.engine.entity.character.player.chat.ChatType
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.engine.entity.character.player.skill.exp.exp
+import world.gregs.voidps.engine.entity.character.player.skill.level.Level.has
+import world.gregs.voidps.engine.entity.character.setAnimation
+import world.gregs.voidps.engine.entity.obj.objectApproach
+import world.gregs.voidps.engine.entity.obj.objectOperate
+import world.gregs.voidps.engine.inject
+import world.gregs.voidps.engine.queue.strongQueue
+import world.gregs.voidps.engine.suspend.approachRange
+import world.gregs.voidps.engine.suspend.delay
+import world.gregs.voidps.engine.suspend.pause
+import world.gregs.voidps.type.Direction
+import world.gregs.voidps.type.Tile
+import world.gregs.voidps.type.Zone
+
+val npcs: NPCs by inject()
+
+objectOperate("Climb-up", "gnome_tree_branch_advanced") {
+    if (!player.has(Skill.Agility, 85)) {
+        return@objectOperate
+    }
+    npcs.gnomeTrainer("Terrorbirds could climb faster than that!", Zone(9263413))
+    player.message("You climb the tree...", ChatType.Filter)
+    player.setAnimation("climb_up")
+    player.start("input_delay", 2)
+    player.strongQueue("branch", 2) {
+        player.message("... to an even higher platform.", ChatType.Filter)
+        player.gnomeStage(4)
+        player.tele(player.tile.add(y = -1, level = 1))
+        player.exp(Skill.Agility, 25.0)
+    }
+}
+
+objectApproach("Run-across", "gnome_sign_post_advanced") {
+    npcs.gnomeTrainer("Come on! I'd be over there by now.", Zone(13457717))
+    player.approachRange(1)
+    // Pausing for 2 ticks to ensure we're in the correct spot.
+    // arriveDelay() wouldn't work as objectApproach is called before Movement.tick where "last_movement" is set
+    pause(2)
+    player.face(Direction.EAST)
+    player.setAnimation("gnome_wall_run", override = true)
+    player.start("input_delay", 4)
+    player.strongQueue("wall-run", 1) {
+        player.exactMove(Tile(2484, 3418, 3), 60, Direction.EAST)
+        player.strongQueue("land", 2) {
+            player.gnomeStage(5)
+            player.exp(Skill.Agility, 25.0)
+            player.clearAnimation()
+        }
+    }
+}
+
+objectApproach("Swing-to", "gnome_pole_advanced") {
+    player.steps.clear()
+    player.face(Direction.NORTH)
+    player.start("input_delay", 14)
+    player.strongQueue("run-up", 1) {
+        player.setAnimation("gnome_run_up")
+        player.exactMove(player.tile.copy(y = 3421), 60, Direction.NORTH)
+        pause(2)
+        player.setAnimation("gnome_jump")
+        player.exactMove(player.tile.copy(y = 3425), 30, Direction.NORTH)
+        pause(1)
+        player.setAnimation("gnome_swing")
+        pause(4)
+        player.exactMove(player.tile.copy(y = 3429), 30, Direction.NORTH)
+        pause(5)
+        player.exactMove(player.tile.copy(y = 3432), 30, Direction.NORTH)
+        delay(2)
+        player.gnomeStage(6)
+        player.exp(Skill.Agility, 25.0)
+    }
+}
+
+objectOperate("Jump-over", "gnome_barrier_advanced") {
+    player.setAnimation("gnome_jump_barrier")
+    player.start("input_delay", 4)
+    player.strongQueue("branch", 1) {
+        player.exactMove(Tile(2485, 3434, 3), 30, Direction.NORTH)
+        pause(2)
+        player.tele(2485, 3436, 0)
+        player.setAnimation("gnome_pipe_land")
+        if (player.gnomeStage == 6) {
+            player.gnomeStage = 0
+            player.inc("gnome_course_advanced_laps")
+            player.exp(Skill.Agility, 605.0)
+        }
+        player.exp(Skill.Agility, 25.0)
+    }
+}
--- game/src/main/kotlin/world/gregs/voidps/world/activity/skill/agility/course/GnomeStronghold.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/skill/agility/course/GnomeStronghold.kts
@@ -1,4 +1,4 @@
-package world.gregs.voidps.world.activity.skill.agility
+package world.gregs.voidps.world.activity.skill.agility.course
 
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.client.variable.start
@@ -6,70 +6,69 @@ import world.gregs.voidps.engine.entity.character.exactMove
 import world.gregs.voidps.engine.entity.character.face
 import world.gregs.voidps.engine.entity.character.move.tele
 import world.gregs.voidps.engine.entity.character.move.walkTo
-import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.npc.NPCs
 import world.gregs.voidps.engine.entity.character.player.chat.ChatType
 import world.gregs.voidps.engine.entity.character.player.clearRenderEmote
 import world.gregs.voidps.engine.entity.character.player.renderEmote
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.player.skill.exp.exp
 import world.gregs.voidps.engine.entity.character.setAnimation
-import world.gregs.voidps.engine.entity.obj.GameObject
 import world.gregs.voidps.engine.entity.obj.objectOperate
+import world.gregs.voidps.engine.inject
 import world.gregs.voidps.engine.queue.softQueue
 import world.gregs.voidps.engine.queue.strongQueue
-import world.gregs.voidps.engine.suspend.delay
 import world.gregs.voidps.type.Direction
 import world.gregs.voidps.type.Tile
+import world.gregs.voidps.type.Zone
 
-var Player.gnomeStage: Int
-    set(value) {
-        val current = this["gnome_course_stage", 0]
-        if (value == current + 1 || value == 0) {
-            this["gnome_course_stage"] = value
-        }
-    }
-    get() = this["gnome_course_stage", 0]
+val npcs: NPCs by inject()
 
 objectOperate("Walk-across", "gnome_log_balance") {
+    npcs.gnomeTrainer("Okay get over that log, quick quick!", listOf(Zone(878901), Zone(878900), Zone(876852)))
+    player.renderEmote = "rope_balance"
     player.walkTo(Tile(2474, 3429), noCollision = true, noRun = true)
     player.message("You walk carefully across the slippery log...", ChatType.Filter)
-
     player.start("input_delay", 8)
     player.strongQueue("log-balance", 8) {
-        player.gnomeStage++
+        player.clearRenderEmote()
+        player.gnomeStage(1)
         player.exp(Skill.Agility, 7.5)
         player.message("... and make it safely to the other side.", ChatType.Filter)
     }
 }
 
 objectOperate("Climb-over", "gnome_obstacle_net") {
-    player.message("You climb the netting.", ChatType.Filter)
+    npcs.gnomeTrainer("Move it, move it, move it!", listOf(Zone(8768252), Zone(876853)))
+    player.message("You climb the netting...", ChatType.Filter)
     player.setAnimation("climb_up")
     player.start("input_delay", 2)
     player.strongQueue("netting", 2) {
-        player.gnomeStage++
+        player.gnomeStage(2)
         player.tele(player.tile.add(y = -2, level = 1))
         player.exp(Skill.Agility, 7.5)
     }
 }
 
 objectOperate("Climb", "gnome_tree_branch_up") {
+    npcs.gnomeTrainer("That's it - straight up", listOf(Zone(5069109), Zone(5071157)))
     player.message("You climb the tree...", ChatType.Filter)
     player.setAnimation("climb_up")
     player.start("input_delay", 2)
-    player.strongQueue("netting", 2) {
-        player.gnomeStage++
+    player.strongQueue("branch", 2) {
+        player.message("... to the platform above.", ChatType.Filter)
+        player.gnomeStage(3)
         player.tele(player.tile.add(y = -3, level = 1))
         player.exp(Skill.Agility, 5.0)
     }
 }
 
 objectOperate("Walk-on", "gnome_balancing_rope") {
+    npcs.gnomeTrainer("Come on scaredy cat, get across that rope!", Zone(9263413))
     player.walkTo(Tile(2483, 3420, 2), noCollision = true, noRun = true)
     player.start("input_delay", 7)
     player.renderEmote = "rope_balance"
     player.strongQueue("rope-balance", 7) {
-        player.gnomeStage++
+        player.gnomeStage(4)
         player.clearRenderEmote()
         player.exp(Skill.Agility, 7.5)
         player.message("You passed the obstacle successfully.", ChatType.Filter)
@@ -81,7 +80,6 @@ objectOperate("Walk-on", "gnome_balancing_rope_end") {
     player.start("input_delay", 7)
     player.renderEmote = "rope_balance"
     player.strongQueue("rope-balance", 7) {
-        player.gnomeStage++
         player.clearRenderEmote()
         player.exp(Skill.Agility, 7.5)
         player.message("You passed the obstacle successfully.", ChatType.Filter)
@@ -92,50 +90,43 @@ objectOperate("Climb-down", "gnome_tree_branch_down") {
     player.message("You climb the tree...", ChatType.Filter)
     player.setAnimation("climb_down")
     player.start("input_delay", 2)
-    player.strongQueue("netting", 2) {
-        player.gnomeStage++
+    player.strongQueue("branch", 2) {
+        player.gnomeStage(5)
         player.tele(player.tile.add(x = 1, y = 1, level = -2))
         player.exp(Skill.Agility, 5.0)
     }
 }
 
 objectOperate("Climb-over", "gnome_obstacle_net_free_standing") {
+    npcs.gnomeTrainer("My Granny can move faster than you.", Zone(876854))
     player.message("You climb the netting.", ChatType.Filter)
     player.setAnimation("climb_up")
     player.start("input_delay", 2)
     player.strongQueue("netting", 2) {
-        player.gnomeStage++
+        player.gnomeStage(6)
         player.tele(player.tile.add(y = 2))
         player.exp(Skill.Agility, 7.5)
     }
 }
 
 objectOperate("Squeeze-through", "gnome_obstacle_pipe_*") {
-    if (player.tile.y >= target.y) {
-        player.walkTo(target.tile.addY(-1))
-        pipe(player, target, 2)
-    } else {
-        pipe(player, target, 1)
-    }
-}
-
-fun pipe(player: Player, target: GameObject, initialDelay: Int) {
-    player.strongQueue("obstacle_pipe", initialDelay) {
+    player.strongQueue("obstacle_pipe", 1) {
         player.face(Direction.NORTH)
         player.message("You pull yourself through the pipes..", ChatType.Filter)
         player.start("input_delay", 8)
         player.strongQueue("obstacle_pipe", 1) {
-            player.setAnimation("10580")
+            player.setAnimation("climb_through_pipe")
             player.exactMove(target.tile.addY(2))
             player.strongQueue("obstacle_pipe", 4) {
                 player.face(Direction.NORTH)
                 player.tele(target.tile.addY(3))
-                player.setAnimation("10580", delay = 1)
+                player.setAnimation("climb_through_pipe", delay = 1)
                 player.exactMove(target.tile.addY(6))
                 player.softQueue("reward", 4) {
-                    if (player.gnomeStage == 5) {
+                    if (player.gnomeStage == 6) {
                         player.gnomeStage = 0
-                        player.exp(Skill.Agility, 39.5)
+                        player.inc("gnome_course_laps")
+                        player.exp(Skill.Agility, 39.0)
                     }
                     player.exp(Skill.Agility, 7.5)
                 }
