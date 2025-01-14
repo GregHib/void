05ffb91edc6c07110aaa2fa771a274174f0567f5
GregHib <greghib@users.noreply.github.com>
2025-01-14 21:39:17 +0000

    Add course failing

--- game/src/main/kotlin/world/gregs/voidps/world/activity/skill/agility/course/WildernessCourse.kts
+++ game/src/main/kotlin/world/gregs/voidps/world/activity/skill/agility/course/WildernessCourse.kts
@@ -3,24 +3,24 @@ package world.gregs.voidps.world.activity.skill.agility.course
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.client.variable.start
 import world.gregs.voidps.engine.data.Settings
-import world.gregs.voidps.engine.entity.character.*
+import world.gregs.voidps.engine.entity.character.exactMove
+import world.gregs.voidps.engine.entity.character.face
 import world.gregs.voidps.engine.entity.character.move.tele
 import world.gregs.voidps.engine.entity.character.move.walkTo
-import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.chat.ChatType
 import world.gregs.voidps.engine.entity.character.player.clearRenderEmote
-import world.gregs.voidps.engine.entity.character.player.movementType
 import world.gregs.voidps.engine.entity.character.player.renderEmote
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.player.skill.exp.exp
 import world.gregs.voidps.engine.entity.character.player.skill.level.Level
+import world.gregs.voidps.engine.entity.character.player.skill.level.Level.has
+import world.gregs.voidps.engine.entity.character.setAnimation
+import world.gregs.voidps.engine.entity.character.turn
 import world.gregs.voidps.engine.entity.obj.GameObjects
 import world.gregs.voidps.engine.entity.obj.objectOperate
 import world.gregs.voidps.engine.inject
 import world.gregs.voidps.engine.queue.Action
 import world.gregs.voidps.engine.queue.strongQueue
-import world.gregs.voidps.engine.suspend.pause
-import world.gregs.voidps.network.login.protocol.visual.update.player.MoveType
 import world.gregs.voidps.type.Direction
 import world.gregs.voidps.type.Tile
 import world.gregs.voidps.type.equals
@@ -31,21 +31,26 @@ import world.gregs.voidps.world.interact.entity.sound.playSound
 val objects: GameObjects by inject()
 
 objectOperate("Open", "wilderness_agility_door_closed") {
+    if (!player.has(Skill.Agility, 52, message = true)) {
+        // TODO proper message
+        return@objectOperate
+    }
     if (player.tile.y > 3916) {
         Door.enter(player, target)
         return@objectOperate
     }
-    val fail = true
+    val disable = Settings["agility.disableCourseFailure", false]
+    val success = disable || Level.success(player.levels.get(Skill.Agility), 200..250)
     player.strongQueue("course_enter") {
         onCancel = {
             player.tele(target.tile.addY(-1))
         }
-        player.start("input_delay", if (fail) 8 else 16)
+        player.start("input_delay", if (success) 16 else 8)
         player.message("You go through the gate and try to edge over the ridge...", ChatType.Filter)
         Door.enter(player, target)
         pause()
         player.renderEmote = "beam_balance"
-        if (fail) {
+        if (!success) {
             fallIntoPit()
             return@strongQueue
         }
@@ -132,7 +137,7 @@ objectOperate("Squeeze-through", "wilderness_obstacle_pipe") {
         player.setAnimation("climb_through_pipe", delay = 30)
         player.exactMove(Tile(3004, 3950), startDelay = 30, delay = 96, direction = Direction.NORTH)
         pause(3)
-        player.exp(Skill.Agility, 12.0)
+        player.exp(Skill.Agility, 12.5)
         player.agilityStage(1)
     }
 }
@@ -182,6 +187,7 @@ objectOperate("Cross", "wilderness_stepping_stone") {
                 player.start("input_delay", 3)
                 player.setAnimation("rope_walk_fall_down")
                 player.face(Direction.WEST)
+                player.clearRenderEmote()
                 player.message("...You lose your footing and fall into the lava.", ChatType.Filter)
                 pause(2)
                 player.damage(player.levels.get(Skill.Constitution) / 5 + 1)
@@ -195,5 +201,71 @@ objectOperate("Cross", "wilderness_stepping_stone") {
         player.message("...You safely cross to the other side.", ChatType.Filter)
         player.exp(Skill.Agility, 20.0)
         player.agilityStage(3)
+    }
+}
+
+objectOperate("Walk-across", "wilderness_log_balance") {
+    player.strongQueue("agility_log") {
+        onCancel = {
+            player.tele(3001, 3946)
+        }
+        player.message("You walk carefully across the slippery log...", ChatType.Filter)
+        val disable = Settings["agility.disableCourseFailure", false]
+        val success = disable || Level.success(player.levels.get(Skill.Agility), 200..250)
+        if (success) {
+            player.start("input_delay", 10)
+            player.walkTo(target.tile, noCollision = true, noRun = true)
+            pause()
+            player.renderEmote = "beam_balance"
+            player.walkTo(Tile(2994, 3945), noCollision = true, noRun = true)
+            pause(7)
+            player.message("You skillfully edge across the gap.", type = ChatType.Filter)
+            player.clearRenderEmote()
+            pause()
+            player.exp(Skill.Agility, 20.0)
+            player.agilityStage(4)
+        } else {
+            player.start("input_delay", 9)
+            player.walkTo(target.tile, noCollision = true, noRun = true)
+            pause()
+            player.renderEmote = "beam_balance"
+            player.walkTo(Tile(2998, 3945), noCollision = true, noRun = true)
+            pause(4)
+            player.message("You slip and fall onto the spikes below.", type = ChatType.Filter)
+            player.setAnimation("rope_walk_fall_down")
+            player.face(Direction.NORTH)
+            pause()
+            player.tele(2998, 10346)
+            player.clearRenderEmote()
+            player.playSound("2h_stab")
+            pause()
+            player.walkTo(Tile(2998, 10345))
+            pause()
+            player.damage(9)
+            player.playSound("male_hit_1", delay = 20)
+        }
+        if (success || Settings["agility.disableFailLapSkip", false]) {
+            player.agilityStage(4)
+        }
+    }
+}
+
+objectOperate("Climb", "wilderness_agility_rocks") {
+    player.start("input_delay", 5)
+    player.strongQueue("agility_rocks") {
+        onCancel = {
+            player.tele(2993, 3937)
+        }
+        player.message("You walk carefully across the slippery log...", ChatType.Filter)
+        player.renderEmote = "climbing"
+        player.walkTo(player.tile.copy(y = 3933), noCollision = true, noRun = true)
+        pause(4)
+        player.clearRenderEmote()
+        player.message("You reach the top.", type = ChatType.Filter)
+        if (player.agilityStage == 4) {
+            player.agilityStage = 0
+            player.exp(Skill.Agility, 499.0)
+            player.inc("wilderness_course_laps")
+        }
     }
 }
