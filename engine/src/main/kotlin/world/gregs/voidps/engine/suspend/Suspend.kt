136826d0f97c20fbf065c34d205ed3d46671c83f
GregHib <greghib@users.noreply.github.com>
2025-01-22 09:33:17 +0000

    Remove playAnimation

--- engine/src/main/kotlin/world/gregs/voidps/engine/suspend/Suspend.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/suspend/Suspend.kt
@@ -1,11 +1,8 @@
 package world.gregs.voidps.engine.suspend
 
-import com.github.michaelbull.logging.InlineLogger
 import world.gregs.voidps.engine.client.ui.dialogue
-import world.gregs.voidps.engine.client.variable.start
 import world.gregs.voidps.engine.entity.character.Character
 import world.gregs.voidps.engine.entity.character.player.Player
-import world.gregs.voidps.engine.entity.character.setAnimation
 
 fun Character.resumeSuspension(): Boolean {
     val suspend = suspension ?: return false
@@ -19,20 +16,4 @@ fun Character.resumeSuspension(): Boolean {
 suspend fun SuspendableContext<Player>.awaitDialogues(): Boolean {
     Suspension.start(character) { player.dialogue == null }
     return true
-}
-
-private val logger = InlineLogger()
-
-context(SuspendableContext<*>) suspend fun Character.playAnimation(id: String, override: Boolean = false, canInterrupt: Boolean = true) {
-    val ticks = setAnimation(id, override = override)
-    if (ticks == -1) {
-        logger.warn { "No animation delay $id" }
-    } else {
-        character.start("movement_delay", ticks)
-        if (canInterrupt) {
-            pause(ticks)
-        } else {
-            delay(ticks)
-        }
-    }
 }
