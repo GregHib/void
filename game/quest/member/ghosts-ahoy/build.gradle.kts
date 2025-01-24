5d520945558e2ef85c206e6b4158658fe01e09bc
GregHib <greghib@users.noreply.github.com>
2025-01-24 23:56:26 +0000

    Move teleports into modules

--- game/quest/member/ghosts-ahoy/build.gradle.kts
+++ game/quest/member/ghosts-ahoy/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
     implementation(project(":game:entity:player:dialogue"))
     implementation(project(":game:api"))
-    implementation(project(":game:activity:shooting-star"))
     testImplementation(project(":game:api:testing"))
 }
