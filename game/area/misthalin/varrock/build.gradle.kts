a1a05e58f9530fe38af2a422fc205f374034f953
GregHib <greghib@users.noreply.github.com>
2025-01-24 23:40:13 +0000

    Move quests into modules

--- game/area/misthalin/varrock/build.gradle.kts
+++ game/area/misthalin/varrock/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     implementation(project(":game:api"))
     implementation(project(":game:quest"))
     implementation(project(":game:skill:runecrafting"))
+    implementation(project(":game:quest:free:demon-slayer"))
     implementation(project(":game:quest:miniquest:alfred-grimhands-barcrawl"))
     testImplementation(project(":game:api:testing"))
 }
