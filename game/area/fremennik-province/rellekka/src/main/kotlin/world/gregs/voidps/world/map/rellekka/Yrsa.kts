249d17e1408d2dd169d1292d7c6a7b1094a585d0
GregHib <greghib@users.noreply.github.com>
2025-01-24 19:57:36 +0000

    Move even more places into modules

--- game/area/fremennik-province/rellekka/src/main/kotlin/world/gregs/voidps/world/map/rellekka/Yrsa.kts
+++ game/area/fremennik-province/rellekka/src/main/kotlin/world/gregs/voidps/world/map/rellekka/Yrsa.kts
@@ -13,7 +13,6 @@ import world.gregs.voidps.engine.entity.character.player.equip.equipped
 import world.gregs.voidps.engine.entity.character.player.flagAppearance
 import world.gregs.voidps.engine.entity.character.player.sex
 import world.gregs.voidps.engine.inject
-import world.gregs.voidps.engine.suspend.SuspendableContext
 import world.gregs.voidps.network.login.protocol.visual.update.player.BodyColour
 import world.gregs.voidps.network.login.protocol.visual.update.player.BodyPart
 import world.gregs.voidps.network.login.protocol.visual.update.player.EquipSlot
---
+++ game/area/kandarin/ourania/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    implementation(project(":game:entity:player:dialogue"))
+    implementation(project(":game:map"))
+    implementation(project(":game:api"))
+    implementation(project(":game:quest"))
+}
