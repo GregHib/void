36a9eda41c05b1f6f6c5293fa14be981cf7bffae
GregHib <greghib@users.noreply.github.com>
2025-01-25 00:00:31 +0000

    Fix teleports

--- game/quest/member/enakhras-lament/build.gradle.kts
+++ game/quest/member/enakhras-lament/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":game:entity:player:dialogue"))
     implementation(project(":game:api"))
+    implementation(project(":game:skill:magic"))
     testImplementation(project(":game:api:testing"))
 }
