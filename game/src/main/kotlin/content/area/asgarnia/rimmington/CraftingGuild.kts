5a0a35dcc5d057e7003a048be4d3e6618eee6b73
GregHib <greghib@users.noreply.github.com>
2025-01-25 19:16:06 +0000

    Move rimmy package

--- game/src/main/kotlin/content/area/asgarnia/rimmington/CraftingGuild.kts
+++ game/src/main/kotlin/content/area/asgarnia/rimmington/CraftingGuild.kts
@@ -1,4 +1,4 @@
-package world.gregs.voidps.world.map.rimmington
+package content.area.asgarnia.rimmington
 
 import com.github.michaelbull.logging.InlineLogger
 import world.gregs.voidps.engine.entity.character.npc.npcOperate
