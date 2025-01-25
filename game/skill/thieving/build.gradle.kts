bdf8880dedc388429ee24e9bddae05851597f398
GregHib <greghib@users.noreply.github.com>
2025-01-25 02:08:02 +0000

    Fix issue with modules with same names

--- game/skill/thieving/build.gradle.kts
+++ game/skill/thieving/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":game:entity:player:dialogue"))
     implementation(project(":game:api"))
+    implementation(project(":game:entity:npc:npc-combat"))
     testImplementation(project(":game:api:testing"))
 }
