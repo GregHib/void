5691c492304d0cc6587ea592b5c2f3ee5fac5e3b
GregHib <greghib@users.noreply.github.com>
2025-01-25 19:19:10 +0000

    Move wildy package

--- game/src/main/kotlin/content/area/wilderness/Web.kts
+++ game/src/main/kotlin/content/area/wilderness/Web.kts
@@ -1,4 +1,4 @@
-package world.gregs.voidps.world.map.wilderness
+package content.area.wilderness
 
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.client.ui.interact.itemOnObjectOperate
