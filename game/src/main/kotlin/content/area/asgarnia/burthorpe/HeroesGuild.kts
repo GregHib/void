0762d2385ae9be8d1e67c5b8889ad8badb3a6a88
GregHib <greghib@users.noreply.github.com>
2025-01-25 19:10:30 +0000

    Move burthope package

--- game/src/main/kotlin/content/area/asgarnia/burthorpe/HeroesGuild.kts
+++ game/src/main/kotlin/content/area/asgarnia/burthorpe/HeroesGuild.kts
@@ -1,4 +1,4 @@
-package world.gregs.voidps.world.map.burthorpe
+package content.area.asgarnia.burthorpe
 
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.client.ui.interact.itemOnObjectOperate
