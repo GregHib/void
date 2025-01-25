2125259fbb45a54e5fd3998c34cac38a84a2229c
GregHib <greghib@users.noreply.github.com>
2025-01-25 16:19:26 +0000

    Fix tests

--- game/area/wilderness/abyss/build.gradle.kts
+++ game/area/wilderness/abyss/build.gradle.kts
@@ -3,5 +3,9 @@ dependencies {
     implementation(project(":game:api"))
     implementation(project(":game:skill:woodcutting"))
     implementation(project(":game:skill:mining"))
+    testImplementation(project(":game:area:misthalin:varrock"))
+    testImplementation(project(":game:area:kandarin:ardougne"))
+    testImplementation(project(":game:area:kandarin:tree-gnome-stronghold"))
+    testImplementation(project(":game:entity:obj:teleport"))
     testImplementation(project(":game:api:testing"))
 }
