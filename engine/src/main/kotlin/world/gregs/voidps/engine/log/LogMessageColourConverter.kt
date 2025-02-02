882cb7b491cd57eab4a6289beb783548c84605e7
GregHib <greghib@users.noreply.github.com>
2025-02-02 18:33:17 +0000

    Update logback format to include colours and not override one another

---
+++ engine/src/main/kotlin/world/gregs/voidps/engine/log/LogMessageColourConverter.kt
@@ -0,0 +1,13 @@
+package world.gregs.voidps.engine.log
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase
+import ch.qos.logback.core.pattern.color.ANSIConstants.*;
+
+class LogMessageColourConverter : ForegroundCompositeConverterBase<ILoggingEvent>() {
+    override fun getForegroundColorCode(event: ILoggingEvent) = when (event.level.toInt()) {
+        Level.ERROR_INT -> RED_FG
+        else -> DEFAULT_FG
+    }
+}
