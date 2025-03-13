e2b87821a8c30572c8425e1b9882f2c2c09352fa
GregHib <greghib@users.noreply.github.com>
2025-03-13 21:51:13 +0000

    Convert diango-code to toml

--- engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/DiangoCodeDefinitions.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/DiangoCodeDefinitions.kt
@@ -1,13 +1,15 @@
 package world.gregs.voidps.engine.data.definition
 
 import com.github.michaelbull.logging.InlineLogger
+import it.unimi.dsi.fastutil.Hash
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
+import it.unimi.dsi.fastutil.objects.ObjectArrayList
+import world.gregs.config.Config
+import world.gregs.config.ConfigReader
 import world.gregs.voidps.engine.data.Settings
 import world.gregs.voidps.engine.data.config.DiangoCodeDefinition
 import world.gregs.voidps.engine.entity.item.Item
-import world.gregs.voidps.engine.get
 import world.gregs.voidps.engine.timedLoad
-import world.gregs.yaml.Yaml
-import world.gregs.yaml.read.YamlReaderConfiguration
 
 class DiangoCodeDefinitions {
 
@@ -18,33 +20,29 @@ class DiangoCodeDefinitions {
     fun getOrNull(code: String) = definitions[code]
 
     @Suppress("UNCHECKED_CAST")
-    fun load(yaml: Yaml = get(), path: String = Settings["definitions.diangoCodes"], itemDefinitions: ItemDefinitions? = null): DiangoCodeDefinitions {
+    fun load(path: String = Settings["definitions.diangoCodes"], itemDefinitions: ItemDefinitions? = null): DiangoCodeDefinitions {
         timedLoad("diango code definition") {
-            val config = object : YamlReaderConfiguration(2, 2) {
-                override fun add(list: MutableList<Any>, value: Any, parentMap: String?) {
-                    super.add(list, if (value is Map<*, *>) {
-                        val id = value["item"] as String
-                        if (itemDefinitions != null && !itemDefinitions.contains(id)) {
-                            logger.warn { "Invalid diango item id: $id" }
+            val definitions = Object2ObjectOpenHashMap<String, DiangoCodeDefinition>(1, Hash.VERY_FAST_LOAD_FACTOR)
+            val reader = object : ConfigReader(50) {
+                override fun set(section: String, key: String, value: Any) {
+                    if (section.startsWith("diango")) {
+                        val stringId = section.removePrefix("diango")
+                        when (key) {
+                            "variable" -> definitions[stringId] = definitions[stringId]?.copy(variable = value as String) ?: DiangoCodeDefinition(variable = value as String)
+                            "add" -> {
+                                val list = value as List<String>
+                                val items = ObjectArrayList<Item>(list.size)
+                                for (id in list) {
+                                    require(itemDefinitions == null || itemDefinitions.contains(id)) { "Invalid diango item id: $id" }
+                                    items.add(Item(id, 1))
+                                }
+                                definitions[stringId] = definitions[stringId]?.copy(add = items) ?: DiangoCodeDefinition(add = items)
+                            }
                         }
-                        Item(id, value["amount"] as? Int ?: 1)
-                    } else {
-                        Item(value as String, amount = 1)
-                    }, parentMap)
-                }
-                override fun set(map: MutableMap<String, Any>, key: String, value: Any, indent: Int, parentMap: String?) {
-                    if (key == "<<") {
-                        map.putAll(value as Map<String, Any>)
-                        return
-                    }
-                    if (indent == 0) {
-                        super.set(map, key, DiangoCodeDefinition(value as Map<String, Any>), indent, parentMap)
-                    } else {
-                        super.set(map, key, value, indent, parentMap)
                     }
                 }
             }
-            val definitions = yaml.load<Any>(path, config) as Map<String, DiangoCodeDefinition>
+            Config.decodeFromFile(path, reader)
             this.definitions = definitions
             definitions.size
         }
