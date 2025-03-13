58a785b601d5bfa5ed8b50084ecdcb4d3910407d
GregHib <greghib@users.noreply.github.com>
2025-03-13 21:22:14 +0000

    Convert animations to toml

--- engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/AnimationDefinitions.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/AnimationDefinitions.kt
@@ -1,10 +1,13 @@
 package world.gregs.voidps.engine.data.definition
 
+import it.unimi.dsi.fastutil.Hash
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap
+import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
+import world.gregs.config.Config
+import world.gregs.config.ConfigReader
 import world.gregs.voidps.cache.definition.data.AnimationDefinition
 import world.gregs.voidps.engine.data.Settings
-import world.gregs.voidps.engine.get
 import world.gregs.voidps.engine.timedLoad
-import world.gregs.yaml.Yaml
 
 class AnimationDefinitions(
     override var definitions: Array<AnimationDefinition>
@@ -14,9 +17,47 @@ class AnimationDefinitions(
 
     override fun empty() = AnimationDefinition.EMPTY
 
-    fun load(yaml: Yaml = get(), path: String = Settings["definitions.animations"]): AnimationDefinitions {
+    fun load(path: String = Settings["definitions.animations"]): AnimationDefinitions {
         timedLoad("animation extra") {
-            decode(yaml, path)
+            val ids = Object2IntOpenHashMap<String>(definitions.size, Hash.VERY_FAST_LOAD_FACTOR)
+            var count = 0
+            val reader = object : ConfigReader(100) {
+                override fun set(section: String, key: String, value: Any) {
+                    when (section) {
+                        "anims" -> {
+                            val id = (value as Long).toInt()
+                            require(id < definitions.size) { "Invalid animation id '$value' for anim '$key'. Maximum id: ${definitions.size}" }
+                            definitions[id].stringId = key
+                            ids[key] = id
+                            count++
+                        }
+                        else -> {
+                            val stringId = section.removePrefix("anims.")
+                            val definition = definitions[ids.getInt(stringId)]
+                            when (key) {
+                                "id" -> definition.id = (value as Long).toInt()
+                                else -> {
+                                    val extras: MutableMap<String, Any> = if (definition.extras == null) {
+                                        val map = Object2ObjectOpenHashMap<String, Any>(2, Hash.VERY_FAST_LOAD_FACTOR)
+                                        definition.extras = map
+                                        map
+                                    } else {
+                                        definition.extras!!
+                                    } as MutableMap<String, Any>
+                                    if (value is Long) {
+                                        extras[key] = value.toInt()
+                                    } else {
+                                        extras[key] = value
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Config.decodeFromFile(path, reader)
+            this.ids = ids
+            count
         }
         return this
     }
--- engine/src/test/kotlin/world/gregs/voidps/engine/data/definition/AnimationDefinitionsTest.kt
+++ engine/src/test/kotlin/world/gregs/voidps/engine/data/definition/AnimationDefinitionsTest.kt
@@ -2,7 +2,6 @@ package world.gregs.voidps.engine.data.definition
 
 import world.gregs.voidps.cache.definition.data.AnimationDefinition
 import world.gregs.voidps.cache.definition.decoder.AnimationDecoder
-import world.gregs.yaml.Yaml
 
 internal class AnimationDefinitionsTest : DefinitionsDecoderTest<AnimationDefinition, AnimationDecoder, AnimationDefinitions>() {
 
@@ -24,7 +23,7 @@ internal class AnimationDefinitionsTest : DefinitionsDecoderTest<AnimationDefini
     }
 
     override fun load(definitions: AnimationDefinitions) {
-        definitions.load(Yaml(), "../data/definitions/animations.yml")
+        definitions.load("../data/definitions/animations.toml")
     }
 
 }
