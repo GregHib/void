c6635249e9c9bea5e118d44f93840c8d25b1c4c1
GregHib <greghib@users.noreply.github.com>
2025-03-14 16:26:17 +0000

    Convert fonts to toml

--- engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/FontDefinitions.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/FontDefinitions.kt
@@ -1,10 +1,12 @@
 package world.gregs.voidps.engine.data.definition
 
+import it.unimi.dsi.fastutil.Hash
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap
+import world.gregs.config.Config
+import world.gregs.config.ConfigReader
 import world.gregs.voidps.cache.definition.data.FontDefinition
 import world.gregs.voidps.engine.data.Settings
-import world.gregs.voidps.engine.get
 import world.gregs.voidps.engine.timedLoad
-import world.gregs.yaml.Yaml
 
 class FontDefinitions(
     override var definitions: Array<FontDefinition>
@@ -14,9 +16,19 @@ class FontDefinitions(
 
     override fun empty() = FontDefinition.EMPTY
 
-    fun load(yaml: Yaml = get(), path: String = Settings["definitions.fonts"]): FontDefinitions {
+    fun load(path: String = Settings["definitions.fonts"]): FontDefinitions {
         timedLoad("font extra") {
-            decode(yaml, path)
+            val ids = Object2IntOpenHashMap<String>(definitions.size, Hash.VERY_FAST_LOAD_FACTOR)
+            val reader = object : ConfigReader(50) {
+                override fun set(section: String, key: String, value: Any) {
+                    val id = (value as Long).toInt()
+                    ids[key] = id
+                    definitions[id].stringId = key
+                }
+            }
+            Config.decodeFromFile(path, reader)
+            this.ids = ids
+            ids.size
         }
         return this
     }
