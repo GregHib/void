59d583958e38fb53db2761ad6033cdaab8dd0bad
GregHib <greghib@users.noreply.github.com>
2025-03-19 23:14:47 +0000

    Convert object spawns to toml

--- game/src/main/kotlin/content/entity/obj/spawn/ObjectSpawns.kt
+++ game/src/main/kotlin/content/entity/obj/spawn/ObjectSpawns.kt
@@ -1,5 +1,6 @@
 package content.entity.obj.spawn
 
+import world.gregs.config.Config
 import world.gregs.voidps.engine.data.Settings
 import world.gregs.voidps.engine.data.definition.ObjectDefinitions
 import world.gregs.voidps.engine.entity.World
@@ -8,34 +9,44 @@ import world.gregs.voidps.engine.entity.obj.GameObjects
 import world.gregs.voidps.engine.get
 import world.gregs.voidps.type.Tile
 import world.gregs.voidps.engine.timedLoad
-import world.gregs.yaml.Yaml
-import world.gregs.yaml.read.YamlReaderConfiguration
 
-@Suppress("UNCHECKED_CAST")
 fun loadObjectSpawns(
     objects: GameObjects,
-    yaml: Yaml = get(),
     path: String = Settings["spawns.objects"],
     definitions: ObjectDefinitions = get(),
 ) = timedLoad("object spawn") {
     objects.reset()
     val membersWorld = World.members
-    var count = 0
-    val config = object : YamlReaderConfiguration() {
-        override fun add(list: MutableList<Any>, value: Any, parentMap: String?) {
-            value as Map<String, Any>
-            val members = value["members"] as? Boolean ?: false
-            if (!membersWorld && members) {
-                return
+    Config.fileReader(path) {
+        while (nextPair()) {
+            require(key() == "spawns")
+            while (nextElement()) {
+                var id = ""
+                var rotation = 0
+                var x = 0
+                var y = 0
+                var level = 0
+                var type = 10
+                var members = false
+                while (nextEntry()) {
+                    when (val key = key()) {
+                        "id" -> id = string()
+                        "x" -> x = int()
+                        "y" -> y = int()
+                        "level" -> level = int()
+                        "rotation" -> rotation = int()
+                        "type" -> type = int()
+                        "members" -> members = boolean()
+                        else -> throw IllegalArgumentException("Unexpected key: '$key' ${exception()}")
+                    }
+                }
+                if (!membersWorld && members) {
+                    continue
+                }
+                val tile = Tile(x, y, level)
+                objects.add(GameObject(definitions.get(id).id, tile.x, tile.y, tile.level, type, rotation))
             }
-            val id = value["id"] as String
-            val tile = Tile.fromMap(value)
-            val type = value["type"] as Int
-            val rotation = value["rotation"] as? Int ?: 0
-            count++
-            objects.add(GameObject(definitions.get(id).id, tile.x, tile.y, tile.level, type, rotation))
         }
     }
-    yaml.load<Any>(path, config)
-    count
+    objects.size
 }
