ea42a56239403abcbcbc6a22965c642e35d79f30
GregHib <greghib@users.noreply.github.com>
2025-03-26 17:22:13 +0000

    Change inventory format

--- engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/InventoryDefinitions.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/InventoryDefinitions.kt
@@ -6,13 +6,9 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
 import it.unimi.dsi.fastutil.objects.ObjectArrayList
 import world.gregs.config.Config
 import world.gregs.voidps.cache.config.data.InventoryDefinition
-import world.gregs.voidps.engine.data.Settings
+import world.gregs.voidps.engine.entity.item.Item
 import world.gregs.voidps.engine.get
 import world.gregs.voidps.engine.timedLoad
-import java.nio.file.Files
-import java.nio.file.Path
-import kotlin.io.path.extension
-import kotlin.io.path.pathString
 
 class InventoryDefinitions(
     override var definitions: Array<InventoryDefinition>
@@ -29,30 +25,37 @@ class InventoryDefinitions(
                 Config.fileReader(path) {
                     while (nextSection()) {
                         val stringId = section()
-                        var id = -1
+                        var invId = -1
                         val extras = Object2ObjectOpenHashMap<String, Any>(0, Hash.VERY_FAST_LOAD_FACTOR)
                         while (nextPair()) {
                             when (val key = key()) {
-                                "id" -> id = int()
+                                "id" -> invId = int()
                                 "defaults" -> {
-                                    val defaults = ObjectArrayList<Map<String, Int>>()
-                                    while (nextEntry()) {
-                                        val item = key()
-                                        val value = int()
-                                        val default = Object2IntOpenHashMap<String>()
-                                        default[item] = value
-                                        defaults.add(default)
+                                    val defaults = ObjectArrayList<Item>()
+                                    while (nextElement()) {
+                                        var id = ""
+                                        var amount = 0
+                                        while (nextEntry()) {
+                                            when (val itemKey = key()) {
+                                                "id" -> id = string()
+                                                "amount" -> amount = int()
+                                                else -> throw IllegalArgumentException("Unexpected key: '$itemKey' ${exception()}")
+                                            }
+                                        }
+                                        require(itemDefs.contains(id)) { "Unable to find inventory item with id '$id' at $path." }
+                                        val item = Item(id, amount)
+                                        defaults.add(item)
                                     }
                                     extras[key] = defaults
                                 }
                                 else -> extras[key] = value()
                             }
                         }
-                        if (id > -1) {
+                        if (invId > -1) {
                             require(!ids.containsKey(stringId)) { "Duplicate inventory found '$stringId' at $path." }
-                            ids[stringId] = id
-                            definitions[id].extras = extras
-                            definitions[id].stringId = stringId
+                            ids[stringId] = invId
+                            definitions[invId].extras = extras
+                            definitions[invId].stringId = stringId
                         }
                     }
                 }
