45364370f235f57e3c41becad49151f6aad41149
GregHib <greghib@users.noreply.github.com>
2025-03-26 00:37:37 +0000

    Flatten gear definitions

--- engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/GearDefinitions.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/data/definition/GearDefinitions.kt
@@ -9,6 +9,7 @@ import world.gregs.config.ConfigReader
 import world.gregs.voidps.engine.client.ui.chat.toIntRange
 import world.gregs.voidps.engine.data.Settings
 import world.gregs.voidps.engine.data.config.GearDefinition
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.item.Item
 import world.gregs.voidps.engine.timedLoad
 import world.gregs.voidps.network.login.protocol.visual.update.player.EquipSlot
@@ -21,52 +22,51 @@ class GearDefinitions {
 
     fun load(path: String = Settings["definitions.gearSets"]): GearDefinitions {
         timedLoad("gear definition") {
-            val definitions = Object2ObjectOpenHashMap<String, List<GearDefinition>>(100, Hash.VERY_FAST_LOAD_FACTOR)
+            val definitions = Object2ObjectOpenHashMap<String, MutableList<GearDefinition>>(100, Hash.VERY_FAST_LOAD_FACTOR)
             Config.fileReader(path) {
-                while (nextPair()) {
-                    val style = key()
-                    val gear = ObjectArrayList<GearDefinition>(4)
-                    while (nextElement()) {
-                        var levels = 0..0
-                        val inventory = ObjectArrayList<List<Item>>(2)
-                        val equipment = Object2ObjectOpenHashMap<EquipSlot, List<Item>>(2)
-                        val extras = Object2ObjectOpenHashMap<String, Any>(0)
-                        while (nextEntry()) {
-                            when (val key = key()) {
-                                "levels" -> levels = string().toIntRange()
-                                "inventory" -> {
-                                    while (nextElement()) {
-                                        val ids = ObjectArrayList<String>(1)
-                                        var amount = 1
-                                        while (nextEntry()) {
-                                            when (key()) {
-                                                "ids" -> while (nextElement()) {
-                                                    ids.add(string())
-                                                }
-                                                "id" -> ids.add(string())
-                                                "amount" -> amount = int()
-                                            }
-                                        }
-                                        val items = ObjectArrayList<Item>(ids.size)
-                                        for (item in ids) {
-                                            items.add(Item(item, amount))
-                                        }
-                                        inventory.add(items)
-                                    }
-                                }
-                                "equipment" -> {
+                while (nextSection()) {
+                    require(section() != "")
+                    var levels = 0..0
+                    var style = ""
+                    val inventory = ObjectArrayList<List<Item>>(2)
+                    val equipment = Object2ObjectOpenHashMap<EquipSlot, List<Item>>(2)
+                    val extras = Object2ObjectOpenHashMap<String, Any>(0)
+                    while (nextPair()) {
+                        when (val key = key()) {
+                            "style" -> style = string()
+                            "levels" -> levels = string().toIntRange()
+                            "inventory" -> {
+                                while (nextElement()) {
+                                    val ids = ObjectArrayList<String>(1)
+                                    var amount = 1
                                     while (nextEntry()) {
-                                        val slot = EquipSlot.valueOf(key().toSentenceCase())
-                                        val items = items(1)
-                                        equipment[slot] = items
+                                        when (key()) {
+                                            "ids" -> while (nextElement()) {
+                                                ids.add(string())
+                                            }
+                                            "id" -> ids.add(string())
+                                            "amount" -> amount = int()
+                                        }
                                     }
+                                    val items = ObjectArrayList<Item>(ids.size)
+                                    for (item in ids) {
+                                        items.add(Item(item, amount))
+                                    }
+                                    inventory.add(items)
                                 }
-                                else -> extras[key] = value()
                             }
+                            "equipment" -> {
+                                while (nextEntry()) {
+                                    val slot = EquipSlot.valueOf(key().toSentenceCase())
+                                    val items = items(1)
+                                    equipment[slot] = items
+                                }
+                            }
+                            else -> extras[key] = value()
                         }
-                        gear.add(GearDefinition(style, levels, equipment, inventory, extras = extras.ifEmpty { null }))
                     }
-                    definitions[style] = gear
+                    definitions.getOrPut(style) { ObjectArrayList(4) }
+                        .add(GearDefinition(style, levels, equipment, inventory, extras = extras.ifEmpty { null }))
                 }
             }
             this.definitions = definitions
