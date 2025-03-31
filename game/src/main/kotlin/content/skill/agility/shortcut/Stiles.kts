58af52c1b47c5b3e2a32b74de526e3b9a20db6fa
GregHib <greghib@users.noreply.github.com>
2025-03-31 11:45:21 +0100

    Add stile shortcuts

---
+++ game/src/main/kotlin/content/skill/agility/shortcut/Stiles.kts
@@ -0,0 +1,57 @@
+package content.skill.agility.shortcut
+
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.player.chat.noInterest
+import world.gregs.voidps.engine.entity.obj.ObjectOption
+import world.gregs.voidps.engine.entity.obj.objectOperate
+import world.gregs.voidps.type.Direction
+
+objectOperate("Climb-over", "freds_farm_stile") {
+    climbStile(Direction.NORTH)
+}
+
+objectOperate("Climb-over", "catherby_stile") {
+    climbStile(Direction.NORTH)
+}
+
+objectOperate("Climb-over", "death_plateau_stile") {
+    climbStile(Direction.NORTH)
+}
+
+objectOperate("Climb-over", "falconry_area_stile") {
+    climbStile(Direction.NORTH)
+}
+
+objectOperate("Climb-over", "ardougne_farm_stile") {
+    climbStile(Direction.EAST)
+}
+
+objectOperate("Climb-over", "falador_farm_stile") {
+    climbStile(Direction.EAST)
+}
+
+objectOperate("Climb-over", "vinesweeper_stile") {
+    val rotation = when (target.rotation) {
+        0, 2 -> Direction.NORTH
+        1, 3 -> Direction.EAST
+        else -> return@objectOperate player.noInterest()
+    }
+    climbStile(rotation)
+}
+
+suspend fun ObjectOption<Player>.climbStile(rotation: Direction) {
+    val direction = when (rotation) {
+        Direction.NORTH -> if (player.tile.y > target.tile.y) Direction.SOUTH else Direction.NORTH
+        Direction.SOUTH -> if (player.tile.y < target.tile.y) Direction.NORTH else Direction.SOUTH
+        Direction.EAST -> if (player.tile.x > target.tile.x) Direction.WEST else Direction.EAST
+        Direction.WEST -> if (player.tile.x < target.tile.x) Direction.EAST else Direction.WEST
+        else -> return player.noInterest()
+    }
+    val start = if (direction == rotation) target.tile else target.tile.minus(direction)
+    player.walkOverDelay(start)
+    player.face(direction)
+    delay()
+    player.anim("rocks_pile_climb")
+    val target = if (direction == rotation) target.tile.add(direction) else target.tile
+    player.exactMoveDelay(target, 30, direction = direction)
+}
---
+++ game/src/test/kotlin/content/skill/agility/shortcut/StilesTest.kt
@@ -0,0 +1,60 @@
+package content.skill.agility.shortcut
+
+import WorldTest
+import objectOption
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
+import world.gregs.voidps.type.Delta
+import world.gregs.voidps.type.Tile
+import kotlin.test.assertEquals
+
+internal class StilesTest : WorldTest() {
+
+    @TestFactory
+    fun `Climb east facing stile`() = listOf(
+        Delta(0, 0) to Tile(2648, 3375),
+        Delta(-1, 0) to Tile(2648, 3375),
+        Delta(0, -1) to Tile(2648, 3375),
+        Delta(0, 1) to Tile(2648, 3375),
+        Delta(1, 0) to Tile(2647, 3375),
+        Delta(2, 0) to Tile(2647, 3375),
+        Delta(1, -1) to Tile(2647, 3375),
+        Delta(1, 1) to Tile(2647, 3375),
+    ).map { (delta, target) ->
+        dynamicTest("Climb east stile from $delta to $target") {
+            val objTile = Tile(2647, 3375)
+            val obj = objects[objTile, "ardougne_farm_stile"]!!
+            val player = createPlayer("", objTile.add(delta))
+
+            player.objectOption(obj, "Climb-over")
+
+            tick(4)
+
+            assertEquals(target, player.tile)
+        }
+    }
+
+    @TestFactory
+    fun `Climb north facing stile`() = listOf(
+        Delta(0, 0) to Tile(3197, 3277),
+        Delta(0, -1) to Tile(3197, 3277),
+        Delta(-1, 0) to Tile(3197, 3277),
+        Delta(1, 0) to Tile(3197, 3277),
+        Delta(0, 1) to Tile(3197, 3276),
+        Delta(0, 2) to Tile(3197, 3276),
+        Delta(-1, 1) to Tile(3197, 3276),
+        Delta(1, 1) to Tile(3197, 3276),
+    ).map { (delta, target) ->
+        dynamicTest("Climb north stile from $delta to $target") {
+            val objTile = Tile(3197, 3276)
+            val obj = objects[objTile, "freds_farm_stile"]!!
+            val player = createPlayer("", objTile.add(delta))
+
+            player.objectOption(obj, "Climb-over")
+
+            tick(4)
+
+            assertEquals(target, player.tile)
+        }
+    }
+}
