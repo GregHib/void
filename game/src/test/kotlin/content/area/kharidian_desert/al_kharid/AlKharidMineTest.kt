fd1b0e89be4ecb6a0afc90aee363d745070e0d57
GregHib <greghib@users.noreply.github.com>
2025-04-14 19:08:21 +0100

    Add al kharid mine shortcut

---
+++ game/src/test/kotlin/content/area/kharidian_desert/al_kharid/AlKharidMineTest.kt
@@ -0,0 +1,57 @@
+package content.area.kharidian_desert.al_kharid
+
+import WorldTest
+import containsMessage
+import objectOption
+import org.junit.jupiter.api.Test
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.type.Tile
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class AlKharidMineTest : WorldTest() {
+
+    @Test
+    fun `Climb up mine shortcut`() {
+        val player = createPlayer(tile = Tile(3303, 3315))
+        player.levels.set(Skill.Agility, 38)
+        val rocks = objects[Tile(3304, 3315), "al_kharid_mine_shortcut_bottom"]!!
+        player.objectOption(rocks, "Climb")
+        tick(5)
+
+        assertEquals(Tile(3307, 3315), player.tile)
+    }
+
+    @Test
+    fun `Can't climb up mine shortcut without levels`() {
+        val player = createPlayer(tile = Tile(3303, 3315))
+        player.levels.set(Skill.Agility, 37)
+        val rocks = objects[Tile(3304, 3315), "al_kharid_mine_shortcut_bottom"]!!
+        player.objectOption(rocks, "Climb")
+        tick(2)
+
+        assertTrue(player.containsMessage("You must have an Agility level of at least 38"))
+    }
+
+    @Test
+    fun `Climb down mine shortcut`() {
+        val player = createPlayer(tile = Tile(3307, 3315))
+        player.levels.set(Skill.Agility, 38)
+        val rocks = objects[Tile(3306, 3315), "al_kharid_mine_shortcut_top"]!!
+        player.objectOption(rocks, "Climb")
+        tick(5)
+
+        assertEquals(Tile(3303, 3315), player.tile)
+    }
+
+    @Test
+    fun `Can't climb down mine shortcut without levels`() {
+        val player = createPlayer(tile = Tile(3307, 3315))
+        player.levels.set(Skill.Agility, 37)
+        val rocks = objects[Tile(3306, 3315), "al_kharid_mine_shortcut_top"]!!
+        player.objectOption(rocks, "Climb")
+        tick(2)
+
+        assertTrue(player.containsMessage("You must have an Agility level of at least 38"))
+    }
+}
