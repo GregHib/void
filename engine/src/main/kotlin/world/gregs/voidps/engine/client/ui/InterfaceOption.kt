1616cf17a457bce1b2bf3eed0fa934113ffa95c9
GregHib <greghib@users.noreply.github.com>
2025-04-17 21:01:13 +0100

    Revert handlers in this PR

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/ui/InterfaceOption.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/ui/InterfaceOption.kt
@@ -1,15 +1,10 @@
 package world.gregs.voidps.engine.client.ui
 
-import it.unimi.dsi.fastutil.Hash
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
-import world.gregs.voidps.cache.definition.data.InterfaceComponentDefinition
-import world.gregs.voidps.engine.data.definition.InterfaceDefinitions
 import world.gregs.voidps.engine.entity.character.mode.interact.Interaction
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.item.Item
 import world.gregs.voidps.engine.event.EventDispatcher
-import world.gregs.voidps.engine.event.wildcardEquals
-import world.gregs.voidps.engine.get
+import world.gregs.voidps.engine.event.Events
 
 data class InterfaceOption(
     override val character: Player,
@@ -33,59 +28,11 @@ data class InterfaceOption(
         4 -> itemSlot.toString()
         else -> null
     }
-
-    companion object {
-        val handlers: MutableMap<String, suspend InterfaceOption.() -> Unit> = Object2ObjectOpenHashMap(2_000, Hash.VERY_FAST_LOAD_FACTOR)
-    }
 }
 
 fun interfaceOption(option: String = "*", component: String = "*", id: String, handler: suspend InterfaceOption.() -> Unit) {
     assert(!id.contains("*")) { "Interface ids cannot contain wildcards. id=$id, component=$component, option='$option'" }
-    var added = false
-    val definitions = get<InterfaceDefinitions>().get(id)
-    if (!option.contains("*")) {
-        for (componentDefinition in definitions.components!!.values) {
-            if (componentDefinition.stringId != "" && wildcardEquals(component, componentDefinition.stringId)) {
-                val key = "$id:${componentDefinition.stringId}:$option"
-                InterfaceOption.handlers[key] = handler
-                added = true
-            }
-        }
-    } else if (option == "*") {
-        for (componentDefinition in definitions.components!!.values) {
-            if (componentDefinition.stringId != "" && wildcardEquals(component, componentDefinition.stringId)) {
-                val key = "$id:${componentDefinition.stringId}"
-                InterfaceOption.handlers[key] = handler
-                added = true
-            }
-        }
-    } else {
-        for (componentDefinition in definitions.components!!.values) {
-            if (componentDefinition.stringId != "" && wildcardEquals(component, componentDefinition.stringId)) {
-                var options = componentDefinition.options
-                if (check(options, option, id, componentDefinition, handler)) {
-                    added = true
-                }
-                options = componentDefinition.extras?.get("options") as? Array<String?>
-                if (check(options, option, id, componentDefinition, handler)) {
-                    added = true
-                }
-            }
-        }
+    Events.handle<InterfaceOption>("interface_option", id, component, option, "*") {
+        handler.invoke(this)
     }
-    assert(added) { "Unable to find interface id=$id, component=$component, option=$option" }
-}
-
-private fun check(options: Array<String?>?, option: String, id: String, componentDefinition: InterfaceComponentDefinition, handler: suspend InterfaceOption.() -> Unit): Boolean {
-    var added = false
-    if (options != null) {
-        for (opt in options) {
-            if (opt != null && wildcardEquals(option, opt)) {
-                val key = "$id:${componentDefinition.stringId}:$opt"
-                InterfaceOption.handlers[key] = handler
-                added = true
-            }
-        }
-    }
-    return added
 }
