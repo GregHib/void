9c57a85162b8304bbb92da3f076c077161a79867
GregHib <greghib@users.noreply.github.com>
2025-05-03 17:01:28 +0100

    Add flax pick chances

--- game/src/main/kotlin/content/entity/obj/Picking.kts
+++ game/src/main/kotlin/content/entity/obj/Picking.kts
@@ -9,6 +9,7 @@ import world.gregs.voidps.engine.entity.obj.remove
 import world.gregs.voidps.engine.inv.add
 import world.gregs.voidps.engine.inv.inventory
 import world.gregs.voidps.engine.timer.toTicks
+import world.gregs.voidps.type.random
 import java.util.concurrent.TimeUnit
 
 val GameObject.pickable: Pickable?
@@ -18,7 +19,9 @@ objectOperate("Pick") {
     val pickable: Pickable = target.pickable ?: return@objectOperate
     if (player.inventory.add(pickable.item)) {
         player.anim("climb_down")
-        target.remove(TimeUnit.SECONDS.toTicks(pickable.respawnDelay))
+        if (random.nextInt(pickable.chance) == 0) {
+            target.remove(TimeUnit.SECONDS.toTicks(pickable.respawnDelay))
+        }
         player.message(pickable.message)
     } else {
         player.inventoryFull()
