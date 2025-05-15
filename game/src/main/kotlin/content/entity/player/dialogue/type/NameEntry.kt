071041828812831611263a261fdfa0554ba69172
lucid-soft <69691787+lucid-soft@users.noreply.github.com>
2025-05-15 12:54:10 -0400

    Change StringEntry to NameEntry, which has a max of 13 characters and has last input recall for player name usage. Change ClanChatDecoder to StringEntryDecoder for new StringEntry which lets user input much longer strings

---
+++ game/src/main/kotlin/content/entity/player/dialogue/type/NameEntry.kt
@@ -0,0 +1,11 @@
+package content.entity.player.dialogue.type
+
+import world.gregs.voidps.engine.client.sendScript
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.event.Context
+import world.gregs.voidps.engine.suspend.NameSuspension
+
+suspend fun Context<Player>.nameEntry(text: String): String {
+    player.sendScript("name_entry", text)
+    return NameSuspension.get(player)
+}
--- network/src/main/kotlin/world/gregs/voidps/network/login/protocol/decode/NameEntryDecoder.kt
+++ network/src/main/kotlin/world/gregs/voidps/network/login/protocol/decode/NameEntryDecoder.kt
@@ -2,15 +2,14 @@ package world.gregs.voidps.network.login.protocol.decode
 
 import io.ktor.utils.io.core.*
 import world.gregs.voidps.network.client.Instruction
+import world.gregs.voidps.network.client.instruction.EnterName
 import world.gregs.voidps.network.login.protocol.Decoder
 import world.gregs.voidps.network.login.protocol.readString
 
-class ClanChatNameDecoder : Decoder(BYTE) {
+class NameEntryDecoder : Decoder(BYTE) {
 
-    override suspend fun decode(packet: ByteReadPacket): Instruction? {
-        val index = packet.readInt()
-        val name = packet.readString()
-        return null
+    override suspend fun decode(packet: ByteReadPacket): Instruction {
+        return EnterName(packet.readString())
     }
 
 }
