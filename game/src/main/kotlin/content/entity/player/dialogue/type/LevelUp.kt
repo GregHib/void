9a670c21ec3838d4e76b8e9ae332f0170e7cc264
GregHib <greghib@users.noreply.github.com>
2025-05-29 13:28:21 +0100

    Remove queue usage for level up dialogue fixes #699

--- game/src/main/kotlin/content/entity/player/dialogue/type/LevelUp.kt
+++ game/src/main/kotlin/content/entity/player/dialogue/type/LevelUp.kt
@@ -10,12 +10,16 @@ import world.gregs.voidps.engine.suspend.ContinueSuspension
 private const val LEVEL_UP_INTERFACE_ID = "dialogue_level_up"
 
 suspend fun SuspendableContext<Player>.levelUp(skill: Skill, text: String) {
+    levelUp(player, skill, text)
+    ContinueSuspension.get(player)
+    player.close(LEVEL_UP_INTERFACE_ID)
+}
+
+fun levelUp(player: Player, skill: Skill, text: String) {
     val lines = text.trimIndent().lines()
     check(player.open(LEVEL_UP_INTERFACE_ID)) { "Unable to open level up interface for $player" }
     for ((index, line) in lines.withIndex()) {
         player.interfaces.sendText(LEVEL_UP_INTERFACE_ID, "line${index + 1}", line)
     }
     player["level_up_icon"] = skill.name
-    ContinueSuspension.get(player)
-    player.close(LEVEL_UP_INTERFACE_ID)
 }
