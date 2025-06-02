8fe3e711ecff0e33daeeffc12521a5004bc7c5f1
Jerry <jerryju@metropolia.fi>
2025-06-02 14:19:17 +0300

    Added Lachtopher and Victoria

---
+++ game/src/main/kotlin/content/entity/npc/Lachtopher.kts
@@ -0,0 +1,14 @@
+package content.entity.npc
+
+import world.gregs.voidps.engine.entity.character.npc.npcOperate
+import content.entity.player.dialogue.Happy
+import content.entity.player.dialogue.Sad
+import content.entity.player.dialogue.Surprised
+import content.entity.player.dialogue.Angry
+import content.entity.player.dialogue.Quiz
+import content.entity.player.dialogue.type.choice
+import content.entity.player.dialogue.type.npc
+import content.entity.player.dialogue.type.player
+
+//npcOperate("Talk-to", "lachtopher*")
+
