28a28dc04f7628a8edcd9af7053d625e20b362e2
RSDEANEO <deane1991@live.co.uk>
2025-06-05 01:08:20 +0100

    added missing spawns for edge / varrock sewer n items

---
+++ game/src/main/kotlin/content/area/misthalin/varrock/BrassKey.kts
@@ -0,0 +1,17 @@
+package content.area.misthalin.varrock
+
+import content.entity.obj.door.enterDoor
+import content.entity.sound.sound
+import world.gregs.voidps.engine.client.message
+import world.gregs.voidps.engine.entity.obj.objectOperate
+import world.gregs.voidps.engine.inv.inventory
+
+objectOperate("Open", "door_27_closed") {
+            if (player.inventory.contains("brass_key")) {
+                player.sound("unlock")
+                enterDoor(target)
+            } else {
+                player.sound("locked")
+                player.message("The door is locked. You need a brass key to open it.")
+            }
+        }
