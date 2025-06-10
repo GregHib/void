1856ee3e68597e7d619c81c2430b64444fab10bf
GregHib <greghib@users.noreply.github.com>
2025-06-10 16:52:47 +0100

    Add npc hp restoration closes #649

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/npc/NPC.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/npc/NPC.kt
@@ -53,6 +53,8 @@ data class NPC(
 
     override lateinit var collision: CollisionStrategy
 
+    var regenCounter = 0
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
