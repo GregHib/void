ee01f23e705cabce9eeae428b180c23b1660d96c
Jerry <207991987+M1krofin@users.noreply.github.com>
2025-06-17 19:57:44 +0300

    Fixed all the changes

--- game/src/main/kotlin/content/entity/npc/combat/melee/Imp.kts
+++ game/src/main/kotlin/content/entity/npc/combat/melee/Imp.kts
@@ -9,13 +9,14 @@ import world.gregs.voidps.engine.timer.npcTimerStart
 import world.gregs.voidps.engine.timer.npcTimerTick
 import world.gregs.voidps.type.Tile
 import world.gregs.voidps.type.random
-import world.gregs.voidps.engine.get
-import world.gregs.voidps.engine.map.collision.Collisions
 import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.queue.queue
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.engine.map.collision.random
 import content.entity.sound.areaSound
 import content.entity.combat.hit.npcCombatDamage
-import world.gregs.voidps.engine.queue.queue
 import content.entity.sound.areaSound
+import content.entity.gfx.areaGfx
 
 private val teleportRadiusMax = 20
 private val teleportRadiusMin = 5
@@ -23,19 +24,11 @@ private val teleportChance = 0.25
 private val teleportChanceHit = 0.10
 private val telePoofVfxRadius = 5
 
-fun isBlocked(tile: Tile): Boolean {
-    val collisions = get<Collisions>()
-    val flags = collisions.get(tile.x, tile.y, tile.level)
-    return flags != 0
-}
-
 fun randomValidTile(npc: NPC): Tile {
     repeat(10) {
-        val dest = npc.tile.toCuboid(teleportRadiusMax).random()
-        if (!isBlocked(dest ) && dest.region == npc.tile.region) {
-            if (npc.tile.distanceTo(dest) >= teleportRadiusMin) {
-                return dest
-            }
+        val dest = npc.tile.toCuboid(teleportRadiusMax).random(npc) ?: return@repeat
+        if (dest.region == npc.tile.region && dest != npc.tile && npc.tile.distanceTo(dest) >= teleportRadiusMin) {
+            return dest
         }
     }
     return npc.tile
@@ -49,20 +42,22 @@ fun teleportImp(npc: NPC, chance: Double, target: Player?) {
         return
     }
 
-    val destination = randomValidTile(npc)
-
     npc.softTimers.restart("teleport_timer")
+    val destination = randomValidTile(npc)
+    if (destination == npc.tile) {
+        return
+    }
 
     npc.softQueue("imp_teleport") {
         areaSound("imp_puff_teleport", npc.tile, telePoofVfxRadius)
-            npc.gfx("imp_puff")
-            delay(1)
-            npc.tele(destination)
-            npc.gfx("imp_puff")
-            npc.mode = PauseMode
-            delay(1)
-            npc.tile = destination
-            npc.steps.clear()
+        areaGfx("imp_puff", npc.tile)
+        npc.steps.clear()
+        val mode = npc.mode
+        npc.mode = PauseMode
+        npc.tele(destination)
+        delay(1)
+        npc.gfx("imp_puff")
+        npc.mode = mode
         }
     }
 
@@ -79,5 +74,7 @@ npcTimerTick("teleport_timer") { npc ->
 }
 
 npcCombatDamage("imp") { npc ->
-    teleportImp(npc, teleportChanceHit, null)
+    if (npc.levels.get(Skill.Constitution) - damage > 0) {
+        teleportImp(npc, teleportChanceHit, null)
+    }
 }
