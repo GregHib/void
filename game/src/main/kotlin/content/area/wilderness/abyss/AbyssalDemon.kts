6cde0219579bb5fa4abfaf5ff59e3f4b81105e62
GregHib <greghib@users.noreply.github.com>
2025-06-17 19:50:00 +0100

    Add abyssal demon teleporting sounds and gfx

--- game/src/main/kotlin/content/area/wilderness/abyss/AbyssalDemon.kts
+++ game/src/main/kotlin/content/area/wilderness/abyss/AbyssalDemon.kts
@@ -1,22 +1,21 @@
 package content.area.wilderness.abyss
 
 import content.entity.combat.hit.npcCombatDamage
-import world.gregs.voidps.engine.data.definition.AreaDefinitions
+import content.entity.sound.sound
 import world.gregs.voidps.engine.entity.character.move.tele
-import world.gregs.voidps.engine.inject
 import world.gregs.voidps.engine.map.collision.random
 import world.gregs.voidps.type.random
 
-val areas: AreaDefinitions by inject()
-
 npcCombatDamage("abyssal_demon") { npc ->
-    val area = areas.getTagged("abyssal").firstOrNull { it.area.contains(npc.tile) } ?: return@npcCombatDamage
-    // TODO anims, gfx, sounds
     if (random.nextInt(6) == 0) {
-        val tile = area.area.random(npc) ?: return@npcCombatDamage
-        npc.tele(tile)
+        val tile = source.tile.toCuboid(1).random(npc) ?: return@npcCombatDamage
+        npc.tele(tile, clearMode = false)
+        npc.anim("abyssal_demon_teleport")
+        npc.gfx("abyssal_demon_teleport")
+        npc.sound("abyssal_demon_teleport")
     } else if (random.nextInt(3) == 0) {
-        val tile = area.area.random(source.collision, size) ?: return@npcCombatDamage
+        val tile = npc.tile.toCuboid(1).random(npc) ?: return@npcCombatDamage
         source.tele(tile)
+        source.gfx("abyssal_demon_teleport")
     }
 }
