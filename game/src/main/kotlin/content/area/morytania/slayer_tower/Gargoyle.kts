bde8471ed46ba10a5307b3c1c4894b92fb502c1a
GregHib <greghib@users.noreply.github.com>
2025-06-17 20:05:12 +0100

    Fix gargoyles

--- game/src/main/kotlin/content/area/morytania/slayer_tower/Gargoyle.kts
+++ game/src/main/kotlin/content/area/morytania/slayer_tower/Gargoyle.kts
@@ -3,27 +3,28 @@ package content.area.morytania.slayer_tower
 import content.entity.combat.attackers
 import content.entity.combat.hit.damage
 import world.gregs.voidps.engine.client.message
-import world.gregs.voidps.engine.client.ui.interact.ItemOnNPC
 import world.gregs.voidps.engine.client.ui.interact.itemOnNPCOperate
 import world.gregs.voidps.engine.entity.character.mode.EmptyMode
 import world.gregs.voidps.engine.entity.character.npc.NPC
+import world.gregs.voidps.engine.entity.character.npc.npcOperate
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
-import world.gregs.voidps.engine.entity.character.player.skill.level.CurrentLevelChanged
 import world.gregs.voidps.engine.entity.character.player.skill.level.npcLevelChange
 import world.gregs.voidps.engine.inv.inventory
-import world.gregs.voidps.engine.inv.remove
 
 npcLevelChange("gargoyle", Skill.Constitution) { npc ->
     if (to <= 90) {
         for (attacker in npc.attackers) {
             attacker.mode = EmptyMode
             if (attacker is Player && attacker["killing_blow", false]) {
-                // TODO message
                 smash(attacker, npc)
             }
         }
     }
+}
+
+npcOperate("Smash", "gargoyle") {
+    smash(player, target)
 }
 
 itemOnNPCOperate("rock_hammer", "gargoyle") {
@@ -32,16 +33,16 @@ itemOnNPCOperate("rock_hammer", "gargoyle") {
 
 fun smash(player: Player, target: NPC) {
     if (!player.inventory.contains("rock_hammer")) {
+        player.message("You need a rock hammer to do that.") // TODO proper message
         return
     }
-    player.anim("pie_accurate")
+    player.anim("pie_accurate") // TODO proper anim
     target.gfx("skip_water_splash")
     val hitpoints = target.levels.get(Skill.Constitution)
     if (hitpoints >= 90) {
         player.message("The gargoyle isn't weak enough to be harmed by the hammer.")
         return
     }
-    // TODO anim
     player.message("The gargoyle cracks apart.")
     target.damage(hitpoints, source = player)
 }
