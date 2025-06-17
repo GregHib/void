f27040c357a76c9001516b857a546f57e62ee46b
GregHib <greghib@users.noreply.github.com>
2025-06-17 21:05:59 +0100

    Add npc and player collision to area.random

--- engine/src/main/kotlin/world/gregs/voidps/engine/map/collision/Collisions.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/map/collision/Collisions.kt
@@ -4,7 +4,9 @@ import org.rsmod.game.pathfinder.StepValidator
 import org.rsmod.game.pathfinder.collision.CollisionFlagMap
 import org.rsmod.game.pathfinder.collision.CollisionStrategies
 import org.rsmod.game.pathfinder.collision.CollisionStrategy
+import org.rsmod.game.pathfinder.flag.CollisionFlag
 import world.gregs.voidps.engine.entity.character.Character
+import world.gregs.voidps.engine.entity.character.npc.NPC
 import world.gregs.voidps.engine.get
 import world.gregs.voidps.type.Area
 import world.gregs.voidps.type.Tile
@@ -31,13 +33,13 @@ fun Collisions.clear(zone: Zone) {
     deallocateIfPresent(zone.tile.x, zone.tile.y, zone.level)
 }
 
-fun Area.random(character: Character): Tile? = random(character.collision, character.size)
+fun Area.random(character: Character): Tile? = random(character.collision, character.size, if (character is NPC && character.def["solid", true]) CollisionFlag.BLOCK_PLAYERS or CollisionFlag.BLOCK_NPCS else 0)
 
-fun Area.random(collision: CollisionStrategy = CollisionStrategies.Normal, size: Int = 1): Tile? {
+fun Area.random(collision: CollisionStrategy = CollisionStrategies.Normal, size: Int = 1, extraFlag: Int = 0): Tile? {
     val steps = get<StepValidator>()
     var tile = random()
     var exit = 100
-    while (!canFit(steps, tile, collision, size)) {
+    while (!canFit(steps, tile, collision, size, extraFlag)) {
         if (--exit <= 0) {
             return null
         }
@@ -46,19 +48,19 @@ fun Area.random(collision: CollisionStrategy = CollisionStrategies.Normal, size:
     return tile
 }
 
-private fun canFit(steps: StepValidator, tile: Tile, collision: CollisionStrategy, size: Int): Boolean {
+private fun canFit(steps: StepValidator, tile: Tile, collision: CollisionStrategy, size: Int, extraFlag: Int): Boolean {
     if (size != 1) {
         for (i in 1 until size) {
-            if (!steps.canTravel(tile.level, tile.x - i, tile.y, 1, 0, size)) {
+            if (!steps.canTravel(tile.level, tile.x - i, tile.y, 1, 0, size, extraFlag)) {
                 return false
             }
-            if (!steps.canTravel(tile.level, tile.x, tile.y - i, 0, 1, size)) {
+            if (!steps.canTravel(tile.level, tile.x, tile.y - i, 0, 1, size, extraFlag)) {
                 return false
             }
-            if (!steps.canTravel(tile.level, tile.x + i, tile.y, -1, 0, size)) {
+            if (!steps.canTravel(tile.level, tile.x + i, tile.y, -1, 0, size, extraFlag)) {
                 return false
             }
-            if (!steps.canTravel(tile.level, tile.x, tile.y + i, 0, -1, size)) {
+            if (!steps.canTravel(tile.level, tile.x, tile.y + i, 0, -1, size, extraFlag)) {
                 return false
             }
         }
@@ -72,6 +74,7 @@ private fun canFit(steps: StepValidator, tile: Tile, collision: CollisionStrateg
         offsetZ = 1,
         size = size,
         collision = collision,
+        extraFlag = extraFlag,
     ) || steps.canTravel(
         x = tile.x,
         z = tile.y + 1,
@@ -80,6 +83,7 @@ private fun canFit(steps: StepValidator, tile: Tile, collision: CollisionStrateg
         offsetZ = -1,
         size = size,
         collision = collision,
+        extraFlag = extraFlag,
     ) || steps.canTravel(
         x = tile.x - 1,
         z = tile.y,
@@ -88,6 +92,7 @@ private fun canFit(steps: StepValidator, tile: Tile, collision: CollisionStrateg
         offsetZ = 0,
         size = size,
         collision = collision,
+        extraFlag = extraFlag,
     ) || steps.canTravel(
         x = tile.x + 1,
         z = tile.y,
@@ -96,5 +101,6 @@ private fun canFit(steps: StepValidator, tile: Tile, collision: CollisionStrateg
         offsetZ = 0,
         size = size,
         collision = collision,
+        extraFlag = extraFlag,
     )
 }
