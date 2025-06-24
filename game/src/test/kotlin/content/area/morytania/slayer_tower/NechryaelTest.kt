70f13f2e310a858db4fce96018c23d8da8dee358
GregHib <greghib@users.noreply.github.com>
2025-06-24 16:16:44 +0100

    Fix nechryael tests

--- game/src/test/kotlin/content/area/morytania/slayer_tower/NechryaelTest.kt
+++ game/src/test/kotlin/content/area/morytania/slayer_tower/NechryaelTest.kt
@@ -1,15 +1,21 @@
 package content.area.morytania.slayer_tower
 
+import FakeRandom
 import WorldTest
 import npcOption
 import org.junit.jupiter.api.Test
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.type.setRandom
 import kotlin.test.assertEquals
 
 class NechryaelTest : WorldTest() {
 
     @Test
     fun `Nechryael death spawns`() {
+        setRandom(object : FakeRandom() {
+            override fun nextInt(until: Int) = 0
+            override fun nextInt(from: Int, until: Int) = 0
+        })
         val player = createPlayer(emptyTile)
         player.levels.set(Skill.Constitution, 50)
         player.levels.set(Skill.Slayer, 80)
@@ -26,6 +32,10 @@ class NechryaelTest : WorldTest() {
 
     @Test
     fun `Death spawns can attack even when in combat`() {
+        setRandom(object : FakeRandom() {
+            override fun nextInt(until: Int) = 0
+            override fun nextInt(from: Int, until: Int) = 0
+        })
         val player = createPlayer(emptyTile)
         player.levels.set(Skill.Constitution, 50)
         player.levels.set(Skill.Slayer, 80)
