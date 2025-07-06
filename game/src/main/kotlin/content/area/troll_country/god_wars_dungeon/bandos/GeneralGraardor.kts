e38d56ff7b6d3f3a6b8a8211ea08ce482322680c
GregHib <greghib@users.noreply.github.com>
2025-07-06 13:24:16 +0100

    Add bandos sergeants spawning

--- game/src/main/kotlin/content/area/troll_country/god_wars_dungeon/bandos/GeneralGraardor.kts
+++ game/src/main/kotlin/content/area/troll_country/god_wars_dungeon/bandos/GeneralGraardor.kts
@@ -7,13 +7,22 @@ import content.entity.proj.shoot
 import content.entity.sound.areaSound
 import content.entity.sound.sound
 import world.gregs.voidps.engine.data.definition.AreaDefinitions
+import world.gregs.voidps.engine.entity.character.npc.NPC
+import world.gregs.voidps.engine.entity.character.npc.NPCs
 import world.gregs.voidps.engine.entity.character.player.Players
+import world.gregs.voidps.engine.entity.npcDespawn
 import world.gregs.voidps.engine.entity.npcSpawn
 import world.gregs.voidps.engine.inject
+import world.gregs.voidps.type.Tile
 import world.gregs.voidps.type.random
 
 val players: Players by inject()
 val areas: AreaDefinitions by inject()
+val npcs: NPCs by inject()
+
+var strongstack: NPC? = null
+var steelwill: NPC? = null
+var grimspike: NPC? = null
 
 npcCombatSwing("general_graardor") { npc ->
     when (random.nextInt(2)) {
@@ -37,7 +46,23 @@ npcCombatSwing("general_graardor") { npc ->
 }
 
 npcSpawn("general_graardor") {
-    // TODO check for minions and respawn if necessary
+    if (strongstack == null) {
+        strongstack = npcs.add("sergeant_strongstack", Tile(2866, 5358, 2))
+    }
+    if (steelwill == null) {
+        steelwill = npcs.add("sergeant_steelwill", Tile(2872, 5352, 2))
+    }
+    if (grimspike == null) {
+        grimspike = npcs.add("sergeant_grimspike", Tile(2868, 5362, 2))
+    }
+}
+
+npcDespawn("sergeant_*") { npc ->
+    when (npc.id) {
+        "sergeant_strongstack" -> strongstack = null
+        "sergeant_steelwill" -> steelwill = null
+        "sergeant_grimspike" -> grimspike = null
+    }
 }
 
 npcCombatAttack("general_graardor") {
