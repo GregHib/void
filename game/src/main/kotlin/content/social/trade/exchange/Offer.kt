fa991566acec2e8d804d6b399df0a061fc98b3f3
GregHib <greghib@users.noreply.github.com>
2025-08-04 20:24:06 +0100

    Add offer history aggregation

--- game/src/main/kotlin/content/social/trade/exchange/Offer.kt
+++ game/src/main/kotlin/content/social/trade/exchange/Offer.kt
@@ -1,6 +1,15 @@
 package content.social.trade.exchange
 
-class Offer {
+data class Offer(
+    val type: OfferType = OfferType.Buy,
+    val item: String = "",
+    val quantity: Int = 0,
+    val price: Int = 0,
+    var state: OfferState = OfferState.PendingBuy,
+    var lastActive: Long = System.currentTimeMillis(),
+    var remaining: Int = quantity,
+    var excess: Int = 0
+) {
 
     companion object {
         val EMPTY = Offer()
