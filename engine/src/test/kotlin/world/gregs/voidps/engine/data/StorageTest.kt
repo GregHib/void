62289103ff30cf3e91441fa48685de87d81809c6
GregHib <greghib@users.noreply.github.com>
2025-08-15 22:25:27 +0100

    Unit test fixes

--- engine/src/test/kotlin/world/gregs/voidps/engine/data/StorageTest.kt
+++ engine/src/test/kotlin/world/gregs/voidps/engine/data/StorageTest.kt
@@ -64,7 +64,8 @@ abstract class StorageTest {
                 put("Bob", ClanRank.Captain)
                 remove("Greg")
             },
-            offers = arrayOf(ExchangeOffer(2, "whip", 1, 2, OfferState.OpenBuy)),
+            offers = Array(6) { if(it == 0) ExchangeOffer(2, "whip", 1, 2, OfferState.OpenBuy) else ExchangeOffer.EMPTY },
+            history = listOf(ExchangeHistory("item", 123, 321))
         )
         storage.save(listOf(override))
 
