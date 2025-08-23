f873dbda1057d92dcb57a4b3a7c6a005977f1a90
GregHib <greghib@users.noreply.github.com>
2025-08-23 13:52:49 +0100

    Same for item on floor items and players

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/ui/interact/ItemOnFloorItem.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/ui/interact/ItemOnFloorItem.kt
@@ -1,6 +1,5 @@
 package world.gregs.voidps.engine.client.ui.interact
 
-import world.gregs.voidps.engine.entity.character.Character
 import world.gregs.voidps.engine.entity.character.mode.interact.Interaction
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.item.Item
@@ -8,13 +7,13 @@ import world.gregs.voidps.engine.entity.item.floor.FloorItem
 import world.gregs.voidps.engine.event.EventDispatcher
 import world.gregs.voidps.engine.event.Events
 
-data class ItemOnFloorItem<C : Character>(
-    override val character: C,
+data class ItemOnFloorItem(
+    override val character: Player,
     val floorItem: FloorItem,
     val item: Item,
     val itemSlot: Int,
     val inventory: String,
-) : Interaction<C>() {
+) : Interaction<Player>() {
     override fun copy(approach: Boolean) = copy().apply { this.approach = approach }
 
     override val size = 3
@@ -27,8 +26,8 @@ data class ItemOnFloorItem<C : Character>(
     }
 }
 
-fun itemOnFloorItemOperate(item: String = "*", floorItem: String = "*", arrive: Boolean = true, handler: suspend ItemOnFloorItem<Player>.() -> Unit) {
-    Events.handle<ItemOnFloorItem<Player>>("item_approach_floor_item", item, floorItem) {
+fun itemOnFloorItemOperate(item: String = "*", floorItem: String = "*", arrive: Boolean = true, handler: suspend ItemOnFloorItem.() -> Unit) {
+    Events.handle<ItemOnFloorItem>("item_approach_floor_item", item, floorItem) {
         if (arrive) {
             arriveDelay()
         }
@@ -36,8 +35,8 @@ fun itemOnFloorItemOperate(item: String = "*", floorItem: String = "*", arrive: 
     }
 }
 
-fun itemOnFloorItemApproach(item: String = "*", floorItem: String = "*", handler: suspend ItemOnFloorItem<Player>.() -> Unit) {
-    Events.handle<ItemOnFloorItem<Player>>("item_approach_floor_item", item, floorItem) {
+fun itemOnFloorItemApproach(item: String = "*", floorItem: String = "*", handler: suspend ItemOnFloorItem.() -> Unit) {
+    Events.handle<ItemOnFloorItem>("item_approach_floor_item", item, floorItem) {
         handler.invoke(this)
     }
 }
--- engine/src/main/kotlin/world/gregs/voidps/engine/client/ui/interact/ItemOnPlayer.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/ui/interact/ItemOnPlayer.kt
@@ -1,19 +1,18 @@
 package world.gregs.voidps.engine.client.ui.interact
 
-import world.gregs.voidps.engine.entity.character.Character
 import world.gregs.voidps.engine.entity.character.mode.interact.TargetInteraction
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.item.Item
 import world.gregs.voidps.engine.event.EventDispatcher
 import world.gregs.voidps.engine.event.Events
 
-data class ItemOnPlayer<C : Character>(
-    override val character: C,
+data class ItemOnPlayer(
+    override val character: Player,
     override val target: Player,
     val item: Item,
     val itemSlot: Int,
     val inventory: String,
-) : TargetInteraction<C, Player>() {
+) : TargetInteraction<Player, Player>() {
     override fun copy(approach: Boolean) = copy().apply { this.approach = approach }
 
     override val size = 4
@@ -25,14 +24,14 @@ data class ItemOnPlayer<C : Character>(
     }
 }
 
-fun itemOnPlayerOperate(item: String = "*", handler: suspend ItemOnPlayer<Player>.() -> Unit) {
-    Events.handle<ItemOnPlayer<Player>>("item_on_operate_player", item) {
+fun itemOnPlayerOperate(item: String = "*", handler: suspend ItemOnPlayer.() -> Unit) {
+    Events.handle<ItemOnPlayer>("item_on_operate_player", item) {
         handler.invoke(this)
     }
 }
 
-fun itemOnPlayerApproach(item: String = "*", handler: suspend ItemOnPlayer<Player>.() -> Unit) {
-    Events.handle<ItemOnPlayer<Player>>("item_on_approach_player", item) {
+fun itemOnPlayerApproach(item: String = "*", handler: suspend ItemOnPlayer.() -> Unit) {
+    Events.handle<ItemOnPlayer>("item_on_approach_player", item) {
         handler.invoke(this)
     }
 }
