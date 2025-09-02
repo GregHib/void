d068afd08751c0a3c0d62b6cf875f63e5acefdcc
jarryd229 <15334102+jarryd229@users.noreply.github.com>
2025-09-02 13:26:43 +1000

    added ways of getting items needed for quest

---
+++ game/src/main/kotlin/content/area/kandarin/tree_gnome_stronghold/Hudo.kts
@@ -0,0 +1,20 @@
+package content.area.kandarin.tree_gnome_stronghold
+
+import content.entity.npc.shop.openShop
+import content.entity.player.dialogue.*
+import content.entity.player.dialogue.type.*
+import world.gregs.voidps.engine.entity.character.npc.npcOperate
+
+npcOperate("Talk-to", "gnome_hudo") {
+    player<Neutral>("Hello there.")
+    npc<Neutral>("Hello there traveller. Would you like some groceries? I have a large selection.")
+    choice {
+        option<Neutral>("No thank you.") {
+            npc<Neutral>("No problem.")
+        }
+        option<Neutral>("I'll have a look.") {
+            npc<Happy>("Great stuff.")
+            player.openShop("grand_tree_groceries")
+        }
+    }
+}
