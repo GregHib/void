af34b552f4010021f04fd1de75f0abe235c22ab5
GregHib <greghib@users.noreply.github.com>
2025-09-10 20:28:16 +0100

    Remove unused dependency

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.9.20")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
 }
 
 repositories {
