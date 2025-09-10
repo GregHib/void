5ce1e62795b644e5e3c2100a162c225e9d5870c4
GregHib <greghib@users.noreply.github.com>
2025-09-10 21:17:51 +0100

    Remove Coordinate3D to avoid KT-54513
    https://youtrack.jetbrains.com/issue/KT-54513/

--- types/src/main/kotlin/world/gregs/voidps/type/RegionLevel.kt
+++ types/src/main/kotlin/world/gregs/voidps/type/RegionLevel.kt
@@ -3,15 +3,15 @@ package world.gregs.voidps.type
 import world.gregs.voidps.type.area.Cuboid
 
 @JvmInline
-value class RegionLevel(val id: Int) : Coordinate3D<RegionLevel> {
+value class RegionLevel(val id: Int) {
 
     constructor(x: Int, y: Int, level: Int) : this(id(x, y, level))
 
-    override val x: Int
+    val x: Int
         get() = x(id)
-    override val y: Int
+    val y: Int
         get() = y(id)
-    override val level: Int
+    val level: Int
         get() = level(id)
     val region: Region
         get() = Region(x, y)
@@ -20,10 +20,30 @@ value class RegionLevel(val id: Int) : Coordinate3D<RegionLevel> {
     val tile: Tile
         get() = Tile(x shl 6, y shl 6, level)
 
-    override fun copy(x: Int, y: Int, level: Int) = RegionLevel(x, y, level)
+    fun copy(x: Int = this.x, y: Int = this.y, level: Int = this.level) = RegionLevel(x, y, level)
 
     fun toCuboid(width: Int = 1, height: Int = 1, levels: Int = 1) = Cuboid(tile, width * 64, height * 64, levels)
     fun toCuboid(radius: Int, levels: Int = 1) = Cuboid(minus(radius, radius).tile, (radius * 2 + 1) * 64, (radius * 2 + 1) * 64, levels)
+
+    fun add(x: Int = 0, y: Int = 0, level: Int = 0) = copy(this.x + x, this.y + y, this.level + level)
+    fun minus(x: Int = 0, y: Int = 0, level: Int = 0) = add(-x, -y, -level)
+    fun delta(x: Int = 0, y: Int = 0, level: Int = 0) = Delta(this.x - x, this.y - y, this.level - level)
+
+    fun add(value: RegionLevel) = add(value.x, value.y, value.level)
+    fun minus(value: RegionLevel) = minus(value.x, value.y, value.level)
+    fun delta(value: RegionLevel) = delta(value.x, value.y, value.level)
+
+    fun add(value: Delta) = add(value.x, value.y, value.level)
+    fun minus(value: Delta) = minus(value.x, value.y, value.level)
+    fun delta(value: Delta) = delta(value.x, value.y, value.level)
+
+    fun add(direction: Direction) = add(direction.delta)
+    fun minus(direction: Direction) = minus(direction.delta)
+    fun delta(direction: Direction) = minus(direction.delta)
+
+    fun addX(value: Int) = add(value, 0, 0)
+    fun addY(value: Int) = add(0, value, 0)
+    fun addLevel(value: Int) = add(0, 0, value)
 
     companion object {
         fun id(x: Int, y: Int, level: Int) = (y and 0xff) + ((x and 0xff) shl 8) + ((level and 0x3) shl 16)
