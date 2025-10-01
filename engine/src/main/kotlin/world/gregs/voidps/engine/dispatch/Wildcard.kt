03e14f8f224d6b6635071923372c3cd69530b9db
GregHib <greghib@users.noreply.github.com>
2025-10-01 19:51:46 +0100

    Delete wildcard annotation

--- engine/src/main/kotlin/world/gregs/voidps/engine/dispatch/Wildcard.kt
+++
@@ -1,8 +0,0 @@
-package world.gregs.voidps.engine.dispatch
-
-/**
- * A meta-annotation used to tell the gradle ScriptMetadataTask
- * which annotation fields can contain wildcards
- */
-@Target(AnnotationTarget.PROPERTY)
-annotation class Wildcard(val type: String)
