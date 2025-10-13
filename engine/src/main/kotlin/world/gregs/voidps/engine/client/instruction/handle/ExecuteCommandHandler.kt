3206b774034fb02f7afcc8080949a0930a365719
GregHib <greghib@users.noreply.github.com>
2025-10-13 20:23:24 +0100

    Add player command tracking

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/ExecuteCommandHandler.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/instruction/handle/ExecuteCommandHandler.kt
@@ -4,7 +4,9 @@ import world.gregs.voidps.engine.client.instruction.InstructionHandler
 import world.gregs.voidps.engine.client.command.Commands
 import world.gregs.voidps.engine.entity.character.move.tele
 import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.player.rights
 import world.gregs.voidps.engine.event.Events
+import world.gregs.voidps.engine.event.Log
 import world.gregs.voidps.network.client.instruction.ExecuteCommand
 
 class ExecuteCommandHandler : InstructionHandler<ExecuteCommand>() {
@@ -28,6 +30,7 @@ class ExecuteCommandHandler : InstructionHandler<ExecuteCommand>() {
             return
         }
         Events.events.launch {
+            Log.event(player, "command", instruction.command, player.rights.name)
             Commands.call(player, instruction.command)
         }
     }
