a4c33988118582eefd9bafb99395f6e2da11c250
GregHib <greghib@users.noreply.github.com>
2025-10-24 23:22:53 +0100

    Update farming tick

--- game/src/main/kotlin/content/skill/farming/Farming.kt
+++ game/src/main/kotlin/content/skill/farming/Farming.kt
@@ -8,13 +8,14 @@ import world.gregs.voidps.engine.client.variable.ListValues
 import world.gregs.voidps.engine.data.definition.VariableDefinitions
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.event.Script
-import world.gregs.voidps.engine.inject
 import world.gregs.voidps.engine.timer.*
 import world.gregs.voidps.type.random
 import java.util.concurrent.TimeUnit
 
 @Script
-class Farming : Api {
+class Farming(
+    val definitions: VariableDefinitions,
+) : Api {
 
     override fun spawn(player: Player) {
         if (!player.contains("farming_offset_mins")) {
@@ -25,80 +26,80 @@ class Farming : Api {
         }
     }
 
-    init {
-        timerStart("farming_tick") { player ->
-            // Next farming tick
-            val remaining = 5 - (epochMinutes() + player["farming_offset_mins", 0]).rem(5)
-            interval = TimeUnit.MINUTES.toTicks(remaining)
-        }
-
-        timerTick("farming_tick") { player ->
-            nextInterval = 500 // 500
-            val epoch = epochMinutes()
-            val delta = epoch - player["last_growth_cycle", epoch - 1]
-            player["last_growth_cycle"] = epoch
-            grow(player, delta / 5)
-        }
+    @Timer("farming_tick")
+    override fun start(player: Player, timer: String, restart: Boolean): Int {
+        val remaining = 5 - (epochMinutes() - player["farming_offset_mins", 0]).rem(5)
+        return TimeUnit.MINUTES.toTicks(remaining)
     }
 
-    val varbits = listOf("allotment_falador_nw")
+    @Timer("farming_tick")
+    override fun tick(player: Player, timer: String): Int {
+        val epoch = epochMinutes()
+        val lastCycle = player["last_growth_cycle", epoch - 1] + 1
+        player["last_growth_cycle"] = epoch
+        for (min in lastCycle..epoch) {
+            val minute = min - player["farming_offset_mins", 0]
+            grow(player, minute)
+        }
+        return 500
+    }
 
-    val definitions: VariableDefinitions by inject()
+    val patches = mapOf(5 to listOf("allotment_falador_nw"))
 
-    fun grow(player: Player, stages: Int) {
-        for (varbit in varbits) {
-            val current: String = player[varbit] ?: continue
-            val type = current.substringAfterLast("_")
-            if (type == "none" || type == "compost" || type == "super" || type == "dead") {
+    fun grow(player: Player, minute: Int) {
+        for ((cycle, varbits) in patches) {
+            if (minute.rem(cycle) != 0) {
                 continue
             }
-            val produce = current.substringBeforeLast("_")
-            if (produce.endsWith("diseased")) {
-                player[varbit] = current.replace("diseased", "dead")
-                amuletOfFarming(player, varbit)
-                continue
-            }
-            val stage = type.toInt()
-            if (current.startsWith("weeds")) {
-                val next = (stage + stages).rem(4)
-                player[varbit] = if (next == 3) {
-                    "weeds_${
-                        when (random.nextInt(3)) {
-                            0 -> "none"
-                            1 -> "compost"
-                            else -> "super"
-                        }
-                    }"
-                } else {
-                    "weeds_$next"
+            for (varbit in varbits) {
+                val current: String = player[varbit] ?: continue
+                val type = current.substringAfterLast("_")
+                if (type == "none" || type == "compost" || type == "super" || type == "dead") {
+                    continue
                 }
-                continue
-            }
-            // TODO use maths and produce to figure out if growth should occur
-            val definition = definitions.get(varbit) ?: continue
-            val int = definition.values.toInt(current)
-            val list = (definition.values as ListValues).values as List<String>
-            var next = current
-            for (i in int until int + stages) {
-                next = list[int + 1]
-                if (next.endsWith("_none")) {
-                    break
+                val produce = current.substringBeforeLast("_")
+                if (produce.endsWith("diseased")) {
+                    player[varbit] = current.replace("diseased", "dead")
+                    amuletOfFarming(player, varbit)
+                    continue
                 }
-                if (random.nextInt(5) == 0) { // TODO chances
-                    val value = list[int + 64] // diseased
-                    if (value != current.replace(produce, "${produce}_diseased")) {
-                        // TODO check
-                        continue
+                if (current.startsWith("weeds")) {
+                    val stage = type.toInt()
+                    val next = (stage + 1).rem(4)
+                    player[varbit] = when (next) {
+                        3 ->
+                            "weeds_${
+                                when (random.nextInt(3)) {
+                                    0 -> "none"
+                                    1 -> "compost"
+                                    else -> "super"
+                                }
+                            }"
+                        else -> "weeds_$next"
                     }
-                    next = value // diseased
+                    continue
                 }
+                val definition = definitions.get(varbit) ?: continue
+                val int = definition.values.toInt(current)
+                val list = (definition.values as ListValues).values as List<String>
+                var next = list[int + 1]
+                if (next.endsWith("_none")) {
+                    // complete
+                    // TODO if compost or super otherwise keep same
+                } else {
+                    if (random.nextInt(5) == 0) { // TODO chances
+                        val value = list[int + 64] // diseased
+                        val produce = current.substringBeforeLast("_")
+                        if (value != current.replace(produce, "${produce}_diseased")) {
+                            // TODO check
+                            continue
+                        }
+                        next = value // diseased
+                    }
+                }
+                player[varbit] = next
+                amuletOfFarming(player, varbit)
             }
-            if (next.endsWith("_none")) {
-                // complete
-                // TODO if compost or super otherwise keep same
-            }
-            player[varbit] = next
-            amuletOfFarming(player, varbit)
         }
     }
 
@@ -110,5 +111,4 @@ class Farming : Api {
         player.sound("farming_amulet")
         player.message("A low hum resonates from the <col=ef1020>Amulet of Nature</col> - it seeks audience with you.")
     }
-
 }
