ff456822173fa48e06bc9aade096a5ab88c3069d
GregHib <greghib@users.noreply.github.com>
2025-10-24 23:39:38 +0100

    Make sure return types are explicit

--- game/src/main/kotlin/content/skill/prayer/active/PrayerDrain.kt
+++ game/src/main/kotlin/content/skill/prayer/active/PrayerDrain.kt
@@ -11,6 +11,7 @@ import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.sound
 import world.gregs.voidps.engine.inject
+import world.gregs.voidps.engine.timer.Timer
 
 class PrayerDrain : Script {
 
