d69c157fcd54e6a9bbdf34bda62dcdfd7a6e1261
GregHib <greghib@users.noreply.github.com>
2025-10-29 14:00:42 +0000

    Don't use approach and operate for now

--- engine/src/main/kotlin/world/gregs/voidps/engine/Api.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/Api.kt
@@ -11,7 +11,7 @@ import world.gregs.voidps.engine.timer.TimerApi
 /**
  * A helper interface made up of all callable methods for easier [world.gregs.voidps.engine.event.Script] usage.
  */
-interface Api : Spawn, LevelChanged, Moved, VariableSet, TimerApi, Approachable, Operation {
+interface Api : Spawn, LevelChanged, Moved, VariableSet, TimerApi {
     companion object {
         fun clear() {
             Spawn.clear()
