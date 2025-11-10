7232ba0c33e778056a2427c22365452f63cb930d
GregHib <greghib@users.noreply.github.com>
2025-11-10 21:22:21 +0000

    Formatting

--- game/src/main/kotlin/content/area/kandarin/feldip_hills/GnormadiumAvlafrim.kt
+++ game/src/main/kotlin/content/area/kandarin/feldip_hills/GnormadiumAvlafrim.kt
@@ -7,7 +7,6 @@ import content.entity.player.dialogue.type.choice
 import content.entity.player.dialogue.type.npc
 import content.quest.questCompleted
 import world.gregs.voidps.engine.Script
-import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.client.ui.open
 import world.gregs.voidps.engine.entity.character.player.Player
 
