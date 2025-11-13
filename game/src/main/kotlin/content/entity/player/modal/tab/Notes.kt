b453b62acfdc6e5025dc2f230df993358cd75052
GregHib <greghib@users.noreply.github.com>
2025-11-13 21:11:29 +0000

    Adding more compost bin functionality

--- game/src/main/kotlin/content/entity/player/modal/tab/Notes.kt
+++ game/src/main/kotlin/content/entity/player/modal/tab/Notes.kt
@@ -3,7 +3,6 @@ package content.entity.player.modal.tab
 import world.gregs.voidps.engine.Script
 
 class Notes : Script {
-
     init {
         interfaceOpened("notes") { id ->
             interfaceOptions.unlockAll(id, "notes", 0..30)
