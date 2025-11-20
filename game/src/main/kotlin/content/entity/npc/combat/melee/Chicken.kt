cee39c4ae6791ce7c33f62ddfd81dd587bc4ec1e
M1krofin <207991987+M1krofin@users.noreply.github.com>
2025-11-20 15:22:48 +0200

    Added Chicken laying eggs mechanic Added Super large egg and one normal egg to freds farm

---
+++ game/src/main/kotlin/content/entity/npc/combat/melee/Chicken.kt
@@ -0,0 +1,33 @@
+package content.entity.npc.combat.melee
+
+import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.entity.character.areaSound
+import world.gregs.voidps.engine.entity.item.floor.FloorItems
+import world.gregs.voidps.engine.inject
+import world.gregs.voidps.engine.timer.Timer
+import world.gregs.voidps.type.random
+
+class Chicken : Script {
+
+    val floorItems: FloorItems by inject()
+
+    init {
+        npcSpawn("chicken*") {
+            areaSound("chicken_defend", tile)
+            say("squawk!")
+            softTimers.start("lay_eggs")
+        }
+        npcTimerStart("lay_eggs") {
+            // Do not have authentic data. Used this so no spamming eggs all the time.
+            random.nextInt(200, 400)
+        }
+        npcTimerTick("lay_eggs") {
+            anim("chicken_defend")
+            // Timed on Rs3 but can be inauthentic for 2011
+            floorItems.add(tile, "egg", disappearTicks = 100)
+            say("squawk!")
+            areaSound("chicken_defend", tile)
+            Timer.CONTINUE
+        }
+    }
+}
