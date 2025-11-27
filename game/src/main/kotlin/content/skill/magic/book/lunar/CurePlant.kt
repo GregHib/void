fca68bba43c5805b17b5725eb33d0a2eb68405af
GregHib <greghib@users.noreply.github.com>
2025-11-27 15:44:36 +0000

    Add plant cure lunar spell

---
+++ game/src/main/kotlin/content/skill/magic/book/lunar/CurePlant.kt
@@ -0,0 +1,43 @@
+package content.skill.magic.book.lunar
+
+import content.skill.magic.spell.removeSpellItems
+import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.client.message
+import world.gregs.voidps.engine.client.variable.start
+import world.gregs.voidps.engine.data.definition.SpellDefinitions
+import world.gregs.voidps.engine.entity.character.areaSound
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.engine.inject
+
+class CurePlant : Script {
+
+    val definitions: SpellDefinitions by inject()
+
+    init {
+        onObjectOperate("lunar_spellbook:cure_plant", "*") { (target) ->
+            if (!target.id.startsWith("farming_")) {
+                return@onObjectOperate
+            }
+            val value = get(target.id, "weeds_life3")
+            if (!value.contains("_diseased")) {
+                if (value.startsWith("weeds_")) {
+                    message("The weeds are healthy enough already.")
+                } else {
+                    message("It's growing just fine.")
+                }
+                return@onObjectOperate
+            }
+            if (!removeSpellItems("cure_plant")) {
+                return@onObjectOperate
+            }
+            val definition = definitions.get("cure_plant")
+            start("movement_delay", 3)
+            anim("lunar_cure_plant")
+            gfx("cure_me")
+            areaSound("lunar_fertilize", target.tile, radius = 10)
+            delay(3)
+            set(target.id, value.replace("_diseased", ""))
+            experience.add(Skill.Magic, definition.experience)
+        }
+    }
+}
