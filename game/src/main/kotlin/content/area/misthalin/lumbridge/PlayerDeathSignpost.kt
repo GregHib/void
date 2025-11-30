2784829d58ba0578815b8e3faeccbe333bd52518
M1krofin <207991987+M1krofin@users.noreply.github.com>
2025-11-30 21:54:13 +0200

    Added two statistic signposts

---
+++ game/src/main/kotlin/content/area/misthalin/lumbridge/PlayerDeathSignpost.kt
@@ -0,0 +1,21 @@
+package content.area.misthalin.lumbridge
+
+import content.entity.player.dialogue.type.statement
+import world.gregs.voidps.engine.Script
+
+class PlayerDeathSignpost : Script {
+    var playerDeathsToday: Int = 0
+
+    init {
+        playerDeath{
+            playerDeathsToday += 1
+        }
+        objectOperate("Read", "lumbridge_signpost_death") {
+            if (playerDeathsToday > 0) {
+                statement("So far today, $playerDeathsToday unlucky adventurers have died on RuneScape and been sent to their respawn location. Be careful out there.")
+            } else {
+                statement("So far today, not a single adventurer on RuneScape has met their end grisly or otherwise. Either the streets are getting safer or adventurers are getting warier.")
+            }
+        }
+    }
+}
