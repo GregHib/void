53d8216329c21590c68fcbb303cd6c43d15d52fa
GregHib <greghib@users.noreply.github.com>
2026-01-05 17:04:47 +0000

    Change how elementals catch players

--- game/src/main/kotlin/content/minigame/sorceress_garden/Elementals.kt
+++ game/src/main/kotlin/content/minigame/sorceress_garden/Elementals.kt
@@ -3,18 +3,14 @@ package content.minigame.sorceress_garden
 import content.entity.proj.shoot
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.ui.open
-import world.gregs.voidps.engine.data.definition.HuntModeDefinitions
 import world.gregs.voidps.engine.data.definition.PatrolDefinitions
 import world.gregs.voidps.engine.entity.character.mode.Patrol
 import world.gregs.voidps.engine.entity.character.move.tele
-import world.gregs.voidps.engine.entity.character.npc.hunt.Hunting
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.Players
 import world.gregs.voidps.engine.entity.character.sound
 import world.gregs.voidps.engine.inject
 import world.gregs.voidps.engine.queue.strongQueue
-import world.gregs.voidps.type.Direction
-import kotlin.getValue
 
 class Elementals : Script {
     val patrols: PatrolDefinitions by inject()
@@ -28,25 +24,30 @@ class Elementals : Script {
 
         huntPlayer("*_elemental*", "spotted") {
             val direction = direction
-            for (player in targets as List<Player>) {
-                if (direction != Direction.NONE && direction != player.tile.delta(tile).toDirection()) {
-                    continue // Skip players that aren't in-front or under.
-                }
-                if (player.queue.contains("sorceress_garden_caught")) {
-                    continue
-                }
-                player.strongQueue("sorceress_garden_caught") {
-                    anim("elemental_pointing")
-                    player.sound("stun_all")
-                    shoot("curse", player.tile)
-                    player.gfx("curse_impact")
-                    player.open("fade_out")
-                    delay(4)
-                    player.tele(2911, 5470)
-                    player.open("fade_in")
-                    player.sound("stunned")
-                }
+            // Catch all players two tiles in-front
+            for (player in players[tile.add(direction)]) {
+                catch(player)
             }
+            for (player in players[tile.add(direction).add(direction)]) {
+                catch(player)
+            }
+        }
+    }
+
+    fun catch(player: Player) {
+        if (player.queue.contains("sorceress_garden_caught")) {
+            return
+        }
+        player.strongQueue("sorceress_garden_caught") {
+            player.anim("elemental_pointing")
+            player.sound("stun_all")
+            player.shoot("curse", player.tile)
+            player.gfx("curse_impact")
+            player.open("fade_out")
+            player.delay(4)
+            player.tele(2911, 5470)
+            player.open("fade_in")
+            player.sound("stunned")
         }
     }
 }
