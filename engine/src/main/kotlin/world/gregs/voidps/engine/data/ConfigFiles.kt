bb727c21fd1f3f907123e6d3fea5d419afa898c0
GregHib <greghib@users.noreply.github.com>
2026-01-20 14:00:29 +0000

    Change how config files track changes

--- engine/src/main/kotlin/world/gregs/voidps/engine/data/ConfigFiles.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/data/ConfigFiles.kt
@@ -20,59 +20,72 @@ import kotlin.io.path.writeBytes
 data class ConfigFiles(
     val map: Map<String, List<String>>,
     val cacheUpdate: Boolean = false,
-    val extensions: Set<String> = emptySet(),
+    val needsUpdate: MutableSet<String> = mutableSetOf(),
+    val lastUpdated: MutableMap<String, Long> = mutableMapOf(),
 ) {
     fun list(path: String): List<String> = map.getOrDefault(path, emptyList())
 
     fun getValue(key: String) = map.getValue(key)
 
     fun find(path: String, type: String = "toml"): String = map.getOrDefault(type, emptyList()).firstOrNull { it.endsWith(path) } ?: throw NoSuchFileException("Unable to find config file '$path' in /data/ directory.")
-}
 
-fun tempCache(cachePath: String = Settings["storage.caching.path"]): File? {
-    if (!Settings["storage.caching.active", false]) {
-        return null
+    fun update(extension: String) {
+        lastUpdated[extension] = System.currentTimeMillis()
+        save()
     }
-    val directory = File(cachePath)
-    if (!directory.exists()) {
-        directory.mkdirs()
+
+    fun needsUpdate(extension: String) = needsUpdate.contains(extension)
+
+    fun save() {
+        val modified = Path.of(Settings["storage.data.modified"])
+        val writer = ArrayWriter(10_000)
+        writer.writeLong(System.currentTimeMillis())
+        writer.writeByte(lastUpdated.size)
+        for ((key, value) in lastUpdated.toList()) {
+            writer.writeString(key)
+            writer.writeLong(value)
+        }
+        modified.writeBytes(writer.toArray())
     }
-    return directory
 }
 
 fun configFiles(): ConfigFiles {
+    val directory = File(Settings["storage.caching.path"])
+    if (!directory.exists()) {
+        directory.mkdirs()
+    }
     val map = Object2ObjectOpenHashMap<String, MutableList<String>>()
     val path = Path.of(Settings["storage.data"])
     val modified = Path.of(Settings["storage.data.modified"])
     val lastUpdated = loadLastUpdate(modified)
-    val extensions = mutableSetOf<String>()
+    val needsUpdate = mutableSetOf<String>()
     timedLoad("config file paths") {
-        walkPath(map, path, lastUpdated, extensions)
+        walkPath(map, path, lastUpdated, needsUpdate)
         map.size
     }
-    return ConfigFiles(map, cacheChanged(lastUpdated), extensions)
+    val cacheChanged = cacheChanged(Path.of(Settings["storage.cache.path"]), lastUpdated.getOrDefault("cache", 0L))
+    return ConfigFiles(map, cacheChanged, needsUpdate, lastUpdated)
 }
 
-fun updateModified() {
-    val modified = Path.of(Settings["storage.data.modified"])
-    val writer = ArrayWriter(8)
-    writer.writeLong(System.currentTimeMillis())
-    modified.writeBytes(writer.toArray())
-}
-
-private fun loadLastUpdate(path: Path): Long {
+private fun loadLastUpdate(path: Path): MutableMap<String, Long> {
     if (path.exists()) {
         val reader = ArrayReader(path.readBytes())
-        return reader.readLong()
+        val map = mutableMapOf<String, Long>()
+        map["cache"] = reader.readLong()
+        val size = reader.readByte()
+        for (i in 0 until size) {
+            map[reader.readString()] = reader.readLong()
+        }
+        return map
     }
-    return 0
+    return mutableMapOf()
 }
 
 private fun walkPath(
     map: MutableMap<String, MutableList<String>>,
     dir: Path,
-    lastUpdated: Long,
-    invalidatedExtensions: MutableSet<String>,
+    lastUpdated: MutableMap<String, Long>,
+    needUpdate: MutableSet<String> = mutableSetOf(),
 ) {
     val name = dir.name
     if (name == "saves" || name == "players" || name == "cache") {
@@ -80,23 +93,20 @@ private fun walkPath(
     }
     for (path in Files.newDirectoryStream(dir)) {
         if (path.isDirectory()) {
-            walkPath(map, path, lastUpdated, invalidatedExtensions)
+            walkPath(map, path, lastUpdated, needUpdate)
             continue
         }
         val extension = path.name.substringAfter('.')
         map.getOrPut(extension) { ObjectArrayList() }.add(path.pathString)
 
         // Check file-type hasn't been marked as invalidated before checking the last modified time for invalidation
-        if (!invalidatedExtensions.contains(extension) && Files.getLastModifiedTime(path).toMillis() > lastUpdated) {
-            invalidatedExtensions.add(extension)
+        if (!needUpdate.contains(extension) && Files.getLastModifiedTime(path).toMillis() > lastUpdated.getOrDefault(extension, 0L)) {
+            needUpdate.add(extension)
         }
     }
 }
 
-private fun cacheChanged(
-    lastUpdated: Long,
-    dir: Path = Path.of(Settings["storage.cache.path"]),
-): Boolean {
+private fun cacheChanged(dir: Path, lastUpdated: Long): Boolean {
     for (path in Files.newDirectoryStream(dir)) {
         if (path.isDirectory() || (!path.extension.startsWith("dat") && !path.extension.startsWith("idx"))) {
             continue
