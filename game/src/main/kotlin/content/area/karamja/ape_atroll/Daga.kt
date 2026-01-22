c9170b3c1da97bbdf88113f77dc9a5e4335c2144
aka MrSlayerGod <31116755+MrSlayerGod@users.noreply.github.com>
2026-01-22 21:18:22 +0000

    added ape atoll shops and dialogues (#844)
    * added ape atolls shops and dialogues
    
    * fixed dialogues and remove class made into one dialogue class

---
+++ game/src/main/kotlin/content/area/karamja/ape_atroll/Daga.kt
@@ -0,0 +1,89 @@
+package content.area.karamja.ape_atroll
+
+import content.entity.npc.shop.openShop
+import content.entity.player.dialogue.Shifty
+import content.entity.player.dialogue.type.choice
+import content.entity.player.dialogue.type.npc
+import content.entity.player.dialogue.type.player
+import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.entity.character.player.chat.inventoryFull
+import world.gregs.voidps.engine.entity.character.player.equip.equipped
+import world.gregs.voidps.engine.inv.inventory
+import world.gregs.voidps.engine.inv.transact.TransactionError
+import world.gregs.voidps.engine.inv.transact.operation.AddItem.add
+import world.gregs.voidps.engine.inv.transact.operation.RemoveItem.remove
+import world.gregs.voidps.network.login.protocol.visual.update.player.EquipSlot
+
+class Daga : Script {
+
+    init {
+
+        npcOperate("Talk-to", "daga") {
+
+            val amulet = equipped(EquipSlot.Amulet)
+
+            if (amulet.id == "monkeyspeak_amulet") {
+
+                npc<Shifty>("Sorry, you don't have enough space in your inventory.")
+
+                choice {
+
+                    option("Yes please.") {
+                        player<Shifty>("Yes, please.")
+                        openShop("dagas_scimitar_smithy")
+                    }
+
+                    option("No, thanks.") {
+                        player<Shifty>("No, thanks.")
+                    }
+
+                    option("Do you have any Dragon Scimitars in stock?") {
+                        player<Shifty>("Do you have any Dragon Scimitars in stock?")
+                        npc<Shifty>(
+                            "It just so happens I recently got a fresh delivery. <br>Do you want to buy one?",
+                        )
+                        choice {
+                            option("Yes.") {
+                                player<Shifty>("Yes, please.")
+
+                                inventory.transaction {
+                                    remove("coin", 100_000)
+                                    add("dragon_scimitar")
+                                }
+                                when (inventory.transaction.error) {
+                                    is TransactionError.Full -> {
+                                        inventoryFull()
+                                        npc<Shifty>("Sorry, you don't have enough space in your inventory.")
+                                    }
+
+                                    TransactionError.None -> {
+                                        npc<Shifty>("There you go. Pleasure doing business with you.")
+                                    }
+
+                                    else -> npc<Shifty>(
+                                        "Sorry, you don't have enough coins. <br>It costs 100,000 gold coins.",
+                                    )
+                                }
+
+                                option("No.") {
+                                    player<Shifty>("No.")
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                npc<Shifty>("Ook! Ah Uh Ah! Ook Ook! Ah!")
+            }
+        }
+
+        npcOperate("Trade", "daga") {
+            val amulet = equipped(EquipSlot.Amulet)
+            if (amulet.id == "monkeyspeak_amulet") {
+                openShop("dagas_scimitar_smithy")
+            } else {
+                npc<Shifty>("Ook! Ah Uh Ah! Ook Ook! Ah!")
+            }
+        }
+    }
+}
---
+++ game/src/main/kotlin/content/area/karamja/ape_atroll/MonkeyShopkeeper.kt
@@ -0,0 +1,53 @@
+package content.area.karamja.ape_atroll
+
+import content.entity.npc.shop.openShop
+import content.entity.player.dialogue.Shifty
+import content.entity.player.dialogue.type.choice
+import content.entity.player.dialogue.type.npc
+import content.entity.player.dialogue.type.player
+import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.entity.character.player.equip.equipped
+import world.gregs.voidps.network.login.protocol.visual.update.player.EquipSlot
+
+class MonkeyShopkeeper : Script {
+
+    init {
+        npcOperate("Talk-to", "hamab,ifaba,tutab,solihib") { (npc) ->
+            val amulet = equipped(EquipSlot.Amulet)
+            val shopName = npc.def["shop", ""]
+
+            if (amulet.id == "monkeyspeak_amulet") {
+                npc<Shifty>(
+                    when (npc.id) {
+                        "ifaba" -> "Would you like to buy or sell anything?"
+                        "hamab" -> "Would you like to buy or sell some crafting materials?"
+                        "solihib" -> "Would you like to buy or sell some foodstuffs?"
+                        else -> "Would you like to buy or sell some magical items?"
+                    },
+                )
+                choice {
+                    option("Yes, please.") {
+                        player<Shifty>("Yes, please.")
+                        openShop(shopName)
+                    }
+                    option("No, thanks.") {
+                        player<Shifty>("No, thanks.")
+                    }
+                }
+            } else {
+                npc<Shifty>("Ook! Ah Uh Ah! Ook Ook! Ah!")
+            }
+        }
+
+        npcOperate("Trade", "hamab,ifaba,tutab,solihib") { (npc) ->
+            val amulet = equipped(EquipSlot.Amulet)
+            val shopName = npc.def["shop", ""]
+
+            if (amulet.id == "monkeyspeak_amulet") {
+                openShop(shopName)
+            } else {
+                npc<Shifty>("Ook! Ah Uh Ah! Ook Ook! Ah!")
+            }
+        }
+    }
+}
