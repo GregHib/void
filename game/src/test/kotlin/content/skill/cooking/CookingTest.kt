9d8c1d3f958b1a64125f2b1e8ba7857acc7c11d9
GregHib <greghib@users.noreply.github.com>
2026-01-24 13:11:30 +0000

    Fix burn chances not applying to cooking on fires

--- game/src/test/kotlin/content/skill/cooking/CookingTest.kt
+++ game/src/test/kotlin/content/skill/cooking/CookingTest.kt
@@ -1,5 +1,6 @@
 package content.skill.cooking
 
+import FakeRandom
 import WorldTest
 import dialogueOption
 import interfaceOption
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Test
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.inv.add
 import world.gregs.voidps.engine.inv.inventory
+import world.gregs.voidps.type.setRandom
 import kotlin.test.assertFalse
 
 internal class CookingTest : WorldTest() {
@@ -32,6 +34,28 @@ internal class CookingTest : WorldTest() {
         assertEquals(1, player.inventory.count("raw_shrimps"))
         assertEquals(2, player.inventory.count("shrimps"))
         assertTrue(player.experience.get(Skill.Cooking) > 0)
+    }
+
+    @Test
+    fun `Cooking has a chance at burning`() {
+        setRandom(object : FakeRandom() {
+            override fun nextInt(until: Int) = until
+        })
+        val start = emptyTile
+        val player = createPlayer(start)
+        player.levels.set(Skill.Cooking, 1)
+        player.inventory.add("raw_shrimps", 2)
+        val fire = createObject("fire_orange", emptyTile.addY(1))
+
+        player.itemOnObject(fire, 0)
+        tick()
+        player.dialogueOption(id = "dialogue_skill_creation", component = "choice1")
+        tick(4)
+
+        assertEquals(1, player.inventory.count("raw_shrimps"))
+        assertEquals(0, player.inventory.count("shrimps"))
+        assertEquals(1, player.inventory.count("burnt_shrimp"))
+        assertEquals(0.0, player.experience.get(Skill.Cooking))
     }
 
     @Test
