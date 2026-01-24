0752f52efd3638e9fa6914bc8313ef57567a796b
GregHib <greghib@users.noreply.github.com>
2026-01-24 16:59:41 +0000

    Add rockslugs

--- game/src/main/kotlin/content/area/fremennik_province/rellekka/Rockslug.kt
+++ game/src/main/kotlin/content/area/fremennik_province/rellekka/Rockslug.kt
@@ -13,7 +13,6 @@ import world.gregs.voidps.engine.entity.character.player.chat.ChatType
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.inv.inventory
 import world.gregs.voidps.engine.inv.remove
-import world.gregs.voidps.engine.queue.softQueue
 
 class Rockslug : Script {
     init {
