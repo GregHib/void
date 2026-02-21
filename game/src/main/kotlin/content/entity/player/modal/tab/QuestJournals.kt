576ececd92b40ca5c2168fd0721361c1ce1ebb41
GregHib <greghib@users.noreply.github.com>
2026-02-21 19:43:00 +0000

    Add quest journal filters closes #861

--- game/src/main/kotlin/content/entity/player/modal/tab/QuestJournals.kt
+++ game/src/main/kotlin/content/entity/player/modal/tab/QuestJournals.kt
@@ -2,10 +2,15 @@ package content.entity.player.modal.tab
 
 import com.github.michaelbull.logging.InlineLogger
 import content.quest.refreshQuestJournal
+import world.gregs.voidps.cache.definition.data.InterfaceDefinition
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.clearCamera
+import world.gregs.voidps.engine.client.sendInterfaceSettings
+import world.gregs.voidps.engine.client.sendScript
 import world.gregs.voidps.engine.client.ui.InterfaceApi
 import world.gregs.voidps.engine.data.definition.QuestDefinitions
+import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.queue.softQueue
 import world.gregs.voidps.engine.timer.Timer
 
 class QuestJournals(val questDefinitions: QuestDefinitions) : Script {
@@ -15,6 +20,8 @@ class QuestJournals(val questDefinitions: QuestDefinitions) : Script {
     init {
         playerSpawn {
             clearCamera()
+            sendVariable("quest_journal_show_all")
+            sendVariable("quest_journal_order")
         }
 
         timerStart("refresh_quest_journal") { 1 }
@@ -29,6 +36,7 @@ class QuestJournals(val questDefinitions: QuestDefinitions) : Script {
 
         interfaceOpened("quest_journals") { id ->
             interfaceOptions.unlock(id, "journals", 0 until 201, "View")
+            interfaceOptions.unlockAll(id, "order", 0 until 3)
             sendVariable("quest_points")
             sendVariable("quest_points_total") // set total quest points available in variables-player.yml
             sendVariable("unstable_foundations")
@@ -45,5 +53,25 @@ class QuestJournals(val questDefinitions: QuestDefinitions) : Script {
             }
             InterfaceApi.openQuestJournal(this, quest.stringId)
         }
+
+        interfaceOption("Filter quests", "quest_journals:filter") {
+            toggle("quest_journal_show_all")
+        }
+
+        interfaceOption(id = "quest_journals:order") {
+            val type = when (it.itemSlot) {
+                0 -> "free_members"
+                1 -> "progress"
+                2 -> "difficulty"
+                else -> return@interfaceOption
+            }
+            if (get("quest_journal_order", "free_members") == type) {
+                toggle("quest_journal_reversed")
+            } else {
+                set("quest_journal_reversed", false)
+                set("quest_journal_order", type)
+            }
+        }
+
     }
 }
