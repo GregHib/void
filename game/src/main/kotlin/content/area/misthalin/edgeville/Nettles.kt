d81051640671844e0c512e44fdd71b9a41cacc20
M1krofin <207991987+M1krofin@users.noreply.github.com>
2026-03-02 16:39:12 +0200

    Added nettles

--- game/src/main/kotlin/content/area/misthalin/edgeville/Nettles.kt
+++ game/src/main/kotlin/content/area/misthalin/edgeville/Nettles.kt
@@ -2,31 +2,49 @@ package content.area.misthalin.edgeville
 
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.message
-import content.entity.combat.hit.damage
 import world.gregs.voidps.engine.entity.character.player.equip.equipped
 import world.gregs.voidps.network.login.protocol.visual.update.player.EquipSlot
 import world.gregs.voidps.engine.entity.character.sound
 import world.gregs.voidps.engine.inv.inventory
 import world.gregs.voidps.engine.entity.obj.remove
+import content.entity.combat.hit.damage
+import content.entity.effect.toxin.poisonDamage
+import content.entity.player.dialogue.type.statement
+import world.gregs.voidps.engine.entity.character.player.male
 import world.gregs.voidps.engine.inv.add
 
 class Nettles : Script {
 
-        init {
-            objectOperate("Pick", "nettles") { (target) ->
-                if (!equipped(EquipSlot.Hands).id.contains("gloves")) {
-                    damage(20)
-                    message("You have been stung by the nettles.")
-                    return@objectOperate
-                }
-                if (!inventory.add("nettles")) {
-                    message("Your inventory is too full to pick the nettles.")
-                    return@objectOperate
-                }
-                anim("climb_down")
-                sound("pick")
-                message("You pick a handful of nettles.")
-                target.remove(15)
+    init {
+        objectOperate("Pick", "nettles") { (target) ->
+            if (inventory.isFull()) {
+                //Rs3 and osrs has this statement.
+                statement("You can't carry any more nettles.")
+                return@objectOperate
             }
+            if (!equipped(EquipSlot.Hands).id.contains("gloves")) {
+                walkToDelay(target.tile)
+                anim("climb_down")
+                delay(2)
+                damage(20, 0, "poison", this)
+                poisonDamage = 0
+                if(male) {
+                    sound("man_defend")
+                } else {
+                    sound("woman_defend")
+                }
+                //Rs3 and osrs has this message.
+                message("You have been stung by the nettles.")
+                return@objectOperate
+            }
+            walkTo(target.tile)
+            delay(1)
+            anim("climb_down")
+            sound("pick")
+            //Rs3 and osrs has this message.
+            message("You pick a handful of nettles.")
+            inventory.add("nettles")
+            target.remove(15)
         }
+    }
 }
