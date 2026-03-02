20ce1be214d6dad18fbf9f938ca7f43e493fe7f8
M1krofin <207991987+M1krofin@users.noreply.github.com>
2026-03-02 19:47:50 +0200

    Spotless

--- game/src/main/kotlin/content/area/misthalin/edgeville/Nettles.kt
+++ game/src/main/kotlin/content/area/misthalin/edgeville/Nettles.kt
@@ -1,24 +1,24 @@
 package content.area.misthalin.edgeville
 
-import world.gregs.voidps.engine.Script
-import world.gregs.voidps.engine.client.message
-import world.gregs.voidps.engine.entity.character.player.equip.equipped
-import world.gregs.voidps.network.login.protocol.visual.update.player.EquipSlot
-import world.gregs.voidps.engine.entity.character.sound
-import world.gregs.voidps.engine.inv.inventory
-import world.gregs.voidps.engine.entity.obj.remove
 import content.entity.combat.hit.damage
 import content.entity.effect.toxin.poisonDamage
 import content.entity.player.dialogue.type.statement
+import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.client.message
+import world.gregs.voidps.engine.entity.character.player.equip.equipped
 import world.gregs.voidps.engine.entity.character.player.male
+import world.gregs.voidps.engine.entity.character.sound
+import world.gregs.voidps.engine.entity.obj.remove
 import world.gregs.voidps.engine.inv.add
+import world.gregs.voidps.engine.inv.inventory
+import world.gregs.voidps.network.login.protocol.visual.update.player.EquipSlot
 
 class Nettles : Script {
 
     init {
         objectOperate("Pick", "nettles") { (target) ->
             if (inventory.isFull()) {
-                //Rs3 and osrs has this statement.
+                // Rs3 and osrs has this statement.
                 statement("You can't carry any more nettles.")
                 return@objectOperate
             }
@@ -28,12 +28,12 @@ class Nettles : Script {
                 delay(2)
                 damage(20, 0, "poison", this)
                 poisonDamage = 0
-                if(male) {
+                if (male) {
                     sound("man_defend")
                 } else {
                     sound("woman_defend")
                 }
-                //Rs3 and osrs has this message.
+                // Rs3 and osrs has this message.
                 message("You have been stung by the nettles.")
                 return@objectOperate
             }
@@ -41,7 +41,7 @@ class Nettles : Script {
             delay(1)
             anim("climb_down")
             sound("pick")
-            //Rs3 and osrs has this message.
+            // Rs3 and osrs has this message.
             message("You pick a handful of nettles.")
             inventory.add("nettles")
             target.remove(15)
