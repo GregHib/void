5598e74374d5e966b69b37925a20300682b2f5f8
GregHib <greghib@users.noreply.github.com>
2026-03-19 19:16:22 +0000

    Add dust devils

---
+++ game/src/main/kotlin/content/area/kharidian_desert/pollnivneach/smoke_dungeon/SmokeDungeon.kt
@@ -0,0 +1,12 @@
+package content.area.kharidian_desert.pollnivneach.smoke_dungeon
+
+import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.client.ui.close
+
+class SmokeDungeon : Script {
+    init {
+            // TODO smoke interface - https://youtu.be/2ARmHyBkO8w?t=108
+//        entered("smoke_dungeon") {}
+//        exited("smoke_dungeon") {}
+    }
+}
