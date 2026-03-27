ebe41bc880a1ac8648cb5f2fb975c330eb173f9f
GregHib <greghib@users.noreply.github.com>
2026-03-27 14:41:13 +0000

    Convert fletching enums

--- game/src/main/kotlin/content/skill/fletching/FletchUnfinished.kt
+++ game/src/main/kotlin/content/skill/fletching/FletchUnfinished.kt
@@ -4,7 +4,8 @@ import content.entity.player.dialogue.type.makeAmount
 import world.gregs.voidps.cache.definition.Params
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.message
-import world.gregs.voidps.engine.data.definition.EnumDefinitions
+import world.gregs.voidps.engine.data.config.RowDefinition
+import world.gregs.voidps.engine.data.definition.Rows
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.chat.ChatType
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
@@ -28,17 +29,16 @@ class FletchUnfinished : Script {
                     maximum = 27,
                     text = "What would you like to fletch?",
                 )
-                EnumDefinitions.intOrNull("unf_fletching_xp", selected) ?: return@weakQueue
-                val level = EnumDefinitions.int("unf_fletching_level", selected)
-                if (!has(Skill.Fletching, level, true)) {
+                val unf = Rows.getOrNull("fletching_unf.${toItem.id}") ?: return@weakQueue
+                if (!has(Skill.Fletching, unf.int("level"), true)) {
                     return@weakQueue
                 }
-                fletch(selected, toItem.id, amount)
+                fletch(selected, unf, toItem.id, amount)
             }
         }
     }
 
-    fun Player.fletch(addItem: String, removeItem: String, amount: Int) {
+    fun Player.fletch(addItem: String, unf: RowDefinition, removeItem: String, amount: Int) {
         if (amount <= 0) {
             return
         }
@@ -47,9 +47,8 @@ class FletchUnfinished : Script {
             return
         }
 
-        val tick = EnumDefinitions.int("unf_fletching_tick", addItem)
-        weakQueue("fletching", tick) {
-            val makeAmount = EnumDefinitions.int("unf_fletching_make_amount", addItem)
+        weakQueue("fletching", unf.int("tick")) {
+            val makeAmount = unf.int("amount")
             val success = inventory.transaction {
                 remove(removeItem)
                 add(addItem, makeAmount)
@@ -61,10 +60,10 @@ class FletchUnfinished : Script {
 
             val itemCreated = getFletched(addItem)
             message("You carefully cut the wood into $itemCreated.", ChatType.Game)
-            val xp = EnumDefinitions.int("unf_fletching_xp", addItem)
-            exp(Skill.Fletching, xp / 10.0)
+            val xp = unf.int("xp") / 10.0
+            exp(Skill.Fletching, xp)
             anim("fletching_log")
-            fletch(addItem, removeItem, amount - 1)
+            fletch(addItem, unf, removeItem, amount - 1)
         }
     }
 
