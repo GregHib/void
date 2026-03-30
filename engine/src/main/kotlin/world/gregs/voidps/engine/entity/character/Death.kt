cf445bdaf03a3f242481b798355cf7370e68c627
GregHib <greghib@users.noreply.github.com>
2026-03-30 19:44:53 +0100

    Add post death

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/Death.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/Death.kt
@@ -13,6 +13,7 @@ interface Death {
     data class OnDeath(
         var dropItems: Boolean = true,
         var teleport: Tile? = null,
+        var respawn: Boolean = true,
     )
 
     fun playerDeath(handler: Player.(OnDeath) -> Unit) {
@@ -20,10 +21,17 @@ interface Death {
         playerHandlers.add(handler)
     }
 
-    fun npcDeath(npc: String = "*", handler: NPC.() -> Unit) {
+    fun npcDeath(npc: String = "*", handler: NPC.(OnDeath) -> Unit) {
         Script.checkLoading()
         Wildcards.find(npc, Wildcard.Npc) { id ->
             npcHandlers.getOrPut(id) { mutableListOf() }.add(handler)
+        }
+    }
+
+    fun npcAfterDeath(npc: String = "*", handler: NPC.() -> Unit) {
+        Script.checkLoading()
+        Wildcards.find(npc, Wildcard.Npc) { id ->
+            npcAfterHandlers.getOrPut(id) { mutableListOf() }.add(handler)
         }
     }
 
@@ -36,8 +44,9 @@ interface Death {
 
     companion object : AutoCloseable {
         private val playerHandlers = ObjectArrayList<Player.(OnDeath) -> Unit>(20)
-        private val npcHandlers = Object2ObjectOpenHashMap<String, MutableList<NPC.() -> Unit>>(20)
+        private val npcHandlers = Object2ObjectOpenHashMap<String, MutableList<NPC.(OnDeath) -> Unit>>(20)
         private val npcDeathHandlers = Object2ObjectOpenHashMap<String, MutableList<NPC.() -> Boolean>>(20)
+        private val npcAfterHandlers = Object2ObjectOpenHashMap<String, MutableList<NPC.() -> Unit>>(20)
 
         fun killed(player: Player): OnDeath {
             val onDeath = OnDeath()
@@ -47,13 +56,27 @@ interface Death {
             return onDeath
         }
 
-        fun killed(npc: NPC) {
+        fun killed(npc: NPC): OnDeath {
+            val onDeath = OnDeath()
             for (handler in npcHandlers[npc.id] ?: emptyList()) {
-                handler.invoke(npc)
+                handler.invoke(npc, onDeath)
             }
             for (handler in npcHandlers["*"] ?: emptyList()) {
-                handler.invoke(npc)
+                handler.invoke(npc, onDeath)
             }
+            return onDeath
+        }
+
+        fun afterDeath(npc: NPC): Boolean {
+            for (handler in npcAfterHandlers[npc.id] ?: emptyList()) {
+                handler.invoke(npc)
+                return true
+            }
+            for (handler in npcAfterHandlers["*"] ?: emptyList()) {
+                handler.invoke(npc)
+                return true
+            }
+            return false
         }
 
         fun canDie(npc: NPC): Boolean {
