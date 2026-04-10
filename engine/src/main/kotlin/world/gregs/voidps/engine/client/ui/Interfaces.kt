5cecb6f8b12eb58dd5e164a5530c79e3f50178b6
GregHib <greghib@users.noreply.github.com>
2026-04-10 13:12:11 +0100

    Fix music tracks not changing between areas closes #936

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/ui/Interfaces.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/ui/Interfaces.kt
@@ -290,6 +290,5 @@ fun Player.playTrack(trackIndex: Int) {
     playMusicTrack(EnumDefinitions.get("music_tracks").int(trackIndex))
     val name = EnumDefinitions.get("music_track_names").string(trackIndex)
     interfaces.sendText("music_player", "currently_playing", name)
-    this["playing_song"] = true
     this["current_track"] = trackIndex
 }
