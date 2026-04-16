7eec4548890f0f1c7fe56a6d04b222efa14ab81c
Harley Gilpin <hg@harleygilpin.com>
2026-04-16 15:48:17 -0700

    feat(bots): add player-vs-player combat action

--- game/src/main/kotlin/content/bot/behaviour/condition/BotInArea.kt
+++ game/src/main/kotlin/content/bot/behaviour/condition/BotInArea.kt
@@ -3,8 +3,8 @@ package content.bot.behaviour.condition
 import world.gregs.voidps.engine.data.definition.Areas
 import world.gregs.voidps.engine.entity.character.player.Player
 
-data class BotInArea(val id: String) : Condition(1000) {
-    override fun keys() = setOf("area:$id")
+data class BotInArea(val id: String, val present: Boolean = true) : Condition(1000) {
+    override fun keys() = if (present) setOf("area:$id") else setOf()
     override fun events() = setOf("area:$id")
-    override fun check(player: Player) = player.tile in Areas[id]
+    override fun check(player: Player) = (player.tile in Areas[id]) == present
 }
