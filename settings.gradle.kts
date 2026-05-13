acf58ebf280b4f74ce457097e7869b1c55b46dad
GregHib <greghib@users.noreply.github.com>
2026-05-13 15:54:35 +0100

    Fix toolchains for running /tools without jdk 21

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,10 @@ dependencyResolutionManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
 rootProject.name = "void"
 
 include("game")
