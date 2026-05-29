f08006538f91fe57e9f813dd2361aea9c5b3911a
GregHib <greghib@users.noreply.github.com>
2026-05-29 19:03:38 +0100

    Fix problematic gradle bot test

--- game/src/test/kotlin/content/bot/behaviour/action/BotPrayTest.kt
+++ game/src/test/kotlin/content/bot/behaviour/action/BotPrayTest.kt
@@ -1,5 +1,6 @@
 package content.bot.behaviour.action
 
+import WorldTest
 import content.bot.Bot
 import content.bot.FakeBehaviour
 import content.bot.FakeWorld
@@ -9,83 +10,25 @@ import content.bot.behaviour.Reason
 import content.bot.behaviour.condition.BotSkillLevel
 import content.skill.prayer.PrayerConfigs
 import content.skill.prayer.isCurses
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
-import org.koin.dsl.module
-import world.gregs.voidps.engine.client.variable.BitwiseValues
-import world.gregs.voidps.engine.data.config.PrayerDefinition
-import world.gregs.voidps.engine.data.config.VariableDefinition
-import world.gregs.voidps.engine.data.definition.PrayerDefinitions
-import world.gregs.voidps.engine.data.definition.VariableDefinitions
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.player.skill.level.Level
 import world.gregs.voidps.engine.entity.character.player.skill.level.PlayerLevels
 
-class BotPrayTest {
+class BotPrayTest : WorldTest() {
 
     private lateinit var bot: Bot
     private lateinit var player: Player
-    private lateinit var prayerDefinitions: PrayerDefinitions
 
     @BeforeEach
     fun setup() {
-        player = Player()
+        player = createPlayer()
         bot = Bot(player)
         player.experience.player = player
         player.levels.link(player, PlayerLevels(player.experience))
-
-        VariableDefinitions.set(
-            mapOf(
-                PrayerConfigs.ACTIVE_PRAYERS to VariableDefinition.VarbitDefinition(
-                    id = 0,
-                    values = BitwiseValues(listOf("protect_from_melee", "piety")),
-                    default = null,
-                    persistent = false,
-                    transmit = false,
-                ),
-                PrayerConfigs.ACTIVE_CURSES to VariableDefinition.VarbitDefinition(
-                    id = 1,
-                    values = BitwiseValues(listOf("turmoil", "soul_split", "berserker", "deflect_melee")),
-                    default = null,
-                    persistent = false,
-                    transmit = false,
-                ),
-                PrayerConfigs.PRAYERS to VariableDefinition.CustomVariableDefinition(
-                    values = world.gregs.voidps.engine.client.variable.StringValues,
-                    default = "normal",
-                    persistent = false,
-                ),
-            ),
-        )
-
-        prayerDefinitions = PrayerDefinitions().apply {
-            definitions = mapOf(
-                "protect_from_melee" to PrayerDefinition(index = 17, level = 43, stringId = "protect_from_melee"),
-                "piety" to PrayerDefinition(index = 25, level = 70, stringId = "piety"),
-                "turmoil" to PrayerDefinition(index = 19, level = 95, isCurse = true, stringId = "turmoil"),
-                "deflect_melee" to PrayerDefinition(index = 9, level = 71, isCurse = true, stringId = "deflect_melee"),
-            )
-        }
-        startKoin {
-            modules(
-                module {
-                    single { prayerDefinitions }
-                },
-            )
-        }
-    }
-
-    @AfterEach
-    fun stop() {
-        stopKoin()
-        VariableDefinitions.clear()
     }
 
     @Test
@@ -197,7 +140,7 @@ class BotPrayTest {
         val state = BotPray("turmoil").update(bot, FakeWorld(), BehaviourFrame(FakeBehaviour()))
 
         assertEquals(BehaviourState.Success, state)
-        assertEquals("curses", player.get(PrayerConfigs.PRAYERS, ""))
+        assertEquals("curses", player[PrayerConfigs.PRAYERS, ""])
         assertTrue(player.containsVarbit(PrayerConfigs.ACTIVE_CURSES, "turmoil"))
         assertFalse(player.containsVarbit(PrayerConfigs.ACTIVE_PRAYERS, "turmoil"))
     }
