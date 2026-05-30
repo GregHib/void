32d8e2d2ec032aacf5172466f74c5108a3b1f0c8
GregHib <greghib@users.noreply.github.com>
2026-05-30 15:56:51 +0100

    Add questStage helper

--- game/src/main/kotlin/content/quest/Quest.kt
+++ game/src/main/kotlin/content/quest/Quest.kt
@@ -3,6 +3,7 @@ package content.quest
 import world.gregs.voidps.engine.client.sendScript
 import world.gregs.voidps.engine.client.ui.open
 import world.gregs.voidps.engine.data.Settings
+import world.gregs.voidps.engine.data.definition.VariableDefinitions
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.item.Item
 
@@ -31,6 +32,12 @@ val quests = setOf(
 )
 
 fun Player.quest(name: String): String = this[name, "unstarted"]
+
+fun Player.questStage(name: String): Int {
+    val value = quest(name)
+    val def = VariableDefinitions.get(name) ?: return 0
+    return def.values.toInt(value)
+}
 
 fun Player.questCompleted(name: String): Boolean {
     if (!quests.contains(name) && Settings["quests.requirements.skipMissing", false]) {
