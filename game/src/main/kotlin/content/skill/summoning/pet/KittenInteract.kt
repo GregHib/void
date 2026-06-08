451d8eff69d36c0a81c3efa206e3d35fa48d5875
Harley Gilpin <hg@harleygilpin.com>
2026-06-08 16:56:16 -0700

    Fix: Set dialogue target once in icthlarinRecap
    Call talkWith(cat) up front and use the id-less npc() overload throughout,
    removing the repeated cat.id argument on every chathead line.

--- game/src/main/kotlin/content/skill/summoning/pet/KittenInteract.kt
+++ game/src/main/kotlin/content/skill/summoning/pet/KittenInteract.kt
@@ -10,6 +10,7 @@ import content.entity.player.dialogue.type.statement
 import content.quest.questCompleted
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.message
+import world.gregs.voidps.engine.client.ui.dialogue.talkWith
 import world.gregs.voidps.engine.client.variable.start
 import world.gregs.voidps.engine.client.variable.stop
 import world.gregs.voidps.engine.data.config.RowDefinition
@@ -219,53 +220,54 @@ suspend fun Player.talkToCatWithAmulet(cat: NPC) {
 }
 
 private suspend fun Player.icthlarinRecap(cat: NPC) {
+    talkWith(cat)
     val asked = inc("cat_icthlarin_asks", 1)
     if (asked >= 4) {
         player<Quiz>("Can you explain to me about Icthlarin and the devourer again?")
-        npc<Happy>(cat.id, "I'm getting a little bit sick of talking about Icthlarin and all that stuff. If you didn't understand it the first time around, nor when I filled you in on a few details and then when I spelt it out to you what happened I don't think you'll ever get it.")
+        npc<Happy>("I'm getting a little bit sick of talking about Icthlarin and all that stuff. If you didn't understand it the first time around, nor when I filled you in on a few details and then when I spelt it out to you what happened I don't think you'll ever get it.")
         player<Quiz>("Aw come on. Just tell me one more time.")
-        npc<Happy>(cat.id, "Look, can we just leave it. Ask one of your adventuring friends.")
+        npc<Happy>("Look, can we just leave it. Ask one of your adventuring friends.")
         player<Happy>("Alright puss.")
         return
     }
     player<Quiz>("Did you understand what went on in that quest with the devourer and Icthlarin?")
-    npc<Happy>(cat.id, "It wasn't all that difficult to understand.")
+    npc<Happy>("It wasn't all that difficult to understand.")
     player<Quiz>("Really? I was, no actually am, still confused.")
-    npc<Happy>(cat.id, "Ok, I'll tell you what I think happened.")
+    npc<Happy>("Ok, I'll tell you what I think happened.")
     player<Happy>("Great! Story time.")
-    npc<Happy>(cat.id, "There once was a high priest of some crazy religion called Klenter.")
+    npc<Happy>("There once was a high priest of some crazy religion called Klenter.")
     player<Quiz>("What, a religion called Klenter? What kind of name is that?")
-    npc<Happy>(cat.id, "No no. The high priest was called Klenter and he was the high priest of Icthlarin, god of the dead.")
+    npc<Happy>("No no. The high priest was called Klenter and he was the high priest of Icthlarin, god of the dead.")
     player<Happy>("Ok, I'm with you that far.")
-    npc<Happy>(cat.id, "He dies, and a struggle starts for his soul. Another god - the Devourer - wants to destroy his soul because she has some dispute with Icthlarin. So she takes on the guise of a human.")
+    npc<Happy>("He dies, and a struggle starts for his soul. Another god - the Devourer - wants to destroy his soul because she has some dispute with Icthlarin. So she takes on the guise of a human.")
     player<Happy>("Ah - the wanderer.")
-    npc<Happy>(cat.id, "Very good. Now she needs help in getting Klenter's soul so she tricks some gullible fool into helping her.")
+    npc<Happy>("Very good. Now she needs help in getting Klenter's soul so she tricks some gullible fool into helping her.")
     player<Quiz>("So who was that?")
-    npc<Happy>(cat.id, "You!")
+    npc<Happy>("You!")
     player<Quiz>("Oh. So why doesn't she have a high priest too like Icthlarin.")
-    npc<Happy>(cat.id, "Well she's the god of destruction. If she had a priest or a temple or anything she would just destroy it.")
+    npc<Happy>("Well she's the god of destruction. If she had a priest or a temple or anything she would just destroy it.")
     player<Quiz>("So if she's so powerful why does she need this gullible fool's help?")
-    npc<Happy>(cat.id, "Because of my kind.")
+    npc<Happy>("Because of my kind.")
     player<Quiz>("Your kind?")
-    npc<Happy>(cat.id, "Cats!")
+    npc<Happy>("Cats!")
     player<Quiz>("What? Why would she care about you? All you ever want is a bit of attention and the odd fish or two.")
-    npc<Happy>(cat.id, "We have other powers, not clear to you. Anyway I'm moving away from the story. The wanderer recruits your help by hypnotising you. Do you remember that?")
+    npc<Happy>("We have other powers, not clear to you. Anyway I'm moving away from the story. The wanderer recruits your help by hypnotising you. Do you remember that?")
     player<Quiz>("Vaguely, I think. Why did she choose me to help her then.")
-    npc<Happy>(cat.id, "Because of me.")
+    npc<Happy>("Because of me.")
     player<Quiz>("I don't understand.")
-    npc<Happy>(cat.id, "Cats are the only things that can open the pyramid's door, so she needed an adventurer with one. So you entered the pyramid with me - under her mind control - and stole a canopic jar containing an organ belonging to Klenter.")
+    npc<Happy>("Cats are the only things that can open the pyramid's door, so she needed an adventurer with one. So you entered the pyramid with me - under her mind control - and stole a canopic jar containing an organ belonging to Klenter.")
     player<Happy>("Ahh I think I'm beginning to get a better understanding.")
-    npc<Happy>(cat.id, "You then started to return with the jar to the Devourer, but as a sting on the tail she made you plant one of her symbols in the ceremonial room of the pyramid. You then tried to flee the pyramid but Icthlarin appeared just as you reached the exit.")
+    npc<Happy>("You then started to return with the jar to the Devourer, but as a sting on the tail she made you plant one of her symbols in the ceremonial room of the pyramid. You then tried to flee the pyramid but Icthlarin appeared just as you reached the exit.")
     player<Quiz>("The guy with the head of a dog?")
-    npc<Happy>(cat.id, "Yes Player. Well he either broke the devourer's hold on you or else Klenter did.")
+    npc<Happy>("Yes Player. Well he either broke the devourer's hold on you or else Klenter did.")
     player<Quiz>("Klenter? I thought he was dead.")
-    npc<Happy>(cat.id, "He was, and still is. Ok, Icthlarin summoned Klenter's soul to torment you into returning the jar, the end result was that you were freed from the devourer's grasp.")
+    npc<Happy>("He was, and still is. Ok, Icthlarin summoned Klenter's soul to torment you into returning the jar, the end result was that you were freed from the devourer's grasp.")
     player<Happy>("So that's when I woke up with that jar in my inventory and had that intolerable ghost harassing me.")
-    npc<Happy>(cat.id, "You then bumbled around for a bit and returned the jar, discovering that the high priest still hadn't completed the final ceremony so you got him all the bits and pieces.")
+    npc<Happy>("You then bumbled around for a bit and returned the jar, discovering that the high priest still hadn't completed the final ceremony so you got him all the bits and pieces.")
     player<Happy>("So then I remembered about the devourer's symbol which I placed in the ceremonial room and had to rush back and warn the priests about the devourer.")
-    npc<Happy>(cat.id, "And the rest is simple enough to piece together.")
+    npc<Happy>("And the rest is simple enough to piece together.")
     player<Happy>("Thanks cat, you know you're quite smart for a fish-eating animated ball of fluff.")
-    npc<Happy>(cat.id, "You say the sweetest things. Hiss.")
+    npc<Happy>("You say the sweetest things. Hiss.")
 }
 
 /** Hellcat catspeak Talk-to: separate option tree with wily/lazy hellcat flavour. */
