941e0a169ffeb1f7e08dd77db0a3b1f422a4f27e
Harley Gilpin <hg@harleygilpin.com>
2026-06-11 09:38:48 -0700

    refactor: use item definition 'full' field for humidify instead of duplicate table data

--- game/src/main/kotlin/content/skill/magic/book/lunar/Humidify.kt
+++ game/src/main/kotlin/content/skill/magic/book/lunar/Humidify.kt
@@ -5,7 +5,6 @@ import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.client.variable.hasClock
 import world.gregs.voidps.engine.client.variable.start
-import world.gregs.voidps.engine.data.definition.Rows
 import world.gregs.voidps.engine.data.definition.Tables
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.player.skill.exp.exp
@@ -16,18 +15,13 @@ import world.gregs.voidps.engine.inv.transact.operation.ReplaceItem.replace
 
 class Humidify : Script {
 
-    private val fills: Map<String, String> by lazy {
-        val table = Tables.getOrNull("humidify") ?: return@lazy emptyMap()
-        table.rows.map { Rows.get(it).itemPair("fill") }.toMap()
-    }
-
     init {
         interfaceOption("Cast", "lunar_spellbook:humidify") {
             val spell = it.component
             if (hasClock("action_delay")) {
                 return@interfaceOption
             }
-            if (inventory.items.none { item -> fills.containsKey(item.id) }) {
+            if (inventory.items.none { item -> item.def.contains("full") }) {
                 message("You have no empty containers to fill.")
                 return@interfaceOption
             }
@@ -42,8 +36,10 @@ class Humidify : Script {
             inventory.transaction {
                 for (index in inventory.indices) {
                     val item = inventory[index]
-                    val full = fills[item.id] ?: continue
-                    replace(index, item.id, full)
+                    if (!item.def.contains("full")) {
+                        continue
+                    }
+                    replace(index, item.id, item.def["full"])
                 }
             }
             if (inventory.transaction.error != TransactionError.None) {
