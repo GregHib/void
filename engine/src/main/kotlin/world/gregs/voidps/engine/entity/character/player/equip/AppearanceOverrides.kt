b09b7b2ba6d86377d0424779ffd4e6939ea5272e
GregHib <greghib@users.noreply.github.com>
2026-06-20 10:25:59 +0100

    Revert "Fix: party hat items not displaying correctly on player head"
    This reverts commit 6353b5214092acc29d43b68c97525b44a5b114ed.

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/player/equip/AppearanceOverrides.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/player/equip/AppearanceOverrides.kt
@@ -1,32 +1,39 @@
 package world.gregs.voidps.engine.entity.character.player.equip
 
 import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap
+import world.gregs.voidps.cache.definition.data.EnumDefinition
 import world.gregs.voidps.engine.data.definition.EnumDefinitions
 import world.gregs.voidps.engine.data.definition.StructDefinitions
 
 class AppearanceOverrides {
 
-    private val maleMid by lazy { load("look_hair_male", "body_look_flat_mid") }
-    private val femaleMid by lazy { load("look_hair_female", "body_look_flat_mid") }
-    private val maleLow by lazy { load("look_hair_male", "body_look_flat_low") }
-    private val femaleLow by lazy { load("look_hair_female", "body_look_flat_low") }
+    private val maleMid: MutableMap<Int, Int> = Int2IntOpenHashMap(32)
+    private val femaleMid: MutableMap<Int, Int> = Int2IntOpenHashMap(48)
+    private val maleLow: MutableMap<Int, Int> = Int2IntOpenHashMap(32)
+    private val femaleLow: MutableMap<Int, Int> = Int2IntOpenHashMap(48)
 
-    fun hairMid(current: Int, male: Boolean): Int = (if (male) maleMid else femaleMid).getOrDefault(current, current) + 0x100
+    fun hairMid(current: Int, male: Boolean): Int = (if (male) this.maleMid else femaleMid).getOrDefault(current, current) + 0x100
 
-    fun hairLow(current: Int, male: Boolean): Int = (if (male) {
+    fun hairLow(current: Int, male: Boolean): Int = if (male) {
         maleLow.getOrDefault(current, 0)
     } else {
         femaleLow.getOrDefault(current, 243)
-    }) + 0x100
+    } + 0x100
 
-    private fun load(enumName: String, key: String): Map<Int, Int> {
-        val map = Int2IntOpenHashMap()
-        for (value in EnumDefinitions.get(enumName).map?.values ?: return map) {
-            val struct = StructDefinitions.getOrNull(value as Int) ?: continue
+    private fun load() {
+        load(maleMid, EnumDefinitions.get("look_hair_male"), "body_look_flat_mid")
+        load(femaleMid, EnumDefinitions.get("look_hair_female"), "body_look_flat_mid")
+        load(maleLow, EnumDefinitions.get("look_hair_male"), "body_look_flat_low")
+        load(femaleLow, EnumDefinitions.get("look_hair_female"), "body_look_flat_low")
+    }
+
+    private fun load(map: MutableMap<Int, Int>, enum: EnumDefinition, key: String) {
+        for (value in enum.map?.values ?: return) {
+            val structId = value as Int
+            val struct = StructDefinitions.getOrNull(structId) ?: continue
             val index: Int = struct.getOrNull("body_look_index") ?: continue
             val replacement: Int = struct.getOrNull(key) ?: continue
             map[index] = replacement
         }
-        return map
     }
 }
