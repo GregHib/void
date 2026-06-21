8cb5610e3f6b21623a29e89c13ccfe7d98dc2a42
M1krofin <207991987+M1krofin@users.noreply.github.com>
2026-06-21 20:00:31 +0300

    Added haybales, sacks, Groats farm crates object operations.

---
+++ game/src/main/kotlin/content/area/misthalin/lumbridge/farm/HayBales.kt
@@ -0,0 +1,47 @@
+package content.area.misthalin.lumbridge.farm
+
+import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.client.message
+import content.entity.player.dialogue.type.player
+import content.entity.player.dialogue.Happy
+import world.gregs.voidps.type.random
+import content.entity.player.dialogue.Sad
+import content.entity.combat.hit.damage
+import world.gregs.voidps.engine.entity.item.floor.FloorItems
+import world.gregs.voidps.engine.inv.inventory
+import world.gregs.voidps.engine.inv.add
+
+class HayBales : Script {
+    //https://x.com/JagexAsh/status/1056603985342275585
+
+    init {
+        objectOperate ("Search", "hay_bales*,hay_bale_*") { (target) ->
+            anim("climb_down")
+            //both rs3 and osrs has these messages.
+            if (target.id.contains("hay_bale_")) {
+                message("You search the hay bale...")
+            } else {
+                message("You search the hay bales...")
+            }
+            delay(2)
+            val roll = random.nextInt(100)
+            when {
+                roll < 2 -> {
+                    damage(10)
+                    player<Sad>("Ow! There's something sharp in there!")
+                }
+                roll < 12 -> {
+                    if (inventory.isFull()) {
+                        FloorItems.add(tile, "needle", disappearTicks = 200, owner = this)
+                    } else {
+                        inventory.add("needle")
+                    }
+                    player<Happy>("Wow! A needle!<br> Now what are the chances of finding that?")
+                }
+                else -> {
+                    message("You find nothing of interest.")
+                }
+            }
+        }
+    }
+}
