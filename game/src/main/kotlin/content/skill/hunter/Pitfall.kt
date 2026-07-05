82907eb90651e7c0394d47060495709d17d750b2
GregHib <greghib@users.noreply.github.com>
2026-07-05 18:02:36 +0100

    Update pitfall messages

--- game/src/main/kotlin/content/skill/hunter/Pitfall.kt
+++ game/src/main/kotlin/content/skill/hunter/Pitfall.kt
@@ -5,32 +5,27 @@ import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.client.ui.chat.plural
 import world.gregs.voidps.engine.data.definition.Rows
-import world.gregs.voidps.engine.data.definition.Tables
 import world.gregs.voidps.engine.entity.character.areaSound
-import world.gregs.voidps.engine.entity.character.npc.NPC
-import world.gregs.voidps.engine.entity.character.npc.NPCs
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.chat.ChatType
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.player.skill.level.Level.has
 import world.gregs.voidps.engine.entity.character.sound
 import world.gregs.voidps.engine.entity.obj.GameObject
-import world.gregs.voidps.engine.inv.add
 import world.gregs.voidps.engine.inv.inventory
 import world.gregs.voidps.engine.inv.remove
 import world.gregs.voidps.type.Direction
-import world.gregs.voidps.type.Tile
 
 class Pitfall : Script {
     init {
         npcOperate("Tease") {
             val target = it.target
             if (!has(Skill.Hunter, 99)) { // TODO
-                message("You need a higher hunter level") // TODO proper message
+                message("You need a hunter level of at least 99 to catch a ${target.def.name.lowercase()}.")
                 return@npcOperate
             }
             if (!inventory.contains("teasing_stick")) {
-                message("You need a teasing stick.")// TODO proper message
+                message("I don't want to lose a finger by poking it. Maybe I should get a professional teasing implement instead.")
                 return@npcOperate
             }
             it.updateInteraction {
@@ -58,7 +53,7 @@ class Pitfall : Script {
         }
 
         objectOperate("Dismantle", "pitfall_*") { (target) ->
-            dismantleTrap("pitfall", target, target.tile)
+            dismantleTrap(target)
         }
 
     }
@@ -66,7 +61,8 @@ class Pitfall : Script {
     private suspend fun Player.layTrap(obj: GameObject) {
         val trap = Rows.getOrNull("traps.pitfall") ?: return
         val level = levels.get(Skill.Hunter)
-        if (!has(Skill.Hunter, trap.int("level"), message = true)) {
+        if (!has(Skill.Hunter, trap.int("level"), message = false)) {
+            message("You need a hunter level of at least 99 to set a pitfall trap here.")
             return
         }
         if (get(obj.id, "empty") != "empty") {
@@ -78,12 +74,8 @@ class Pitfall : Script {
             message("You may setup only $max ${"trap".plural(max)} at a time at your Hunter level.")
             return
         }
-        if (!inventory.contains("knife")) {
-            message("You need a knife to lay this trap.") // TODO proper message
-            return
-        }
-        if (!inventory.contains("logs")) {
-            message("You need logs to lay this trap.") // TODO proper message
+        if (!inventory.contains("knife") || !inventory.contains("logs")) {
+            message("You need some logs and a knife to set a pitfall trap.")
             return
         }
         // TODO collapse timer
@@ -96,33 +88,12 @@ class Pitfall : Script {
         set(obj.id, "spiked")
     }
 
-    private suspend fun Player.dismantleTrap(trapId: String, target: GameObject, tile: Tile) {
-        val id = Tables.npc("traps.$trapId.npc")
-        val npc = NPCs.findOrNull(tile, id) ?: return
-        if (npc["owner", ""] != accountName) {
-            message("This is not your trap.")
-            return
-        }
-        val trap = Rows.get("traps.$trapId")
-        val items = trap.itemList("items")
-        if (inventory.spaces < items.size) {
-            val slots = items.size - inventory.spaces
-            message("You don't have enough inventory space. You need $slots more free ${"slot".plural(slots)}.")
-            return
-        }
-        anim(trap.anim("take_down_anim"))
-        sound("trap_dismantle", delay = 25)
-        delay(2)
-        removeTrap(target, npc)
-        for (item in items) {
-            inventory.add(item)
-        }
+    private suspend fun Player.dismantleTrap(target: GameObject) {
         message("You dismantle the trap.", ChatType.Filter)
-    }
-
-    private fun Player.removeTrap(target: GameObject, npc: NPC) {
+        anim("lay_trap_small")
+        delay(1)
+        sound("take_branches")
         dec("trap_count")
-        NPCs.remove(npc)
         set(target.id, "empty")
     }
 
