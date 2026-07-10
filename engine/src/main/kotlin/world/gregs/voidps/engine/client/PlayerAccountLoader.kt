bc4dde13c1d9576def59bcc0422543bad276341b
GregHib <greghib@users.noreply.github.com>
2026-07-10 17:36:28 +0100

    Fix logging in requiring display name instead of account name

--- engine/src/main/kotlin/world/gregs/voidps/engine/client/PlayerAccountLoader.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/client/PlayerAccountLoader.kt
@@ -39,7 +39,7 @@ class PlayerAccountLoader(
 
     override fun exists(username: String): Boolean = storage.exists(username)
 
-    override fun password(username: String): String? = accountDefinitions.get(username)?.passwordHash
+    override fun password(username: String): String? = accountDefinitions.getByAccount(username)?.passwordHash
 
     /**
      * @return flow of instructions for the player to be controlled with
