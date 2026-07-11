3070542409be033bfe99a452eb158f3948a75482
GregHib <greghib@users.noreply.github.com>
2026-07-11 11:55:29 +0100

    Reuse String.an for MysteryBox.kt

--- game/src/main/kotlin/content/activity/event/random/MysteryBox.kt
+++ game/src/main/kotlin/content/activity/event/random/MysteryBox.kt
@@ -3,6 +3,7 @@ package content.activity.event.random
 import content.entity.player.inv.item.addOrDrop
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.message
+import world.gregs.voidps.engine.client.ui.chat.an
 import world.gregs.voidps.engine.entity.item.drop.DropTables
 import world.gregs.voidps.engine.get
 import world.gregs.voidps.engine.inv.inventory
@@ -28,16 +29,15 @@ class MysteryBox : Script {
             addOrDrop(drop.id, drop.amount)
             val name = drop.def.name.lowercase()
             val found = when {
-                drop.amount > 1 -> "${drop.amount} x $name"
-                name.first() in "aeiou" -> "an $name"
-                else -> "a $name"
+                drop.amount > 1 -> " ${drop.amount} x $name"
+                else -> "${name.an()} $name"
             }
             val flavour = when (drop.def.cost * drop.amount) {
                 in 0..100 -> "Better luck next time!"
                 in 101..500 -> "Well, it could have been worse."
                 else -> "Excellent!"
             }
-            message("Inside the box you find $found! $flavour")
+            message("Inside the box you find${found}! $flavour")
         }
     }
 }
