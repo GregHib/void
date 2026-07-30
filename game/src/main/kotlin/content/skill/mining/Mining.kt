634aff82f59d426f75840837a172ea3b3f08870f
Skal Fate <20464880+skalfate@users.noreply.github.com>
2026-07-30 11:33:03 -0600

    Fixed mining rune essence giving both a rune essence and pure essence (#1097)
    * Fixed mining rune essence giving both a rune essence and pure essence together per success at level 30 and after.
    
    * Apply suggestion from @GregHib
    
    ---------
    
    Co-authored-by: Greg <GregHib@users.noreply.github.com>

--- game/src/main/kotlin/content/skill/mining/Mining.kt
+++ game/src/main/kotlin/content/skill/mining/Mining.kt
@@ -105,6 +105,9 @@ class Mining : Script {
                     }
                 }
                 for (item in ores) {
+                    if (item == "rune_essence" && has(Skill.Mining, 30) && World.members) {
+                        continue
+                    }
                     if (item == "pure_essence" && !World.members) {
                         continue
                     }
