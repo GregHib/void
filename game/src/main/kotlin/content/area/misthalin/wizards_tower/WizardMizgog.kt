96403fbf9d80ed9c8f0267b599184c138a4ed43e
GregHib <greghib@users.noreply.github.com>
2026-07-31 13:22:57 +0100

    Prevent opening mizgog's amulet shop without imp catcher quest completed fixes #1090

--- game/src/main/kotlin/content/area/misthalin/wizards_tower/WizardMizgog.kt
+++ game/src/main/kotlin/content/area/misthalin/wizards_tower/WizardMizgog.kt
@@ -14,6 +14,7 @@ import content.entity.player.dialogue.type.player
 import content.entity.player.dialogue.type.statement
 import content.quest.quest
 import content.quest.questComplete
+import content.quest.questCompleted
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.clearCamera
 import world.gregs.voidps.engine.client.command.adminCommand
@@ -46,75 +47,14 @@ class WizardMizgog : Script {
 
     init {
         npcOperate("Talk-to", "wizard_mizgog") { (target) ->
-            when (quest("imp_catcher")) {
-                "unstarted" -> {
-                    player<Neutral>("Give me a quest!")
-                    npc<Happy>("Give me a quest what?")
-                    choice {
-                        option<Neutral>("Give me a quest please.") {
-                            npc<Happy>("Well seeing as you asked nicely... I could do with some help.")
-                            npc<Sad>("The wizard Grayzag next door decided he didn't like me so he enlisted an army of hundreds of imps.")
-                            npc<Sad>("These imps stole all sorts of my things. Most of these things I don't really care about, just eggs and balls of string and things.")
-                            npc<Sad>("But they stole my four magical beads. There was a red one, a yellow one, a black one, and a white one.")
-                            npc<Sad>("These imps have now spread out all over the kingdom. Could you get my beads back for me?")
-                            choice("Start the Imp Catcher quest?") {
-                                option("Yes.") {
-                                    set("imp_catcher", "started")
-                                    if (inventory.contains(beads)) {
-                                        player<Happy>("Well this is a surprising turn of events!")
-                                        npc<Quiz>("What?")
-                                        player<Happy>("Well I just so happen to have all of those beads on me!")
-                                        npc<Angry>("Are you saying that you stole my beads all this time and I've been blaming these imps!?")
-                                        player<Neutral>("No, not at all! I just found them throughout my travels and presumed somebody would need them at some point.")
-                                        npc<Angry>("Bah! Fine.")
-                                        cutscene(target)
-                                    } else {
-                                        player<Happy>("I'll try.")
-                                        npc<Happy>("That's great, thank you.")
-                                    }
-                                }
-                                option("No.") {
-                                    player<Neutral>("I've better things to do than chase imps.")
-                                    npc<Angry>("Well if you're not interested in the quests I have to give you, don't waste my time by asking me for them.")
-                                }
-                            }
-                        }
-                        option("Give me a quest or else!")
-                        option("Just stop messing around and give me a quest!")
-                    }
-                }
-                "started" -> {
-                    npc<Happy>("So how are you doing finding my beads?")
-                    if (inventory.contains(beads)) {
-                        player<Happy>("I've got all four beads. It was hard work I can tell you.")
-                        cutscene(target)
-                    } else if (inventory.any(beads)) {
-                        player<Happy>("I have found some of your beads.")
-                        npc<Neutral>("Come back when you have them all. The colour of the four beads that I need are red, yellow, black, and white. Go chase some imps!")
-                    } else {
-                        player<Sad>("I haven't found any yet.") // TODO proper message
-                        npc<Neutral>("Come back when you have them all. The colour of the four beads that I need are red, yellow, black, and white. Go chase some imps!")
-                    }
-                }
-                "given_beads" -> questCompleted()
-                "completed" -> {
-                    npc<Happy>("What can I do for you, adventurer?")
-                    choice {
-                        option<Neutral>("Got any more quests?") {
-                            npc<Neutral>("No, everything is good with the world today.")
-                        }
-                        option("Do you know any interesting spells you could teach me?") {
-                            player<Neutral>("Do you know any interesting spells you could teach me?")
-                            npc<Laugh>("I don't think so, the type of magic I study involves years of meditation and research.")
-                        }
-                        option<Quiz>("Do you have any more amulets of accuracy?") {
-                            openShop("mizgogs_amulets_of_accuracy")
-//                            npc<Neutral>("I have a few spare. I'd like one of each coloured bead again in return, though! Black, white, yellow and red.")
-//                            player<Quiz>("I don't have them all on me at the moment. I'll come back when I have them!")
-//                            npc<Neutral>("Very well. See you soon!")
-                        }
-                    }
-                }
+            questDialogue(target)
+        }
+
+        npcOperate("Trade", "wizard_mizgog") { (target) ->
+            if (questCompleted("imp_catcher")) {
+                openShop(target.def["shop"])
+            } else {
+                questDialogue(target)
             }
         }
 
@@ -124,6 +64,79 @@ class WizardMizgog : Script {
                 GameObjects.replace(table, "imp_catcher_table", Tile(3102, 3163, 2), rotation = 1, ticks = 6)
             }
             set("imp_catcher", "given_beads")
+        }
+    }
+
+    private suspend fun Player.questDialogue(target: NPC) {
+        when (quest("imp_catcher")) {
+            "unstarted" -> {
+                player<Neutral>("Give me a quest!")
+                npc<Happy>("Give me a quest what?")
+                choice {
+                    option<Neutral>("Give me a quest please.") {
+                        npc<Happy>("Well seeing as you asked nicely... I could do with some help.")
+                        npc<Sad>("The wizard Grayzag next door decided he didn't like me so he enlisted an army of hundreds of imps.")
+                        npc<Sad>("These imps stole all sorts of my things. Most of these things I don't really care about, just eggs and balls of string and things.")
+                        npc<Sad>("But they stole my four magical beads. There was a red one, a yellow one, a black one, and a white one.")
+                        npc<Sad>("These imps have now spread out all over the kingdom. Could you get my beads back for me?")
+                        choice("Start the Imp Catcher quest?") {
+                            option("Yes.") {
+                                set("imp_catcher", "started")
+                                if (inventory.contains(beads)) {
+                                    player<Happy>("Well this is a surprising turn of events!")
+                                    npc<Quiz>("What?")
+                                    player<Happy>("Well I just so happen to have all of those beads on me!")
+                                    npc<Angry>("Are you saying that you stole my beads all this time and I've been blaming these imps!?")
+                                    player<Neutral>("No, not at all! I just found them throughout my travels and presumed somebody would need them at some point.")
+                                    npc<Angry>("Bah! Fine.")
+                                    cutscene(target)
+                                } else {
+                                    player<Happy>("I'll try.")
+                                    npc<Happy>("That's great, thank you.")
+                                }
+                            }
+                            option("No.") {
+                                player<Neutral>("I've better things to do than chase imps.")
+                                npc<Angry>("Well if you're not interested in the quests I have to give you, don't waste my time by asking me for them.")
+                            }
+                        }
+                    }
+                    option("Give me a quest or else!")
+                    option("Just stop messing around and give me a quest!")
+                }
+            }
+            "started" -> {
+                npc<Happy>("So how are you doing finding my beads?")
+                if (inventory.contains(beads)) {
+                    player<Happy>("I've got all four beads. It was hard work I can tell you.")
+                    cutscene(target)
+                } else if (inventory.any(beads)) {
+                    player<Happy>("I have found some of your beads.")
+                    npc<Neutral>("Come back when you have them all. The colour of the four beads that I need are red, yellow, black, and white. Go chase some imps!")
+                } else {
+                    player<Sad>("I haven't found any yet.") // TODO proper message
+                    npc<Neutral>("Come back when you have them all. The colour of the four beads that I need are red, yellow, black, and white. Go chase some imps!")
+                }
+            }
+            "given_beads" -> questCompleted()
+            "completed" -> {
+                npc<Happy>("What can I do for you, adventurer?")
+                choice {
+                    option<Neutral>("Got any more quests?") {
+                        npc<Neutral>("No, everything is good with the world today.")
+                    }
+                    option("Do you know any interesting spells you could teach me?") {
+                        player<Neutral>("Do you know any interesting spells you could teach me?")
+                        npc<Laugh>("I don't think so, the type of magic I study involves years of meditation and research.")
+                    }
+                    option<Quiz>("Do you have any more amulets of accuracy?") {
+                        openShop("mizgogs_amulets_of_accuracy")
+    //                            npc<Neutral>("I have a few spare. I'd like one of each coloured bead again in return, though! Black, white, yellow and red.")
+    //                            player<Quiz>("I don't have them all on me at the moment. I'll come back when I have them!")
+    //                            npc<Neutral>("Very well. See you soon!")
+                    }
+                }
+            }
         }
     }
 
