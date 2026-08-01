6b0c8abae5754c454d13e10fc9c016f71aad7b4c
GregHib <greghib@users.noreply.github.com>
2026-08-01 12:10:40 +0100

    Use summoning combat level on members worlds, add combat level up messages

--- game/src/main/kotlin/content/entity/player/combat/CombatLevel.kt
+++ game/src/main/kotlin/content/entity/player/combat/CombatLevel.kt
@@ -1,6 +1,7 @@
 package content.entity.player.combat
 
 import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.entity.World
 import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.combatLevel
@@ -13,8 +14,8 @@ class CombatLevel : Script {
 
     init {
         playerSpawn {
-            combatLevel = calculateCombatLevel(levels)
-            summoningCombatLevel = calculateCombatLevel(levels, true)
+            combatLevel = calculateCombatLevel(levels, summoning = World.members)
+            summoningCombatLevel = calculateCombatLevel(levels, summoning = true)
         }
 
         for (skill in Skill.entries.filter { it.ordinal <= 6 || it.ordinal == 23 }.toTypedArray()) {
@@ -23,8 +24,37 @@ class CombatLevel : Script {
     }
 
     fun recalculate(player: Player, skill: Skill, from: Int, to: Int) {
-        player.combatLevel = calculateCombatLevel(player.levels)
-        player.summoningCombatLevel = calculateCombatLevel(player.levels, true)
+        val previous = player.summoningCombatLevel
+        val level = calculateCombatLevel(player.levels, summoning = World.members)
+        player.combatLevel = level
+        player.summoningCombatLevel = calculateCombatLevel(player.levels, summoning = true)
+        if (level > previous) {
+            when (level) {
+                90, 100, 110, 120, 130 -> {
+                    // 90 - https://youtu.be/Jjn4-f9eSMM?t=47
+                    // 100 - https://youtu.be/sk72rWo2VPI?t=219
+                    // 130 - https://youtu.be/bHnKZatcA2s?t=10
+                    player.message("<red>Well done! You've reached the Combat level $level milestone!")
+                }
+                126 -> {
+                    // f2p - https://youtu.be/31_wDe_HJsU?t=64
+                    // mem - https://youtu.be/uKJpOSzeTpg?t=53
+                    player.message("<red>Congratulations! Your Combat level is now 126 - the highest possible Combat level on free worlds!")
+                }
+                138 -> {
+                    // https://youtu.be/o6Ga2FGG1Dc?t=329
+                    player.message("<red>Congratulations! Your Combat level is now 138! You've achieved the highest Combat level possible!")
+                }
+                else -> {
+                    // 30 - https://youtu.be/b4vTaGjIk8Q?t=11
+                    // 80 - https://youtu.be/GKKIH2hca0E?t=68
+                    // 112 - https://youtu.be/3GG4KkmPUrU?t=70
+                    // 125 - https://youtu.be/lFY_jQn3pfE?t=54
+                    // 128 - https://youtu.be/tBB914-pLEw?t=28
+                    player.message("Congratulations! You've just reached Combat level ${level}!")
+                }
+            }
+        }
     }
 
     fun calculateCombatLevel(levels: Levels, summoning: Boolean = false): Int {
