3d562d3bfd8958d115a152bfd6a2e95695580804
GregHib <greghib@users.noreply.github.com>
2026-08-02 16:08:08 +0100

    Tweak abyssal demon test

--- game/src/test/kotlin/content/area/wilderness/abyss/AbyssalDemonTest.kt
+++ game/src/test/kotlin/content/area/wilderness/abyss/AbyssalDemonTest.kt
@@ -34,6 +34,7 @@ class AbyssalDemonTest : WorldTest() {
     fun `Abyssal demon teleports around in combat`() {
         setRandom(object : FakeRandom() {
             override fun nextInt(until: Int) = if (until == 6) 6 else 0
+            override fun nextInt(from: Int, until: Int) = 0
         })
         val player = createPlayer(emptyTile)
         player.levels.set(Skill.Constitution, 50)
