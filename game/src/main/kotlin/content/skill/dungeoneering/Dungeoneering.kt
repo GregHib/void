8eb7c58306dada13660e3e452b244db7ba993919
GregHib <greghib@users.noreply.github.com>
2026-08-03 21:25:30 +0100

    Notes and started

--- game/src/main/kotlin/content/skill/dungeoneering/Dungeoneering.kt
+++ game/src/main/kotlin/content/skill/dungeoneering/Dungeoneering.kt
@@ -9,6 +9,12 @@ import world.gregs.voidps.engine.client.command.stringArg
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.entity.character.move.tele
 import world.gregs.voidps.engine.entity.character.player.chat.ChatType
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
+import world.gregs.voidps.engine.entity.item.drop.ItemDrop
+import world.gregs.voidps.engine.entity.item.floor.FloorItems
+import world.gregs.voidps.type.Delta
+import world.gregs.voidps.type.Tile
+import world.gregs.voidps.type.random
 
 class Dungeoneering : Script {
     init {
@@ -47,6 +53,8 @@ class Dungeoneering : Script {
             var tile = startRoom.tile
             delay(1) // Delay to avoid ghosting # 1000
             tele(instance.tile.add(tile.x * 16 + 8, tile.y * 16 + 8))
+
+            spawnItems(instance.tile, 0, dungeon)
         }
 
         adminCommand("dungeon") {
@@ -98,5 +106,65 @@ class Dungeoneering : Script {
                 room.open(this, dungeon)
             }
         }
+    }
+
+    private val fish = arrayOf("heim_crab", "red_eye", "dusk_eel", "giant_flatfish", "short_finned_eel", "web_snipper", "bouldabass", "salve_eel", "blue_crab", "cave_moray")
+
+    private fun spawnItems(instance: Tile, complexity: Int, dungeon: DungeonMap) {
+        val items = mutableListOf<ItemDrop>()
+        val startRoom = dungeon.start()
+        val tile = instance.add(startRoom.tile.x * 16, startRoom.tile.y * 16)
+        val teamCount = 1
+        val fishCount = 7 + teamCount * 2
+        val highestConstitution = 10
+        repeat(fishCount) {
+            val index = random.nextInt(0, highestConstitution / 10)
+            val fish = fish[index]
+            items.add(ItemDrop(fish))
+        }
+        // TODO highest levels
+        if (dungeon.traverse { _, door, _ -> door is DungeonDoor.Blocked && door.skill == Skill.Mining }.isNotEmpty()) { // TODO mining puzzles
+            // TODO pickaxe
+        }
+        if (dungeon.traverse { _, door, _ -> door is DungeonDoor.Blocked && door.skill == Skill.Woodcutting }.isNotEmpty()) { // TODO mining puzzles
+            // TODO hatchet
+        }
+        // TODO dragonfire shield if dragon spawn
+        items.add(ItemDrop("rusty_coins", 490..510))
+        val tiles = listOf(
+            Delta(x = 10, y = 14),
+            Delta(x = 11, y = 14),
+            Delta(x = 14, y = 11),
+            Delta(x = 14, y = 10),
+            Delta(x = 9, y = 11),
+            Delta(x = 10, y = 11),
+            Delta(x = 11, y = 11),
+            Delta(x = 9, y = 10),
+            Delta(x = 10, y = 10),
+            Delta(x = 11, y = 10)
+        )
+        for (drop in items) {
+            val delta = tiles.random(random)
+            val tx = rotateX(delta.x, delta.y, startRoom.rotation, 15)
+            val ty = rotateY(delta.x, delta.y, startRoom.rotation, 15)
+            val item = drop.toItem()
+            FloorItems.add(tile.add(tx, ty), item.id, item.amount)
+        }
+    }
+
+    private fun rotateX(x: Int, y: Int, rotation: Int, size: Int) = when (rotation) {
+        0 -> x
+        1 -> size - y
+        2 -> size - x
+        3 -> y
+        else -> x
+    }
+
+    private fun rotateY(x: Int, y: Int, rotation: Int, size: Int) = when (rotation) {
+        0 -> y
+        1 -> x
+        2 -> size - y
+        3 -> size - x
+        else -> y
     }
 }
