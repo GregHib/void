c06c5cd9def21605ca979ac5b4309453f3ffed0b
GregHib <greghib@users.noreply.github.com>
2026-08-13 11:07:10 +0100

    Formatting

--- game/src/main/kotlin/content/area/karamja/shilo_village/Yohnus.kt
+++ game/src/main/kotlin/content/area/karamja/shilo_village/Yohnus.kt
@@ -5,8 +5,6 @@ import content.entity.player.dialogue.Happy
 import content.entity.player.dialogue.Neutral
 import content.entity.player.dialogue.type.choice
 import content.entity.player.dialogue.type.npc
-import content.entity.player.dialogue.type.player
-import content.quest.questCompleted
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.ui.dialogue.talkWith
 import world.gregs.voidps.engine.entity.character.npc.NPCs
