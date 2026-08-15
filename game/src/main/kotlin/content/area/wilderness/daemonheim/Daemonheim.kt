74dd5b7f1c1dde10493a69e5ca284aec18077dd9
GregHib <greghib@users.noreply.github.com>
2026-08-15 17:10:58 +0100

    Add daemonheim exit

---
+++ game/src/main/kotlin/content/area/wilderness/daemonheim/Daemonheim.kt
@@ -0,0 +1,18 @@
+package content.area.wilderness.daemonheim
+
+import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.entity.character.move.tele
+import world.gregs.voidps.type.Direction
+import world.gregs.voidps.type.Tile
+
+class Daemonheim : Script {
+    init {
+        objectOperate("Jump-down", "rand_dragonkin_ruin_collapsed_floor_01_var") {
+            face(Direction.NORTH)
+            anim("rand_player_agility_jump_down")
+            delay(1)
+            exactMoveDelay(Tile(3454, 3724, 1), startDelay = 10, delay = 30)
+            tele(3454, 3725, 0)
+        }
+    }
+}
