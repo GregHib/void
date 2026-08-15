c1c02a6b7fe836b64a3b435ca90c645b0c5cf990
GregHib <greghib@users.noreply.github.com>
2026-08-15 12:00:25 +0100

    Formatting

--- game/src/main/kotlin/content/skill/smithing/Furnace.kt
+++ game/src/main/kotlin/content/skill/smithing/Furnace.kt
@@ -11,7 +11,6 @@ import world.gregs.voidps.engine.entity.character.player.equip.equipped
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.entity.character.player.skill.exp.exp
 import world.gregs.voidps.engine.entity.character.player.skill.level.Interpolation
-import world.gregs.voidps.engine.entity.character.player.skill.level.Level
 import world.gregs.voidps.engine.entity.character.player.skill.level.Level.has
 import world.gregs.voidps.engine.entity.character.sound
 import world.gregs.voidps.engine.entity.item.Item
