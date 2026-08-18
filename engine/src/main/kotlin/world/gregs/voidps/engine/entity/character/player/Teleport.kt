0f898e83d0c096abade13148e9b6f65214442ea7
GregHib <greghib@users.noreply.github.com>
2026-08-18 15:10:36 +0100

    Remove weird delay from teleport tele

--- engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/player/Teleport.kt
+++ engine/src/main/kotlin/world/gregs/voidps/engine/entity/character/player/Teleport.kt
@@ -120,7 +120,6 @@ interface Teleport {
                 player.gfx("teleport_$type")
                 player.animDelay("teleport_$type")
                 player.tele(tile)
-                player.delay(1)
                 if (sound) {
                     player.sound("teleport_land_${type}")
                 }
