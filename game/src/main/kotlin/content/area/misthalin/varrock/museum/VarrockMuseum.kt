d8fcb2bf1c881f18109136188bbffe961de73997
GregHib <greghib@users.noreply.github.com>
2026-08-23 18:41:12 +0100

    Move varrock museum files

---
+++ game/src/main/kotlin/content/area/misthalin/varrock/museum/VarrockMuseum.kt
@@ -0,0 +1,11 @@
+package content.area.misthalin.varrock.museum
+
+import world.gregs.voidps.engine.Script
+
+class VarrockMuseum : Script {
+    init {
+        objectOperate("Look-at") {
+
+        }
+    }
+}
