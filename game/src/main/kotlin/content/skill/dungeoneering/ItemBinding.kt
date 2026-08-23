27bcd6a4087c36c298d16a006231f61d29c03b51
GregHib <greghib@users.noreply.github.com>
2026-08-23 15:16:54 +0100

    Remove unused imports

--- game/src/main/kotlin/content/skill/dungeoneering/ItemBinding.kt
+++ game/src/main/kotlin/content/skill/dungeoneering/ItemBinding.kt
@@ -8,10 +8,8 @@ import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.inv.inventory
 import world.gregs.voidps.engine.inv.remove
-import world.gregs.voidps.engine.inv.removeToLimit
 import world.gregs.voidps.engine.inv.transact.TransactionError
 import world.gregs.voidps.engine.inv.transact.operation.AddItem.add
-import world.gregs.voidps.engine.inv.transact.operation.RemoveItem.remove
 import world.gregs.voidps.engine.inv.transact.operation.RemoveItemLimit.removeToLimit
 import world.gregs.voidps.engine.inv.transact.operation.ReplaceItem.replace
 
