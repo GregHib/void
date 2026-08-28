c1402cf6a6d69385c3ee09c07a15d8ece619b9ce
GregHib <greghib@users.noreply.github.com>
2026-08-28 16:12:56 +0100

    Add script to add missing examines

---
+++ tools/src/main/kotlin/world/gregs/voidps/tools/definition/TomlExamines.kt
@@ -0,0 +1,74 @@
+package world.gregs.voidps.tools.definition
+
+import world.gregs.yaml.Yaml
+import java.io.File
+
+/**
+ * Goes through config files of a particular type and fills in missing
+ * examines using data from rs3 json dumps
+ */
+object TomlExamines {
+    val examines = mutableMapOf<Int, Pair<String, String>>()
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val yaml = Yaml()
+        val list = yaml.load<List<Map<String, Any>>>("${System.getProperty("user.home")}\\Downloads\\locs.json")
+        for (element in list) {
+            val id = element["id"] as Int
+            val examine = element["examine"] as String
+            val name = element["name"] as String
+            if (examine.isNotBlank()) {
+                examines[id] = Pair(examine, name)
+            }
+        }
+        for (file in File("./data/").walkTopDown()) {
+            if (!file.isFile || !file.name.endsWith(".objs.toml")) {
+                continue
+            }
+            var first = true
+            val lines = mutableListOf<String>()
+            var id = -1
+            var stringId = ""
+            var found = false
+            var clone = false
+            for (line in file.readLines()) {
+                if (line.startsWith("id")) {
+                    id = line.split("=")[1].trim().toInt()
+                } else if (line.startsWith("examine")) {
+                    found = true
+                } else if (line.startsWith("clone")) {
+                    clone = true
+                } else if (line.startsWith("[")) {
+                    if (!found && !first) {
+                        notFound(lines, stringId, id, clone)
+                    }
+                    first = false
+                    stringId = line
+                    found = false
+                    clone = false
+                }
+                lines.add(line)
+            }
+            if (!found && !first) {
+                notFound(lines, stringId, id, clone)
+            }
+            if (lines.last().trim() != "") {
+                lines.add("")
+            }
+            file.writeText(lines.joinToString(System.lineSeparator()))
+        }
+    }
+
+    private fun notFound(lines: MutableList<String>, stringId: String, id: Int, clone: Boolean) {
+        if (clone) {
+            println("Skip clone $stringId $id")
+            return
+        }
+        val (examine, name) = examines[id] ?: return
+        val last = lines.removeLast()
+        println("Found examine: $stringId $name = \"$examine\"")
+        lines.add("examine = \"$examine\"")
+        lines.add(last)
+    }
+}
