15fd8320441027127d28afa86d9e5ef110ad23a2
GregHib <greghib@users.noreply.github.com>
2026-09-02 15:55:25 +0100

    Formatting

--- game/src/main/kotlin/content/area/morytania/braindeath_island/Davey.kt
+++ game/src/main/kotlin/content/area/morytania/braindeath_island/Davey.kt
@@ -16,7 +16,6 @@ import world.gregs.voidps.engine.entity.character.player.Player
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.inv.add
 import world.gregs.voidps.engine.inv.inventory
-import world.gregs.voidps.engine.inv.remove
 import world.gregs.voidps.engine.inv.replace
 import world.gregs.voidps.type.Tile
 
