87f3ef7da972788d17e0f28b714142ab3cdfb9e9
potato <princess@localhost>
2026-09-11 12:31:59 -0500

    Added cooked sardines to the doogle sardine recipe

--- game/src/main/kotlin/content/quest/member/gertrudes_cat/DoogleSardineRecipe.kt
+++ game/src/main/kotlin/content/quest/member/gertrudes_cat/DoogleSardineRecipe.kt
@@ -8,9 +8,20 @@ import world.gregs.voidps.engine.inv.remove
 
 class DoogleSardineRecipe : Script {
     init {
+        /**
+         * Source: https://runescape.wiki/w/Transcript:Gertrude%27s_Cat
+         * "Use doogle leaves, raw sardine or sardine on Fluffs"
+         * Implies the recipe also permits cooked sardine
+         */
         itemOnItem("doogle_leaves", "raw_sardine") { _, _ ->
             inventory.remove("doogle_leaves")
             inventory.remove("raw_sardine")
+            message("You rub the doogle leaves over the sardine.")
+            inventory.add("doogle_sardine")
+        }
+        itemOnItem("doogle_leaves", "sardine") { _, _ ->
+            inventory.remove("doogle_leaves")
+            inventory.remove("sardine")
             message("You rub the doogle leaves over the sardine.")
             inventory.add("doogle_sardine")
         }
