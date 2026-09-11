0d931e751cde589d26fff5d0daa17c026129fa80
potato <princess@localhost>
2026-09-11 09:13:56 -0500

    Added near-complete animations to the broken fence. Now needs proper timing.

--- game/src/main/kotlin/content/area/misthalin/varrock/BrokenFenceSawmill.kt
+++ game/src/main/kotlin/content/area/misthalin/varrock/BrokenFenceSawmill.kt
@@ -3,6 +3,7 @@ package content.area.misthalin.varrock
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.entity.character.player.chat.ChatType
+import world.gregs.voidps.engine.entity.character.player.clearRenderEmote
 import world.gregs.voidps.engine.entity.character.player.renderEmote
 import world.gregs.voidps.type.Tile
 
@@ -14,12 +15,16 @@ class BrokenFenceSawmill : Script {
         objectOperate("Squeeze-under", "fence_varrock_lumbermill") {
             when (tile) {
                 EAST_TILE -> {
+                    anim("human_squeeze_under", override = true)
                     message("You squeeze underneath the fence", ChatType.Filter)
                     walkOverDelay(WEST_TILE)
+                    clearAnim()
                 }
                 WEST_TILE -> {
+                    anim("human_squeeze_under_2", override = true)
                     message("You squeeze underneath the fence", ChatType.Filter)
                     walkOverDelay(EAST_TILE)
+                    clearAnim()
                 }
                 else -> {
                     message("You need to get closer to use that!")
