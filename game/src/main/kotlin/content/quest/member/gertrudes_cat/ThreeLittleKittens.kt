4777d26fda9341586eae390a4fbd4d04bd3579c6
potato <princess@localhost>
2026-09-13 11:10:16 -0500

    Created three_little_kittens NPC Further fleshed out the drop logic of the Three Little Kittens item. Not perfect but works.

--- game/src/main/kotlin/content/quest/member/gertrudes_cat/ThreeLittleKittens.kt
+++ game/src/main/kotlin/content/quest/member/gertrudes_cat/ThreeLittleKittens.kt
@@ -2,20 +2,31 @@ package content.quest.member.gertrudes_cat
 
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.message
+import world.gregs.voidps.engine.entity.character.npc.NPC
+import world.gregs.voidps.engine.entity.character.npc.NPCs
 import world.gregs.voidps.engine.inv.inventory
 import world.gregs.voidps.engine.inv.remove
+import world.gregs.voidps.type.Tile
 
 private const val THREE_LITTLE_KITTENS_STRING_NAME = "three_little_kittens"
 
 class ThreeLittleKittens : Script {
     init {
+        // This works, but if the player moves then the kittens aren't shown properly running away.
+        // Putting it in a queue does not fix it.
         itemOption("Drop", THREE_LITTLE_KITTENS_STRING_NAME) {
+            val playerLoc = this.tile
             inventory.remove(THREE_LITTLE_KITTENS_STRING_NAME)
             set(KITTENS_FOUND, false)
             message("You place the kittens on the floor.")
+
             pause(2)
-            this.say("Mew!")
+            val threeLittleKittens: NPC = NPCs.add(THREE_LITTLE_KITTENS_STRING_NAME, playerLoc)
+            threeLittleKittens.walkTo(Tile(3294, 3507))
+            threeLittleKittens.say("Mew!")
+
             pause(2)
+            threeLittleKittens.despawn(0)
             message("The kittens have run off.")
             return@itemOption
         }
