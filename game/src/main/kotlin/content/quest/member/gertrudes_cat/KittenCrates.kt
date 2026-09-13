01b50252fae22abc754a1a1b6feedaec3ebefd12
potato <princess@localhost>
2026-09-13 09:46:23 -0500

    KittenCrates logic should be about finished now. Still needs to be replaced with jiggling crates when appropriate.

--- game/src/main/kotlin/content/quest/member/gertrudes_cat/KittenCrates.kt
+++ game/src/main/kotlin/content/quest/member/gertrudes_cat/KittenCrates.kt
@@ -2,29 +2,35 @@ package content.quest.member.gertrudes_cat
 
 import content.area.misthalin.varrock.KITTENS_HIDING_SPOT
 import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.data.definition.Areas
+import world.gregs.voidps.engine.inv.add
+import world.gregs.voidps.engine.inv.inventory
 import world.gregs.voidps.type.Tile
 
-
+private const val KITTENS_FOUND = "three_little_kittens_found"
 
 class KittenCrates : Script {
-    private var kittenCrates = mutableSetOf<Tile>()
-
     init {
-        objectSpawn("Crate") {
-            if(this.tile in Areas["kitten_search_area"]){
-                kittenCrates += tile
-            }
-        }
-        objectOperate("Search", "Crate") { interact ->
+        objectOperate("Search", "crate_17") { interact ->
             val player = interact.player
             val crate = interact.target
+
+            if(get(KITTENS_FOUND, false)) {
+                return@objectOperate
+            }
             if(interact.target.tile !in Areas["kitten_search_area"]) {
                 return@objectOperate
             }
+
             var kittenCrate = get(KITTENS_HIDING_SPOT, Tile(0, 0))
             if(crate.tile == kittenCrate){
-                // Cat found!
+                if(player.inventory.isFull()){
+                    message("<red>You find three little kittens but you have no space for them in your backpack.")
+                    return@objectOperate
+                }
+                set(KITTENS_FOUND, true)
+                player.inventory.add("three_little_kittens")
             }
         }
     }
