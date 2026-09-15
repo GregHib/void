84933839a8218f1c2394be6d2077b08ff02fb449
GregHib <greghib@users.noreply.github.com>
2026-09-15 21:44:01 +0100

    Add command for viewing adventurer's logs in-game

--- game/src/main/kotlin/content/entity/player/AdventurersLogs.kt
+++ game/src/main/kotlin/content/entity/player/AdventurersLogs.kt
@@ -1,18 +1,36 @@
 package content.entity.player
 
+import content.entity.player.command.find
+import content.quest.questJournal
 import world.gregs.voidps.engine.Script
+import world.gregs.voidps.engine.client.command.playerCommand
+import world.gregs.voidps.engine.client.command.stringArg
+import world.gregs.voidps.engine.client.ui.chat.plural
 import world.gregs.voidps.engine.client.ui.chat.toDigitGroupString
+import world.gregs.voidps.engine.client.variable.hasClock
+import world.gregs.voidps.engine.client.variable.start
 import world.gregs.voidps.engine.data.RecentEvent
 import world.gregs.voidps.engine.data.Settings
+import world.gregs.voidps.engine.data.definition.AccountDefinitions
+import world.gregs.voidps.engine.data.definition.FontDefinitions
 import world.gregs.voidps.engine.data.definition.QuestDefinitions
 import world.gregs.voidps.engine.data.definition.Tables
 import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.player.Players
 import world.gregs.voidps.engine.entity.character.player.isAdmin
+import world.gregs.voidps.engine.entity.character.player.name
 import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.get
 import world.gregs.voidps.engine.timer.epochSeconds
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.TimeUnit
 
-class AdventurersLogs : Script {
+class AdventurersLogs(
+    val accounts: AccountDefinitions,
+    val fonts: FontDefinitions,
+) : Script {
     init {
         experience { skill, from, to ->
             if (to < 14_000_000 && from / 1_000_000 != to / 1_000_000) {
@@ -44,6 +62,49 @@ class AdventurersLogs : Script {
 
             logEvent("Levelled up ${skill.name}", "I levelled my ${skill.name} skill, I am now level $to.")
         }
+
+        playerCommand(
+            "adventurers_log",
+            stringArg("player-name", optional = true, autofill = accounts.displayNames.keys),
+            desc = "Check the adventurer's logs of a player that's online (blank for self)",
+            handler = ::listLogs
+        )
+    }
+
+    private val formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm")
+
+    private fun listLogs(player: Player, args: List<String>) {
+        val target = Players.find(player, args.getOrNull(0)) ?: return
+        if (player.hasClock("commands_delay")) {
+            return
+        }
+        player.start("commands_delay", 1)
+        val list = mutableListOf<String>()
+        val time = player["playtime", 0].toLong()
+        val days = TimeUnit.SECONDS.toDays(time)
+        val hours = TimeUnit.SECONDS.toDays(time).rem(24)
+        list.add("<maroon>${target.name}")
+        list.add("<blue>Time Played:</col> $days ${"Day".plural(days)} $hours ${"Hour".plural(hours)}")
+        list.add("")
+        list.add("<blue>My Recent Events")
+        if (target.recentEvents.isEmpty()) {
+            list.add("No recent events.")
+            list.add("I don't have any recent events yet. I need to do more adventuring.")
+        } else {
+            val font = fonts.get("p12_full")
+            for (event in target.recentEvents.reversed()) {
+                val instant = Instant.ofEpochSecond(event.time.toLong())
+                val dateTime = instant.atZone(ZoneId.systemDefault())
+                val timestamp = dateTime.format(formatter)
+                list.add("<navy>${event.title}")
+                for (line in font.splitLines(event.description, 400)) {
+                    list.add(line)
+                }
+                list.add("(${timestamp})")
+                list.add("")
+            }
+        }
+        player.questJournal("Adventurer's Log", list)
     }
 
     companion object {
