a90245045e38069f0cdf28bf1890a06e15c9cd52
GregHib <greghib@users.noreply.github.com>
2026-09-15 11:24:20 +0100

    Add boss kill tracking

--- game/src/main/kotlin/content/entity/player/stat/BossTracker.kt
+++ game/src/main/kotlin/content/entity/player/stat/BossTracker.kt
@@ -1,9 +1,11 @@
 package content.entity.player.stat
 
+import content.entity.combat.damageDealers
 import world.gregs.voidps.engine.Script
 import world.gregs.voidps.engine.client.command.playerCommand
 import world.gregs.voidps.engine.client.message
 import world.gregs.voidps.engine.entity.character.player.Player
+import world.gregs.voidps.engine.entity.character.player.skill.Skill
 import world.gregs.voidps.engine.timer.epochMilliseconds
 import java.util.concurrent.TimeUnit
 
@@ -16,31 +18,58 @@ class BossTracker : Script {
         playerCommand("kill_counts", desc = "Toggle whether boss kill counts are displayed") {
             message("Boss kill counts are now: ${if (toggle("kill_counts")) "enabled" else "disabled"}.")
         }
+
+        npcLevelChanged(Skill.Constitution) { skill, from, to ->
+            val categories: Set<String> = def.getOrNull("categories") ?: return@npcLevelChanged
+            if (!categories.contains("boss")) {
+                return@npcLevelChanged
+            }
+            val max = levels.getMax(skill)
+            if (to == max && from != max) { // Reset if healed back to max
+                clear("${id}_kill_timer")
+            } else if (from == max && to != max) { // Start when hit down from max
+                set("${id}_kill_timer", epochMilliseconds())
+            } else if (to <= 0 && from > 0) {
+                val count = damageDealers.size
+                val start = get("${id}_kill_timer", 0L)
+                for ((char, damage) in damageDealers) {
+                    if (char is Player) {
+                        kill(char, "Your ${def.name} kill count is", "${id}_kills")
+                        record(char, start, id, count, "Fight duration")
+                    }
+                }
+            }
+        }
     }
 
     companion object {
-        fun start(player: Player, timer: String) {
-            player["${timer}_timer"] = epochMilliseconds()
-        }
-
-        fun stop(player: Player, timer: String, prefix: String = "Duration") {
-            val start = player["${timer}_timer", 0L]
-            if (start == 0L) {
-                return
-            }
+        fun record(player: Player, start: Long, timer: String, teamSize: Int, prefix: String = "Duration") {
             val duration = epochMilliseconds() - start
             val best = player["${timer}_fastest", 0L]
-            if (duration > best) {
-                player["${timer}_fastest"] = duration
+            if (duration > best && !player["insta_kill", false] && !player["god_mode", false]) { // No cheating!
+                player["${timer}_fastest"] = duration.toInt()
             }
             if (player["boss_timers", false]) {
                 val time = "<red>${timestamp(duration)}</col>"
+                val teamPrefix = if (teamSize > 1) "Team size: <red>${teamSize} players</col> " else ""
                 if (duration > best) {
-                    player.message("$prefix: $time (new personal best)")
+                    player.message("$teamPrefix$prefix: $time (new personal best)")
                 } else {
-                    player.message("$prefix: $time. Personal best: ${timestamp(best)}")
+                    player.message("$teamPrefix$prefix: $time. Personal best: ${timestamp(best)}")
                 }
             }
+        }
+
+        fun start(player: Player, timer: String) {
+            player["${timer}_kill_timer"] = epochMilliseconds()
+        }
+
+        fun stop(player: Player, timer: String, teamSize: Int = 1, prefix: String = "Duration") {
+            val start = player["${timer}_kill_timer", 0L]
+            if (start == 0L) {
+                return
+            }
+            record(player, start, timer, teamSize, prefix)
         }
 
         fun kill(player: Player, prefix: String, tracker: String) {
@@ -56,7 +85,9 @@ class BossTracker : Script {
             val seconds = TimeUnit.MILLISECONDS.toSeconds(duration).rem(60)
             val millis = duration.rem(1000) / 10
             return if (hours > 0) {
-                "$hours:$minutes:$seconds.$millis"
+                "$hours:$minutes:$seconds"
+            } else if (minutes > 0) {
+                "$minutes:$seconds"
             } else {
                 "$minutes:$seconds.$millis"
             }
